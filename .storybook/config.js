68a829437cce00f1bb34386c054e9bdc42445653
Milos Mosovsky <milos.mosovsky@toptal.com>
2019-01-09 20:18:56 +0100

    Update storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,25 @@
-import { configure } from '@storybook/react';
+import { configure, setAddon, addDecorator } from '@storybook/react'
+import chaptersAddon from 'react-storybook-addon-chapters'
+import { withOptions } from '@storybook/addon-options'
+import { themes } from '@storybook/components'
+import { withKnobs } from '@storybook/addon-knobs'
+
+const DECORATORS = [
+  withOptions({
+    name: 'Toptal',
+    theme: themes.light
+  }),
+  withKnobs
+]
+
+const ADDONS = [chaptersAddon]
+
+DECORATORS.forEach(decorator => addDecorator(decorator))
+ADDONS.forEach(addon => setAddon(addon))
 
 function loadStories() {
-  require('./stories/index.js');
+  require('./stories/index.js')
   // You can require as many stories as you need.
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
