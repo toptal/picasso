c5c803820b8b92b67fcf59c32001099f43471603
Milos Mosovsky <milos.mosovsky@toptal.com>
2019-01-10 20:45:17 +0100

    Refactor provider to be used as Singleton

--- .storybook/stories/TextField.jsx
+++ .storybook/stories/TextField.jsx
@@ -4,25 +4,26 @@ import TextField from '../../components/TextField'
 import SearchIcon from '@material-ui/icons/Search'
 
 storiesOf('TextField', module)
-  .add('text', () => <TextField label="Search..." />)
+  .add('text', () => <TextField label='Search...' />)
   .add('text with Icon', () => (
-    <TextField label="Search..." Icon={<SearchIcon />} />
+    <TextField label='Search...' Icon={<SearchIcon />} />
   ))
   .add('select', () => (
     <div style={{ width: '300px' }}>
       <TextField
-        label="Gender"
+        label='Gender'
         select
         fullWidth
         SelectProps={{
           native: true
-        }}>
-        <option value="" />
-        <option value="male">Male</option>
-        <option value="female">Female</option>
+        }}
+      >
+        <option value='' />
+        <option value='male'>Male</option>
+        <option value='female'>Female</option>
       </TextField>
     </div>
   ))
   .add('textarea', () => (
-    <TextField label="Description..." rows={4} multiline />
+    <TextField label='Description...' rows={4} multiline />
   ))
