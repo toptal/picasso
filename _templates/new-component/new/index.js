85395fe8c58f022729f0a360f97d64ed595a75b7
denieler <daniel.ostapenko@toptal.com>
2019-01-15 11:24:02 +0100

    Add multiple ways of using the generate:component command

---
+++ _templates/new-component/new/index.js
@@ -0,0 +1,18 @@
+const DEFAULT_NEWCOMPONENT_COMMAND = 'new'
+
+module.exports = {
+  prompt: ({ prompter, args }) => {
+    let argvName = process.argv[process.argv.length - 1]
+
+    if (argvName !== DEFAULT_NEWCOMPONENT_COMMAND) {
+      return Promise.resolve({ name: argvName })
+    } else if (args.name) {
+      return Promise.resolve({ name: args.name })
+    }
+    return prompter.prompt({
+      type: 'input',
+      name: 'name',
+      message: "What's the component name?"
+    })
+  }
+}
