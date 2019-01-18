ae233e9f8d0eff80fc7af510a204e5adf4e14dcc
denieler <daniel.ostapenko@toptal.com>
2019-01-18 01:00:57 +0100

    Fix the styles shape to the common way

--- components/Checkbox/styles.js
+++ components/Checkbox/styles.js
@@ -19,46 +19,36 @@ PicassoProvider.override(() => ({
   }
 }))
 
-const root = {
-  '&:hover $uncheckedIcon': {
-    border: `1px solid ${pallete.primary.main}`
-  }
-}
-
-const disabled = {
-  '&:hover $uncheckedIcon': {
-    border: `1px solid ${pallete.grey[50]}`
-  }
-}
-
-const checkedIcon = {
-  height: `${2 * pallete.unit}px`,
-  width: `${2 * pallete.unit}px`,
-  transition: 'all .1s ease',
-  background: pallete.primary.main,
-  border: `1px solid ${pallete.primary.dark}`
-}
-
-const uncheckedIcon = {
-  height: `${2 * pallete.unit}px`,
-  width: `${2 * pallete.unit}px`,
-  transition: 'all .1s ease',
-  background: pallete.common.white,
-  border: `1px solid ${pallete.grey[50]}`
-}
-
-const indeterminateIcon = {
-  height: `${2 * pallete.unit}px`,
-  width: `${2 * pallete.unit}px`,
-  transition: 'all .1s ease',
-  background: pallete.primary.main,
-  border: `1px solid ${pallete.primary.dark}`
-}
-
 export default {
-  root,
-  disabled,
-  checkedIcon,
-  uncheckedIcon,
-  indeterminateIcon
+  root: {
+    '&:hover $uncheckedIcon': {
+      border: `1px solid ${pallete.primary.main}`
+    }
+  },
+  disabled: {
+    '&:hover $uncheckedIcon': {
+      border: `1px solid ${pallete.grey[50]}`
+    }
+  },
+  checkedIcon: {
+    height: `${2 * pallete.unit}px`,
+    width: `${2 * pallete.unit}px`,
+    transition: 'all .1s ease',
+    background: pallete.primary.main,
+    border: `1px solid ${pallete.primary.dark}`
+  },
+  uncheckedIcon: {
+    height: `${2 * pallete.unit}px`,
+    width: `${2 * pallete.unit}px`,
+    transition: 'all .1s ease',
+    background: pallete.common.white,
+    border: `1px solid ${pallete.grey[50]}`
+  },
+  indeterminateIcon: {
+    height: `${2 * pallete.unit}px`,
+    width: `${2 * pallete.unit}px`,
+    transition: 'all .1s ease',
+    background: pallete.primary.main,
+    border: `1px solid ${pallete.primary.dark}`
+  }
 }
