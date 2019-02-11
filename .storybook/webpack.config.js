88ed74315e004404ef93e68dda8315aa83d2f72a
denieler <daniel.ostapenko@toptal.com>
2019-02-12 00:27:04 +0200

    Rollback fix for storybook build on CI

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,10 +1,6 @@
 const path = require('path')
 
 module.exports = (baseConfig, env, config) => {
-  if (env === 'PRODUCTION') {
-    config.devtool = false
-  }
-
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     use: [
