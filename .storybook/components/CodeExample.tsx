9824d555b4a625ea7c1a42f619d16e676b0b7720
denieler <daniel.ostapenko@toptal.com>
2019-02-11 20:26:44 +0200

    Add accordion for code examples

--- .storybook/components/CodeExample.tsx
+++ .storybook/components/CodeExample.tsx
@@ -4,6 +4,8 @@ import Editor from './Editor'
 import ComponentRenderer from './ComponentRenderer'
 import Spacer from '../../components/Spacer'
 
+import { Accordion, Button } from '../../components'
+
 interface Props {
   title: string
   description: string
@@ -18,9 +20,18 @@ const Container = ({
   style?: any
 }) => <div style={{ width: '100%', ...style }}>{children}</div>
 
+const Distribute = ({
+  children,
+  justify
+}: {
+  children: React.ReactNode
+  justify: 'space-between' | 'space-around' | 'center'
+}) => <div style={{ display: 'flex', justifyContent: justify }}>{children}</div>
+
 class CodeExample extends React.Component<Props, {}> {
   state = {
-    sourceCode: ''
+    sourceCode: '',
+    isEditorVisible: false
   }
 
   componentDidMount() {
@@ -33,26 +44,42 @@ class CodeExample extends React.Component<Props, {}> {
     return require(`!raw-loader!../../components/${code}`)
   }
 
+  handleShowEditor = () => {
+    const { isEditorVisible } = this.state
+    this.setState({ isEditorVisible: !isEditorVisible })
+  }
+
   handleChangeCode = _.debounce((value: string) => {
     this.setState({ sourceCode: value })
   }, 400)
 
   render() {
     const { code } = this.props
-    const { sourceCode } = this.state
+    const { sourceCode, isEditorVisible } = this.state
+
+    const SourceCodeEditor = (
+      <Container style={{ backgroundColor: '#141414', padding: '1em 0' }}>
+        <Editor
+          id="some-component-example"
+          mode="jsx"
+          value={sourceCode}
+          onChange={this.handleChangeCode}
+        />
+      </Container>
+    )
 
     return (
       <Container>
-        <Spacer top={2} bottom={2}>
-          <ComponentRenderer sourceCode={sourceCode} exampleCodePath={code} />
-        </Spacer>
-        <Container style={{ backgroundColor: '#141414', padding: '1em 0' }}>
-          <Editor
-            id="some-component-example"
-            mode="jsx"
-            value={sourceCode}
-            onChange={this.handleChangeCode}
-          />
+        <Distribute justify="space-between">
+          <Spacer top={2} bottom={2}>
+            <ComponentRenderer sourceCode={sourceCode} exampleCodePath={code} />
+          </Spacer>
+          <Button variant="flat" size="small" onClick={this.handleShowEditor}>
+            Show source code
+          </Button>
+        </Distribute>
+        <Container>
+          <Accordion Details={SourceCodeEditor} expanded={isEditorVisible} />
         </Container>
       </Container>
     )
