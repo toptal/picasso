bb7b1972ae1091a856d945c265678d88fec2bb81
Vytautas <vytautas.butkus@toptal.com>
2019-03-11 18:01:09 +0200

    Add support info

--- .storybook/stories/Picasso.js
+++ .storybook/stories/Picasso.js
@@ -3,7 +3,9 @@ import { storiesOf } from '@storybook/react'
 
 import README from '../../README.md'
 import CHANGELOG from '../../CHANGELOG.md'
+import SUPPORT from '../../docs/SUPPORT.md'
 
 storiesOf('Picasso', module)
   .add('Readme', doc(README))
   .add('Changelog', doc(CHANGELOG))
+  .add('Support Info', doc(SUPPORT))
