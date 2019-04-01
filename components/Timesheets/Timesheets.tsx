8efe9a99c1854c1eb1c079b80eb589f19aaaad7d
denieler <daniel.ostapenko@toptal.com>
2019-04-01 15:35:18 +0300

    [FX-73] Add Show More for Timesheets component
    [FX-73] Add Loading state
    
    [FX-73] Add visual test snapshot
    
    [FX-73] Move styles from Link root component
    
    [FX-73] Fix tests
    
    [FX-73] Remove styles file for Link

--- components/Timesheets/Timesheets.tsx
+++ components/Timesheets/Timesheets.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 import { withStyles } from '@material-ui/core/styles'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 
 import Table from '../Table'
 import Typography from '../Typography'
 import TimesheetItem from '../TimesheetItem'
+import Link from '../Link'
 import { Classes } from '../styles/types'
 import styles from './styles'
 
+const MAX_TIMESHEETS_SHOWN_WHEN_COLLAPSED = 3
+
 interface Props {
   classes: Classes
+  /** Number of timesheets records shown when the list is collapsed */
+  collapsedCount: number
   /** Timesheets list */
   timesheets: {
     id: number
@@ -26,39 +32,99 @@ interface Props {
   onView: (timesheetId: number) => void
   /** Callback invoked when the Unsubmit button clicked on the timesheet record */
   onUnsubmit: (timesheetId: number) => void
+  /** Callback invoked when the Show More Timesheets button clicked. You should load here more timesheets to show */
+  onShowMore: () => void | Promise<void>
+}
+
+enum State {
+  Collapsed,
+  Loading,
+  Full
 }
 
 export const Timesheets: React.FunctionComponent<Props> = props => {
-  const { timesheets, onEdit, onView, onUnsubmit } = props
+  const {
+    classes,
+    collapsedCount,
+    timesheets,
+    onEdit,
+    onView,
+    onUnsubmit,
+    onShowMore
+  } = props
+
+  const [state, setState] = useState(State.Collapsed)
+  const timesheetsToShow =
+    onShowMore && (state === State.Collapsed || state === State.Loading)
+      ? timesheets.slice(0, collapsedCount)
+      : timesheets
+
+  const handleShowMoreClick = async () => {
+    if (!onShowMore) {
+      return
+    }
+
+    const promise = onShowMore()
+
+    if (promise) {
+      setState(State.Loading)
+      await promise
+      setState(State.Full)
+    } else {
+      setState(State.Full)
+    }
+  }
 
   return (
-    <Table>
-      <Table.Head>
-        <Table.Row>
-          <Table.Cell>
-            <Typography weight='bold'>Timesheets</Typography>
-          </Table.Cell>
-        </Table.Row>
-      </Table.Head>
-      <Table.Body>
-        {timesheets.map(timesheet => (
-          <Table.Row key={timesheet.id}>
+    <Fragment>
+      <Table>
+        <Table.Head>
+          <Table.Row>
             <Table.Cell>
-              <TimesheetItem
-                {...timesheet}
-                onUnsubmit={onUnsubmit}
-                onEdit={onEdit}
-                onView={onView}
-              />
+              <Typography weight='bold'>Timesheets</Typography>
             </Table.Cell>
           </Table.Row>
-        ))}
-      </Table.Body>
-    </Table>
+        </Table.Head>
+        <Table.Body>
+          {timesheetsToShow.map(timesheet => (
+            <Table.Row key={timesheet.id}>
+              <Table.Cell>
+                <TimesheetItem
+                  {...timesheet}
+                  onUnsubmit={onUnsubmit}
+                  onEdit={onEdit}
+                  onView={onView}
+                />
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+
+      {onShowMore && state === State.Collapsed && (
+        <div className={classes.footer}>
+          <Link
+            onClick={handleShowMoreClick}
+            underline='none'
+            className={classes.showMoreLink}
+          >
+            <Typography variant='caption'>Show more timesheets</Typography>
+            <ExpandMoreIcon className={classes.expandMoreIcon} />
+          </Link>
+        </div>
+      )}
+
+      {onShowMore && state === State.Loading && (
+        <div className={classes.footer}>
+          <Typography variant='caption'>Loading...</Typography>
+        </div>
+      )}
+    </Fragment>
   )
 }
 
 Timesheets.defaultProps = {
+  collapsedCount: MAX_TIMESHEETS_SHOWN_WHEN_COLLAPSED,
   onEdit: () => {},
   onUnsubmit: () => {},
   onView: () => {}
--- components/Timesheets/styles.ts
+++ components/Timesheets/styles.ts
@@ -1,3 +1,23 @@
-import { createStyles } from '@material-ui/core/styles'
+import { Theme, createStyles } from '@material-ui/core/styles'
 
-export default () => createStyles({})
+export default ({ palette }: Theme) =>
+  createStyles({
+    footer: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      minHeight: '3rem'
+    },
+    showMoreLink: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      cursor: 'pointer'
+    },
+    expandMoreIcon: {
+      fontSize: '0.75rem',
+      color: palette.text.primary,
+      marginLeft: '0.5em'
+    }
+  })
