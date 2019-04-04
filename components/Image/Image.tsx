0ae03f127acc31b83ae346e7b0d03afd15295185
Daniel O <danielostapenko@gmail.com>
2019-04-04 17:10:03 +0300

    [FX-14] Add Image component (#146)
    * [FX-14] Add Image component
    
    [FX-14] Change rounded to circular type
    
    [FX-14] Add srcSet and Picasso example
    
    [FX-14] Fix alt text
    
    [FX-14] Change picture
    
    * [FX-14] Add visual test snapshots

---
+++ components/Image/Image.tsx
@@ -0,0 +1,49 @@
+import React, { CSSProperties } from 'react'
+import cx from 'classnames'
+import { withStyles } from '@material-ui/core/styles'
+
+import { Classes } from '../styles/types'
+import styles from './styles'
+
+type VariantType = 'default' | 'circular'
+
+export interface Props {
+  /** Image alt text */
+  alt: string
+  classes: Classes
+  className?: string
+  /** Image url */
+  src: string
+  /** A set of image sources */
+  srcSet?: string
+  /** Image shape type */
+  variant?: VariantType
+  style?: CSSProperties
+}
+
+export const Image: React.FunctionComponent<Props> = props => {
+  const { src, srcSet, alt, classes, className, variant, style } = props
+
+  return (
+    <img
+      src={src}
+      srcSet={srcSet}
+      alt={alt}
+      className={cx(
+        {
+          [classes.circular]: variant === 'circular'
+        },
+        className
+      )}
+      style={style}
+    />
+  )
+}
+
+Image.defaultProps = {
+  variant: 'default'
+}
+
+Image.displayName = 'Image'
+
+export default withStyles(styles)(Image)
