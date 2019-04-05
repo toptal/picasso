f38761fdbf01eccdc8430c73824f607975514a65
Vedran Ivanac <vedran.ivanac@toptal.com>
2019-04-05 11:57:47 +0200

    Migrate Buttongroup to TS (#148)

---
+++ components/ButtonGroup/styles.ts
@@ -0,0 +1,27 @@
+import { createStyles } from '@material-ui/core/styles'
+
+export default () =>
+  createStyles({
+    root: {
+      display: 'inline-block',
+
+      '& $button + $button': {
+        margin: '0'
+      }
+    },
+    button: {
+      '&:first-child': {
+        borderTopRightRadius: 'unset',
+        borderBottomRightRadius: 'unset',
+        borderRight: 'none'
+      },
+      '&:last-child': {
+        borderTopLeftRadius: 'unset',
+        borderBottomLeftRadius: 'unset'
+      },
+      '&:not(:first-child):not(:last-child)': {
+        borderRadius: 'unset',
+        borderRight: 'none'
+      }
+    }
+  })
