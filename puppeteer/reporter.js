a6952710dc5a4c63d91d7f21133eb592591b3a39
Miloš Mošovský <milos.mosovsky@toptal.com>
2019-04-25 11:39:04 +0200

    chore(docs): Speedup storybook builds on CI (#237)

--- puppeteer/reporter.js
+++ puppeteer/reporter.js
@@ -3,6 +3,7 @@
 const fs = require('fs')
 const path = require('path')
 const ejs = require('ejs')
+const exec = require('child_process').execSync
 
 const config = require('./config')
 const { createSnapshotName, assignOutputDir } = require('./utils')
