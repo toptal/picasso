aad5d32aec054ae17559db4ad27c5ac5e6b585cc
denieler <daniel.ostapenko@toptal.com>
2019-05-02 16:41:38 +0300

    chore: [FX-141] Fix shadow for the Menu in Select component

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -1,5 +1,14 @@
-import { createStyles } from '@material-ui/core/styles'
+import { Theme, createStyles } from '@material-ui/core/styles'
 
+import { PicassoProvider } from '../Picasso'
 import '../List/styles'
+
+PicassoProvider.override(({ shadows }: Theme) => ({
+  MuiMenu: {
+    paper: {
+      boxShadow: shadows[2]
+    }
+  }
+}))
 
 export default () => createStyles({})
