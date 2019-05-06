570cf73dc9bd41a6684c19dc25fc255eee040d72
Peter Marosi <peter.marosi@toptal.com>
2019-05-06 09:45:51 +0200

    style(eslint): allow multi comp definition (only SFC) (#270)

--- src/components/TimesheetItem/TimesheetItem.tsx
+++ src/components/TimesheetItem/TimesheetItem.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from 'react'
+import React, { Fragment, FunctionComponent } from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import cx from 'classnames'
 
@@ -25,98 +25,96 @@ interface Props extends StandardProps {
   onUnsubmit: (timesheetId: number) => void
 }
 
-export class TimesheetItem extends Component<Props, {}> {
-  static defaultProps = {}
-  static displayName = 'TimesheetItem'
-
-  renderInfo() {
-    const {
-      id,
-      classes,
-      startDate,
-      endDate,
-      statusMessage,
-      canView,
-      onView,
-      isErrorStatus
-    } = this.props
-
-    const timesheetInfo = (
-      <Fragment>
-        <Typography variant='caption' inline>
-          From
-        </Typography>
-        <Typography variant='caption' weight='bold' inline>
+const renderTimesheetItemInfo = ({
+  id,
+  classes,
+  startDate,
+  endDate,
+  statusMessage,
+  canView,
+  onView,
+  isErrorStatus
+}: Props) => {
+  const timesheetInfo = (
+    <Fragment>
+      <Typography variant='caption' inline>
+        From
+      </Typography>
+      <Typography variant='caption' weight='bold' inline>
+        {' '}
+        {startDate}{' '}
+      </Typography>
+      <Typography variant='caption' inline>
+        till
+      </Typography>
+      <Typography variant='caption' weight='bold' inline>
+        {' '}
+        {endDate}
+      </Typography>
+      {statusMessage && (
+        <Typography
+          variant='caption'
+          weight={isErrorStatus ? 'bold' : 'regular'}
+          className={isErrorStatus ? classes.overdueMessage : undefined}
+          inline
+        >
           {' '}
-          {startDate}{' '}
+          {statusMessage}
         </Typography>
-        <Typography variant='caption' inline>
-          till
-        </Typography>
-        <Typography variant='caption' weight='bold' inline>
-          {' '}
-          {endDate}
-        </Typography>
-        {statusMessage && (
-          <Typography
-            variant='caption'
-            weight={isErrorStatus ? 'bold' : 'regular'}
-            className={isErrorStatus ? classes.overdueMessage : undefined}
-            inline
-          >
-            {' '}
-            {statusMessage}
-          </Typography>
-        )}
-      </Fragment>
-    )
+      )}
+    </Fragment>
+  )
 
-    return (
-      <div className={classes.timesheetInfo}>
-        {canView ? (
-          <Link
-            underline='none'
-            className={classes.timesheetInfoLink}
-            onClick={() => onView(id)}
-          >
-            {timesheetInfo}
-          </Link>
-        ) : (
-          timesheetInfo
-        )}
-      </div>
-    )
-  }
-
-  renderActionButtons() {
-    const { classes, id, canUnsubmit, canEdit, onUnsubmit, onEdit } = this.props
-
-    return (
-      <div className={classes.controls}>
-        {canUnsubmit && (
-          <Button size='small' onClick={() => onUnsubmit(id)}>
-            Unsubmit Timesheet
-          </Button>
-        )}
-        {canEdit && (
-          <Button size='small' onClick={() => onEdit(id)}>
-            Edit
-          </Button>
-        )}
-      </div>
-    )
-  }
-
-  render() {
-    const { classes, className, style } = this.props
-
-    return (
-      <div className={cx(classes.timesheet, className)} style={style}>
-        {this.renderInfo()}
-        {this.renderActionButtons()}
-      </div>
-    )
-  }
+  return (
+    <div className={classes.timesheetInfo}>
+      {canView ? (
+        <Link
+          underline='none'
+          className={classes.timesheetInfoLink}
+          onClick={() => onView(id)}
+        >
+          {timesheetInfo}
+        </Link>
+      ) : (
+        timesheetInfo
+      )}
+    </div>
+  )
 }
+
+const renderActionButtons = ({
+  classes,
+  id,
+  canUnsubmit,
+  canEdit,
+  onUnsubmit,
+  onEdit
+}: Props) => (
+  <div className={classes.controls}>
+    {canUnsubmit && (
+      <Button size='small' onClick={() => onUnsubmit(id)}>
+        Unsubmit Timesheet
+      </Button>
+    )}
+    {canEdit && (
+      <Button size='small' onClick={() => onEdit(id)}>
+        Edit
+      </Button>
+    )}
+  </div>
+)
+
+const TimesheetItem: FunctionComponent<Props> = props => {
+  const { classes, className, style } = props
+
+  return (
+    <div className={cx(classes.timesheet, className)} style={style}>
+      {renderTimesheetItemInfo(props)}
+      {renderActionButtons(props)}
+    </div>
+  )
+}
+
+TimesheetItem.displayName = 'TimesheetItem'
 
 export default withStyles(styles)(TimesheetItem)
