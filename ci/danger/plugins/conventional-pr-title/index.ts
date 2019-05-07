a13d9f28fa543a2ac1278633db9e53367b4793fe
Milos Mosovsky <milos.mosovsky@toptal.com>
2019-05-07 23:29:57 +0200

    chore(ci): enforce conventional PR title with danger.js

---
+++ ci/danger/plugins/conventional-pr-title/index.ts
@@ -0,0 +1,36 @@
+import { DangerDSLType } from 'danger/distribution/dsl/DangerDSL'
+import load from '@commitlint/load'
+import lint from '@commitlint/lint'
+import table from 'markdown-table'
+
+declare var danger: DangerDSLType
+
+export declare function fail(message: string): void
+
+const conventionalPRTitle = async () => {
+  const { rules, parserPreset } = await load()
+  const { title } = danger.github.pr
+
+  const result = await lint(
+    title,
+    rules,
+    parserPreset ? { parserOpts: parserPreset.parserOpts } : {}
+  )
+
+  if (!result.valid) {
+    if (result.errors.length > 0) {
+      let message = `PR title doesn't conform the conventional commit guidelines. \n\n**Errors**:\n\n`
+
+      message += table([
+        ['Message'],
+        ...result.errors.map(error => [error.message])
+      ])
+
+      message += `\n\n💡 For a guidance on how to fix this problem please refer to [https://www.conventionalcommits.org](https://www.conventionalcommits.org)`
+
+      fail(message)
+    }
+  }
+}
+
+export default conventionalPRTitle
