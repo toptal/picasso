5e86002bde28e15baf36b1d3e0d1f4d82ba5caa6
Miloš Mošovský <milos.mosovsky@toptal.com>
2019-05-07 23:38:01 +0200

    chore(ci): enforce conventional PR title with danger.js (#289)

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,5 +1,7 @@
 import { schedule } from 'danger'
 
 import conventionalCommits from './ci/danger/plugins/conventional-commit'
+import conventionalPRTitle from './ci/danger/plugins/conventional-pr-title'
 
 schedule(conventionalCommits)
+schedule(conventionalPRTitle)
