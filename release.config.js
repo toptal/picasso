33e405b797665d383bb6b5bbdd32418b6cf00f8a
Milos Mosovsky <milos.mosovsky@toptal.com>
2019-05-15 20:23:32 +0200

    fix(ci): copy package.json to root and refactor releases

--- release.config.js
+++ release.config.js
@@ -1,28 +1,51 @@
 /* eslint-disable */
 
 module.exports = {
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
+  analyzeCommits: ['@semantic-release/commit-analyzer'], // Determine the type of release by analyzing commits with conventional-changelog
+  verifyConditions: [
+    '@semantic-release/changelog', // Verify the presence and the validity of the configuration
+    '@semantic-release/npm', // Verify the presence and the validity of the npm authentication and release configuration
+    '@semantic-release/github', // Verify the presence and the validity of the GitHub authentication and release configuration
+    '@semantic-release/git' // Verify the presence and the validity of the Git authentication and release configuration
+  ],
+  generateNotes: ['@semantic-release/release-notes-generator'], // Generate release notes for the commits added since the last release with conventional-changelog
+  prepare: [
+    '@semantic-release/changelog', // Create or update the changelog file in the local project repository
     [
-      '@semantic-release/npm',
-      {
-        pkgRoot: 'build'
-      }
-    ],
-    '@semantic-release/github',
-    [
-      '@semantic-release/exec',
+      '@semantic-release/exec', // Execute a shell command to bump next version inside README.md
       {
         prepareCmd: './bin/bump-version ${nextRelease.version}'
       }
     ],
     [
+      '@semantic-release/npm', // Update the package.json version and create the npm package tarball
+      {
+        pkgRoot: 'build'
+      }
+    ],
+    [
+      '@semantic-release/exec', // Execute a shell command to copy package.json from build/ directory back to the root
+      {
+        prepareCmd: './ci/release/copy-assets'
+      }
+    ],
+    [
       '@semantic-release/git',
       {
-        assets: ['CHANGELOG.md', 'README.md', 'package.json']
+        assets: ['CHANGELOG.md', 'README.md', 'package.json'] // Push a release commit and tag, including globed files
       }
     ]
-  ]
+  ],
+  publish: [
+    '@semantic-release/github', // Publish a GitHub release
+    [
+      '@semantic-release/npm', // Publish the package on the npm registry
+      {
+        pkgRoot: 'build'
+      }
+    ]
+  ],
+  success: ['@semantic-release/github'], // Add a comment to GitHub issues and pull requests resolved in the release
+  fail: ['@semantic-release/github'], // Open a GitHub issue when a release fails
+  plugins: [] // Reset default plugins and use custom steps defined in this file
 }
