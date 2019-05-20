db7c4e6bb5ceb3fb7a78675e2ce18aa9d7781e8b
denieler <daniel.ostapenko@toptal.com>
2019-05-20 18:43:07 +0300

    chore: [FX-204] Add correct types for examples synced with design

--- src/components/Typography/story/Types.example.jsx
+++ src/components/Typography/story/Types.example.jsx
@@ -4,13 +4,18 @@ import { Typography, Container } from '@toptal/picasso'
 const TypographyTypesExample = () => (
   <div>
     <Container bottom='small'>
-      <Typography size='small'>Body Small</Typography>
+      <Typography size='large' color='black'>
+        Body Large
+      </Typography>
     </Container>
     <Container bottom='small'>
-      <Typography>Body Medium</Typography>
+      <Typography color='black'>Body Medium Black</Typography>
     </Container>
     <Container bottom='small'>
-      <Typography size='large'>Body Large</Typography>
+      <Typography>Body Medium Grey</Typography>
+    </Container>
+    <Container bottom='small'>
+      <Typography size='small'>Body Small Grey</Typography>
     </Container>
     <Container bottom='small'>
       <Typography variant='hint'>Hint</Typography>
