0eb2beae230ead3aa98d8d455be900d3df294c38
Miloš Mošovský <milos.mosovsky@toptal.com>
2019-05-24 12:19:25 +0200

    fix(table): sync style of the Table component with BASE (#365)
    BREAKING CHANGE: Decrease font size and paddings inside Table.* components

--- src/components/TableRow/styles.ts
+++ src/components/TableRow/styles.ts
@@ -1,7 +1,19 @@
-import { createStyles } from '@material-ui/core/styles'
+import { Theme, createStyles } from '@material-ui/core/styles'
 
 import { PicassoProvider } from '../Picasso'
 
 PicassoProvider.override(() => ({}))
 
-export default () => createStyles({})
+export default ({ palette, spacing }: Theme) =>
+  createStyles({
+    root: {
+      height: 'auto',
+
+      '&:nth-of-type(even)': {
+        background: '#f8f9f9'
+      }
+    },
+    head: {
+      borderBottom: `${spacing.borderWidth} solid ${palette.grey[100]}`
+    }
+  })
