7844bba4a077d4308ad422f1a9b059dd1afe68de
Vedran Ivanac <vedran.ivanac@toptal.com>
2019-05-27 15:34:28 +0200

    feat(select): add ability to add icon to `Select` component (#371)
    From now you can add `Icon` component to the sides of `Select`.
    
    You can check [Select](https://picasso.toptal.net/?path=/story/forms-folder--select#with-icon) documentation.

---
+++ src/components/Select/story/WithIcon.example.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import { Select, Container } from '@toptal/picasso'
+import { Cog } from '@toptal/picasso/Icon'
+
+const SelectWithIconExample = () => {
+  const [value, setValue] = useState()
+
+  const handleChange = event => {
+    setValue(event.target.value)
+  }
+
+  return (
+    <Container flex direction='column'>
+      <Container bottom='small'>
+        <Select
+          onChange={handleChange}
+          options={OPTIONS}
+          value={value}
+          icon={<Cog />}
+          placeholder='Choose option...'
+          width='auto'
+        />
+      </Container>
+      <Container bottom='small'>
+        <Select
+          onChange={handleChange}
+          options={OPTIONS}
+          value={value}
+          iconPosition='end'
+          icon={<Cog />}
+          placeholder='Choose option...'
+          width='auto'
+        />
+      </Container>
+      <Container bottom='small'>
+        <Select
+          disabled
+          onChange={handleChange}
+          options={OPTIONS}
+          value={value}
+          icon={<Cog />}
+          placeholder='Choose option...'
+          width='auto'
+        />
+      </Container>
+    </Container>
+  )
+}
+
+const OPTIONS = [
+  { value: '1', text: 'Option 1' },
+  { value: '2', text: 'Option 2' },
+  { value: '3', text: 'Option 3' },
+  { value: '4', text: 'Option 4' }
+]
+
+export default SelectWithIconExample
