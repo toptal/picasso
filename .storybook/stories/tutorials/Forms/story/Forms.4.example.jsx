453d62d8537d7bc4b1a2dd47d103764c40f438b2
Uladzimir Havenchyk <uladzimir@toptal.com>
2019-05-29 10:29:36 +0300

    docs: add form step using labels in tutorial

---
+++ .storybook/stories/tutorials/Forms/story/Forms.4.example.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Grid, Checkbox, Select, TextField, Form } from '@toptal/picasso'
+
+const FormsExample = () => (
+  <div>
+    <Grid justify='center' alignItems='center'>
+      <Grid.Item small={12} medium={8} large={6}>
+        <Form>
+          <Form.Field>
+            <Form.Label>Select talent</Form.Label>
+            <Select options={OPTIONS} />
+          </Form.Field>
+
+          <Form.Field>
+            <Form.Label required htmlFor='job-title'>
+              Job title
+            </Form.Label>
+            <TextField id='job-title' fullWidth />
+          </Form.Field>
+
+          <Form.Field>
+            <Grid direction='row'>
+              <Grid.Item small={6}>
+                <Form.Field>
+                  <Form.Label>Current country</Form.Label>
+                  <Select options={OPTIONS} />
+                </Form.Field>
+              </Grid.Item>
+              <Grid.Item small={6}>
+                <Form.Field>
+                  <Form.Label htmlFor='city'>City of residence</Form.Label>
+                  <TextField id='city' fullWidth />
+                </Form.Field>
+              </Grid.Item>
+            </Grid>
+          </Form.Field>
+
+          <Form.Field>
+            <Form.Label htmlFor='description'>Full description</Form.Label>
+            <TextField id='description' fullWidth />
+          </Form.Field>
+
+          <Form.Field>
+            <Form.Label>Desired Commitment</Form.Label>
+            <Select options={OPTIONS} />
+          </Form.Field>
+
+          <Form.Field>
+            <Checkbox label='We match you to talent' />
+            <Form.Hint>
+              A Toptal Director of Engineering ewill work with you to understand
+              you needs and fins you the right talent for you project.
+            </Form.Hint>
+          </Form.Field>
+        </Form>
+      </Grid.Item>
+    </Grid>
+  </div>
+)
+
+const OPTIONS = [
+  { value: '1', text: 'Option 1' },
+  { value: '2', text: 'Option 2' },
+  { value: '3', text: 'Option 3' },
+  { value: '4', text: 'Option 4' }
+]
+
+export default FormsExample
