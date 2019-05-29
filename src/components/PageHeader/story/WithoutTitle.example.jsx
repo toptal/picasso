8ded7f6781e026d5d33875f227e95b863754098f
Vedran Ivanac <vedran.ivanac@toptal.com>
2019-05-29 12:28:27 +0200

    feat(page.header): add variant prop (#392)

---
+++ src/components/PageHeader/story/WithoutTitle.example.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Page } from '@toptal/picasso'
+
+const PageHeaderDefaultExample = () => (
+  <div style={{ height: '3.75em' }}>
+    <Page.Header />
+  </div>
+)
+
+export default PageHeaderDefaultExample
