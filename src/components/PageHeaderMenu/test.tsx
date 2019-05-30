0fd62ff6e02432ab3f3b21a37acac05a1c0beab9
Vedran Ivanac <vedran.ivanac@toptal.com>
2019-05-30 12:58:46 +0200

    feat(page-header-menu): wrap header Menu to Page.HeaderMenu (#406)
    BREAKING CHANGE: UserBadge children is now wrapped to special component
    which is exported as `Page.HeaderMenu` which accepts aggregated props
    for `UserBadge` component. You should replace direct usage of `UserBadge`
    inside `Header` with this new component.

---
+++ src/components/PageHeaderMenu/test.tsx
@@ -0,0 +1,39 @@
+import React, { FunctionComponent } from 'react'
+/* eslint-disable-next-line */
+import { render, cleanup } from 'react-testing-library'
+
+import Picasso, { OmitInternalProps } from '../Picasso'
+import Menu from '../Menu'
+import PageHeaderMenu, { Props } from './PageHeaderMenu'
+
+const TestPageHeaderMenu: FunctionComponent<OmitInternalProps<Props>> = ({
+  children,
+  name,
+  avatar
+}) => (
+  <Picasso loadFonts={false}>
+    <PageHeaderMenu name={name} avatar={avatar}>
+      {children}
+    </PageHeaderMenu>
+  </Picasso>
+)
+
+afterEach(cleanup)
+
+describe('PageHeaderMenu', () => {
+  test('default render', () => {
+    const { container } = render(
+      <TestPageHeaderMenu
+        name='Jacqueline Roque'
+        avatar='./jacqueline-with-flowers-1954-square.jpg'
+      >
+        <Menu>
+          <Menu.Item>My Account</Menu.Item>
+          <Menu.Item>Log Out</Menu.Item>
+        </Menu>
+      </TestPageHeaderMenu>
+    )
+
+    expect(container).toMatchSnapshot()
+  })
+})
