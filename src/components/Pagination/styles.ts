ffd9f4e9fb05403e14fcab683d3df269b6ea3eb2
Vedran Ivanac <vedran.ivanac@toptal.com>
2019-05-31 11:30:18 +0200

    feat(pagination): sync with base design

---
+++ src/components/Pagination/styles.ts
@@ -0,0 +1,11 @@
+import { createStyles } from '@material-ui/core/styles'
+
+export default () =>
+  createStyles({
+    rangeButton: {
+      width: '1.5em'
+    },
+    ellipsis: {
+      padding: '0 1em'
+    }
+  })
