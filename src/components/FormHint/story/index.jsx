9d8f39da0cc0d2e8f9d5b2954b0bc78dc7041618
Uladzimir Havenchyk <uladzimir@toptal.com>
2019-06-04 02:59:05 -0700

    chore(docs): cleanup useless tests and remove snapshots (#426)

--- src/components/FormHint/story/index.jsx
+++ src/components/FormHint/story/index.jsx
@@ -2,16 +2,6 @@ import PicassoBook from '~/.storybook/components/PicassoBook'
 
 import { FormHint } from '../FormHint'
 
-const chapter = PicassoBook.connectToPage(page =>
-  page
-    .createChapter(
-      'Form Hint',
-      `Add the <Form.Hint> component if you need to add
-    some extra information for your form control`
-    )
-    .addExample('FormHint/story/FormHint.example.jsx', 'Form hint')
-)
-
 const componentDocs = PicassoBook.createComponentDocs(FormHint, 'Form.Hint')
 
-export default { chapter, componentDocs }
+export default { componentDocs }
