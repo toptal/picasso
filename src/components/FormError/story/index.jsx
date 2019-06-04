4c7d463c865a6c6b6fce421df6d8cef6a8a59317
Uladzimir Havenchyk <uladzimir@toptal.com>
2019-06-04 02:59:05 -0700

    chore: cleanup useless tests and remove snapshots (#426)

---
+++ src/components/FormError/story/index.jsx
@@ -0,0 +1,7 @@
+import PicassoBook from '~/.storybook/components/PicassoBook'
+
+import { FormError } from '../FormError'
+
+const componentDocs = PicassoBook.createComponentDocs(FormError, 'Form.Error')
+
+export default { componentDocs }
