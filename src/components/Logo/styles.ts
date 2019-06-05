06f75b54555c8903503229a06292f35f1610a16f
Daniel O <daniel.ostapenko@toptal.com>
2019-06-05 18:36:30 +0300

    fix: [FX-263] Fix header and logo components heights (#434)
    * fix: [FX-263] Fix heights of the Header and Logo
    
    * fix: [FX-263] Update visual screenshots

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -3,11 +3,11 @@ import { Theme, createStyles } from '@material-ui/core/styles'
 export default ({ palette }: Theme) =>
   createStyles({
     logo: {
-      fontSize: '1.75em',
+      fontSize: '1.875em',
       verticalAlign: 'baseline'
     },
     logoEmblem: {
-      fontSize: '1.75em',
+      fontSize: '1.875em',
       verticalAlign: 'baseline'
     },
     default: {
