1ae74bbe6d0a23a8a837480b30994664531920f7
Vedran Ivanac <vedran.ivanac@toptal.com>
2019-06-06 11:59:25 +0200

    docs(select): fix placeholder typo

--- src/components/Select/story/Error.example.jsx
+++ src/components/Select/story/Error.example.jsx
@@ -14,7 +14,7 @@ const SelectErrorExample = () => {
       onChange={handleChange}
       options={OPTIONS}
       value={value}
-      placeholder='Choose option...'
+      placeholder='Choose an option...'
       width='auto'
     />
   )
