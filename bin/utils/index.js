d367c8d89c28e2374cf88933e2ad03290e963b02
denieler <daniel.ostapenko@toptal.com>
2019-06-25 17:18:43 +0300

    chore(ci): [FX-303] Refactor the structure of the final package

--- bin/utils/index.js
+++ bin/utils/index.js
@@ -1,8 +1,6 @@
 /* eslint-disable no-console */
 
 const chalk = require('chalk')
-const fs = require('fs-extra')
-const path = require('path')
 
 // General logging
 const log = function (text, color) {
@@ -10,30 +8,6 @@ const log = function (text, color) {
   console.log(chalk[color](text))
 }
 
-const generatePackageJson = (
-  sourceData,
-  outDir,
-  version = false,
-  entry = './index.js'
-) => {
-  log(`Creating package.json in: ${outDir}/package.json`)
-  fs.ensureDirSync(outDir)
-  const out = path.resolve(outDir, './package.json')
-
-  const data = {
-    ...sourceData,
-    main: entry,
-    module: entry
-  }
-
-  if (version) {
-    data.version = version
-  }
-
-  fs.writeFileSync(out, JSON.stringify(data, null, 2))
-}
-
 module.exports = {
-  log,
-  generatePackageJson
+  log
 }
--- release.config.js
+++ release.config.js
@@ -1,51 +1,22 @@
 /* eslint-disable */
 
 module.exports = {
-  analyzeCommits: ['@semantic-release/commit-analyzer'], // Determine the type of release by analyzing commits with conventional-changelog
-  verifyConditions: [
-    '@semantic-release/changelog', // Verify the presence and the validity of the configuration
-    '@semantic-release/npm', // Verify the presence and the validity of the npm authentication and release configuration
-    '@semantic-release/github', // Verify the presence and the validity of the GitHub authentication and release configuration
-    '@semantic-release/git' // Verify the presence and the validity of the Git authentication and release configuration
-  ],
-  generateNotes: ['@semantic-release/release-notes-generator'], // Generate release notes for the commits added since the last release with conventional-changelog
-  prepare: [
-    '@semantic-release/changelog', // Create or update the changelog file in the local project repository
-    [
-      '@semantic-release/exec', // Execute a shell command to bump next version inside README.md
-      {
-        prepareCmd: './ci/release/bump-version ${nextRelease.version}'
-      }
-    ],
-    [
-      '@semantic-release/npm', // Update the package.json version and create the npm package tarball
-      {
-        pkgRoot: 'build'
-      }
-    ],
-    [
-      '@semantic-release/exec', // Execute a shell command to update package.json to new version
-      {
-        prepareCmd: './ci/release/prepare-assets ${nextRelease.version}'
-      }
-    ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+    '@semantic-release/npm',
     [
       '@semantic-release/git',
       {
-        assets: ['CHANGELOG.md', 'README.md', 'package.json', './build'] // Push a release commit and tag, including globed files
+        assets: ['CHANGELOG.md', 'README.md', 'package.json', './build']
       }
-    ]
-  ],
-  publish: [
-    '@semantic-release/github', // Publish a GitHub release
+    ],
     [
-      '@semantic-release/npm', // Publish the package on the npm registry
+      '@semantic-release/exec',
       {
-        pkgRoot: 'build'
+        prepareCmd: "echo 'export VERSION=${nextRelease.version}' > .version"
       }
     ]
-  ],
-  success: ['@semantic-release/github'], // Add a comment to GitHub issues and pull requests resolved in the release
-  fail: ['@semantic-release/github'], // Open a GitHub issue when a release fails
-  plugins: [] // Reset default plugins and use custom steps defined in this file
+  ]
 }
