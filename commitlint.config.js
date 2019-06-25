9263d14608d55dc48d3d9abc4e8ea48a8f5abb86
Uladzimir Havenchyk <uladzimir@toptal.com>
2019-06-25 16:39:58 +0300

    chore: adjust commitlint to support PascalCase in scope

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [1, 'always', ['chore', 'feat', 'fix', 'docs']]
+    'type-enum': [1, 'always', ['chore', 'feat', 'fix', 'docs']],
+    'scope-case': [2, 'always', ['pascal-case', 'lowercase']]
   }
 }
