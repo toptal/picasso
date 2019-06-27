aa8e710becbb5ea3df590446a2a9434746bb1c3e
Uladzimir Havenchyk <uladzimir@toptal.com>
2019-06-27 16:44:20 +0300

    fix(Table): sync styles with base (#504)
    Use the same color for hover state and transition as is used for MenuItem

--- src/components/TableRow/styles.ts
+++ src/components/TableRow/styles.ts
@@ -1,19 +1,31 @@
 import { Theme, createStyles } from '@material-ui/core/styles'
 
+import { alpha } from '../styles'
 import { PicassoProvider } from '../Picasso'
 
 PicassoProvider.override(() => ({}))
 
-export default ({ palette, spacing }: Theme) =>
+export default ({ palette, spacing, transitions }: Theme) =>
   createStyles({
     root: {
       height: 'auto',
 
       '&:nth-of-type(even)': {
-        background: '#f8f9f9'
+        background: alpha(palette.grey.lighter!, 0.32)
+      },
+
+      '&$hover:hover': {
+        backgroundColor: palette.blue.lighter
       }
     },
+
     head: {
       borderBottom: `${spacing.borderWidth} solid ${palette.grey.lighter}`
+    },
+
+    hover: {
+      transition: transitions.create('background-color', {
+        duration: transitions.duration.shortest
+      })
     }
   })
