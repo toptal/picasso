1cb44b37c302586462d0d716f26aac97868af298
denieler <daniel.ostapenko@toptal.com>
2019-07-08 18:21:05 +0300

    refactor(Autocomplete): [FX-143] Refactor some logic in Autocomplete

--- src/components/Autocomplete/Autocomplete.tsx
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -24,8 +24,6 @@ type Item = {
   label: string
 }
 
-type Value = string | null
-
 /**
  * Alias for all valid HTML props for `<input>` element.
  * Does not include React's `ref` or `key`.
@@ -55,35 +53,8 @@ export interface Props
   onChange?: (event: ChangeEvent<HTMLInputElement>) => void
 }
 
-const getFilteredOptions = (
-  options: Item[],
-  value: Value,
-  minLength?: number
-) => {
-  if (!isMatchingMinLength(value, minLength) || !value) {
-    return options
-  }
-
-  return options.filter(option => isSubstring(value, option.label))
-}
-
-const getRelevantOption = (options: Item[], value: Value): Item | null => {
-  if (!value || !value.trim()) {
-    return null
-  }
-
-  const filteredOptions = getFilteredOptions(options, value)
-
-  return (
-    filteredOptions.find(option => isSubstring(value, option.label)) || null
-  )
-}
-
-const isMatchingMinLength = (value: Value, minLength?: number) => {
-  const inputValue = value || ''
-
-  return !minLength || inputValue.length >= minLength
-}
+const isMatchingMinLength = (value: string, minLength?: number) =>
+  !minLength || value.length >= minLength
 
 export const Autocomplete: FunctionComponent<Props> = ({
   classes,
@@ -115,14 +86,17 @@ export const Autocomplete: FunctionComponent<Props> = ({
         openMenu,
         selectItem
       }) => {
-        const filteredOptions = getFilteredOptions(options!, inputValue)
+        const val = (inputValue || '').trim()
+        const filteredOptions = options!.filter(option =>
+          isSubstring(val, option.label)
+        )
 
-        const isTyping = Boolean(inputValue)
+        const isTyping = Boolean(val)
         const hasOptions = Boolean(filteredOptions.length)
 
         const canOpen =
           isOpen &&
-          isMatchingMinLength(inputValue, minLength) &&
+          isMatchingMinLength(val, minLength) &&
           !loading &&
           (hasOptions || isTyping)
 
@@ -154,10 +128,14 @@ export const Autocomplete: FunctionComponent<Props> = ({
         } = getInputProps({
           onFocus: openMenu,
           onBlur: () => {
-            const option = getRelevantOption(options!, inputValue)
+            if (!val.length || !filteredOptions.length) {
+              return
+            }
 
-            if (option) {
-              selectItem(option.label)
+            const firstOption = filteredOptions[0]
+
+            if (firstOption) {
+              selectItem(firstOption.label)
             }
           },
           onChange: (event: ChangeEvent<HTMLInputElement>) => {
@@ -165,10 +143,12 @@ export const Autocomplete: FunctionComponent<Props> = ({
               clearSelection()
             }
 
-            if (isMatchingMinLength(event.target.value, minLength)) {
-              event.persist()
-              onChangeDebounced(event)
+            if (!isMatchingMinLength(event.target.value, minLength)) {
+              return
             }
+
+            event.persist()
+            onChangeDebounced(event)
           }
         })
 
