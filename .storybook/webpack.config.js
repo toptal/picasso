ef2e3bcd6cdef5bd79a20f2b27fb715b74318e70
Uladzimir Havenchyk <uladzimir@toptal.com>
2019-08-01 10:21:50 +0300

    fix: enable transpileOnly in development

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,20 +1,23 @@
 const webpack = require('webpack')
 const path = require('path')
 const HardSourceWebpackPlugin = require('hard-source-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const ForkTsCheckerNotifierWebpackPlugin = require('fork-ts-checker-notifier-webpack-plugin')
 
 // example: /components/Button/Button.tsx
 const COMPONENT_DECLARATION_FILE_REGEXP = /components\/(.*)\/\1.tsx$/
 const LAB_COMPONENT_DECLARATION_FILE_REGEXP = /components\/lab\/(.*)\/\1.tsx$/
 
 const { env } = process
+const isDevelopment = env.NODE_ENV !== 'production' && env.NODE_ENV !== 'test'
 
 const tsConfigFile = path.join(process.cwd(), './.storybook/tsconfig.json')
 const tsLoader = {
   loader: require.resolve('ts-loader'),
   options: {
     configFile: tsConfigFile,
-    transpileOnly: true,
-    experimentalWatchApi: true
+    transpileOnly: isDevelopment,
+    experimentalWatchApi: isDevelopment
   }
 }
 
@@ -62,15 +65,20 @@ module.exports = ({ config }) => {
   config.plugins.push(
     new webpack.DefinePlugin({
       TEST_ENV: JSON.stringify(env.TEST_ENV)
-    }),
-    new ForkTsCheckerWebpackPlugin(),
-    new ForkTsCheckerNotifierWebpackPlugin({
-      title: 'Picasso',
-      excludeWarnings: true,
-      skipSuccessful: true,
-      skipFirstNotification: true
     })
   )
+
+  if (isDevelopment) {
+    config.plugins.push(
+      new ForkTsCheckerWebpackPlugin(),
+      new ForkTsCheckerNotifierWebpackPlugin({
+        title: 'Picasso',
+        excludeWarnings: true,
+        skipSuccessful: true,
+        skipFirstNotification: true
+      })
+    )
+  }
 
   if (env.CACHE) {
     config.plugins.push(new HardSourceWebpackPlugin())
