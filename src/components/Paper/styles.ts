cf51cd51823ef2d142186b517de36a72a70b0aa9
Daniel O <daniel.ostapenko@toptal.com>
2019-08-16 09:44:25 +0200

    fix(paper): fix default text color (#594)

--- src/components/Paper/styles.ts
+++ src/components/Paper/styles.ts
@@ -2,6 +2,12 @@ import { createStyles } from '@material-ui/core/styles'
 
 import { PicassoProvider } from '../Picasso'
 
-PicassoProvider.override(() => ({}))
+PicassoProvider.override(() => ({
+  MuiPaper: {
+    root: {
+      color: 'unset'
+    }
+  }
+}))
 
 export default () => createStyles({})
