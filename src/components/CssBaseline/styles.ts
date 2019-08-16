bf9052c416cc23e6b527579afa46c9a4129c1f55
Daniel O <daniel.ostapenko@toptal.com>
2019-08-16 14:54:34 +0300

    fix(CssBaseline): [FX-368] Use our own css baseline (#595)

--- src/components/CssBaseline/styles.ts
+++ src/components/CssBaseline/styles.ts
@@ -1,16 +1,27 @@
-import { createStyles } from '@material-ui/core/styles'
+import { Theme, createStyles } from '@material-ui/core/styles'
 
-export default () =>
+export default ({ palette }: Theme) =>
   createStyles({
     '@global': {
       html: {
-        boxSizing: 'initial'
+        boxSizing: 'initial',
+        '-webkit-font-smoothing': 'antialiased',
+        '-moz-osx-font-smoothing': 'grayscale'
+      },
+      '*, *::before, *::after': {
+        boxSizing: 'inherit'
       },
       body: {
         display: 'flex',
         flexDirection: 'column',
         minHeight: '100vh',
-        color: '#000'
+        margin: 0,
+        backgroundColor: palette.common.white
+      },
+      '@media print': {
+        body: {
+          backgroundColor: palette.common.white
+        }
       },
       '#root': {
         display: 'flex',
