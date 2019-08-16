9de4ba1224bedee18068bb4e411a34a2432893ba
denieler <daniel.ostapenko@toptal.com>
2019-08-16 12:16:46 +0300

    fix(Slider): [FX-378] Move Slider from lab to core

--- src/components/lab/Slider/styles.ts
+++ src/components/lab/Slider/styles.ts
@@ -1,23 +1,36 @@
 import { Theme, createStyles } from '@material-ui/core/styles'
 
 import { rem } from '../../styles'
+import { PicassoProvider } from '../../Picasso'
+
+PicassoProvider.override(() => ({
+  MuiSlider: {
+    thumb: {
+      '&$active': {
+        boxShadow: 'none'
+      }
+    }
+  }
+}))
 
 export default ({ palette }: Theme) =>
   createStyles({
+    root: {
+      color: palette.grey.main
+    },
+    rail: {
+      height: rem('1px')
+    },
     track: {
       backgroundColor: palette.grey.main,
       height: rem('1px')
     },
     thumb: {
+      backgroundColor: palette.primary.main,
       border: `${rem('2px')} solid ${palette.common.white}`,
       height: rem('13px'),
       width: rem('13px'),
       '&:hover': {
-        boxShadow: 'none'
-      }
-    },
-    activated: {
-      '& $thumb': {
         boxShadow: 'none'
       }
     }
