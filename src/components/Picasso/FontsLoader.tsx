4d2790c6e931c404f060354a715fdc7aa55bbd2f
denieler <daniel.ostapenko@toptal.com>
2019-08-27 16:52:36 +0300

    fix: [FX-391] Fix loading fonts multiple times

--- src/components/Picasso/FontsLoader.tsx
+++ src/components/Picasso/FontsLoader.tsx
@@ -1,23 +1,44 @@
-import React from 'react'
+import { useLayoutEffect } from 'react'
 
 const PROXIMA_NOVA_FONT = 'https://use.typekit.net/rlr4crj.css'
 
 // After the file is loaded to apply it
 // we have to change rel to 'stylesheet'
 // https://alligator.io/html/preload-prefetch
-const applyLoadedFont = (e: React.SyntheticEvent) => {
+const applyLoadedFont = (e: Event) => {
   const target = e.target as HTMLLinkElement
 
   target.rel = 'stylesheet'
 }
 
-const FontsLoader = () => (
-  <link
-    as='style'
-    href={PROXIMA_NOVA_FONT}
-    onLoad={applyLoadedFont}
-    rel='preload'
-  />
-)
+const findFontsLoader = () => {
+  const links = Array.from(document.getElementsByTagName('link'))
+
+  return links.find(
+    link =>
+      link.as === 'style' &&
+      link.href === PROXIMA_NOVA_FONT &&
+      (link.rel === 'stylesheet' || link.rel === 'preload')
+  )
+}
+
+const FontsLoader = () => {
+  useLayoutEffect(() => {
+    const existingFontLoader = findFontsLoader()
+
+    if (!existingFontLoader) {
+      const link = document.createElement('link')
+
+      link.as = 'style'
+      link.href = PROXIMA_NOVA_FONT
+      link.rel = 'preload'
+      link.addEventListener('load', applyLoadedFont)
+
+      document.body.appendChild(link)
+    }
+  }, [])
+
+  return null
+}
 
 export default FontsLoader
