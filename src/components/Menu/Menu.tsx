bd9e194c364759e331be13d92bc2e44461fbb254
Alexey Shchur <alexey.shchur@toptal.com>
2019-09-11 08:21:30 +0300

    fix(Menu): polish drilldown functionality

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -2,7 +2,8 @@ import React, {
   HTMLAttributes,
   forwardRef,
   ReactElement,
-  useState
+  useState,
+  ReactNode
 } from 'react'
 import MUIMenuList, { MenuListProps } from '@material-ui/core/MenuList'
 import { withStyles } from '@material-ui/core/styles'
@@ -26,57 +27,97 @@ interface StaticProps {
   Item: typeof MenuItem
 }
 
+function extendMenuItemsWithChevrons(
+  children: ReactNode,
+  onClick: (arg: number) => void
+) {
+  return React.Children.toArray(children).map((child, idx) => {
+    const childElement = child as ReactElement
+
+    if (childElement.props.menu) {
+      child = (
+        /* eslint-disable react/jsx-props-no-spreading */
+        <MenuItem {...childElement.props} onClick={() => onClick(idx)}>
+          <Container inline flex alignItems='center' style={{ flex: 1 }}>
+            <Container style={{ flex: 1 }}>
+              <WrappedStringMenuItemContent>
+                {childElement.props.children}
+              </WrappedStringMenuItemContent>
+            </Container>
+            <Chevron16 />
+          </Container>
+        </MenuItem>
+        /* eslint-enable react/jsx-props-no-spreading */
+      )
+    }
+
+    return child
+  })
+}
+
+function getActiveChildRecursively(
+  children: ReactNode,
+  activeChildPath: number[]
+): ReactElement {
+  const currentLevelActiveChild = React.Children.toArray(children)[
+    activeChildPath[0]
+  ] as ReactElement
+
+  if (activeChildPath.length === 1) {
+    return currentLevelActiveChild
+  } else {
+    return getActiveChildRecursively(
+      currentLevelActiveChild.props.menu.props.children,
+      activeChildPath.slice(1)
+    )
+  }
+}
+
 // eslint-disable-next-line react/display-name
 export const Menu = forwardRef<HTMLUListElement, Props>(function Menu(
   { children, className, classes, style, ...rest },
   ref
 ) {
-  const [activeChildIdx, setActiveChildIdx] = useState(-1)
+  const [activeChildPath, setActiveChildPath] = useState<number[]>([])
 
-  if (activeChildIdx !== -1) {
-    const activeChild = React.Children.toArray(children).find((child, idx) => {
-      return idx === activeChildIdx
-    }) as ReactElement
+  function handleDrilldown(innerIdx: number) {
+    const newPath = [...activeChildPath, innerIdx]
+
+    setActiveChildPath(newPath)
+  }
+
+  function handleBack() {
+    setActiveChildPath(activeChildPath.slice(0, -1))
+  }
+
+  let activeMenuProps = {}
+
+  if (activeChildPath.length) {
+    const activeChild = getActiveChildRecursively(children, activeChildPath)
 
     /* eslint-disable react/jsx-key */
-    children = [
-      <MenuItem onClick={() => setActiveChildIdx(-1)}>
+    const backButton = (
+      <MenuItem onClick={handleBack}>
         <Container inline flex alignItems='center' style={{ flex: 1 }}>
           <BackMinor16 />
           <Container style={{ flex: 1 }}>
             <WrappedStringMenuItemContent>Back</WrappedStringMenuItemContent>
           </Container>
         </Container>
-      </MenuItem>,
-      activeChild.props.menu
-    ]
+      </MenuItem>
+    )
     /* eslint-disable react/jsx-key */
+
+    activeMenuProps = activeChild.props.menu.props
+    children = [
+      backButton,
+      ...extendMenuItemsWithChevrons(
+        activeChild.props.menu.props.children,
+        handleDrilldown
+      )
+    ]
   } else {
-    children = React.Children.toArray(children).map((child, idx) => {
-      const childElement = child as ReactElement
-
-      if (childElement.props.menu) {
-        child = (
-          /* eslint-disable react/jsx-props-no-spreading */
-          <MenuItem
-            {...childElement.props}
-            onClick={() => setActiveChildIdx(idx)}
-          >
-            <Container inline flex alignItems='center' style={{ flex: 1 }}>
-              <Container style={{ flex: 1 }}>
-                <WrappedStringMenuItemContent>
-                  {childElement.props.children}
-                </WrappedStringMenuItemContent>
-              </Container>
-              <Chevron16 />
-            </Container>
-          </MenuItem>
-          /* eslint-enable react/jsx-props-no-spreading */
-        )
-      }
-
-      return child
-    })
+    children = extendMenuItemsWithChevrons(children, handleDrilldown)
   }
 
   return (
@@ -87,6 +128,8 @@ export const Menu = forwardRef<HTMLUListElement, Props>(function Menu(
       className={className}
       style={style}
       classes={classes}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...activeMenuProps}
     >
       {children}
     </MUIMenuList>
