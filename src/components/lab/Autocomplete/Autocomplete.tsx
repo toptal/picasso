ba0eeca01aba682cb6eccd1f306a358ff861addd
OleksandrNechai <oleksandr.nechai@toptal.com>
2019-09-17 12:11:15 +0300

    feat(autocomplete): add custom renderer for an option

--- src/components/lab/Autocomplete/Autocomplete.tsx
+++ src/components/lab/Autocomplete/Autocomplete.tsx
@@ -38,7 +38,7 @@ type Item = {
  */
 type HTMLInputProps = InputHTMLAttributes<HTMLInputElement>
 
-export interface Props
+export interface Props<T>
   extends StandardProps,
     Omit<
       HTMLInputProps,
@@ -69,7 +69,7 @@ export interface Props
   /** Label to show when no options were found */
   noOptionsText?: string
   /** List of options */
-  options?: Item[]
+  options?: T[]
   /** The minimum number of characters a user must type before a search is performed */
   minLength?: number
   /**  Callback invoked when key is pressed */
@@ -81,243 +81,241 @@ export interface Props
   startAdornment?: ReactNode
   /** Custom input component */
   inputComponent?: ComponentType<InputProps>
+  /** Callback responsible for rendering the option given the option and its index in the list of options */
+  renderOption?: (option: T, index?: number) => ReactNode
 }
 
 const isMatchingMinLength = (value: string, minLength?: number) =>
   !minLength || value.length >= minLength
 
-const getItemText = (item: Maybe<Item>) =>
+const getItemText: <T extends Item>(item: Maybe<T>) => string = item =>
   (item && item.text) || EMPTY_INPUT_VALUE
 
-const getItemValue = (item: Maybe<Item>) =>
+const getItemValue: <T extends Item>(item: Maybe<T>) => string | null = item =>
   (item && (item.value || item.text)) || null
 
-const isSelected = (item: Item, selectedItem: Item | null) =>
+const isSelected: <T extends Item>(
+  item: T,
+  selectedItem: T | null
+) => boolean = (item, selectedItem) =>
   getItemValue(item) === getItemValue(selectedItem)
 
-export const Autocomplete = forwardRef<HTMLInputElement, Props>(
-  function Autocomplete(
-    {
-      classes,
-      className,
-      defaultInputValue,
-      inputValue: inputValueProp,
-      onChange: onInputChange,
-      defaultValue,
-      value,
-      onSelect,
-      debounceTime,
-      loading,
-      minLength,
-      placeholder,
-      noOptionsText,
-      options,
-      style,
-      width,
-      allowAny,
-      onKeyDown,
-      inputComponent,
-      ...rest
-    },
-    ref
-  ) {
-    const [
-      selectedItemValue,
-      setSelectedItemValue
-    ] = useControlledAndUncontrolledState(defaultValue, value, onSelect!)
+export const Autocomplete = forwardRef(function Autocomplete<T extends Item>(
+  {
+    classes,
+    className,
+    defaultInputValue,
+    inputValue: inputValueProp,
+    onChange: onInputChange,
+    defaultValue,
+    value,
+    onSelect,
+    debounceTime,
+    loading,
+    minLength,
+    placeholder,
+    noOptionsText,
+    options,
+    style,
+    width,
+    allowAny,
+    onKeyDown,
+    inputComponent,
+    renderOption,
+    ...rest
+  }: Props<T>,
+  ref: React.Ref<HTMLInputElement>
+) {
+  const [
+    selectedItemValue,
+    setSelectedItemValue
+  ] = useControlledAndUncontrolledState(defaultValue, value, onSelect!)
 
-    const selectedItem =
-      selectedItemValue === null
-        ? null
-        : options!.find(option => getItemValue(option) === selectedItemValue)
+  const selectedItem =
+    selectedItemValue === null
+      ? null
+      : options!.find(option => getItemValue(option) === selectedItemValue)
 
-    if (selectedItem === undefined) {
-      window.console.warn(
-        `Autocomplete: There is no option for the given value \`${value}\``
-      )
-      return null
-    }
-
-    const onInputChangeDebounced = useCallback(
-      debounceTime === 0
-        ? onInputChange!
-        : debounce(onInputChange!, debounceTime),
-      [onInputChange, debounceTime]
+  if (selectedItem === undefined) {
+    window.console.warn(
+      `Autocomplete: There is no option for the given value \`${value}\``
     )
-
-    const [inputValue, setInputValue] = useControlledAndUncontrolledInput(
-      defaultInputValue || getItemText(selectedItem),
-      inputValueProp,
-      onInputChangeDebounced
-    )
-
-    const handleInputValueChange = (newInputValue: string) => {
-      if (newInputValue !== inputValue) {
-        setInputValue(newInputValue)
-      }
-    }
-
-    const handleSelectItem = (item: Item | null) => {
-      setSelectedItemValue(getItemValue(item))
-    }
-
-    const matchingOptions =
-      getItemText(selectedItem) === inputValue
-        ? options!
-        : options!.filter(item => isSubstring(inputValue, getItemText(item)))
-
-    const currentSelectedItemIndex = selectedItem
-      ? matchingOptions.indexOf(selectedItem)
-      : null
-
-    const downshiftStateReducer = (
-      state: DownshiftState<Item>,
-      changes: StateChangeOptions<Item>
-    ): Partial<StateChangeOptions<Item>> => {
-      switch (changes.type) {
-        case Downshift.stateChangeTypes.controlledPropUpdatedSelectedItem:
-          return { ...changes, highlightedIndex: currentSelectedItemIndex }
-        case Downshift.stateChangeTypes.changeInput:
-          return { ...changes, highlightedIndex: FIRST_ITEM_INDEX }
-        case Downshift.stateChangeTypes.mouseUp:
-        case Downshift.stateChangeTypes.blurInput:
-          const hasInput = inputValue.length > 0
-
-          if (
-            allowAny &&
-            hasInput &&
-            inputValue !== getItemText(selectedItem)
-          ) {
-            return {
-              ...changes,
-              inputValue,
-              selectedItem: null
-            }
-          }
-          break
-      }
-      return changes
-    }
-
-    const downshiftItemToString = (item: Item | null) =>
-      item === null
-        ? allowAny
-          ? inputValue
-          : EMPTY_INPUT_VALUE
-        : getItemText(item)
-
-    return (
-      <Downshift
-        inputValue={inputValue}
-        onInputValueChange={handleInputValueChange}
-        selectedItem={selectedItem}
-        onChange={handleSelectItem}
-        itemToString={downshiftItemToString}
-        stateReducer={downshiftStateReducer}
-      >
-        {({
-          getMenuProps,
-          getInputProps,
-          getItemProps,
-          isOpen,
-          highlightedIndex,
-          selectItem,
-          setState
-        }) => {
-          const hasMatchingOptions = matchingOptions.length > 0
-          const canOpen =
-            isOpen && isMatchingMinLength(inputValue, minLength) && !loading
-
-          const optionsMenu = (
-            <ScrollMenu selectedIndex={highlightedIndex}>
-              {hasMatchingOptions ? (
-                matchingOptions.map((option, index) => (
-                  <Menu.Item
-                    key={getItemValue(option)}
-                    selected={highlightedIndex === index}
-                    disabled={isSelected(option, selectedItem)}
-                    /* eslint-disable-next-line react/jsx-props-no-spreading */
-                    {...getItemProps({ item: option, index })}
-                  >
-                    {getItemText(option)}
-                  </Menu.Item>
-                ))
-              ) : (
-                <Menu.Item disabled>{noOptionsText}</Menu.Item>
-              )}
-            </ScrollMenu>
-          )
-
-          const handleFocusOrClick = () => {
-            if (!isOpen) {
-              let newInputValue = inputValue
-              const isInputSelectedItem =
-                inputValue === getItemText(selectedItem)
-
-              if (!allowAny || isInputSelectedItem) {
-                newInputValue = EMPTY_INPUT_VALUE
-              }
-              setState({
-                isOpen: true,
-                inputValue: newInputValue,
-                highlightedIndex: currentSelectedItemIndex || FIRST_ITEM_INDEX
-              })
-            }
-          }
-
-          const InputComponent = inputComponent || Input
-
-          const inputProps = getInputProps({
-            onFocus: handleFocusOrClick,
-            onClick: handleFocusOrClick,
-            onKeyDown: (event: KeyboardEvent<HTMLInputElement>) => {
-              if (
-                event.key === 'Backspace' &&
-                inputValue === EMPTY_INPUT_VALUE
-              ) {
-                selectItem(null)
-              }
-              onKeyDown!(event, inputValue)
-            }
-          })
-
-          return (
-            <div
-              className={cx(
-                classes.root,
-                className,
-                classes[`root${capitalize(width!)}`]
-              )}
-              style={style}
-            >
-              <InputComponent
-                /* eslint-disable-next-line react/jsx-props-no-spreading */
-                {...rest}
-                // eslint-disable-next-line react/jsx-props-no-spreading
-                {...inputProps}
-                defaultValue={inputProps.defaultValue as string | undefined}
-                value={inputProps.value as string | undefined}
-                onChange={e => {
-                  inputProps.onChange!(e as FormEvent<HTMLInputElement>)
-                }}
-                ref={ref}
-                classes={{}}
-                placeholder={
-                  selectedItem ? getItemText(selectedItem) : placeholder
-                }
-                icon={loading ? <Loader size='small' /> : null}
-                iconPosition='end'
-                width={width}
-              />
-              {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-              <div {...getMenuProps()}>{canOpen ? optionsMenu : null}</div>
-            </div>
-          )
-        }}
-      </Downshift>
-    )
+    return null
   }
-)
+
+  const onInputChangeDebounced = useCallback(
+    debounceTime === 0
+      ? onInputChange!
+      : debounce(onInputChange!, debounceTime),
+    [onInputChange, debounceTime]
+  )
+
+  const [inputValue, setInputValue] = useControlledAndUncontrolledInput(
+    defaultInputValue || getItemText(selectedItem),
+    inputValueProp,
+    onInputChangeDebounced
+  )
+
+  const handleInputValueChange = (newInputValue: string) => {
+    if (newInputValue !== inputValue) {
+      setInputValue(newInputValue)
+    }
+  }
+
+  const handleSelectItem = (item: T | null) => {
+    setSelectedItemValue(getItemValue(item))
+  }
+
+  const matchingOptions =
+    getItemText(selectedItem) === inputValue
+      ? options!
+      : options!.filter(item => isSubstring(inputValue, getItemText(item)))
+
+  const currentSelectedItemIndex = selectedItem
+    ? matchingOptions.indexOf(selectedItem)
+    : null
+
+  const downshiftStateReducer = (
+    state: DownshiftState<T>,
+    changes: StateChangeOptions<T>
+  ): Partial<StateChangeOptions<T>> => {
+    switch (changes.type) {
+      case Downshift.stateChangeTypes.controlledPropUpdatedSelectedItem:
+        return { ...changes, highlightedIndex: currentSelectedItemIndex }
+      case Downshift.stateChangeTypes.changeInput:
+        return { ...changes, highlightedIndex: FIRST_ITEM_INDEX }
+      case Downshift.stateChangeTypes.mouseUp:
+      case Downshift.stateChangeTypes.blurInput:
+        const hasInput = inputValue.length > 0
+
+        if (allowAny && hasInput && inputValue !== getItemText(selectedItem)) {
+          return {
+            ...changes,
+            inputValue,
+            selectedItem: null
+          }
+        }
+        break
+    }
+    return changes
+  }
+
+  const downshiftItemToString = (item: T | null) =>
+    item === null
+      ? allowAny
+        ? inputValue
+        : EMPTY_INPUT_VALUE
+      : getItemText(item)
+
+  return (
+    <Downshift
+      inputValue={inputValue}
+      onInputValueChange={handleInputValueChange}
+      selectedItem={selectedItem}
+      onChange={handleSelectItem}
+      itemToString={downshiftItemToString}
+      stateReducer={downshiftStateReducer}
+    >
+      {({
+        getMenuProps,
+        getInputProps,
+        getItemProps,
+        isOpen,
+        highlightedIndex,
+        selectItem,
+        setState
+      }) => {
+        const hasMatchingOptions = matchingOptions.length > 0
+        const canOpen =
+          isOpen && isMatchingMinLength(inputValue, minLength) && !loading
+
+        const optionsMenu = (
+          <ScrollMenu selectedIndex={highlightedIndex}>
+            {hasMatchingOptions ? (
+              matchingOptions.map((option, index) => (
+                <Menu.Item
+                  key={getItemValue(option)}
+                  selected={highlightedIndex === index}
+                  disabled={isSelected(option, selectedItem)}
+                  /* eslint-disable-next-line react/jsx-props-no-spreading */
+                  {...getItemProps({ item: option, index })}
+                >
+                  {renderOption
+                    ? renderOption(option, index)
+                    : getItemText(option)}
+                </Menu.Item>
+              ))
+            ) : (
+              <Menu.Item disabled>{noOptionsText}</Menu.Item>
+            )}
+          </ScrollMenu>
+        )
+
+        const handleFocusOrClick = () => {
+          if (!isOpen) {
+            let newInputValue = inputValue
+            const isInputSelectedItem = inputValue === getItemText(selectedItem)
+
+            if (!allowAny || isInputSelectedItem) {
+              newInputValue = EMPTY_INPUT_VALUE
+            }
+            setState({
+              isOpen: true,
+              inputValue: newInputValue,
+              highlightedIndex: currentSelectedItemIndex || FIRST_ITEM_INDEX
+            })
+          }
+        }
+
+        const InputComponent = inputComponent || Input
+
+        const inputProps = getInputProps({
+          onFocus: handleFocusOrClick,
+          onClick: handleFocusOrClick,
+          onKeyDown: (event: KeyboardEvent<HTMLInputElement>) => {
+            if (event.key === 'Backspace' && inputValue === EMPTY_INPUT_VALUE) {
+              selectItem(null)
+            }
+            onKeyDown!(event, inputValue)
+          }
+        })
+
+        return (
+          <div
+            className={cx(
+              classes.root,
+              className,
+              classes[`root${capitalize(width!)}`]
+            )}
+            style={style}
+          >
+            <InputComponent
+              /* eslint-disable-next-line react/jsx-props-no-spreading */
+              {...rest}
+              // eslint-disable-next-line react/jsx-props-no-spreading
+              {...inputProps}
+              defaultValue={inputProps.defaultValue as string | undefined}
+              value={inputProps.value as string | undefined}
+              onChange={e => {
+                inputProps.onChange!(e as FormEvent<HTMLInputElement>)
+              }}
+              ref={ref}
+              classes={{}}
+              placeholder={
+                selectedItem ? getItemText(selectedItem) : placeholder
+              }
+              icon={loading ? <Loader size='small' /> : null}
+              iconPosition='end'
+              width={width}
+            />
+            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+            <div {...getMenuProps()}>{canOpen ? optionsMenu : null}</div>
+          </div>
+        )
+      }}
+    </Downshift>
+  )
+})
 
 Autocomplete.defaultProps = {
   allowAny: true,
