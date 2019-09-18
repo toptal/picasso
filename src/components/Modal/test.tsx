55738ca0b4217f901a5a20528e665842b8a7fa38
Vedran Ivanac <vedran.ivanac@toptal.com>
2019-09-18 23:47:43 +0200

    feat(Modal): export useModal and add test

--- src/components/Modal/test.tsx
+++ src/components/Modal/test.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { render } from '@testing-library/react'
+import React, { Fragment } from 'react'
+import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/react/cleanup-after-each'
 
 import Picasso from '../Picasso'
 import Button from '../Button'
 import Modal, { Props as ModalProps } from './Modal'
+import { useModal } from './use-modal'
 import { Props as ModalActionsProps } from '../ModalActions/ModalActions'
 import { Props as ModalTitleProps } from '../ModalTitle/ModalTitle'
 import { Props as ModalContentProps } from '../ModalContent/ModalContent'
@@ -61,4 +62,74 @@ test('renders Modal', () => {
   )
 
   expect(modalRoot).toMatchSnapshot()
+})
+
+test('useModal opens and closes modal', () => {
+  const TestComponent = () => {
+    const [showModal, hideModal] = useModal(() => (
+      <Modal open>
+        <p>Modal content</p>
+        <Button onClick={hideModal}>Hide</Button>
+      </Modal>
+    ))
+
+    return <Button onClick={showModal}>Show</Button>
+  }
+
+  const { getByText, queryByText, baseElement } = render(
+    <Picasso loadFonts={false}>
+      <TestComponent />
+    </Picasso>
+  )
+
+  const showModal = getByText('Show')
+
+  fireEvent.click(showModal)
+
+  expect(queryByText('Modal content')).toBeTruthy()
+  expect(baseElement).toMatchSnapshot()
+
+  const hideModal = getByText('Hide')
+
+  fireEvent.click(hideModal)
+
+  expect(queryByText('Modal content')).toBeFalsy()
+  expect(baseElement).toMatchSnapshot()
+})
+
+test('useModal shows multiple modals at the same time', () => {
+  const TestComponent = () => {
+    const [showFirstModal] = useModal(() => (
+      <Modal open>
+        <p>First modal content</p>
+      </Modal>
+    ))
+
+    const [showSecondModal] = useModal(() => (
+      <Modal open>
+        <p>Second modal content</p>
+      </Modal>
+    ))
+
+    return (
+      <Fragment>
+        <Button onClick={showFirstModal}>Show first</Button>
+        <Button onClick={showSecondModal}>Show second</Button>
+      </Fragment>
+    )
+  }
+
+  const { getByText, queryByText, baseElement } = render(
+    <Picasso loadFonts={false}>
+      <TestComponent />
+    </Picasso>
+  )
+
+  fireEvent.click(getByText('Show first'))
+  fireEvent.click(getByText('Show second'))
+
+  expect(queryByText('First modal content')).toBeTruthy()
+  expect(queryByText('Second modal content')).toBeTruthy()
+
+  expect(baseElement).toMatchSnapshot()
 })
