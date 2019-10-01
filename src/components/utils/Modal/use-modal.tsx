902eb5713d8ac55586364dcac8b51c0727f2d35f
denieler <daniel.ostapenko@toptal.com>
2019-10-01 16:36:37 +0300

    chore: [FX-497] Add deprecation warning for useModal hook

--- src/components/utils/Modal/use-modal.tsx
+++ src/components/utils/Modal/use-modal.tsx
@@ -1,3 +1,14 @@
-import { useModal } from 'react-modal-hook'
+import { useEffect } from 'react'
+import { useModal as useReactModalHook, ModalType } from 'react-modal-hook'
+
+const useModal = (component: ModalType, inputs: any[] = []) => {
+  useEffect(() => {
+    window.console.warn(
+      'useModal hook is going to be deprecated in Picasso v4. Please, use useModals hook instead.'
+    )
+  }, [])
+
+  return useReactModalHook(component, inputs)
+}
 
 export { useModal }
