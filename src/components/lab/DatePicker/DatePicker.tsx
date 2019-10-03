e3a5bfc3e14189c3be21dfc1cbe5866ac7222146
Uladzimir Havenchyk <uladzimir@toptal.com>
2019-10-03 20:35:27 +0300

    feat: add value to the input

--- src/components/lab/DatePicker/DatePicker.tsx
+++ src/components/lab/DatePicker/DatePicker.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import format from 'date-fns/format'
 
 import { ClickAwayListener } from '../../utils'
 import { Input, Container } from '../..'
@@ -7,26 +8,43 @@ import Calendar, { DateOrDateRangeType, DateRangeType } from '../Calendar'
 interface Props {
   range?: boolean
   onSelect: (value: DateOrDateRangeType) => void
+  value?: Date
 }
 
-export const DatePicker = ({ onSelect, range }: Props) => {
+function isDateRange(value: DateOrDateRangeType): value is DateRangeType {
+  return Array.isArray(value)
+}
+
+const formatDate = (date: Date) => format(date, 'MMM d, yyyy')
+const formatDateRange = (dates: DateRangeType) =>
+  dates.map(formatDate).join(' - ')
+
+const formatValue = (value: DateOrDateRangeType) => {
+  if (isDateRange(value)) {
+    return formatDateRange(value)
+  } else {
+    return formatDate(value)
+  }
+}
+
+export const DatePicker = ({ onSelect, range, value: initialValue }: Props) => {
   const [calendarOpened, setCalendarOpened] = useState(false)
+  const [inputValue, setInputValue] = useState<string | undefined>(
+    initialValue ? formatValue(initialValue) : undefined
+  )
 
   const openCalendar = () => setCalendarOpened(true)
   const closeCalendar = () => setCalendarOpened(false)
 
   const handleSelect = (value: DateOrDateRangeType) => {
-    if (range) {
-      onSelect(value as DateRangeType)
-    } else {
-      onSelect(value as Date)
-    }
+    setInputValue(formatValue(value))
+    onSelect(value)
   }
 
   return (
     <ClickAwayListener onClickAway={closeCalendar}>
       <Container inline>
-        <Input onFocus={openCalendar} />
+        <Input value={inputValue} onFocus={openCalendar} />
 
         <Calendar open={calendarOpened} onSelect={handleSelect} range={range} />
       </Container>
