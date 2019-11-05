6ea23c5bffde3fadcfbe1fa80e461910e464f871
denieler <daniel.ostapenko@toptal.com>
2019-11-05 14:15:19 +0200

    docs: [FX-510] Add docs for SidebarItem

--- src/components/lab/SidebarItem/story/index.jsx
+++ src/components/lab/SidebarItem/story/index.jsx
@@ -4,7 +4,86 @@ import { SidebarItem } from '../SidebarItem'
 
 const componentDocs = PicassoBook.createComponentDocs(
   SidebarItem,
-  'Sidebar.Item'
+  'Sidebar.Item',
+  undefined,
+  {
+    icon: {
+      name: 'icon',
+      type: 'ReactElement',
+      description: 'Pass icon to be used as part of item'
+    },
+    selected: {
+      name: 'selected',
+      defaultValue: 'false',
+      type: 'boolean',
+      description: 'Highlights the item as selected'
+    },
+    disabled: {
+      name: 'disabled',
+      type: 'boolean',
+      description: 'Whether to render disabled item'
+    },
+    collapsible: {
+      name: 'collapsible',
+      defaultValue: 'false',
+      type: 'boolean',
+      description: 'If item has menu defines can menu be collapsed'
+    },
+    menu: {
+      name: 'menu',
+      type: 'ReactElement',
+      description: 'Renders nested sidebar menu'
+    },
+    onClick: {
+      name: 'onClick',
+      type: {
+        name: 'function',
+        description:
+          '(event: React.MouseEvent<HTMLElement, MouseEvent>) => void'
+      },
+      description: 'Callback when item is clicked'
+    },
+    as: {
+      name: 'as',
+      type: {
+        name: 'enum',
+        enums: [
+          '"symbol"',
+          '"abbr"',
+          '"address"',
+          '"article"',
+          '"aside"',
+          '"b"',
+          '"bdi"',
+          '"bdo"',
+          '"big"',
+          '"blockquote"',
+          '"caption"',
+          '"cite"',
+          '"code"',
+          '"dd"',
+          '"del"',
+          '"details"',
+          '"dfn"',
+          '"dt"',
+          '"em"',
+          '"figcaption"',
+          '... 95 more ...'
+        ]
+      },
+      description: 'Component name to render the menu item as'
+    },
+    className: {
+      name: 'className',
+      type: 'string',
+      description: 'Classnames applied to root element'
+    },
+    style: {
+      name: 'style',
+      type: 'CSSProperties',
+      description: 'Style applied to root element'
+    }
+  }
 )
 
 export default {
