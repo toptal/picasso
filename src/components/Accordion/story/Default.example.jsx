399baa69902671d2ea8e11fe74bba6bf55b7e41e
OleksandrNechai <oleksandr.nechai@toptal.com>
2019-11-06 13:11:24 +0200

    fix: visual tests

--- src/components/Accordion/story/Default.example.jsx
+++ src/components/Accordion/story/Default.example.jsx
@@ -4,11 +4,7 @@ import { Accordion } from '@toptal/picasso'
 const AccordionDefaultExample = () => (
   <div style={{ width: '430px' }}>
     <Accordion content={<DetailsDogDefinitionPanel />}>
-      <div>
-        A dog is a type of domesticated animal. Known for its loyalty and
-        faithfulness, it can be found as a welcome guest in many households
-        across the world.
-      </div>
+      <div>What is a dog?</div>
     </Accordion>
   </div>
 )
