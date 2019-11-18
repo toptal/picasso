7293e2487beb30cf116180b83db6fc7768d730a8
OleksandrNechai <oleksandr.nechai@toptal.com>
2019-11-18 16:40:49 +0200

    chore(button): remove @ts-ignore from the stories

--- src/components/Button/story/Augmentation.example.tsx
+++ src/components/Button/story/Augmentation.example.tsx
@@ -4,28 +4,22 @@ import { Settings16 } from '@toptal/picasso/Icon'
 
 const ButtonAugmentationExample = () => (
   <div>
-    {/*
-        // @ts-ignore - until we fix as typings */}
     <Button as={Link} href='/#home'>
       Link
     </Button>
-    {/*
-      // @ts-ignore - until we fix as typings */}
+
     <Button as={Link} href='/#home' icon={<Settings16 />}>
       Link
     </Button>
-    {/*
-      // @ts-ignore - until we fix as typings */}
+
     <Button as={Link} href='/#home' variant='secondary-red'>
       Link
     </Button>
-    {/*
-      // @ts-ignore - until we fix as typings */}
+
     <Button as={Link} href='/#home' disabled>
       Link
     </Button>
-    {/*
-      // @ts-ignore - until we fix as typings */}
+
     <Button as={Link} href='/#home' loading>
       Link
     </Button>
