5844bf9cb9fc7b32b6b3165d4905207fb05000c1
denieler <daniel.ostapenko@toptal.com>
2019-11-26 14:59:15 +0200

    feat: [FX-580] Add package.json copy version to build folder

--- bin/utils/index.js
+++ bin/utils/index.js
@@ -4,10 +4,29 @@ const chalk = require('chalk')
 const fs = require('fs-extra')
 const path = require('path')
 
+const BUILD_FOLDER = 'build'
+
 // General logging
 const log = function (text, color) {
   if (!color) color = 'blue'
   console.log(chalk[color](text))
+}
+
+const setVersionPackageJson = (dir, version) => {
+  log('')
+  const packageJsonContent = require(path.resolve(dir, './package.json'))
+  const outputPackageJson = path.resolve(dir, `./${BUILD_FOLDER}/package.json`)
+
+  log(`Updating version for package.json in: ${outputPackageJson}`)
+
+  fs.ensureDirSync(dir)
+
+  const data = {
+    ...packageJsonContent,
+    version
+  }
+
+  fs.writeFileSync(outputPackageJson, JSON.stringify(data, null, 2) + '\n')
 }
 
 const generatePackageJson = (
@@ -35,5 +54,6 @@ const generatePackageJson = (
 
 module.exports = {
   log,
-  generatePackageJson
+  generatePackageJson,
+  setVersionPackageJson
 }
