69f7e52288a8e561fdb36c20b06a10f34583a487
denieler <daniel.ostapenko@toptal.com>
2019-11-27 11:05:21 +0200

    chore: [FX-580] Fix code style

--- bin/utils/log.js
+++ bin/utils/log.js
@@ -1,8 +1,7 @@
 /* eslint-disable no-console */
 const chalk = require('chalk')
 
-const log = function (text, color) {
-  if (!color) color = 'blue'
+const log = function (text, color = 'blue') {
   console.log(chalk[color](text))
 }
 
