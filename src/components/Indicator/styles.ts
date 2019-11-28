d298320c1a043b6dd4da2267fbb6df5fa13116de
Michal Slabej <michal.slabej@toptal.com>
2019-11-28 10:58:01 +0100

    feat(Indicator): add Indicator component (#890)

---
+++ src/components/Indicator/styles.ts
@@ -0,0 +1,19 @@
+import { Theme, createStyles } from '@material-ui/core/styles'
+
+export default ({ palette }: Theme) =>
+  createStyles({
+    root: {
+      width: '0.5rem',
+      height: '0.5rem',
+      borderRadius: '50%'
+    },
+    red: {
+      background: palette.red.main
+    },
+    yellow: {
+      background: palette.yellow.main
+    },
+    blue: {
+      background: palette.blue.main
+    }
+  })
