865a8a16d4a416170bdfd1fc4a45a52734d7dbc9
Vedran Ivanac <vedran.ivanac@toptal.com>
2019-12-05 10:33:51 +0100

    feat(Input): explain disable unsupported props

--- packages/picasso/src/utils/disable-unsupported-props.ts
+++ packages/picasso/src/utils/disable-unsupported-props.ts
@@ -1,4 +1,4 @@
-type PIndex = { [index: string]: any }
+type PIndex = { [index: string]: unknown }
 
 export default <P extends object>(
   props: P,
@@ -8,6 +8,8 @@ export default <P extends object>(
 ) => {
   const featurePropNames = Object.keys(featureProps)
 
+  // Check if in props there are props with same value as feature props, if
+  // so it will mean we need to check further for unsupported props and reset them
   if (
     featurePropNames.every(
       propName =>
@@ -19,6 +21,8 @@ export default <P extends object>(
 
   const unsupportedPropNames = Object.keys(unsupportedProps)
 
+  // Check for unsupported props if there are props that are enabled, if so
+  // warn developer in console and reset props to turn off unsupported props
   if (unsupportedPropNames.some(propName => (props as PIndex)[propName])) {
     console.warn(
       `${componentName} doesn't support: ${unsupportedPropNames.join(
