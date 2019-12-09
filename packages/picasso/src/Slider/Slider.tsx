6c9e13504c605c74d94f24b0c25ce25dbf9dd618
OleksandrNechai <oleksandr.nechai@toptal.com>
2019-12-09 14:32:29 +0200

    feat(slider): add tooltip and range features

--- packages/picasso/src/Slider/Slider.tsx
+++ packages/picasso/src/Slider/Slider.tsx
@@ -1,90 +1,75 @@
-import React, { forwardRef, useState, useEffect, ChangeEvent } from 'react'
+import React, { forwardRef } from 'react'
 import { withStyles } from '@material-ui/core/styles'
-import MUISlider from '@material-ui/core/Slider'
-import { StandardProps } from '@toptal/picasso-shared'
+import MUISlider, {
+  SliderProps,
+  ValueLabelProps
+} from '@material-ui/core/Slider'
+import { Tooltip } from '@toptal/picasso'
 
 import styles from './styles'
 
-export interface Props extends StandardProps {
-  /** Minimum slider value */
-  min?: number
-  /** Maximum slider value */
-  max?: number
-  /** Controlled value of the component */
-  value?: number
-  /** Step for the thumb movement */
-  step?: number
-  /** Whether component is disabled or not */
-  disabled?: boolean
-  /** Callback invoked when slider changes its state. */
-  onChange?: (event: ChangeEvent<{}>, value: number) => void
+export type Props = SliderProps
+
+type ValueLabelComponentProps = ValueLabelProps & {
+  valueLabelFormat?:
+    | string
+    | ((value: number, index: number) => React.ReactNode)
+  index?: number
 }
+
+const DefaultValueLabelComponent: React.FunctionComponent<
+  ValueLabelComponentProps
+> = ({ children, open, value, valueLabelFormat, index }) => (
+  <Tooltip
+    arrow
+    content={
+      valueLabelFormat &&
+      (typeof valueLabelFormat === 'string'
+        ? valueLabelFormat
+        : valueLabelFormat(value, index || 0))
+    }
+    open={open}
+    placement='top'
+  >
+    {children}
+  </Tooltip>
+)
 
 export const Slider = forwardRef<HTMLElement, Props>(function Slider(
   {
     classes,
-    onChange = () => {},
-    min,
-    max,
-    value: initialValue,
-    step,
-    disabled
+    valueLabelDisplay,
+    valueLabelFormat,
+    ValueLabelComponent: UserDefinedValueLabelComponent,
+    ...rest
   },
   ref
 ) {
-  const [value, setValue] = useState<number>(initialValue!)
+  const shouldDisplayLabel =
+    valueLabelDisplay === 'on' ||
+    valueLabelDisplay === 'auto' ||
+    UserDefinedValueLabelComponent ||
+    valueLabelFormat
 
-  const getNormalizedValue = (denormalizedValue: number): number => {
-    if (denormalizedValue < 0) {
-      return 0
-    }
-
-    if (denormalizedValue > max!) {
-      return max as number
-    }
-
-    return denormalizedValue
-  }
-
-  const handleChange = (
-    event: ChangeEvent<{}>,
-    newValue: number | number[]
-  ) => {
-    const newNormalizedValue = getNormalizedValue(newValue as number)
-
-    setValue(newNormalizedValue)
-    onChange(event, newNormalizedValue)
-  }
-
-  useEffect(() => {
-    const currentValue = getNormalizedValue(initialValue!)
-
-    setValue(currentValue)
-  }, [initialValue])
+  const ValueLabelComponent = shouldDisplayLabel
+    ? UserDefinedValueLabelComponent || DefaultValueLabelComponent
+    : undefined
 
   return (
     <MUISlider
+      {...rest}
       ref={ref}
-      min={min}
-      max={max}
-      value={value}
-      onChange={handleChange}
       classes={classes}
-      step={step}
-      disabled={disabled}
+      ValueLabelComponent={ValueLabelComponent}
+      valueLabelFormat={valueLabelFormat}
     />
   )
 })
 
-Slider.defaultProps = {}
-
 Slider.displayName = 'Slider'
 
 Slider.defaultProps = {
-  max: 100,
-  min: 0,
-  onChange: () => {},
-  value: 0
+  defaultValue: 0
 }
 
 export default withStyles(styles)(Slider)
