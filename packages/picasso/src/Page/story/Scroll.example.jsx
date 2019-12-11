924a0eac807d797e5ccaaa09cb50b71349ee301a
Vedran Ivanac <vedran.ivanac@toptal.com>
2019-12-11 15:21:13 +0100

    fix(Dropdown): trying to fix problems with Popper

--- packages/picasso/src/Page/story/Scroll.example.jsx
+++ packages/picasso/src/Page/story/Scroll.example.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Page,
   Container,
@@ -6,11 +6,35 @@ import {
   Dropdown,
   Menu,
   Sidebar,
-  Button
+  Button,
+  Autocomplete
 } from '@toptal/picasso'
+import { isSubstring } from '@toptal/picasso/utils'
 import { Globe16, Profile16, PortfolioDesigner16 } from '@toptal/picasso/Icon'
 
 const handleClick = () => window.alert('Item clicked')
+
+const allOptions = [
+  { text: 'Belarus' },
+  { text: 'Croatia' },
+  { text: 'Lithuania' },
+  { text: 'Slovakia' },
+  { text: 'Ukraine' }
+]
+
+const EMPTY_INPUT_VALUE = ''
+const getDisplayValue = item => (item ? item.text : EMPTY_INPUT_VALUE)
+const filterOptions = (str = '') => {
+  if (str === '') {
+    return allOptions
+  }
+
+  const result = allOptions.filter(option =>
+    isSubstring(str, getDisplayValue(option))
+  )
+
+  return result.length > 0 ? result : null
+}
 
 const PageScrollExample = () => (
   <div style={{ /* maxHeight: '40rem', */ overflowY: 'scroll' }}>
@@ -75,124 +99,151 @@ const RightContent = () => (
   </Page.HeaderMenu>
 )
 
-const Content = () => (
-  <Container top='small' bottom='small' left='small' right='small'>
-    <Typography align='center' variant='heading' size='large'>
-      Scrollable example
-    </Typography>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Sollicitudin ac orci
-      phasellus egestas tellus rutrum tellus pellentesque eu. Elementum
-      facilisis leo vel fringilla est ullamcorper eget nulla. Massa id neque
-      aliquam vestibulum. Lorem donec massa sapien faucibus et molestie ac
-      feugiat sed. In aliquam sem fringilla ut morbi tincidunt augue interdum
-      velit. Erat velit scelerisque in dictum non. Eros donec ac odio tempor
-      pellentesque. Nascetur ridiculus mus mauris vitae ultricies leo. In nulla
-      posuere sollicitudin aliquam ultrices sagittis orci. Volutpat blandit
-      aliquam etiam erat velit. Mi proin sed libero enim sed faucibus turpis in.
-      Odio morbi quis commodo odio. Convallis aenean et tortor at risus viverra
-      adipiscing at in. Lorem donec massa sapien faucibus et molestie ac. Non
-      enim praesent elementum facilisis leo vel fringilla est ullamcorper.
-      Interdum velit euismod in pellentesque massa placerat duis ultricies
-      lacus. Feugiat nibh sed pulvinar proin gravida hendrerit lectus a. Nisl
-      rhoncus mattis rhoncus urna neque. Pretium lectus quam id leo in vitae
-      turpis massa. Diam phasellus vestibulum lorem sed risus ultricies
-      tristique nulla. Dui faucibus in ornare quam viverra orci sagittis. Sed
-      nisi lacus sed viverra. Phasellus vestibulum lorem sed risus ultricies
-      tristique nulla. Amet mauris commodo quis imperdiet massa tincidunt nunc
-      pulvinar. Eu nisl nunc mi ipsum. Arcu dictum varius duis at consectetur
-      lorem. Cras semper auctor neque vitae tempus quam pellentesque. Tincidunt
-      praesent semper feugiat nibh sed. Ullamcorper dignissim cras tincidunt
-      lobortis feugiat vivamus. Consequat mauris nunc congue nisi vitae. Fusce
-      ut placerat orci nulla. Sit amet nisl suscipit adipiscing bibendum est.
-      Nibh tortor id aliquet lectus proin.
-    </p>
-    <Dropdown
-      content={
-        <Menu data-testid='menu'>
-          <Menu.Item onClick={handleClick}>First item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-        </Menu>
-      }
-    >
-      <Button variant='primary'>
-        Open Large Dropdown
-        <Dropdown.Arrow />
-      </Button>
-    </Dropdown>
-    <Dropdown
-      content={
-        <Menu data-testid='menu'>
-          <Menu.Item onClick={handleClick}>First item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-          <Menu.Item onClick={handleClick}>Second item</Menu.Item>
-        </Menu>
-      }
-    >
-      <Button variant='primary'>
-        Open Small Dropdown
-        <Dropdown.Arrow />
-      </Button>
-    </Dropdown>
-    <p>
-      t egestas dui id. Tortor dignissim convallis aenean et tortor at risus.
-      Mauris in aliquam sem fringilla ut morbi tincidunt augue interdum. Nisl
-      suscipit adipiscing bibendum est ultricies integer. Dolor magna eget est
-      lorem ipsum dolor. Cursus eget nunc scelerisque viverra mauris. Id nibh
-      tortor id aliquet lectus proin. Amet consectetur adipiscing elit duis
-      tristique. Cursus risus at ultrices mi tempus imperdiet nulla malesuada
-      pellentesque. Nascetur ridiculus mus mauris vitae ultricies leo. In nulla
-      posuere sollicitudin aliquam ultrices sagittis orci. Volutpat blandit
-      aliquam etiam erat velit. Mi proin sed libero enim sed faucibus turpis in.
-      Odio morbi quis commodo odio. Convallis aenean et tortor at risus viverra
-      adipiscing at in. Lorem donec massa sapien faucibus et molestie ac. Non
-      enim praesent elementum facilisis leo vel fri t egestas dui id. Tortor
-      dignissim convallis aenean et tortor at risus. Mauris in aliquam sem
-      fringilla ut morbi tincidunt augue interdum. Nisl suscipit adipiscing
-      bibendum est ultricies integer. Dolor magna eget est lorem ipsum dolor.
-      Cursus eget nunc scelerisque viverra mauris. Id nibh tortor id aliquet
-      lectus proin. Amet consectetur adipiscing elit duis tristique. Cursus
-      risus at ultrices mi tempus imperdiet nulla malesuada pellentesque.
-      Nascetur ridiculus mus mauris vitae ultricies leo. In nulla posuere
-      sollicitudin aliquam ultrices sagittis orci. Volutpat blandit aliquam
-      etiam erat velit. Mi proin sed libero enim sed faucibus turpis in. Odio
-      morbi quis commodo odio. Convallis aenean et tortor at risus viverra
-      adipiscing at in. Lorem donec massa sapien faucibus et molestie ac. Non
-      enim praesent elementum facilisis leo vel fri rhoncus mattis rhoncus urna
-      neque. Pretium lectus quam id leo in vitae turpis massa. Diam phasellus
-      vestibulum lorem sed risus ultricies tristique nulla. Dui faucibus in
-      ornare quam viverra orci sagittis. Sed nisi lacus sed viverra. Phasellus
-      vestibulum lorem sed risus ultricies tristique nulla. Amet mauris commodo
-      quis imperdiet massa tincidunt nunc pulvinar. Eu nisl nunc mi ipsum. Arcu
-      dictum varius duis at consectetur lorem. Cras semper auctor neque vitae
-      tempus quam pellentesque. Tincidunt praesent semper feugiat nibh sed.
-      Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus. Consequat
-      mauris nunc congue nisi vitae. Fusce ut placerat orci nulla. Sit amet nisl
-      suscipit adipiscing bibendum est. Nibh tortor id aliquet lectus proin.
-    </p>
-  </Container>
-)
+const Content = () => {
+  const [value, setValue] = useState(EMPTY_INPUT_VALUE)
+  const [options, setOptions] = useState(allOptions)
+
+  return (
+    <Container top='small' bottom='small' left='small' right='small'>
+      <Typography align='center' variant='heading' size='large'>
+        Scrollable example
+      </Typography>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sollicitudin ac orci
+        phasellus egestas tellus rutrum tellus pellentesque eu. Elementum
+        facilisis leo vel fringilla est ullamcorper eget nulla. Massa id neque
+        aliquam vestibulum. Lorem donec massa sapien faucibus et molestie ac
+        feugiat sed. In aliquam sem fringilla ut morbi tincidunt augue interdum
+        velit. Erat velit scelerisque in dictum non. Eros donec ac odio tempor
+        pellentesque. Nascetur ridiculus mus mauris vitae ultricies leo. In
+        nulla posuere sollicitudin aliquam ultrices sagittis orci. Volutpat
+        blandit aliquam etiam erat velit. Mi proin sed libero enim sed faucibus
+        turpis in. Odio morbi quis commodo odio. Convallis aenean et tortor at
+        risus viverra adipiscing at in. Lorem donec massa sapien faucibus et
+        molestie ac. Non enim praesent elementum facilisis leo vel fringilla est
+        ullamcorper. Interdum velit euismod in pellentesque massa placerat duis
+        ultricies lacus. Feugiat nibh sed pulvinar proin gravida hendrerit
+        lectus a. Nisl rhoncus mattis rhoncus urna neque. Pretium lectus quam id
+        leo in vitae turpis massa. Diam phasellus vestibulum lorem sed risus
+        ultricies tristique nulla. Dui faucibus in ornare quam viverra orci
+        sagittis. Sed nisi lacus sed viverra. Phasellus vestibulum lorem sed
+        risus ultricies tristique nulla. Amet mauris commodo quis imperdiet
+        massa tincidunt nunc pulvinar. Eu nisl nunc mi ipsum. Arcu dictum varius
+        duis at consectetur lorem. Cras semper auctor neque vitae tempus quam
+        pellentesque. Tincidunt praesent semper feugiat nibh sed. Ullamcorper
+        dignissim cras tincidunt lobortis feugiat vivamus. Consequat mauris nunc
+        congue nisi vitae. Fusce ut placerat orci nulla. Sit amet nisl suscipit
+        adipiscing bibendum est. Nibh tortor id aliquet lectus proin.
+      </p>
+      <Dropdown
+        content={
+          <Menu data-testid='menu'>
+            <Menu.Item onClick={handleClick}>First item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+          </Menu>
+        }
+      >
+        <Button variant='primary'>
+          Open Large Dropdown
+          <Dropdown.Arrow />
+        </Button>
+      </Dropdown>
+      <Dropdown
+        content={
+          <Menu data-testid='menu'>
+            <Menu.Item onClick={handleClick}>First item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+            <Menu.Item onClick={handleClick}>Second item</Menu.Item>
+          </Menu>
+        }
+      >
+        <Button variant='primary'>
+          Open Small Dropdown
+          <Dropdown.Arrow />
+        </Button>
+      </Dropdown>
+      <Autocomplete
+        placeholder='Start typing country...'
+        value={value}
+        options={options}
+        onSelect={item => {
+          console.log('onSelect returns item object:', item)
+
+          const itemValue = getDisplayValue(item)
+
+          if (value !== itemValue) {
+            setValue(itemValue)
+          }
+        }}
+        onChange={newValue => {
+          console.log('onChange returns just item value:', newValue)
+
+          setOptions(filterOptions(newValue))
+          setValue(newValue)
+        }}
+        getDisplayValue={getDisplayValue}
+      />
+      <p>
+        t egestas dui id. Tortor dignissim convallis aenean et tortor at risus.
+        Mauris in aliquam sem fringilla ut morbi tincidunt augue interdum. Nisl
+        suscipit adipiscing bibendum est ultricies integer. Dolor magna eget est
+        lorem ipsum dolor. Cursus eget nunc scelerisque viverra mauris. Id nibh
+        tortor id aliquet lectus proin. Amet consectetur adipiscing elit duis
+        tristique. Cursus risus at ultrices mi tempus imperdiet nulla malesuada
+        pellentesque. Nascetur ridiculus mus mauris vitae ultricies leo. In
+        nulla posuere sollicitudin aliquam ultrices sagittis orci. Volutpat
+        blandit aliquam etiam erat velit. Mi proin sed libero enim sed faucibus
+        turpis in. Odio morbi quis commodo odio. Convallis aenean et tortor at
+        risus viverra adipiscing at in. Lorem donec massa sapien faucibus et
+        molestie ac. Non enim praesent elementum facilisis leo vel fri t egestas
+        dui id. Tortor dignissim convallis aenean et tortor at risus. Mauris in
+        aliquam sem fringilla ut morbi tincidunt augue interdum. Nisl suscipit
+        adipiscing bibendum est ultricies integer. Dolor magna eget est lorem
+        ipsum dolor. Cursus eget nunc scelerisque viverra mauris. Id nibh tortor
+        id aliquet lectus proin. Amet consectetur adipiscing elit duis
+        tristique. Cursus risus at ultrices mi tempus imperdiet nulla malesuada
+        pellentesque. Nascetur ridiculus mus mauris vitae ultricies leo. In
+        nulla posuere sollicitudin aliquam ultrices sagittis orci. Volutpat
+        blandit aliquam etiam erat velit. Mi proin sed libero enim sed faucibus
+        turpis in. Odio morbi quis commodo odio. Convallis aenean et tortor at
+        risus viverra adipiscing at in. Lorem donec massa sapien faucibus et
+        molestie ac. Non enim praesent elementum facilisis leo vel fri rhoncus
+        mattis rhoncus urna neque. Pretium lectus quam id leo in vitae turpis
+        massa. Diam phasellus vestibulum lorem sed risus ultricies tristique
+        nulla. Dui faucibus in ornare quam viverra orci sagittis. Sed nisi lacus
+        sed viverra. Phasellus vestibulum lorem sed risus ultricies tristique
+        nulla. Amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar.
+        Eu nisl nunc mi ipsum. Arcu dictum varius duis at consectetur lorem.
+        Cras semper auctor neque vitae tempus quam pellentesque. Tincidunt
+        praesent semper feugiat nibh sed. Ullamcorper dignissim cras tincidunt
+        lobortis feugiat vivamus. Consequat mauris nunc congue nisi vitae. Fusce
+        ut placerat orci nulla. Sit amet nisl suscipit adipiscing bibendum est.
+        Nibh tortor id aliquet lectus proin.
+      </p>
+    </Container>
+  )
+}
 
 export default PageScrollExample
