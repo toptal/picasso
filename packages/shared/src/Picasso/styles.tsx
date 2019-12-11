d516e1c82c650683b302c9cf982135bf0a2ed04c
denieler <daniel.ostapenko@toptal.com>
2019-12-11 15:30:22 +0200

    chore: [FX-653] Fail visual tests with bigger ems for Picasso

--- packages/shared/src/Picasso/styles.tsx
+++ packages/shared/src/Picasso/styles.tsx
@@ -16,6 +16,8 @@ export default ({ typography }: Theme) =>
       // https://css-tricks.com/inheriting-box-sizing-probably-slightly-better-best-practice/
       '& *, & *::before, & *::after': {
         boxSizing: 'inherit'
-      }
+      },
+
+      fontSize: '2em'
     }
   })
