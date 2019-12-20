2a906b35304103c51317e0e8e77fb8ba41579a3e
Dmitry Maklygin <dmitry.maklygin@toptal.com>
2019-12-20 13:49:31 +0300

    chore: export `Autocomplete` internal props

--- packages/picasso/src/Autocomplete/index.ts
+++ packages/picasso/src/Autocomplete/index.ts
@@ -1,3 +1,6 @@
+import { OmitInternalProps } from '@toptal/picasso-shared'
+
+import { Props as OuterProps } from './Autocomplete'
 export { default } from './Autocomplete'
-export * from './Autocomplete'
 export * from './types'
+export type Props = OmitInternalProps<OuterProps>
