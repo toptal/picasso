b0ff7f600ee9711ffcd3d63858a04e5b52f76492
OleksandrNechai <oleksandr.nechai@toptal.com>
2020-01-13 12:54:53 +0200

    fix(counterblock): hover effect

--- packages/picasso-lab/src/CounterBlock/styles.ts
+++ packages/picasso-lab/src/CounterBlock/styles.ts
@@ -1,16 +1,19 @@
 import { createStyles, Theme } from '@material-ui/core/styles'
 import { rem, alpha } from '@toptal/picasso-shared'
+
 export default ({ palette }: Theme) =>
   createStyles({
     root: {
       display: 'flex',
       flexDirection: 'column',
-      backgroundColor: alpha(palette.grey.lighter!, 0.32),
+      backgroundColor: alpha(palette.grey.lighter, 0.32),
       padding: '0.5rem 1rem',
       minWidth: rem('150px'),
       border: 'none',
+      textDecoration: 'none'
+    },
+    clickable: {
       cursor: 'pointer',
-      textDecoration: 'none',
       '&:hover': {
         backgroundColor: palette.grey.lighter
       }
