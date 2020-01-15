6c27c0a5ffaaf44acc9b6cd970d6f36670623200
Uladzimir Havenchyk <uladzimir@toptal.com>
2020-01-15 09:38:12 +0300

    chore: update docs

--- packages/picasso-lab/src/DatePicker/story/Autocomplete.example.tsx
+++ packages/picasso-lab/src/DatePicker/story/Autocomplete.example.tsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, FormEvent } from 'react'
 import { DatePicker } from '@toptal/picasso-lab'
-import { Form } from '@toptal/picasso'
+import { Form, Button, Container } from '@toptal/picasso'
 
 const AutocompleteExample = () => {
   const [datepickerValue, setDatepickerValue] = useState<Date>()
 
   return (
     <div style={{ height: '50vh' }}>
-      <Form>
+      <Form
+        onSubmit={(e: FormEvent<HTMLFormElement>) => {
+          e.preventDefault()
+        }}
+      >
         <Form.Field>
           <Form.Label>Disabled by default</Form.Label>
           <DatePicker
@@ -24,7 +28,7 @@ const AutocompleteExample = () => {
         <Form.Field>
           <Form.Label>Enabled</Form.Label>
           <DatePicker
-            name='date'
+            name='cc-exp'
             value={datepickerValue}
             autoComplete='cc-exp'
             onChange={date => {
@@ -35,6 +39,12 @@ const AutocompleteExample = () => {
             }}
           />
         </Form.Field>
+
+        <Container top='small'>
+          <Button type='submit'>
+            Submit to include value to the browser autocomplete
+          </Button>
+        </Container>
       </Form>
     </div>
   )
