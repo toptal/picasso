608b7594141b484a295ee99d4d0e2a3d6e2438e7
Oleksandr Nechai <oleksandr.nechai@toptal.com>
2020-01-16 17:58:03 +0200

    refactor(container): minimize inline styles usage,  instead  use classes (#996)

--- packages/picasso/src/Container/styles.ts
+++ packages/picasso/src/Container/styles.ts
@@ -1,16 +1,47 @@
+import { capitalize } from '@material-ui/core/utils/helpers'
 import { Theme, createStyles } from '@material-ui/core/styles'
 import { Color } from '@material-ui/core'
 import { SimplePaletteColorOptions } from '@material-ui/core/styles/createPalette'
 import {
-  SpacingEnum,
   SpacingType,
   spacingToRem,
   createPropertiesStyles
 } from '@toptal/picasso-shared'
 
-const spacingVariants = Object.keys(SpacingEnum).filter(variant =>
-  Number.isNaN(Number(variant))
-)
+import kebabToCamelCase from '../utils/kebab-to-camel-case'
+
+const alignItemsVariants = [
+  'flex-start',
+  'flex-end',
+  'center',
+  'stretch',
+  'baseline'
+] as const
+
+const justifyContentVariants = [
+  'flex-start',
+  'flex-end',
+  'center',
+  'space-between',
+  'space-around',
+  'space-evenly'
+] as const
+
+const directionVariants = ['top', 'left', 'bottom', 'right'] as const
+
+const spacingVariants = [
+  'xsmall',
+  'small',
+  'medium',
+  'large',
+  'xlarge'
+] as const
+
+export type AlignItemsType = typeof alignItemsVariants[number]
+export type JustifyContentType = typeof justifyContentVariants[number]
+type Direction = typeof directionVariants[number]
+type Spacing = typeof spacingVariants[number]
+type MapOfClasses = Record<string, Record<string, string>>
 
 const paddings = spacingVariants.reduce((acc, variant) => {
   acc[`${variant}Padding`] = {
@@ -34,6 +65,43 @@ const colorVariant = (colorOptions?: SimplePaletteColorOptions | Color) => {
   })
 }
 
+const marginClassDef = (direction: Direction, spacing: Spacing) => ({
+  [`margin${capitalize(direction)}`]: spacingToRem(spacing)
+})
+
+const marginClasses = (direction: Direction) => {
+  return {
+    [`${direction}${'xsmall'}Margin`]: marginClassDef(direction, 'xsmall'),
+    [`${direction}${'small'}Margin`]: marginClassDef(direction, 'small'),
+    [`${direction}${'medium'}Margin`]: marginClassDef(direction, 'medium'),
+    [`${direction}${'large'}Margin`]: marginClassDef(direction, 'large'),
+    [`${direction}${'xlarge'}Margin`]: marginClassDef(direction, 'xlarge')
+  }
+}
+
+const margins: MapOfClasses = {
+  ...marginClasses('top'),
+  ...marginClasses('left'),
+  ...marginClasses('bottom'),
+  ...marginClasses('right')
+}
+
+const alignItems: MapOfClasses = {}
+
+alignItemsVariants.forEach(variant => {
+  alignItems[`${kebabToCamelCase(variant)}AlignItems`] = {
+    alignItems: variant
+  }
+})
+
+const justifyContent: MapOfClasses = {}
+
+justifyContentVariants.forEach(variant => {
+  justifyContent[`${kebabToCamelCase(variant)}JustifyContent`] = {
+    justifyContent: variant
+  }
+})
+
 export default ({ palette }: Theme) =>
   createStyles({
     bordered: {
@@ -47,9 +115,11 @@ export default ({ palette }: Theme) =>
         display: 'inline-flex'
       }
     },
+
     column: {
       flexDirection: 'column'
     },
+
     inline: {
       display: 'inline-block'
     },
@@ -66,5 +136,8 @@ export default ({ palette }: Theme) =>
 
     greyVariant: colorVariant(palette.grey),
 
-    ...paddings
+    ...paddings,
+    ...margins,
+    ...alignItems,
+    ...justifyContent
   })
