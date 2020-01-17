5f7e7900ec4a4c58f0ddb860812d42718b9592b2
Luis Felipe Zaguini <luisfelipe.zaguini@toptal.com>
2020-01-17 13:06:40 -0300

    chore: simplified multiple select example

--- packages/picasso/src/Select/story/Multiple.example.jsx
+++ packages/picasso/src/Select/story/Multiple.example.jsx
@@ -6,29 +6,14 @@ const OPTIONS = [
   { value: '2', text: 'Option 2' },
   { value: '3', text: 'Option 3' },
   { value: '4', text: 'Option 4' },
-  { value: 'none', text: 'Clear selection' },
-  { value: 'all', text: 'Select all' }
+  { value: 'none', text: 'Clear selection' }
 ]
-
-const EXCEPTIONS = ['none', 'all']
 
 const SelectDefaultExample = () => {
   const [values, setValues] = useState([])
 
   const handleChange = event => {
-    let value
-
-    if (event.target.value.includes('all')) {
-      value = OPTIONS.filter(option => !EXCEPTIONS.includes(option.value)).map(
-        option => option.value
-      )
-    } else if (event.target.value.includes('none')) {
-      value = []
-    } else {
-      value = event.target.value
-    }
-
-    setValues(value)
+    setValues(event.target.value.includes('none') ? [] : event.target.value)
   }
 
   return (
