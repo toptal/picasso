3edee4850be74c39311e297c503b986ef9f194b5
Vedran Ivanac <vedran.ivanac@toptal.com>
2020-01-23 11:58:47 +0100

    fix(Tooltip): export props type

--- packages/picasso/src/Tooltip/index.ts
+++ packages/picasso/src/Tooltip/index.ts
@@ -1,1 +1,5 @@
+import { OmitInternalProps } from '@toptal/picasso-shared'
+
+import { Props as TooltipProps } from './Tooltip'
 export { default } from './Tooltip'
+export type Props = OmitInternalProps<TooltipProps>
