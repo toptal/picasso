dacffde0b2150efe1cd869ad429cbfeb0142c7a4
denieler <daniel.ostapenko@toptal.com>
2020-01-30 17:16:39 +0200

    chore: [FX-698] Add some text descriptions

--- packages/picasso-forms/src/Form/story/index.jsx
+++ packages/picasso-forms/src/Form/story/index.jsx
@@ -10,14 +10,37 @@ Form is a wrapper for 'react-final-form' Form component. It also
 provides inside all the necessary input components types.
     `
   )
-  .addExample('Form/story/Default.example.tsx', 'Default', 'picasso-form')
+  .addExample(
+    'Form/story/Default.example.tsx',
+    {
+      title: 'Default',
+      description: `
+A general look of the form includes the examples of all the input
+types supported by picasso-forms.
+`
+    },
+    'picasso-form'
+  )
   .addExample(
     'Form/story/CustomValidator.example.tsx',
-    'Custom validator',
+    {
+      title: 'Custom validator',
+      description: `
+We have a 'required' validator included by default to each input type,
+however, you may need custom validators for more complex types of fields.
+`
+    },
     'picasso-form'
   ) // picasso-skip-visuals
   .addExample(
     'Form/story/BackendCommunication.example.tsx',
-    'Backend communication',
+    {
+      title: 'Backend communication',
+      description: `
+The form usually need to send data to backend, so we need to have
+backend communication and display the process of submission and
+the results. The form-level results are represented by notifications.
+`
+    },
     'picasso-form'
   ) // picasso-skip-visuals
