5a1eccbea2d091a1374c804d20d4e87406c091b7
denieler <daniel.ostapenko@toptal.com>
2020-02-04 11:08:46 +0200

    chore: [FX-742] Switch example to tsx

--- packages/picasso/src/Select/story/SearchBehavior.example.tsx
+++ packages/picasso/src/Select/story/SearchBehavior.example.tsx
@@ -1,11 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, ChangeEvent } from 'react'
 import { Select, Button, Container } from '@toptal/picasso'
 
 const SelectSearchBehaviourExample = () => {
   const [value, setValue] = useState()
   const [options, setOptions] = useState(OPTIONS_WHEN_SEARCH_DISABLED)
 
-  const handleChange = event => {
+  const handleChange = (
+    event: ChangeEvent<{
+      name?: string | undefined
+      value: string | string[] | number
+    }>
+  ) => {
     console.log('Select value:', event.target.value)
     setValue(event.target.value)
   }
