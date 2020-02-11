1577142d8910ed51ef78491d5b4587fedc24c201
Uladzimir Havenchyk <uladzimir@toptal.com>
2020-02-11 11:18:02 +0300

    chore: use danger from davinci

--- dangerfile.ts
+++
@@ -1,7 +0,0 @@
-import { schedule } from 'danger'
-
-import conventionalCommits from './ci/danger/plugins/conventional-commit'
-import conventionalPRTitle from './ci/danger/plugins/conventional-pr-title'
-
-schedule(conventionalCommits)
-schedule(conventionalPRTitle)
