b1e09df8f7b66e07c48b4ba5367739da96f22d45
Oleksandr Nechai <oleksandr.nechai@toptal.com>
2020-02-13 19:37:58 +0200

    fix: revert .storybook/addons.js
    Co-Authored-By: Uladzimir Havenchyk <uladzimir@toptal.com>

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -3,4 +3,3 @@ import '@storybook/addon-viewport/register'
 require('./addons/menu-expander/register')
 require('./addons/anchor-link-handler/register')
 require('./addons/document-title/register')
-require('./addons/viewport/register')
