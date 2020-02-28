b294b62a7bd06a1c0d248da5902bf9e51e1a605e
Oleksandr Nechai <oleksandr.nechai@toptal.com>
2020-02-28 14:19:28 +0200

    feat: add     Breadcrumbs component (#1132)

---
+++ packages/picasso-lab/src/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,30 @@
+import React, { forwardRef, ReactNode } from 'react'
+import { BaseProps, CompoundedComponentWithRef } from '@toptal/picasso-shared'
+import { ChevronRight16 } from '@toptal/picasso/Icon'
+import { Breadcrumbs as MuiBreadcrumbs } from '@material-ui/core'
+
+import Item from '../BreadcrumbsItem'
+import './styles'
+
+interface StaticProps {
+  Item: typeof Item
+}
+
+export interface Props extends BaseProps {
+  /** Content of Breadcrumbs */
+  children: ReactNode
+}
+
+export const Breadcrumbs = forwardRef<HTMLElement, Props>(function Breadcrumbs(
+  props,
+  ref
+) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <MuiBreadcrumbs ref={ref} separator={<ChevronRight16 />} {...props} />
+}) as CompoundedComponentWithRef<Props, HTMLElement, StaticProps>
+
+Breadcrumbs.displayName = 'Breadcrumbs'
+
+Breadcrumbs.Item = Item
+
+export default Breadcrumbs
---
+++ packages/picasso-lab/src/Breadcrumbs/story/index.jsx
@@ -0,0 +1,19 @@
+import Breadcrumbs from '../Breadcrumbs'
+import breadcrumbsItemStory from '../../BreadcrumbsItem/story'
+
+import PicassoBook from '~/.storybook/components/PicassoBook'
+
+const page = PicassoBook.section('Lab').createPage(
+  'Breadcrumbs',
+  'Breadcrumbs component indicates the current page’s location within a navigational hierarchy.'
+)
+
+page
+  .createTabChapter('Props')
+  .addComponentDocs({ component: Breadcrumbs, name: 'Breadcrumbs' })
+  .addComponentDocs(breadcrumbsItemStory.componentDocs)
+
+page
+  .createChapter()
+  .addExample('Breadcrumbs/story/Default.example.tsx', 'Default')
+  .addExample('Breadcrumbs/story/SimpleLinks.example.tsx', 'Simple links')
