a78f0418bd44459c093224c141fa92762c928f8a
Carlos Gines <carlos.gines@toptal.com>
2020-03-19 18:31:30 +0100

    fix: make use notifications functions identity stable

--- packages/picasso/src/utils/Notifications/use-notifications.tsx
+++ packages/picasso/src/utils/Notifications/use-notifications.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, ReactElement, forwardRef } from 'react'
+import React, { ReactNode, ReactElement, forwardRef, useMemo } from 'react'
 import cx from 'classnames'
 import { useSnackbar, OptionsObject } from 'notistack'
 import { withStyles } from '@material-ui/core/styles'
@@ -50,53 +50,55 @@ const StyledNotification = withStyles(styles)(
 export const useNotifications = () => {
   const { enqueueSnackbar, closeSnackbar } = useSnackbar()
 
-  const getNotification = (variant?: VariantType) => (
-    content: ReactNode,
-    icon?: ReactElement,
-    options?: OptionsObject
-  ) => {
-    const closeNotification = () => {
-      if (!notificationId) {
-        return
+  return useMemo(() => {
+    const getNotification = (variant?: VariantType) => (
+      content: ReactNode,
+      icon?: ReactElement,
+      options?: OptionsObject
+    ) => {
+      const closeNotification = () => {
+        if (!notificationId) {
+          return
+        }
+
+        closeSnackbar(notificationId)
       }
+      const notificationId = enqueueSnackbar('', {
+        anchorOrigin: defaultPosition,
+        // eslint-disable-next-line react/display-name
+        content: (key: string) => (
+          <StyledNotification
+            content={content}
+            icon={icon}
+            key={key}
+            variant={variant}
+            onClose={closeNotification}
+          />
+        ),
+        ...options
+      })
 
-      closeSnackbar(notificationId)
+      return notificationId
     }
-    const notificationId = enqueueSnackbar('', {
-      anchorOrigin: defaultPosition,
-      // eslint-disable-next-line react/display-name
-      content: (key: string) => (
-        <StyledNotification
-          content={content}
-          icon={icon}
-          key={key}
-          variant={variant}
-          onClose={closeNotification}
-        />
-      ),
-      ...options
-    })
 
-    return notificationId
-  }
+    const showCustomNotification = (
+      Content: ReactElement,
+      position?: SnackbarOrigin,
+      options?: OptionsObject
+    ) =>
+      enqueueSnackbar('', {
+        anchorOrigin: position || defaultPosition,
+        // eslint-disable-next-line react/display-name
+        children: (key: string) => React.cloneElement(Content, { key }),
+        ...options
+      })
 
-  const showCustomNotification = (
-    Content: ReactElement,
-    position?: SnackbarOrigin,
-    options?: OptionsObject
-  ) =>
-    enqueueSnackbar('', {
-      anchorOrigin: position || defaultPosition,
-      // eslint-disable-next-line react/display-name
-      children: (key: string) => React.cloneElement(Content, { key }),
-      ...options
-    })
-
-  return {
-    showError: getNotification('red'),
-    showInfo: getNotification(),
-    showSuccess: getNotification('green'),
-    showCustomNotification: showCustomNotification,
-    closeNotification: closeSnackbar
-  }
+    return {
+      showError: getNotification('red'),
+      showInfo: getNotification(),
+      showSuccess: getNotification('green'),
+      showCustomNotification: showCustomNotification,
+      closeNotification: closeSnackbar
+    }
+  }, [closeSnackbar, enqueueSnackbar])
 }
