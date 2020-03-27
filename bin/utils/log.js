8b8949defde88035ecdcc3dccd70360856825d46
Boris Yordanov <boris.yordanov@toptal.com>
2020-03-27 15:27:47 +0700

    fix: function spacing lint error

--- bin/utils/log.js
+++ bin/utils/log.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 const chalk = require('chalk')
 
-const log = function (text, color = 'blue') {
+const log = function(text, color = 'blue') {
   console.log(chalk[color](text))
 }
 
