fee43fb49d2540f7d75e187bb32e5f35ce0561b6
Uladzimir Havenchyk <uladzimir@toptal.com>
2020-03-30 11:02:16 +0300

    chore: refactor testing utils

--- packages/picasso/src/test-utils/index.tsx
+++ packages/picasso/src/test-utils/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react'
+import { ReactElement } from 'react'
 import { render, RenderOptions, RenderResult } from '@testing-library/react'
 
 import TestingPicasso from './TestingPicasso'
@@ -6,10 +6,7 @@ import TestingPicasso from './TestingPicasso'
 const customRender = (
   ui: ReactElement,
   options?: Omit<RenderOptions, 'queries'>
-): RenderResult => {
-  return render(<TestingPicasso>{ui}</TestingPicasso>, options)
-}
+): RenderResult => render(ui, { wrapper: TestingPicasso, ...options })
 
-export { default as TestingPicasso } from './TestingPicasso'
 export * from '@testing-library/react'
-export { customRender as render }
+export { customRender as render, TestingPicasso }
