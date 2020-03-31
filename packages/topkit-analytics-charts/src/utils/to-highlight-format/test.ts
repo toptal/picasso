319db0d1d8826aae85fd312438d538d64907e5b5
Boris Yordanov <boris.yordanov@toptal.com>
2020-03-31 17:32:22 +0700

    fix: tests

--- packages/topkit-analytics-charts/src/utils/to-highlight-format/test.ts
+++ packages/topkit-analytics-charts/src/utils/to-highlight-format/test.ts
@@ -35,12 +35,16 @@ const CHART_DATA = [
 ]
 
 test('convert highlight data', () => {
-  const convertedHighlightsData = toHighlightFormat(CHART_DATA, [
-    {
-      data: ['2020-10-21', '2020-11-02'],
-      color: 'red'
-    }
-  ])
+  const convertedHighlightsData = toHighlightFormat(
+    CHART_DATA,
+    [
+      {
+        data: ['2020-10-21', '2020-11-02'],
+        color: 'red'
+      }
+    ],
+    'x'
+  )
   expect(convertedHighlightsData).toEqual([
     { color: 'red', from: 1, to: 2 },
     { color: 'red', from: 13, to: 14 }
