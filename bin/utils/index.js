d7532490266fbfc0c83ed8fd1ab950c9aa025cf1
Miloš Mošovský <milos.mosovsky@toptal.com>
2020-05-01 06:41:04 +0200

    docs: add correct npm badge to the readme (#1270)

--- bin/utils/index.js
+++ bin/utils/index.js
@@ -1,13 +1,11 @@
 const { log } = require('./log')
 const { copyPackageJson } = require('./copy-package-json')
 const { copyReadme } = require('./copy-readme')
-const { bumpReadmeVersion } = require('./bump-readme-version')
 const { safeExec } = require('./safe-exec')
 
 module.exports = {
   log,
   copyPackageJson,
   copyReadme,
-  bumpReadmeVersion,
   safeExec
 }
