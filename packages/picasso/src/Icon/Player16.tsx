f72ba6031ca7cdb2de454ceef5e792967eb092ed
Anvar Tuykin <anvar.tuykin@toptal.com>
2020-05-08 13:23:41 +0300

    feat(Icon): add player icon

---
+++ packages/picasso/src/Icon/Player16.tsx
@@ -0,0 +1,52 @@
+import React, { forwardRef, Ref } from 'react'
+import cx from 'classnames'
+import { withStyles } from '@material-ui/core/styles'
+import { StandardProps } from '@toptal/picasso-shared'
+
+import kebabToCamelCase from '../utils/kebab-to-camel-case'
+import styles from './styles'
+const BASE_SIZE = 16
+type ScaleType = 1 | 2 | 3 | 4
+export interface Props extends StandardProps {
+  scale?: ScaleType
+  color?: string
+  base?: number
+}
+const SvgPlayer16 = forwardRef(function SvgPlayer16(
+  props: Props,
+  ref: Ref<SVGSVGElement>
+) {
+  const {
+    classes: availableClasses,
+    className,
+    style = {},
+    color,
+    scale,
+    base
+  } = props
+  const classes = [availableClasses.root, className]
+  const scaledSize = base || BASE_SIZE * Math.ceil(scale || 1)
+  const colorClassName = kebabToCamelCase(`${color}`)
+
+  if (availableClasses[colorClassName]) {
+    classes.push(availableClasses[colorClassName])
+  }
+
+  const svgStyle = {
+    minWidth: `${scaledSize}px`,
+    minHeight: `${scaledSize}px`,
+    ...style
+  }
+  return (
+    <svg
+      viewBox='0 0 16 16'
+      className={cx(...classes)}
+      style={svgStyle}
+      ref={ref}
+    >
+      <path d='M16 2v12H0V2h16zm-1 1H1v10h14V3zM6 5l5 3-5 3V5zm1.001 1.766v2.467L9.057 8 7.001 6.766z' />
+    </svg>
+  )
+})
+SvgPlayer16.displayName = 'SvgPlayer16'
+export default withStyles(styles)(SvgPlayer16)
