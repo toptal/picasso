9d28a9b7594b236e3da5ee1fde6952198b7dbc4e
Alexander Lesnenko <alxnddr@users.noreply.github.com>
2020-05-12 13:33:07 +0300

    fix(Select): fix scrolling on hover of boundary elements (#1292)

--- packages/picasso/src/Select/story/Default.example.jsx
+++ packages/picasso/src/Select/story/Default.example.jsx
@@ -24,7 +24,17 @@ const OPTIONS = [
   { value: '1', text: 'Option 1' },
   { value: '2', text: 'Option 2' },
   { value: '3', text: 'Option 3' },
-  { value: '4', text: 'Option 4' }
+  { value: '4', text: 'Option 4' },
+  { value: '5', text: 'Option 5' },
+  { value: '6', text: 'Option 6' },
+  { value: '7', text: 'Option 7' },
+  { value: '8', text: 'Option 8' },
+  { value: '9', text: 'Option 9' },
+  { value: '10', text: 'Option 10' },
+  { value: '11', text: 'Option 11' },
+  { value: '12', text: 'Option 12' },
+  { value: '13', text: 'Option 13' },
+  { value: '14', text: 'Option 14' }
 ]
 
 export default Example
