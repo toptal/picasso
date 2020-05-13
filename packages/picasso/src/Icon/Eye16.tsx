aed40a07f31fd93b92818f8603fff89c4ca48001
Nikita Luchko <nikita.luchko@toptal.com>
2020-05-13 19:58:00 +0400

    feat(icon): add eye and ownerDefault icons (#1306)
    The `ownerDefault` name reflects the naming in sketch file.

---
+++ packages/picasso/src/Icon/Eye16.tsx
@@ -0,0 +1,52 @@
+import React, { forwardRef, Ref } from 'react'
+import cx from 'classnames'
+import { withStyles } from '@material-ui/core/styles'
+import { StandardProps } from '@toptal/picasso-shared'
+
+import kebabToCamelCase from '../utils/kebab-to-camel-case'
+import styles from './styles'
+const BASE_SIZE = 16
+type ScaleType = 1 | 2 | 3 | 4
+export interface Props extends StandardProps {
+  scale?: ScaleType
+  color?: string
+  base?: number
+}
+const SvgEye16 = forwardRef(function SvgEye16(
+  props: Props,
+  ref: Ref<SVGSVGElement>
+) {
+  const {
+    classes: availableClasses,
+    className,
+    style = {},
+    color,
+    scale,
+    base
+  } = props
+  const classes = [availableClasses.root, className]
+  const scaledSize = base || BASE_SIZE * Math.ceil(scale || 1)
+  const colorClassName = kebabToCamelCase(`${color}`)
+
+  if (availableClasses[colorClassName]) {
+    classes.push(availableClasses[colorClassName])
+  }
+
+  const svgStyle = {
+    minWidth: `${scaledSize}px`,
+    minHeight: `${scaledSize}px`,
+    ...style
+  }
+  return (
+    <svg
+      viewBox='0 0 16 16'
+      className={cx(...classes)}
+      style={svgStyle}
+      ref={ref}
+    >
+      <path d='M8 3c2.946 0 5.612 1.667 8 5-2.388 3.333-5.054 5-8 5-2.946 0-5.612-1.667-8-5 2.388-3.333 5.054-5 8-5zm0 1C5.671 4 3.507 5.207 1.482 7.703L1.247 8l.011.015c2.014 2.59 4.168 3.89 6.484 3.98L8 12c2.329 0 4.493-1.207 6.518-3.703L14.752 8l-.01-.015c-2.014-2.59-4.168-3.89-6.484-3.98L8 4zm0 1a3 3 0 110 6 3 3 0 010-6zm0 1a2 2 0 100 4 2 2 0 000-4z' />
+    </svg>
+  )
+})
+SvgEye16.displayName = 'SvgEye16'
+export default withStyles(styles)(SvgEye16)
---
+++ packages/picasso/src/Icon/Eye24.tsx
@@ -0,0 +1,52 @@
+import React, { forwardRef, Ref } from 'react'
+import cx from 'classnames'
+import { withStyles } from '@material-ui/core/styles'
+import { StandardProps } from '@toptal/picasso-shared'
+
+import kebabToCamelCase from '../utils/kebab-to-camel-case'
+import styles from './styles'
+const BASE_SIZE = 24
+type ScaleType = 1 | 2 | 3 | 4
+export interface Props extends StandardProps {
+  scale?: ScaleType
+  color?: string
+  base?: number
+}
+const SvgEye24 = forwardRef(function SvgEye24(
+  props: Props,
+  ref: Ref<SVGSVGElement>
+) {
+  const {
+    classes: availableClasses,
+    className,
+    style = {},
+    color,
+    scale,
+    base
+  } = props
+  const classes = [availableClasses.root, className]
+  const scaledSize = base || BASE_SIZE * Math.ceil(scale || 1)
+  const colorClassName = kebabToCamelCase(`${color}`)
+
+  if (availableClasses[colorClassName]) {
+    classes.push(availableClasses[colorClassName])
+  }
+
+  const svgStyle = {
+    minWidth: `${scaledSize}px`,
+    minHeight: `${scaledSize}px`,
+    ...style
+  }
+  return (
+    <svg
+      viewBox='0 0 24 24'
+      className={cx(...classes)}
+      style={svgStyle}
+      ref={ref}
+    >
+      <path d='M12 4c4.418 0 8.418 2.667 12 8-3.582 5.333-7.582 8-12 8s-8.418-2.667-12-8c3.582-5.333 7.582-8 12-8zm0 1C8.155 5 4.605 7.241 1.332 11.833L1.214 12l.118.167c3.19 4.474 6.641 6.717 10.373 6.829L12 19c3.845 0 7.395-2.241 10.668-6.833l.117-.167-.117-.167c-3.19-4.474-6.641-6.717-10.373-6.829L12 5zm0 2a5 5 0 110 10 5 5 0 010-10zm0 1a4 4 0 100 8 4 4 0 000-8z' />
+    </svg>
+  )
+})
+SvgEye24.displayName = 'SvgEye24'
+export default withStyles(styles)(SvgEye24)
