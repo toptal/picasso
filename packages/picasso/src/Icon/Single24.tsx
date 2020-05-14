1c4aee60c5de8350583a8ebbbfc938e16c485d39
Nikita Luchko <nikita.luchko@toptal.com>
2020-05-14 16:48:07 +0400

    feat(icon): change single and multi icons (#1308)
    As was discussed with designers.

--- packages/picasso/src/Icon/Single24.tsx
+++ packages/picasso/src/Icon/Single24.tsx
@@ -44,7 +44,7 @@ const SvgSingle24 = forwardRef(function SvgSingle24(
       style={svgStyle}
       ref={ref}
     >
-      <path d='M19 5v14H5V5h14zm-1 1H6v12h12V6z' />
+      <path d='M12 0l10.392 6v12L12 24 1.608 18V6L12 0zm0 1.155L2.607 6.577v10.845L12 22.844l9.392-5.422V6.577L12 1.155z' />
     </svg>
   )
 })
