19e401aa180139f3ca0ec2415405039dcee2252f
Luis Felipe Zaguini <26530524+zaguiini@users.noreply.github.com>
2020-05-15 16:18:39 -0300

    feat(icon): add report icon (#1314)

---
+++ packages/picasso/src/Icon/Report16.tsx
@@ -0,0 +1,52 @@
+import React, { forwardRef, Ref } from 'react'
+import cx from 'classnames'
+import { withStyles } from '@material-ui/core/styles'
+import { StandardProps } from '@toptal/picasso-shared'
+
+import kebabToCamelCase from '../utils/kebab-to-camel-case'
+import styles from './styles'
+const BASE_SIZE = 16
+type ScaleType = 1 | 2 | 3 | 4
+export interface Props extends StandardProps {
+  scale?: ScaleType
+  color?: string
+  base?: number
+}
+const SvgReport16 = forwardRef(function SvgReport16(
+  props: Props,
+  ref: Ref<SVGSVGElement>
+) {
+  const {
+    classes: availableClasses,
+    className,
+    style = {},
+    color,
+    scale,
+    base
+  } = props
+  const classes = [availableClasses.root, className]
+  const scaledSize = base || BASE_SIZE * Math.ceil(scale || 1)
+  const colorClassName = kebabToCamelCase(`${color}`)
+
+  if (availableClasses[colorClassName]) {
+    classes.push(availableClasses[colorClassName])
+  }
+
+  const svgStyle = {
+    minWidth: `${scaledSize}px`,
+    minHeight: `${scaledSize}px`,
+    ...style
+  }
+  return (
+    <svg
+      viewBox='0 0 16 16'
+      className={cx(...classes)}
+      style={svgStyle}
+      ref={ref}
+    >
+      <path d='M15 0v12H6l-4 4v-4H0V0h15zm-1 1H1v10h2v2.586L5.586 11H14V1zM8 3v4H7V3h1zM7 8h1v1H7V8z' />
+    </svg>
+  )
+})
+SvgReport16.displayName = 'SvgReport16'
+export default withStyles(styles)(SvgReport16)
