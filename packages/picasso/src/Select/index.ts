a5b4cee10893c2ff91ff5cfe42772eca02314040
denieler <daniel.ostapenko@toptal.com>
2020-05-22 10:47:25 +0300

    chore: [FX-959] Remove unused export

--- packages/picasso/src/Select/index.ts
+++ packages/picasso/src/Select/index.ts
@@ -1,3 +1,3 @@
-export type { Props, Props as SelectProps } from './Select'
+export type { Props as SelectProps } from './Select'
 export type { Option, ValueType } from './types'
 export { default } from './Select'
