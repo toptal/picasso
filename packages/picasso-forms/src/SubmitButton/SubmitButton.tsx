c4aab7bd8e51888119d7a7697385239acbf33cdc
Daniel O <daniel.ostapenko@toptal.com>
2020-05-22 19:30:04 +0300

    fix(forms): fix wrong references in types in picass-forms (#1330)

--- packages/picasso-forms/src/SubmitButton/SubmitButton.tsx
+++ packages/picasso-forms/src/SubmitButton/SubmitButton.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
-import { Button, Props as ButtonProps } from '@toptal/picasso/Button'
+import { Button, ButtonProps } from '@toptal/picasso'
 import { useFormState } from 'react-final-form'
 
-export type Props = Omit<ButtonProps, 'type'>
+// Intersection with the type { id?: string } is needed here because of
+// TS compiler issue https://github.com/microsoft/TypeScript/issues/34793
+export type Props = Omit<ButtonProps, 'type'> & {
+  id?: string
+}
 
 export const SubmitButton = (props: Props) => {
   const { submitting } = useFormState()
