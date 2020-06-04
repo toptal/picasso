8b879acd2668c80c8c2e49ec73a498ff14b98e2f
Sergiu Butnarasu <sergiu.butnarasu@toptal.com>
2020-06-04 14:17:43 +0300

    feat: add company icon

---
+++ packages/picasso/src/Icon/Company16.tsx
@@ -0,0 +1,52 @@
+import React, { forwardRef, Ref } from 'react'
+import cx from 'classnames'
+import { withStyles } from '@material-ui/core/styles'
+import { StandardProps } from '@toptal/picasso-shared'
+
+import kebabToCamelCase from '../utils/kebab-to-camel-case'
+import styles from './styles'
+const BASE_SIZE = 16
+type ScaleType = 1 | 2 | 3 | 4
+export interface Props extends StandardProps {
+  scale?: ScaleType
+  color?: string
+  base?: number
+}
+const SvgCompany16 = forwardRef(function SvgCompany16(
+  props: Props,
+  ref: Ref<SVGSVGElement>
+) {
+  const {
+    classes: availableClasses,
+    className,
+    style = {},
+    color,
+    scale,
+    base
+  } = props
+  const classes = [availableClasses.root, className]
+  const scaledSize = base || BASE_SIZE * Math.ceil(scale || 1)
+  const colorClassName = kebabToCamelCase(`${color}`)
+
+  if (availableClasses[colorClassName]) {
+    classes.push(availableClasses[colorClassName])
+  }
+
+  const svgStyle = {
+    minWidth: `${scaledSize}px`,
+    minHeight: `${scaledSize}px`,
+    ...style
+  }
+  return (
+    <svg
+      viewBox='0 0 16 16'
+      className={cx(...classes)}
+      style={svgStyle}
+      ref={ref}
+    >
+      <path d='M8 0v9h8v7H0V0h8zM7 1H1v14h6V1zm8 9H8v5h7v-5zM5 12v1H3v-1h2zm8 0v1h-3v-1h3zM5 9v1H3V9h2zm0-3v1H3V6h2zm0-3v1H3V3h2z' />
+    </svg>
+  )
+})
+SvgCompany16.displayName = 'SvgCompany16'
+export default withStyles(styles)(SvgCompany16)
