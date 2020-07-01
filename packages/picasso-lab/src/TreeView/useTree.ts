3712d6969128344b79d0448d2fe0419c89066919
Dmitry Maklygin <dmitry.maklygin@toptal.com>
2020-07-01 21:08:07 +0300

    feat(treeview): add a scroll offset for each node (#1403)

--- packages/picasso-lab/src/TreeView/useTree.ts
+++ packages/picasso-lab/src/TreeView/useTree.ts
@@ -1,17 +1,14 @@
-import { createRef, useLayoutEffect, useMemo, useState } from 'react'
+import { useMemo } from 'react'
 import * as d3 from 'd3' // eslint-disable-line import/no-duplicates
 import { HierarchyPointNode } from 'd3' // eslint-disable-line import/no-duplicates
 
-import {
-  DEFAULT_HEIGHT,
-  DEFAULT_WIDTH,
-  HORIZONTAL_MARGIN,
-  VERTICAL_MARGIN
-} from './variables'
+import { useNodes } from './useNodes'
+import { DEFAULT_HEIGHT, DEFAULT_WIDTH, HORIZONTAL_MARGIN } from './variables'
 import { DynamicPointLink, DynamicPointNode, TreeNodeInterface } from './types'
 
 export interface UseTreeArguments {
   data: TreeNodeInterface
+  nodeWidth?: number
 }
 
 export interface UseTreeResponse {
@@ -20,126 +17,73 @@ export interface UseTreeResponse {
   selectedNode: DynamicPointNode | undefined
 }
 
-export interface LevelInfo {
-  top: number
-  width: number
-  height: number
+const calculateNodeXPosition = (
+  node: HierarchyPointNode<TreeNodeInterface>,
+  {
+    leaves,
+    nodeWidth
+  }: { leaves: HierarchyPointNode<TreeNodeInterface>[]; nodeWidth: number }
+) => {
+  if (!node.children || !node.children.length) {
+    const index = leaves.findIndex(leaf => leaf === node)
+
+    node.x = Math.floor(index - leaves.length / 2) * nodeWidth
+  } else {
+    node.x =
+      node.children.reduce((acc, child) => {
+        calculateNodeXPosition(child, { leaves, nodeWidth })
+
+        acc += child.x
+
+        return acc
+      }, 0) / node.children.length
+  }
+
+  return node
 }
 
-const updatePositions = (nodes: DynamicPointNode[]) => {
-  const levels: Record<number, LevelInfo> = {}
+export const useTree = ({
+  data,
+  nodeWidth = DEFAULT_WIDTH
+}: UseTreeArguments): UseTreeResponse => {
+  const fullNodeWidth = nodeWidth + 2 * HORIZONTAL_MARGIN
 
-  nodes
-    .sort((left, right) => left.depth - right.depth)
-    .forEach(node => {
-      const {
-        scrollWidth: width,
-        scrollHeight: height
-      } = node.ref.current!.firstElementChild!
-
-      if (!height || !width) {
-        return
-      }
-
-      node.y = 0
-      node.rect = {
-        width,
-        height
-      }
-
-      if (!node.parent) {
-        levels[node.depth] = {
-          width,
-          height,
-          top: 0
-        }
-      } else {
-        const previousLevel = levels[node.depth - 1]
-
-        node.y = previousLevel.top + previousLevel.height + VERTICAL_MARGIN
-
-        levels[node.depth] = {
-          width,
-          height: Math.max(levels[node.depth]?.height || 0, height),
-          top: previousLevel.top + previousLevel.height + VERTICAL_MARGIN
-        }
-      }
-    })
-
-  return levels
-}
-
-const getDynamicNodes = (
-  nodes: HierarchyPointNode<TreeNodeInterface>[]
-): DynamicPointNode[] => {
-  return nodes.map(node => {
-    return Object.assign(node, {
-      ref: createRef<HTMLElement>(),
-      rect: {
-        width: 0,
-        height: 0
-      }
-    })
-  })
-}
-
-export const useTree = ({ data }: UseTreeArguments): UseTreeResponse => {
-  const [levels, setLevels] = useState<Record<number, LevelInfo>>()
   const rootNode = useMemo(() => {
     const root = d3.hierarchy(data)
 
-    return d3
+    const rootNode = d3
       .tree<TreeNodeInterface>()
-      .nodeSize([DEFAULT_WIDTH + 2 * HORIZONTAL_MARGIN, DEFAULT_HEIGHT])(root)
-  }, [data])
-  // create a dynamic nodes
-  const dynamicNodes = useMemo(() => getDynamicNodes(rootNode.descendants()), [
-    rootNode
-  ])
-  // `nodes` depend on levels and dynamicNodes, so it's another entity
-  const nodes = useMemo<DynamicPointNode[]>(() => {
-    if (!levels) {
-      return dynamicNodes
-    }
+      .nodeSize([fullNodeWidth, DEFAULT_HEIGHT])(root)
+    const leaves = rootNode.leaves()
 
-    return dynamicNodes.map(node => {
-      const previousLevel = levels[node.depth - 1]
-
-      if (previousLevel) {
-        node.y = previousLevel.top + previousLevel.height + VERTICAL_MARGIN
-      }
-
-      return node
+    return calculateNodeXPosition(rootNode, {
+      leaves,
+      nodeWidth: fullNodeWidth
     })
-  }, [dynamicNodes, levels])
+  }, [data])
+
+  const nodes = useNodes(rootNode)
 
   const links = useMemo(
     () =>
-      nodes.reduce<DynamicPointLink[]>((acc, nodePoint) => {
-        if (nodePoint.parent) {
-          acc.push({
-            source: nodePoint.parent,
-            target: nodePoint
-          })
-        }
-        return acc
-      }, []),
+      nodes.reduce<DynamicPointLink[]>(
+        (acc: DynamicPointLink[], nodePoint: DynamicPointNode) => {
+          if (nodePoint.parent) {
+            acc.push({
+              source: nodePoint.parent,
+              target: nodePoint
+            })
+          }
+          return acc
+        },
+        []
+      ),
     [nodes]
   )
 
   const selectedNode = useMemo(() => nodes.find(node => node.data.selected), [
     nodes
   ])
-
-  useLayoutEffect(() => {
-    const rootNode = dynamicNodes[0]
-
-    if (!rootNode.ref.current) {
-      return
-    }
-
-    setLevels(updatePositions(dynamicNodes))
-  }, [dynamicNodes])
 
   return {
     nodes,
