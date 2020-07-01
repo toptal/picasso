c5e22272f8396251e7c9355e7663e4924eb39c0b
Daniel Ostapenko <daniel.ostapenko@toptal.com>
2020-07-01 16:57:07 +0300

    fix(Grid): fix for classes prop in Grid components

---
+++ packages/picasso/src/GridItem/test.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from 'react'
+import { render } from '@toptal/picasso/test-utils'
+import { OmitInternalProps } from '@toptal/picasso-shared'
+
+import GridItem, { Props } from './GridItem'
+
+const TestGridItem: FunctionComponent<OmitInternalProps<Props>> = () => (
+  <GridItem />
+)
+
+describe('GridItem', () => {
+  test('default render', () => {
+    const { container } = render(<TestGridItem />)
+
+    expect(container).toMatchSnapshot()
+  })
+})
