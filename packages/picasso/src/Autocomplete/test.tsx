647a2b3c74aef9753de6d14de20e42e4c2bbb86c
Aleksandr Shumilov <alexander.shumilov@toptal.com>
2020-07-14 10:38:30 +0000

    fix: ignore titleCase for Autocomplete options

--- packages/picasso/src/Autocomplete/test.tsx
+++ packages/picasso/src/Autocomplete/test.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
-import { render, fireEvent } from '@toptal/picasso/test-utils'
+import { render, fireEvent, PicassoConfig } from '@toptal/picasso/test-utils'
 import { OmitInternalProps } from '@toptal/picasso-shared'
+import * as titleCaseModule from 'ap-style-title-case'
 
 import Autocomplete, { Props } from './Autocomplete'
+
+jest.mock('ap-style-title-case')
 
 const testOptions = [
   { text: 'Belarus', value: 'BY' },
@@ -12,14 +15,27 @@ const testOptions = [
   { text: 'Ukraine', value: 'UA' }
 ]
 
-const renderAutocomplete = (props: OmitInternalProps<Props>) => {
+const renderAutocomplete = (
+  props: OmitInternalProps<Props>,
+  picassoConfig?: PicassoConfig
+) => {
   return render(
     <Autocomplete
       /* eslint-disable-next-line react/jsx-props-no-spreading */
       {...props}
-    />
+    />,
+    undefined,
+    picassoConfig
   )
 }
+
+let spiedOnTitleCase: jest.SpyInstance
+beforeEach(() => {
+  spiedOnTitleCase = jest.spyOn(titleCaseModule, 'default')
+})
+afterEach(() => {
+  spiedOnTitleCase.mockReset()
+})
 
 const placeholder = 'Placeholder text'
 
@@ -268,6 +284,25 @@ describe('Autocomplete', () => {
 
     fireEvent.focus(input)
     expect(api.baseElement.textContent).toContain('Custom renderer')
+  })
+
+  test('should not transform options text to title case when Picasso titleCase property is true', () => {
+    const placeholder = 'Choose an option...'
+    const { getByPlaceholderText } = renderAutocomplete(
+      {
+        options: testOptions,
+        value: '',
+        placeholder
+      },
+      {
+        titleCase: true
+      }
+    )
+
+    const input = getByPlaceholderText(placeholder)
+    fireEvent.focus(input)
+
+    expect(spiedOnTitleCase).toBeCalledTimes(0)
   })
 
   describe('Autofill', () => {
