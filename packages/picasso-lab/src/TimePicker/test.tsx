a3d89f4db5fdb8732397b84b918ecc850a54a9c2
Elvio Cavalcante <elvio.cavalcante@toptal.com>
2020-07-17 13:09:44 -0300

    feat(labs): implement TimePicker component (#1426)
    * feat(labs): implement TimePicker component
    
    * feat(labs): implement TimePicker tests
    
    * feat: add fallback input for safari
    
    * fix: forward props to component
    
    * feat: forward the change event
    
    * test: update the snapshot for time picker
    
    * fix: add px label to style

---
+++ packages/picasso-lab/src/TimePicker/test.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { render, fireEvent } from '@toptal/picasso/test-utils'
+
+import TimePicker from './TimePicker'
+
+describe('TimePicker', () => {
+  test('default render', () => {
+    const time = '21:00'
+
+    const { container } = render(
+      <TimePicker value={time} onChange={() => {}} />
+    )
+
+    expect(container).toMatchSnapshot()
+  })
+
+  test('custom time rendering', () => {
+    const time = '22:50'
+    const nextTime = '18:30'
+    const handleChange = jest.fn()
+
+    const { getByDisplayValue } = render(
+      <TimePicker value={time} onChange={handleChange} />
+    )
+
+    const input = getByDisplayValue(time)
+
+    fireEvent.change(input, { target: { value: nextTime } })
+
+    expect(handleChange).toHaveBeenCalledTimes(1)
+  })
+})
