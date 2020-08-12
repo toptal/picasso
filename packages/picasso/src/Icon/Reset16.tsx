a542dd909a564b0a6a483df8c97a866bcf420ab1
Trofim Samusev <trofim.samusev@toptal.com>
2020-08-12 11:59:43 +0300

    feat(icon): add reset icon

---
+++ packages/picasso/src/Icon/Reset16.tsx
@@ -0,0 +1,52 @@
+import React, { forwardRef, Ref } from 'react'
+import cx from 'classnames'
+import { withStyles } from '@material-ui/core/styles'
+import { StandardProps } from '@toptal/picasso-shared'
+
+import kebabToCamelCase from '../utils/kebab-to-camel-case'
+import styles from './styles'
+const BASE_SIZE = 16
+type ScaleType = 1 | 2 | 3 | 4
+export interface Props extends StandardProps {
+  scale?: ScaleType
+  color?: string
+  base?: number
+}
+const SvgReset16 = forwardRef(function SvgReset16(
+  props: Props,
+  ref: Ref<SVGSVGElement>
+) {
+  const {
+    classes: availableClasses,
+    className,
+    style = {},
+    color,
+    scale,
+    base
+  } = props
+  const classes = [availableClasses.root, className]
+  const scaledSize = base || BASE_SIZE * Math.ceil(scale || 1)
+  const colorClassName = kebabToCamelCase(`${color}`)
+
+  if (availableClasses[colorClassName]) {
+    classes.push(availableClasses[colorClassName])
+  }
+
+  const svgStyle = {
+    minWidth: `${scaledSize}px`,
+    minHeight: `${scaledSize}px`,
+    ...style
+  }
+  return (
+    <svg
+      viewBox='0 0 16 16'
+      className={cx(...classes)}
+      style={svgStyle}
+      ref={ref}
+    >
+      <path d='M5.5 6.5h-1V3.756a5.5 5.5 0 103.72-1.252l-.22-.004v-1a6.5 6.5 0 11-4.69 2H1.5v-1h4v4z' />
+    </svg>
+  )
+})
+SvgReset16.displayName = 'SvgReset16'
+export default withStyles(styles)(SvgReset16)
