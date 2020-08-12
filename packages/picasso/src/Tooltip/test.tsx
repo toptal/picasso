b72d469222c47eb7c34953311213f22af92d13ef
Piotr Krasuski <piotr.krasuski@toptal.com>
2020-08-12 21:52:39 +0200

    fix(Helpbox): update failing snapshots

--- packages/picasso/src/Tooltip/test.tsx
+++ packages/picasso/src/Tooltip/test.tsx
@@ -1,22 +1,19 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 
-import Container from '../Container'
 import Tooltip from './Tooltip'
 
 describe('Tooltip', () => {
   test('default render', () => {
     // If you don't provide `id` prop, it falls back to a randomly generated id.
     const { container } = render(
-      <Container>
-        <Tooltip
-          id='aria-describedby-id-mock'
-          content='Content goes here...'
-          open
-        >
-          <span>Test</span>
-        </Tooltip>
-      </Container>
+      <Tooltip
+        id='aria-describedby-id-mock'
+        content='Content goes here...'
+        open
+      >
+        <span>Test</span>
+      </Tooltip>
     )
 
     expect(container).toMatchSnapshot()
