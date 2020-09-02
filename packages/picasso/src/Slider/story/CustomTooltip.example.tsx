3ed94f3fa1cadc33d83652baadb32e69d7becf2b
Henrique Schreiner <henrique.schreiner@toptal.com>
2020-09-02 14:31:56 -0300

    feat(tooltip): add compact size for tooltip component (#1532)

--- packages/picasso/src/Slider/story/CustomTooltip.example.tsx
+++ packages/picasso/src/Slider/story/CustomTooltip.example.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { ReactNode, ChangeEvent } from 'react'
 import { Container, Slider, Tooltip } from '@toptal/picasso'
 
-const BottomTooltip = ({ children, open, value }) => (
-  <Tooltip arrow placement='bottom' open={open} content={value}>
+const BottomTooltip = ({
+  children,
+  open,
+  value
+}: {
+  children: ReactNode
+  open: boolean
+  value: ReactNode
+}) => (
+  <Tooltip arrow placement='bottom' open={open} content={value} compact>
     {children}
   </Tooltip>
 )
 
 const CustomSliderTooltip = () => {
-  const handleChange = (event, value) => {
+  const handleChange = (event: ChangeEvent<{}>, value: number | number[]) => {
     window.console.log('onChange: ', value)
   }
 
@@ -18,6 +26,7 @@ const CustomSliderTooltip = () => {
         onChange={handleChange}
         tooltip='auto'
         TooltipComponent={BottomTooltip}
+        compact
       />
     </Container>
   )
