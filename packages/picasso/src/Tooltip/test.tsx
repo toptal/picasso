5a8947ba13f0217ee9339e3207f8d5aaac021446
Luis Felipe Zaguini <26530524+zaguiini@users.noreply.github.com>
2020-09-17 23:06:33 -0300

    fix(tooltip): trigger immediately on touch (#1565)
    * fix(tooltip): trigger tooltip event on touch
    
    * fix(tooltip): change to touch screen detection via media query
    
    * chore(tooltip): add tests
    
    * fix(tooltip): polish usetooltiphandlers implementation
    
    * fix(tooltip): enhance tooltip direct child's click handler
    
    this is to avoid tooltip's click handler from being fired on swipe on touch screens
    
    * chore(tooltip): enhance touch screen detection and better code structure
    
    * fix(tooltip): prevent default on tooltip click on touch screens
    
    this is necessary to stop propagating the events while using the tooltip
    in a place where the bubbling click might trigger other
    actions (e.g. checkboxlabel example on typographyoverflow).

--- packages/picasso/src/Tooltip/test.tsx
+++ packages/picasso/src/Tooltip/test.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { fireEvent } from '@testing-library/react'
 import { render } from '@toptal/picasso/test-utils'
+import { Button } from '@toptal/picasso'
+import { ClickAwayListener, isPointerDevice } from '@toptal/picasso/utils'
+import { act } from 'react-dom/test-utils'
 
 import Container from '../Container'
 import Tooltip from './Tooltip'
+
+const mockedIsPointerDevice = isPointerDevice as jest.Mock
 
 describe('Tooltip', () => {
   test('default render', () => {
@@ -20,5 +26,130 @@ describe('Tooltip', () => {
     )
 
     expect(container).toMatchSnapshot()
+  })
+
+  describe('on touch screens', () => {
+    beforeEach(() => {
+      mockedIsPointerDevice.mockReturnValue(false)
+    })
+
+    afterEach(() => {
+      mockedIsPointerDevice.mockClear()
+    })
+
+    test('opens tooltip on touch', async () => {
+      const { getByText, queryByText, unmount } = render(
+        <Tooltip content='Hello'>
+          <Button>Tap me</Button>
+        </Tooltip>
+      )
+
+      act(() => {
+        fireEvent.click(getByText('Tap me'))
+      })
+
+      expect(queryByText('Hello')).toBeInTheDocument()
+
+      unmount()
+    })
+  })
+
+  describe('on fine pointer devices', () => {
+    beforeEach(() => {
+      jest.useFakeTimers()
+      mockedIsPointerDevice.mockReturnValue(true)
+    })
+
+    afterEach(() => {
+      jest.useRealTimers()
+      mockedIsPointerDevice.mockClear()
+    })
+
+    test('opens tooltip on hover on short delay', () => {
+      const { getByText, queryByText, unmount } = render(
+        <Tooltip content='Hello'>
+          <Button>Hover me</Button>
+        </Tooltip>
+      )
+
+      act(() => {
+        fireEvent.mouseEnter(getByText('Hover me'))
+        jest.advanceTimersByTime(200)
+      })
+
+      expect(queryByText('Hello')).toBeInTheDocument()
+
+      unmount()
+    })
+
+    test('opens tooltip on hover on long delay then closes it on mouse out', async () => {
+      const { getByText, queryByText, unmount } = render(
+        <Tooltip content='Hello'>
+          <Button>Hover me</Button>
+        </Tooltip>
+      )
+
+      act(() => {
+        fireEvent.mouseEnter(getByText('Hover me'))
+        jest.advanceTimersByTime(500)
+      })
+
+      expect(queryByText('Hello')).toBeInTheDocument()
+
+      act(() => {
+        fireEvent.mouseLeave(getByText('Hover me'))
+        jest.advanceTimersByTime(1500)
+      })
+
+      expect(queryByText('Hello')).not.toBeInTheDocument()
+
+      unmount()
+    })
+
+    test('opens tooltip on click then closes it on outside click', () => {
+      const Component = () => {
+        const [open, setOpen] = useState(false)
+
+        return (
+          <div>
+            <ClickAwayListener onClickAway={() => setOpen(false)}>
+              <div>
+                <Tooltip open={open} content='Hello'>
+                  <Button onClick={() => setOpen(true)}>
+                    Hover then click me
+                  </Button>
+                </Tooltip>
+              </div>
+            </ClickAwayListener>
+            <Button>Click outside!</Button>
+          </div>
+        )
+      }
+
+      const { getByText, queryByText } = render(<Component />)
+
+      const handler = getByText('Hover then click me')
+
+      act(() => {
+        fireEvent.mouseEnter(handler)
+        jest.advanceTimersByTime(200)
+      })
+
+      expect(queryByText('Hello')).not.toBeInTheDocument()
+
+      act(() => {
+        fireEvent.click(handler)
+        jest.advanceTimersByTime(200)
+      })
+
+      expect(queryByText('Hello')).toBeInTheDocument()
+
+      act(() => {
+        fireEvent.click(getByText('Click outside!'))
+        jest.advanceTimersByTime(1500)
+      })
+
+      expect(queryByText('Hello')).not.toBeInTheDocument()
+    })
   })
 })
