9abe825cb3be5b83645beb5aa11e154bd8b8c657
Evgeniy Demin <evgeniy.demin@toptal.com>
2020-09-24 14:43:29 +0200

    feat(OverviewBlock): update to the latest design
    BREAKING CHANGE: visual differences and approach to multiline
    variant of OverviewBlock component. The list of visual changes:
    - OverviewBlock: change horizontal padding to 12px
    - OverviewBlock: change horizontal padding to 24px (for regular variant)
    - OverviewBlock: add vertical separators
    - OverviewBlock: change background color to white
    - OverviewBlock: change clickable block background color to blue on hover
    - OverviewBlockGroup: add 16px of vertical padding
    - OverviewBlockGroup: add 1px border with 8px radius
    - OverviewBlockGroup: stop adding margin between blocks
    - OverviewBlockGroup: add align and blockWidth property
    - OverviewBlockRow: introduced to cover multiline variant

---
+++ packages/picasso-lab/src/OverviewBlockRow/styles.ts
@@ -0,0 +1,26 @@
+import { createStyles, Theme } from '@material-ui/core/styles'
+
+export default ({ palette, sizes }: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      borderRadius: sizes.borderRadius.medium,
+      padding: '1rem 0',
+      border: `${sizes.borderWidth} solid ${palette.grey.lighter}`,
+      justifyContent: 'flex-start',
+      '& > *': {
+        flex: '1 0'
+      },
+      '&:not(:first-child)': {
+        borderTop: 0,
+        borderTopLeftRadius: 0,
+        borderTopRightRadius: 0
+      },
+      '&:not(:last-child)': {
+        borderBottom: 0,
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0,
+        paddingBottom: 0
+      }
+    }
+  })
