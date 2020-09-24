2208b747d988114608bd9a0ebe2c08cdd0282d3d
Mateusz Derks <mateusz.derks@toptal.com>
2020-09-24 14:46:46 +0200

    feat(icons): add rank icons (#1582)
    Introduce RankOne, RankTwo and RankThree icons in 16px and 24px variants.

--- packages/picasso/src/Icon/Candidates24.tsx
+++ packages/picasso/src/Icon/Candidates24.tsx
@@ -6,6 +6,7 @@ import { StandardProps } from '@toptal/picasso-shared'
 import kebabToCamelCase from '../utils/kebab-to-camel-case'
 import styles from './styles'
 const BASE_SIZE = 24
+
 type ScaleType = 1 | 2 | 3 | 4
 export interface Props extends StandardProps {
   scale?: ScaleType
@@ -37,6 +38,7 @@ const SvgCandidates24 = forwardRef(function SvgCandidates24(
     minHeight: `${scaledSize}px`,
     ...style
   }
+
   return (
     <svg
       viewBox='0 0 24 24'
@@ -48,5 +50,6 @@ const SvgCandidates24 = forwardRef(function SvgCandidates24(
     </svg>
   )
 })
+
 SvgCandidates24.displayName = 'SvgCandidates24'
 export default withStyles(styles)(SvgCandidates24)
--- packages/picasso/src/Icon/Copy16.tsx
+++ packages/picasso/src/Icon/Copy16.tsx
@@ -6,6 +6,7 @@ import { StandardProps } from '@toptal/picasso-shared'
 import kebabToCamelCase from '../utils/kebab-to-camel-case'
 import styles from './styles'
 const BASE_SIZE = 16
+
 type ScaleType = 1 | 2 | 3 | 4
 export interface Props extends StandardProps {
   scale?: ScaleType
@@ -37,6 +38,7 @@ const SvgCopy16 = forwardRef(function SvgCopy16(
     minHeight: `${scaledSize}px`,
     ...style
   }
+
   return (
     <svg
       viewBox='0 0 16 16'
@@ -48,5 +50,6 @@ const SvgCopy16 = forwardRef(function SvgCopy16(
     </svg>
   )
 })
+
 SvgCopy16.displayName = 'SvgCopy16'
 export default withStyles(styles)(SvgCopy16)
