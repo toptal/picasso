bbaae1767788684b69918f8a61250cce7acc3680
Esen Dzhailobaev <esen.dzhailobaev@toptal.com>
2020-09-25 19:24:02 +0600

    fix(ScrollMenu): calculation in useLayoutEffect (#1580)
    * fix(ScrollMenu): calculation in useLayoutEfffect
    
    * fix: get rid of state modification inside useLayoutEffect
    
    Co-authored-by: Teimur Gasanov <teimur@toptal.com>

--- packages/picasso/src/ScrollMenu/ScrollMenu.tsx
+++ packages/picasso/src/ScrollMenu/ScrollMenu.tsx
@@ -1,6 +1,5 @@
 import React, {
-  useState,
-  useEffect,
+  useLayoutEffect,
   useRef,
   createRef,
   FunctionComponent
@@ -48,7 +47,7 @@ const ScrollMenu: FunctionComponent<Props> = ({
 }) => {
   const menuRef = useRef<HTMLDivElement | null>(null)
   const firstItemRef = createRef<HTMLElement>()
-  const [prevSelectedIndex, setPrevSelectedIndex] = useState(selectedIndex)
+  const prevSelectedIndex = useRef(selectedIndex)
 
   const renderChildren = React.Children.map(children, (child, index) => {
     if (index === 0 && child) {
@@ -58,7 +57,7 @@ const ScrollMenu: FunctionComponent<Props> = ({
     return child
   })
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (!menuRef.current || !firstItemRef.current) {
       return
     }
@@ -81,7 +80,7 @@ const ScrollMenu: FunctionComponent<Props> = ({
     if (!isHighlightedItemInScrollView) {
       const moveDirection = getMoveDirection(
         selectedIndex,
-        prevSelectedIndex,
+        prevSelectedIndex.current,
         bottomVisibleItem
       )
       let scrollTop = 0
@@ -95,7 +94,7 @@ const ScrollMenu: FunctionComponent<Props> = ({
       menuRef.current.scrollTop = scrollTop
     }
 
-    setPrevSelectedIndex(selectedIndex)
+    prevSelectedIndex.current = selectedIndex
   }, [firstItemRef, selectedIndex, prevSelectedIndex])
 
   return (
