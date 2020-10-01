34794b79b590b6805b0133ff4c2c43dd66c80b78
Teimur Gasanov <teimur@toptal.com>
2020-10-01 13:47:28 +0600

    fix: fix value is not set when options dynamic (#1591)
    * fix: fix value is not set when options dynamic
    
    * chore: add test

--- packages/picasso/src/Select/Select.tsx
+++ packages/picasso/src/Select/Select.tsx
@@ -6,7 +6,8 @@ import React, {
   useRef,
   useState,
   useCallback,
-  useMemo
+  useMemo,
+  useEffect
 } from 'react'
 import cx from 'classnames'
 import NativeSelect from '@material-ui/core/NativeSelect'
@@ -275,6 +276,7 @@ const removeDuplicatedOptions = (options: Option[]) =>
     const innerIndex = options.findIndex(
       innerOption => innerOption.value === option.value
     )
+
     return innerIndex === index
   })
 
@@ -294,6 +296,16 @@ const purifyProps = (props: Props<any, any>): Props<ValueType, boolean> => {
 
   return disableUnsupportedProps('Select', props, sizeOptions)
 }
+
+const getSelectedOptions = (
+  options: Option[],
+  value?: ValueType | ValueType[]
+) =>
+  options.filter(option =>
+    Array.isArray(value)
+      ? value.includes(String(option.value))
+      : value === String(option.value)
+  )
 
 const renderOptions = ({
   options,
@@ -323,6 +335,7 @@ const renderOptions = ({
       option
     )
     const selection = getSelection(options, value)
+
     return (
       <SelectOption
         key={option.key || option.value}
@@ -406,11 +419,7 @@ export const Select = documentable(
 
       const inputWrapperRef = useRef<HTMLDivElement>(null)
       const [selectedOptions, setSelectedOptions] = useState(
-        allOptions.filter(option =>
-          Array.isArray(value)
-            ? isOptionInSelectedValues(option, value)
-            : value === String(option.value)
-        )
+        getSelectedOptions(allOptions, value)
       )
       const select = useMemo(
         () =>
@@ -434,25 +443,17 @@ export const Select = documentable(
         [allOptions, filterOptionsValue, getDisplayValue]
       )
 
-      const selectedIndexes = useMemo(
-        () =>
-          options.reduce(
-            (selected: number[], option: Option, index: number) =>
-              select.isOptionSelected(option) ? [...selected, index] : selected,
-            []
-          ),
-        [options, select]
-      )
-
-      const prevValue = useRef(value)
-      if (prevValue.current !== value) {
-        const select = getSelection(
+      useEffect(() => {
+        const newSelect = getSelection(
           removeDuplicatedOptions([...allOptions, ...selectedOptions]),
           value
         )
-        setInputValue(select.display(getDisplayValue!))
-        prevValue.current = value
-      }
+
+        setInputValue(newSelect.display(getDisplayValue!))
+        setSelectedOptions(getSelectedOptions(allOptions, value))
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, [value, allOptions, getDisplayValue])
 
       const readOnlyInput = multiple || allOptions.length <= searchThreshold!
 
@@ -502,6 +503,7 @@ export const Select = documentable(
         if (isInSelectedValues) {
           return value!.filter(value => value !== option.value)
         }
+
         return [...value, String(option.value)]
       }
       const handleSelect = useCallback(
