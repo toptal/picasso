658fe52fe29ee8f10213fdbf50fb72f7f0f4e73a
Huy Tran <tran.huy@toptal.com>
2020-10-07 16:18:30 +0700

    fix: autocomplete custom render example

--- packages/picasso/src/Autocomplete/story/CustomOptionRenderer.example.tsx
+++ packages/picasso/src/Autocomplete/story/CustomOptionRenderer.example.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Item } from '@toptal/picasso/Autocomplete'
 import { Typography, Container, Autocomplete } from '@toptal/picasso'
+import { isSubstring } from '@toptal/picasso/utils'
 
 const renderOtherOption = (value: string) => (
-  <Typography size='medium' color='inherit' weight='semibold'>
+  <Typography size='medium' color='dark-grey' weight='semibold'>
     Search for: {value}
   </Typography>
 )
@@ -14,7 +15,9 @@ interface Country extends Item {
   code: string
 }
 
-const options: Country[] = [
+const EMPTY_INPUT_VALUE = ''
+
+const allOptions: Country[] = [
   { country: 'Belarus', capital: 'Minsk', code: 'BE' },
   { country: 'Croatia', capital: 'Zagreb', code: 'HR' },
   { country: 'Lithuania', capital: 'Vilnius', code: 'LU' },
@@ -22,33 +25,58 @@ const options: Country[] = [
   { country: 'Ukraine', capital: 'Kyiv', code: 'UA' }
 ]
 
-const CustomOptionRenderer = () => (
-  <div>
-    <Autocomplete
-      value=''
-      placeholder='Start typing country...'
-      options={options}
-      onSelect={(item: Item) => {
-        window.console.log('onSelect returns item object:', item)
-        window.console.log('selected capital:', item.capital)
-      }}
-      getKey={(item: Item) => (item as Country).code}
-      getDisplayValue={(item: Item | null) =>
-        (item && (item as Country).country) || ''
-      }
-      renderOption={(option: Partial<Country>, index) => (
-        <Container>
-          <Typography size='medium' weight='semibold'>
-            {option.country}
-          </Typography>
-          <Typography size='inherit' style={{ fontSize: '12px' }}>
-            {option.capital} ({index})
-          </Typography>
-        </Container>
-      )}
-      renderOtherOption={renderOtherOption}
-    />
-  </div>
-)
+const getDisplayValue = (item: Item | null) =>
+  (item && (item as Country).country) || ''
+
+const filterOptions = (str: string) =>
+  str !== ''
+    ? allOptions.filter(option => isSubstring(str, getDisplayValue(option)))
+    : allOptions
+
+const CustomOptionRenderer = () => {
+  const [value, setValue] = useState(EMPTY_INPUT_VALUE)
+  const [options, setOptions] = useState(allOptions)
+
+  return (
+    <div>
+      <Autocomplete
+        showOtherOption
+        value={value}
+        placeholder='Start typing country...'
+        options={options}
+        getKey={(item: Item) => (item as Country).code}
+        renderOption={(option: Partial<Country>, index) => (
+          <Container>
+            <Typography size='medium' weight='semibold'>
+              {option.country}
+            </Typography>
+            <Typography size='inherit' style={{ fontSize: '12px' }}>
+              {option.capital} ({index})
+            </Typography>
+          </Container>
+        )}
+        renderOtherOption={renderOtherOption}
+        onSelect={(option: Partial<Country>) => {
+          window.console.log('onSelect returns item object:', option)
+          window.console.log('selected capital:', option.capital)
+
+          const itemValue = getDisplayValue(option)
+
+          if (value !== itemValue) {
+            setValue(itemValue)
+          }
+        }}
+        onOtherOptionSelect={newValue => {
+          setValue(newValue)
+          setOptions(filterOptions(newValue))
+        }}
+        onChange={newValue => {
+          setOptions(filterOptions(newValue))
+          setValue(newValue)
+        }}
+      />
+    </div>
+  )
+}
 
 export default CustomOptionRenderer
