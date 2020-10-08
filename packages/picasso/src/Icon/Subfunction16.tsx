3a94c9a1f4d249d72d4e0c3b102ab0e7d1d127d3
Esen Dzhailobaev <esen.dzhailobaev@toptal.com>
2020-10-08 20:10:26 +0600

    feat(icon): add org structure icons These include: - Function icon - Subfunction icon - Special Group icon

---
+++ packages/picasso/src/Icon/Subfunction16.tsx
@@ -0,0 +1,55 @@
+import React, { forwardRef, Ref } from 'react'
+import cx from 'classnames'
+import { withStyles } from '@material-ui/core/styles'
+import { StandardProps } from '@toptal/picasso-shared'
+
+import kebabToCamelCase from '../utils/kebab-to-camel-case'
+import styles from './styles'
+const BASE_SIZE = 16
+
+type ScaleType = 1 | 2 | 3 | 4
+export interface Props extends StandardProps {
+  scale?: ScaleType
+  color?: string
+  base?: number
+}
+const SvgSubfunction16 = forwardRef(function SvgSubfunction16(
+  props: Props,
+  ref: Ref<SVGSVGElement>
+) {
+  const {
+    classes: availableClasses,
+    className,
+    style = {},
+    color,
+    scale,
+    base
+  } = props
+  const classes = [availableClasses.root, className]
+  const scaledSize = base || BASE_SIZE * Math.ceil(scale || 1)
+  const colorClassName = kebabToCamelCase(`${color}`)
+
+  if (availableClasses[colorClassName]) {
+    classes.push(availableClasses[colorClassName])
+  }
+
+  const svgStyle = {
+    minWidth: `${scaledSize}px`,
+    minHeight: `${scaledSize}px`,
+    ...style
+  }
+
+  return (
+    <svg
+      viewBox='0 0 16 16'
+      className={cx(...classes)}
+      style={svgStyle}
+      ref={ref}
+    >
+      <path d='M10 1v5H8v2h5v3.05a2.5 2.5 0 11-1 0V9H3v2h2v5H0v-5h2V8h5V6H5V1h5zM4 12H1v3h3v-3zm8.5 0a1.5 1.5 0 100 3 1.5 1.5 0 000-3zM9 2H6v3h3V2z' />
+    </svg>
+  )
+})
+
+SvgSubfunction16.displayName = 'SvgSubfunction16'
+export default withStyles(styles)(SvgSubfunction16)
