d01a5a0dd523759f661edbd2e5b8395a01b51851
Esen Dzhailobaev <esen.dzhailobaev@toptal.com>
2020-10-12 19:27:33 +0600

    feat(icons): update org structure icons, add pod (#1614)
    Updated with pixel-perfect versions:
    - function
    - subfunction
    - special group
    Added:
    - pod

--- packages/picasso/src/Icon/Pod16.tsx
+++ packages/picasso/src/Icon/Pod16.tsx
@@ -13,7 +13,7 @@ export interface Props extends StandardProps {
   color?: string
   base?: number
 }
-const SvgSpecialGroup16 = forwardRef(function SvgSpecialGroup16(
+const SvgPod16 = forwardRef(function SvgPod16(
   props: Props,
   ref: Ref<SVGSVGElement>
 ) {
@@ -46,10 +46,10 @@ const SvgSpecialGroup16 = forwardRef(function SvgSpecialGroup16(
       style={svgStyle}
       ref={ref}
     >
-      <path d='M12 0a3 3 0 011.777 5.418A3.994 3.994 0 0115.995 8.8L16 9h-1a3 3 0 00-5.995-.176L9 9h-.013l.482.977 2.284.332a.5.5 0 01.277.853l-1.652 1.61.39 2.275a.5.5 0 01-.726.527L8 14.5l-2.042 1.074a.5.5 0 01-.726-.527l.39-2.274-1.652-1.61a.5.5 0 01.277-.854l2.284-.332L7.012 9H7a3 3 0 00-5.995-.176L1 9H0a4 4 0 012.223-3.585 3 3 0 113.554.002 4.001 4.001 0 012.001 2.266.495.495 0 01.443-.002 4.018 4.018 0 012.002-2.266A3 3 0 0112 0zM8 9.26l-.805 1.631-1.801.262 1.303 1.27-.309 1.794L8 13.37l1.611.847-.308-1.794 1.302-1.27-1.8-.262L8 9.26zM4 1a2 2 0 100 4 2 2 0 000-4zm8 0a2 2 0 100 4 2 2 0 000-4z' />
+      <path d='M4 9a3 3 0 110 6 3 3 0 010-6zm8 0a3 3 0 110 6 3 3 0 010-6zm-8 1a2 2 0 100 4 2 2 0 000-4zm8 0a2 2 0 100 4 2 2 0 000-4zM4 1a3 3 0 110 6 3 3 0 010-6zm8 0a3 3 0 110 6 3 3 0 010-6zM4 2a2 2 0 100 4 2 2 0 000-4zm8 0a2 2 0 100 4 2 2 0 000-4z' />
     </svg>
   )
 })
 
-SvgSpecialGroup16.displayName = 'SvgSpecialGroup16'
-export default withStyles(styles)(SvgSpecialGroup16)
+SvgPod16.displayName = 'SvgPod16'
+export default withStyles(styles)(SvgPod16)
--- packages/picasso/src/Icon/Pod24.tsx
+++ packages/picasso/src/Icon/Pod24.tsx
@@ -5,7 +5,7 @@ import { StandardProps } from '@toptal/picasso-shared'
 
 import kebabToCamelCase from '../utils/kebab-to-camel-case'
 import styles from './styles'
-const BASE_SIZE = 16
+const BASE_SIZE = 24
 
 type ScaleType = 1 | 2 | 3 | 4
 export interface Props extends StandardProps {
@@ -13,7 +13,7 @@ export interface Props extends StandardProps {
   color?: string
   base?: number
 }
-const SvgSpecialGroup16 = forwardRef(function SvgSpecialGroup16(
+const SvgPod24 = forwardRef(function SvgPod24(
   props: Props,
   ref: Ref<SVGSVGElement>
 ) {
@@ -41,15 +41,15 @@ const SvgSpecialGroup16 = forwardRef(function SvgSpecialGroup16(
 
   return (
     <svg
-      viewBox='0 0 16 16'
+      viewBox='0 0 24 24'
       className={cx(...classes)}
       style={svgStyle}
       ref={ref}
     >
-      <path d='M12 0a3 3 0 011.777 5.418A3.994 3.994 0 0115.995 8.8L16 9h-1a3 3 0 00-5.995-.176L9 9h-.013l.482.977 2.284.332a.5.5 0 01.277.853l-1.652 1.61.39 2.275a.5.5 0 01-.726.527L8 14.5l-2.042 1.074a.5.5 0 01-.726-.527l.39-2.274-1.652-1.61a.5.5 0 01.277-.854l2.284-.332L7.012 9H7a3 3 0 00-5.995-.176L1 9H0a4 4 0 012.223-3.585 3 3 0 113.554.002 4.001 4.001 0 012.001 2.266.495.495 0 01.443-.002 4.018 4.018 0 012.002-2.266A3 3 0 0112 0zM8 9.26l-.805 1.631-1.801.262 1.303 1.27-.309 1.794L8 13.37l1.611.847-.308-1.794 1.302-1.27-1.8-.262L8 9.26zM4 1a2 2 0 100 4 2 2 0 000-4zm8 0a2 2 0 100 4 2 2 0 000-4z' />
+      <path d='M6 13a5 5 0 110 10 5 5 0 010-10zm12 0a5 5 0 110 10 5 5 0 010-10zM6 14a4 4 0 100 8 4 4 0 000-8zm12 0a4 4 0 100 8 4 4 0 000-8zM6 1a5 5 0 110 10A5 5 0 016 1zm12 0a5 5 0 110 10 5 5 0 010-10zM6 2a4 4 0 100 8 4 4 0 000-8zm12 0a4 4 0 100 8 4 4 0 000-8z' />
     </svg>
   )
 })
 
-SvgSpecialGroup16.displayName = 'SvgSpecialGroup16'
-export default withStyles(styles)(SvgSpecialGroup16)
+SvgPod24.displayName = 'SvgPod24'
+export default withStyles(styles)(SvgPod24)
