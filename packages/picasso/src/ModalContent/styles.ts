0ccef2c1c16dd4c7f80e8053a770ed98d4e144ae
Leonardo Cristofani <leonardo.cristofani@toptal.com>
2020-10-13 23:28:50 -0300

    feat(modal): update visual tests

--- packages/picasso/src/ModalContent/styles.ts
+++ packages/picasso/src/ModalContent/styles.ts
@@ -4,20 +4,21 @@ export default () =>
   createStyles({
     modalContent: {
       padding: '2rem',
-      overflow: 'auto',
-      flex: '1 1 auto'
+      flex: '1 1 auto',
+      overflowY: 'auto'
     },
     fadedWrapper: {
       display: 'flex',
       position: 'relative',
       flex: '1 1 auto',
-      overflow: 'scroll'
+      overflowY: 'hidden'
     },
     fadedWrapperEffect: {
       top: 0,
       left: 0,
       right: 0,
       bottom: 0,
+      zIndex: 1,
       position: 'absolute',
       pointerEvents: 'none',
       background: `linear-gradient( 0deg,white 0%,rgba(255,255,255,0) 2rem,rgba(255,255,255,0) calc(100% - 2rem),white 100% )`
