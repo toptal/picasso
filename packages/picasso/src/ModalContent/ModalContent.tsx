eba9a2902aa87d809fd8e919a22c4eced1851bae
Leonardo Cristofani <leonardo.cristofani@toptal.com>
2020-10-20 10:39:46 -0300

    feat(modal): add scroll shades (#1628)
    * feat(modal): add scroll shades
    
    * feat(modal): fix unit tests
    
    Co-authored-by: Boris Yordanov <boris.yordanov@toptal.com>

--- packages/picasso/src/ModalContent/ModalContent.tsx
+++ packages/picasso/src/ModalContent/ModalContent.tsx
@@ -4,6 +4,8 @@ import cx from 'classnames'
 import { StandardProps } from '@toptal/picasso-shared'
 
 import styles from './styles'
+import { useCombinedRefs } from '../utils'
+import useScrollableShades from './use-scrollable-shades'
 
 export interface Props extends StandardProps, HTMLAttributes<HTMLDivElement> {
   /** Content of Modal */
@@ -12,15 +14,31 @@ export interface Props extends StandardProps, HTMLAttributes<HTMLDivElement> {
 
 export const ModalContent = forwardRef<HTMLDivElement, Props>(
   function ModalContent({ children, classes, className, style, ...rest }, ref) {
+    /**
+     * This is necessary to ensure if ref is not passed in, there's still a ref to calculate
+     * when to show the scrollable shades or not.
+     */
+    const modalContentRef = useCombinedRefs<HTMLDivElement>(ref)
+
+    const { top, bottom } = useScrollableShades(modalContentRef)
+
     return (
-      <div
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        {...rest}
-        ref={ref}
-        className={cx(classes.root, className)}
-        style={style}
-      >
-        {children}
+      <div className={cx(classes.shadedWrapper)}>
+        <div
+          className={cx(classes.shadedWrapperEffect, {
+            [classes.topShade]: top && !bottom,
+            [classes.bottomShade]: bottom && !top,
+            [classes.topBottomShades]: top && bottom
+          })}
+        />
+        <div // eslint-disable-next-line react/jsx-props-no-spreading
+          {...rest}
+          style={style}
+          ref={modalContentRef}
+          className={cx(classes.modalContent, className)}
+        >
+          {children}
+        </div>
       </div>
     )
   }
