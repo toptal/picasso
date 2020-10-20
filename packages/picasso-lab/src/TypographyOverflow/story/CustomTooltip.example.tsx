f51e914735e5326400ad3f53af41ee7f35cfabc6
OleksandrNechai <oleksandr.nechai@toptal.com>
2020-10-20 18:45:24 +0300

    fix: revew comments

--- packages/picasso-lab/src/TypographyOverflow/story/CustomTooltip.example.tsx
+++ packages/picasso-lab/src/TypographyOverflow/story/CustomTooltip.example.tsx
@@ -1,25 +1,20 @@
 import React from 'react'
 import { TypographyOverflow } from '@toptal/picasso-lab'
+import { Typography } from '@toptal/picasso'
 
 const Example = () => {
-  const someHtmlFromServer =
-    'This <b>typography</b> is very long and therefore it overflows.'
-
   return (
-    <div style={{ width: 300, marginTop: 220 }}>
+    <div style={{ width: 300, marginTop: 100 }}>
       <TypographyOverflow
         data-testid='ellipsed-text'
         tooltipContent={
-          <p
-            dangerouslySetInnerHTML={{
-              __html: someHtmlFromServer
-            }}
-          />
+          <Typography color='yellow' weight='semibold'>
+            This typography is very long and therefore it overflows.
+          </Typography>
         }
-        dangerouslySetInnerHTML={{
-          __html: someHtmlFromServer
-        }}
-      />
+      >
+        This typography is very long and therefore it overflows.
+      </TypographyOverflow>
     </div>
   )
 }
