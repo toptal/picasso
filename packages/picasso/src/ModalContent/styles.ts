5e4df3f3c15a5ea49ea1c40cf02f6f2ef268ac80
Leonardo Cristofani <leonardo.cristofani@toptal.com>
2020-10-20 10:39:46 -0300

    feat(modal): add scroll shades (#1628)
    * feat(modal): add scroll shades
    
    * feat(modal): fix unit tests
    
    Co-authored-by: Boris Yordanov <boris.yordanov@toptal.com>

--- packages/picasso/src/ModalContent/styles.ts
+++ packages/picasso/src/ModalContent/styles.ts
@@ -2,9 +2,33 @@ import { createStyles } from '@material-ui/core'
 
 export default () =>
   createStyles({
-    root: {
+    modalContent: {
       padding: '1rem 2rem',
       overflow: 'auto',
       flex: '1 1 auto'
+    },
+    shadedWrapper: {
+      display: 'flex',
+      position: 'relative',
+      flex: '1 1 auto',
+      overflowY: 'hidden'
+    },
+    shadedWrapperEffect: {
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      zIndex: 1,
+      position: 'absolute',
+      pointerEvents: 'none'
+    },
+    topShade: {
+      background: 'linear-gradient(180deg, white 0%, rgba(255,255,255,0) 5rem)'
+    },
+    bottomShade: {
+      background: 'linear-gradient(0deg, white 0%, rgba(255,255,255,0) 5rem)'
+    },
+    topBottomShades: {
+      background: `linear-gradient(0deg, white 0%, rgba(255,255,255,0) 5rem, rgba(255,255,255,0) calc(100% - 5rem), white 100%)`
     }
   })
