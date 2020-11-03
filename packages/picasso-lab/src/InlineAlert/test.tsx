585424ce7825356e254f3f50d2de6dc83a400e6f
Boris Yordanov <boris.yordanov@toptal.com>
2020-11-03 17:18:33 +0100

    chore: inlinealert tests

--- packages/picasso-lab/src/InlineAlert/test.tsx
+++ packages/picasso-lab/src/InlineAlert/test.tsx
@@ -1,35 +1,15 @@
 import React, { ReactNode } from 'react'
-import { render, fireEvent } from '@toptal/picasso/test-utils'
-import { OmitInternalProps } from '@toptal/picasso-shared'
+import { render } from '@toptal/picasso/test-utils'
 
-import Alert, { Props } from './Alert'
+import InlineAlert from './InlineAlert'
 
-const renderAlert = (
-  children: ReactNode,
-  props: OmitInternalProps<Props, 'children'>
-) => {
-  const { onClose } = props
-
-  return render(<Alert onClose={onClose}>{children}</Alert>)
-}
+const renderInlineAlert = (children: ReactNode) =>
+  render(<InlineAlert>{children}</InlineAlert>)
 
 describe('Alert', () => {
   test('default render', () => {
-    const { container } = renderAlert('test example string', {})
+    const { container } = renderInlineAlert('test example string')
 
     expect(container).toMatchSnapshot()
-  })
-
-  describe('with `prop.onClose` is passed', () => {
-    test('calls `prop.onClose`', () => {
-      const onClose = jest.fn()
-      const { getByTitle } = renderAlert('test example string', { onClose })
-
-      const closeButton = getByTitle('Close alert')
-
-      fireEvent.click(closeButton)
-
-      expect(onClose).toHaveBeenCalledTimes(1)
-    })
   })
 })
