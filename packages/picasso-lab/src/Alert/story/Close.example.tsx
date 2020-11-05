f8b5e85712af3b89e8c8ce97b5bddd5eb41ef15a
Boris Yordanov <boris.yordanov@toptal.com>
2020-11-05 09:45:36 +0100

    feat: add Alert.Inline component (#1656)
    - Added Alert.Inline component
    - Refactored Alert to use v5 components

--- packages/picasso-lab/src/Alert/story/Close.example.tsx
+++ packages/picasso-lab/src/Alert/story/Close.example.tsx
@@ -7,8 +7,8 @@ const mockOnClose = () => {
 
 const Example = () => (
   <Alert onClose={mockOnClose}>
-    The time zone in your profile is set to (UTC -08:00) America - Los Angeles,
-    but we’ve detected a change to (UTC -03:00) America - Cordoba.
+    The time zone in your profile is set to (UTC-08:00) America - Los Angeles,
+    but we’ve detected a change to (UTC-03:00) America - Cordoba.
   </Alert>
 )
 
