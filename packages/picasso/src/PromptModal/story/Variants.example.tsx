92b89b30f174dea88605b3c4994269bd6f60bc2b
Henrique Schreiner <henrique.schreiner@toptal.com>
2020-11-09 17:38:14 -0300

    fix(PromptModal): update variants

--- packages/picasso/src/PromptModal/story/Variants.example.tsx
+++ packages/picasso/src/PromptModal/story/Variants.example.tsx
@@ -4,73 +4,42 @@ import { useNotifications, useModal } from '@toptal/picasso/utils'
 
 const PromptModalDefaultExample = () => {
   const {
-    showModal: showModalDefault,
-    hideModal: hideModalDefault,
-    isOpen: isOpenDefault
+    showModal: showModalPositive,
+    hideModal: hideModalPositive,
+    isOpen: isOpenPositive
   } = useModal()
+
   const {
-    showModal: showModalRed,
-    hideModal: hideModalRed,
-    isOpen: isOpenRed
+    showModal: showModalNegative,
+    hideModal: hideModalNegative,
+    isOpen: isOpenNegative
   } = useModal()
-  const {
-    showModal: showModalBlue,
-    hideModal: hideModalBlue,
-    isOpen: isOpenBlue
-  } = useModal()
-  const {
-    showModal: showModalGreen,
-    hideModal: hideModalGreen,
-    isOpen: isOpenGreen
-  } = useModal()
+
   const { showInfo } = useNotifications()
 
   return (
     <div id='modal-container' style={{ width: '400px', height: '50px' }}>
       <Container flex>
-        <Button onClick={showModalDefault}>Open default prompt</Button>
+        <Button onClick={showModalPositive}>Open positive</Button>
         <PromptModal
-          open={isOpenDefault}
-          title='Default variant'
-          message='This is default variant.'
+          variant='positive'
+          open={isOpenPositive}
+          title='Positive variant'
+          message='This is positive variant.'
           onSubmit={async () => showInfo('Submitted')}
-          onClose={hideModalDefault}
+          onClose={hideModalPositive}
           // for purpose of code example
           container={() => document.getElementById('modal-container')!}
         />
 
-        <Button onClick={showModalRed}>Open red prompt</Button>
+        <Button onClick={showModalNegative}>Open negative</Button>
         <PromptModal
-          variant='red'
-          open={isOpenRed}
-          title='Red variant'
-          message='This is red variant.'
+          variant='negative'
+          open={isOpenNegative}
+          title='Negative variant'
+          message='This is negative variant.'
           onSubmit={async () => showInfo('Submitted')}
-          onClose={hideModalRed}
-          // for purpose of code example
-          container={() => document.getElementById('modal-container')!}
-        />
-
-        <Button onClick={showModalBlue}>Open blue prompt</Button>
-        <PromptModal
-          variant='blue'
-          open={isOpenBlue}
-          title='Blue variant'
-          message='This is blue variant.'
-          onSubmit={async () => showInfo('Submitted')}
-          onClose={hideModalBlue}
-          // for purpose of code example
-          container={() => document.getElementById('modal-container')!}
-        />
-
-        <Button onClick={showModalGreen}>Open green prompt</Button>
-        <PromptModal
-          variant='green'
-          open={isOpenGreen}
-          title='Green variant'
-          message='This is green variant.'
-          onSubmit={async () => showInfo('Submitted')}
-          onClose={hideModalGreen}
+          onClose={hideModalNegative}
           // for purpose of code example
           container={() => document.getElementById('modal-container')!}
         />
