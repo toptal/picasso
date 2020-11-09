70165dc31bdc534f942cc9235dbf291b067a9574
Teimur Gasanov <teimur@toptal.com>
2020-11-09 19:22:05 +0600

    chore: refactor rendering columns

--- packages/picasso-lab/src/DetailedList/DetailedList.tsx
+++ packages/picasso-lab/src/DetailedList/DetailedList.tsx
@@ -30,6 +30,35 @@ export const DetailedList = forwardRef<HTMLTableElement, Props>(
     const classes = useStyles()
     const rowsCount = Math.round(items.length / 2)
 
+    const renderSingleColumn = () =>
+      items.map(item => (
+        <tr key={item.label} className={classes.row}>
+          <DetailedListItem label={item.label} value={item.value} />
+        </tr>
+      ))
+
+    const renderTwoColumns = () => {
+      const rowsIndices = generateSequence(rowsCount)
+
+      return rowsIndices.map(rowIndex => {
+        const item = items[rowIndex * 2]
+        const nextItem = items[rowIndex * 2 + 1]
+
+        return (
+          <tr key={item.label} className={classes.row}>
+            <DetailedListItem
+              label={item.label}
+              value={item.value}
+              fullWidth={!nextItem}
+            />
+            {nextItem && (
+              <DetailedListItem label={nextItem.label} value={nextItem.value} />
+            )}
+          </tr>
+        )
+      })
+    }
+
     return (
       <table
         // eslint-disable-next-line react/jsx-props-no-spreading
@@ -39,34 +68,7 @@ export const DetailedList = forwardRef<HTMLTableElement, Props>(
           [classes.stripped]: stripped
         })}
       >
-        <tbody>
-          {isSmall
-            ? items.map(item => (
-                <tr key={item.label} className={classes.row}>
-                  <DetailedListItem label={item.label} value={item.value} />
-                </tr>
-              ))
-            : generateSequence(rowsCount).map(rowIndex => {
-                const item = items[rowIndex * 2]
-                const nextItem = items[rowIndex * 2 + 1]
-
-                return (
-                  <tr key={item.label} className={classes.row}>
-                    <DetailedListItem
-                      label={item.label}
-                      value={item.value}
-                      fullWidth={!nextItem}
-                    />
-                    {nextItem && (
-                      <DetailedListItem
-                        label={nextItem.label}
-                        value={nextItem.value}
-                      />
-                    )}
-                  </tr>
-                )
-              })}
-        </tbody>
+        <tbody>{isSmall ? renderSingleColumn() : renderTwoColumns()}</tbody>
       </table>
     )
   }
