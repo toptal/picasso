d76b4ea604a7ef87a13b2d193306c96f0fa7d515
Boris Yordanov <boris.yordanov@toptal.com>
2020-11-13 15:00:20 +0100

    fix: skeletonloader exports

--- packages/picasso-lab/src/SkeletonLoader/index.ts
+++ packages/picasso-lab/src/SkeletonLoader/index.ts
@@ -1,6 +1,1 @@
-import { OmitInternalProps } from '@toptal/picasso-shared'
-
-import { StaticProps } from './SkeletonLoader'
-
 export { default } from './SkeletonLoader'
-export type SkeletonLoaderProps = OmitInternalProps<StaticProps>
