0a93d1230d81da5df204e61e9d32821e513a8e15
Teimur Gasanov <teymurgg321@gmail.com>
2020-11-17 17:16:27 +0600

    feat(Radio): [FX-1405] align with BASE 2.0 design
    fix(Autocomplete): remove checkmark and update padding (#1686)
    
    feat: [FX-1173] Add detailed list component
    
    - Added compound DetailedList component

---
+++ packages/picasso-lab/src/DetailedListItem/DetailedListItem.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react'
+
+export interface Props {
+  /** Item label */
+  label: ReactNode
+  /** Item value */
+  children: ReactNode
+}
+
+export const DetailedListItem = ({ children }: Props) => children
+
+DetailedListItem.defaultProps = {}
+DetailedListItem.displayName = 'DetailedListItem'
+
+export default DetailedListItem
