0cd069c5a21251a43b199701f4e38ffe9534e2ab
Teimur Gasanov <teymurgg321@gmail.com>
2020-11-18 00:54:12 +0600

    feat: [FX-1173] Add detailed list component
    - Added compound DetailedList component

---
+++ packages/picasso-lab/src/DetailedListColumn/index.ts
@@ -0,0 +1,7 @@
+import { OmitInternalProps } from '@toptal/picasso-shared'
+
+import { Props } from './DetailedListColumn'
+
+export type DetailedListColumnProps = OmitInternalProps<Props>
+
+export { default } from './DetailedListColumn'
