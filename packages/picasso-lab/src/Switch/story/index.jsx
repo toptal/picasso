61b018787edec2c8684a42791d7f54436e18be53
Romeo Copaciu <romeo.copaciu@toptal.com>
2020-11-18 17:49:46 +0200

    feat: [FX-1387] Add Switch component
    - Switch: new lab component based on Material UI Counterpart

---
+++ packages/picasso-lab/src/Switch/story/index.jsx
@@ -0,0 +1,20 @@
+import { Switch } from '../Switch'
+import PicassoBook from '~/.storybook/components/PicassoBook'
+
+const page = PicassoBook.section('Lab').createPage('Switch', null)
+
+page
+  .createTabChapter('Props')
+  .addComponentDocs({ component: Switch, name: 'Switch' })
+
+page
+  .createChapter()
+  .addExample('Switch/story/Uncontrolled.example.tsx', {
+    title: 'Uncontrolled',
+    description: 'Can control its state by itself'
+  })
+  .addExample('Switch/story/Controlled.example.tsx', {
+    title: 'Controlled',
+    description: 'Stateless Switch, state should be controlled using prop'
+  })
+  .addExample('Switch/story/Disabled.example.tsx', 'Disabled')
