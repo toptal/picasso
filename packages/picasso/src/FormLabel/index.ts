0f25bc8045fbc3618e153fc66f657726b06b18cb
Daniel Ostapenko <daniel.ostapenko@toptal.com>
2020-11-25 11:07:54 +0200

    chore: [FX-1376] Fix type export warning

--- packages/picasso/src/FormLabel/index.ts
+++ packages/picasso/src/FormLabel/index.ts
@@ -3,5 +3,5 @@ import { OmitInternalProps } from '@toptal/picasso-shared'
 import { Props } from './FormLabel'
 
 export { default } from './FormLabel'
-export { RequiredVariant } from './FormLabel'
+export type { RequiredVariant } from './FormLabel'
 export type FormLabelProps = OmitInternalProps<Props>
