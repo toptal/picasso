c5216d4a0967c6fd83231bc2d698d3733cc5b524
Vedran Ivanac <vedran.ivanac@toptal.com>
2020-11-26 11:02:24 +0100

    fix(Checkbox): fix CheckboxGroup with nested Checkbox (#1721)

--- packages/picasso-forms/src/Radio/Radio.tsx
+++ packages/picasso-forms/src/Radio/Radio.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Radio as PicassoRadio, RadioProps } from '@toptal/picasso'
 import { Field } from 'react-final-form'
+
+import { RadioGroupContext } from '../RadioGroup'
 
 // Intersection with the type { name?: string } is needed here because of
 // TS compiler issue https://github.com/microsoft/TypeScript/issues/34793
@@ -8,16 +10,21 @@ export type Props = RadioProps & {
   name?: string
 }
 
-const Radio = (props: Props) => (
-  <Field name={props.name!} type='radio' value={props.value}>
-    {({ input }) => (
-      <PicassoRadio
-        checked={input.checked}
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        {...props}
-      />
-    )}
-  </Field>
-)
+const Radio = ({ name, ...rest }: Props) => {
+  const groupName = useContext(RadioGroupContext)
+
+  return (
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    <Field name={name || groupName!} type='radio' value={rest.value}>
+      {({ input }) => (
+        <PicassoRadio
+          checked={input.checked}
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...rest}
+        />
+      )}
+    </Field>
+  )
+}
 
 export default Radio
