da2619801b88a2722c197d3af00926fd5a147264
Aleksandr Shumilov <alexander.shumilov@toptal.com>
2020-11-30 20:04:57 +0800

    feat(RectangularTag): [FX-1051] add Rectangular Tag component
    - Tag.Rectangular component was added

---
+++ packages/picasso/src/TagRectangular/test.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { render, PicassoConfig } from '@toptal/picasso/test-utils'
+import { OmitInternalProps } from '@toptal/picasso-shared'
+import * as titleCaseModule from 'ap-style-title-case'
+
+import TagRectangular, { Props } from './TagRectangular'
+
+jest.mock('ap-style-title-case')
+
+const renderTag = (
+  children: string,
+  { titleCase }: OmitInternalProps<Props, 'children'>,
+  picassoConfig?: PicassoConfig
+) =>
+  render(
+    <TagRectangular titleCase={titleCase}>{children}</TagRectangular>,
+    undefined,
+    picassoConfig
+  )
+
+// Mock console error since toThrow() outputs the error message with stacktrace
+let mockedConsoleError: jest.SpyInstance
+let spiedOnTitleCase: jest.SpyInstance
+
+beforeEach(() => {
+  spiedOnTitleCase = jest.spyOn(titleCaseModule, 'default')
+  mockedConsoleError = jest.spyOn(console, 'error')
+  mockedConsoleError.mockImplementation(() => {})
+})
+afterEach(() => {
+  spiedOnTitleCase.mockReset()
+  mockedConsoleError.mockRestore()
+})
+
+describe('TagRectangular', () => {
+  it('renders rectangular Tag', () => {
+    const { container } = renderTag('Reactangular Tag', {})
+
+    expect(container).toMatchSnapshot()
+  })
+
+  describe('when Picasso titleCase property is true', () => {
+    it('transforms text to title case', () => {
+      const TEXT_CONTENT = 'Test ld7'
+
+      renderTag(TEXT_CONTENT, {}, { titleCase: true })
+
+      expect(spiedOnTitleCase).toBeCalledWith(TEXT_CONTENT)
+    })
+  })
+
+  describe('when Picasso titleCase property is true but the component property overrides it', () => {
+    it('does not transform text to title case ', () => {
+      renderTag('test pf9', { titleCase: false }, { titleCase: true })
+      expect(spiedOnTitleCase).toBeCalledTimes(0)
+    })
+  })
+})
