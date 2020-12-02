525d74698797b80d95ce848302229db6f5079ffa
Boris Yordanov <boris.yordanov@toptal.com>
2020-12-02 13:39:31 +0200

    feat(PageBanner): use Notification component as base (#1739)
    BREAKING CHANGE
    - variant prop removed
    - icon prop removed
    - PageBanner.Message removed

--- packages/picasso/src/PageBanner/PageBanner.tsx
+++ packages/picasso/src/PageBanner/PageBanner.tsx
@@ -1,94 +1,40 @@
-import React, {
-  ReactNode,
-  forwardRef,
-  HTMLAttributes,
-  ReactElement,
-  useContext,
-  FunctionComponent
-} from 'react'
-import { Theme, makeStyles } from '@material-ui/core/styles'
-import cx from 'classnames'
+import React, { ReactNode, forwardRef, HTMLAttributes } from 'react'
 import { BaseProps, CompoundedComponentWithRef } from '@toptal/picasso-shared'
 
-import Container, { VariantType } from '../Container'
-import styles from './styles'
-import { PageContextProps } from '../Page/types'
-import { PageContext } from '../Page/Page'
+import Notification from '../Notification'
+import NotificationActions from '../NotificationActions'
 
 export interface Props extends BaseProps, HTMLAttributes<HTMLDivElement> {
   /** Children components */
   children: ReactNode
-  /** Color variant of Banner */
-  variant?: VariantType
-  /** Add <Icon /> before Banner content  */
-  icon?: ReactElement
 }
 
 export interface StaticProps {
-  Message: FunctionComponent
-  Actions: FunctionComponent
-}
-
-const useStyles = makeStyles<Theme, Props>(styles, {
-  name: 'PicassoPageBanner'
-})
-
-const useActionStyles = makeStyles<Theme>(styles, {
-  name: 'PicassoPageBannerActions'
-})
-
-const Message: FunctionComponent = ({ children }) => <div>{children}</div>
-const Actions: FunctionComponent = props => {
-  const classes = useActionStyles(props)
-
-  return <div className={classes.actions}>{props.children}</div>
+  Actions: typeof NotificationActions
 }
 
 export const PageBanner = forwardRef<HTMLDivElement, Props>(function PageBanner(
   props,
   ref
 ) {
-  const { className, style, children, variant, icon, ...rest } = props
-  const classes = useStyles(props)
-  const { width, fullWidth } = useContext<PageContextProps>(PageContext)
-
-  const innerClassName = cx(
-    {
-      [classes.fullWidth]: fullWidth || width === 'full',
-      [classes.wide]: width === 'wide'
-    },
-    classes.content
-  )
+  const { children, className, ...rest } = props
 
   return (
-    <Container
+    <Notification
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...rest}
       ref={ref}
-      className={cx(classes.root, className)}
-      style={style}
-      variant={variant}
-      flex
+      className={className}
     >
-      <Container variant={variant} className={innerClassName} flex>
-        {icon && (
-          <Container flex alignItems='center' className={classes.iconWrapper}>
-            {icon}
-          </Container>
-        )}
-        <Container className={classes.main}>{children}</Container>
-      </Container>
-    </Container>
+      {children}
+    </Notification>
   )
 }) as CompoundedComponentWithRef<Props, HTMLElement, StaticProps>
 
-PageBanner.defaultProps = {
-  variant: 'yellow'
-}
+PageBanner.defaultProps = {}
 
 PageBanner.displayName = 'PageBanner'
 
-PageBanner.Message = Message
-PageBanner.Actions = Actions
+PageBanner.Actions = NotificationActions
 
 export default PageBanner
