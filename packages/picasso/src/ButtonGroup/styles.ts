93e252c343c25baf3f640fc71bc3555a27294f8a
Vedran Ivanac <vedran.ivanac@toptal.com>
2020-12-04 10:23:01 +0100

    fix(ButtonGroup): add comments

--- packages/picasso/src/ButtonGroup/styles.ts
+++ packages/picasso/src/ButtonGroup/styles.ts
@@ -4,6 +4,8 @@ const baseButtonProps = {
   transitionProperty: 'color, background',
 
   '&:active, &$active, &:hover, &$hovered, &:focus, &$focused': {
+    // border overlap to keep proper border width, but on state change
+    // we need to move up overlapped border
     zIndex: 1
   }
 }
@@ -16,12 +18,14 @@ const firstButtonProps = {
 
 const middleButtonProps = {
   borderRadius: 0,
+  // prevents border between 2 buttons to be wider then specified
   marginLeft: '-1px'
 }
 
 const lastButtonProps = {
   borderTopLeftRadius: 0,
   borderBottomLeftRadius: 0,
+  // prevents border between 2 buttons to be wider then specified
   marginLeft: '-1px'
 }
 
@@ -31,24 +35,31 @@ export default () =>
       display: 'flex',
       justifyContent: 'flex-start',
 
+      // default case
       '& [data-component-type="button"]': {
         ...baseButtonProps,
 
+        // first item
         '&:first-child:not(:last-child)': firstButtonProps,
+        // middle item
         '&:not(:first-child):not(:last-child)': middleButtonProps,
+        // last item
         '&:last-child:not(:first-child)': lastButtonProps
       },
 
+      // nested case when button is nested in first item in ButtonGroup
       '& :first-child:not(:last-child) [data-component-type="button"]': {
         ...baseButtonProps,
         ...firstButtonProps
       },
 
+      // nested case when button is nested in middle item in ButtonGroup
       '& :not(:first-child):not(:last-child) [data-component-type="button"]': {
         ...baseButtonProps,
         ...middleButtonProps
       },
 
+      // nested case when button is nested in last item in ButtonGroup
       '& :last-child:not(:first-child) [data-component-type="button"]': {
         ...baseButtonProps,
         ...lastButtonProps
