d6a549e96e951b2ff85a18833a12f0b6cfb87101
Uladzimir Havenchyk <uladzimir@toptal.com>
2020-12-09 19:55:48 +0300

    feat(lab): rename Subheader to PageHead (#1764)

---
+++ packages/picasso-lab/src/PageHead/index.ts
@@ -0,0 +1,6 @@
+import { OmitInternalProps } from '@toptal/picasso-shared'
+
+import { Props } from './PageHead'
+
+export { default } from './PageHead'
+export type PageHeadProps = OmitInternalProps<Props>
