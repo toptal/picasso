47a7c94b3de54fea6c989a87d40dafcd43131a30
Sergiu Butnarasu <sergiu.butnarasu@toptal.com>
2020-12-14 16:16:36 +0200

    chore: add test script

--- packages/picasso-lab/src/TypographyOverflow/story/index.jsx
+++ packages/picasso-lab/src/TypographyOverflow/story/index.jsx
@@ -52,3 +52,20 @@ page
       await makeScreenshot()
     }
   })
+  .addExample('TypographyOverflow/story/WithDelay.example.tsx', {
+    title: 'With delay',
+    effect: async (testPage, makeScreenshot) => {
+      /**
+       * TODO: Revert to testPage.hover once the issue below is fixed
+       * https://github.com/puppeteer/puppeteer/issues/4820
+       */
+      // When ellipsed text is hovered then tooltip should appear
+      await testPage.click('[data-testid="default-delay-tooltip"]')
+      await testPage.waitFor(300)
+      await makeScreenshot()
+
+      await testPage.click('[data-testid="long-delay-tooltip"]')
+      await testPage.waitFor(500)
+      await makeScreenshot()
+    }
+  })
