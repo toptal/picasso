94719a4faaf11e309272a5a9587ce020d39e2162
Leonardo Cristofani <leonardo.cristofani@toptal.com>
2020-12-14 09:50:11 -0300

    fix: select component after search (#1773)

--- packages/picasso/src/Select/useSelect.ts
+++ packages/picasso/src/Select/useSelect.ts
@@ -154,16 +154,12 @@ const useSelect = ({
     onSelect(event, item)
   }
 
-  const handleItemOnMouseDown = useCallback(
-    (event: React.MouseEvent) => {
-      // This prevents the activeElement from being changed
-      // to the item so it can remain with the current activeElement
-      // which is a more common use case.
-      event.preventDefault()
-      focusRef(selectRef)
-    },
-    [selectRef]
-  )
+  const handleItemOnMouseDown = useCallback((event: React.MouseEvent) => {
+    // This prevents the activeElement from being changed
+    // to the item so it can remain with the current activeElement
+    // which is a more common use case.
+    event.preventDefault()
+  }, [])
 
   const close = useCallback(() => {
     setOpen(false)
