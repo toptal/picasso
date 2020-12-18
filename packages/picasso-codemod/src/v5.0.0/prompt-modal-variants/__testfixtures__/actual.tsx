0c9af9b479898f0ce391d1bc82571d3ac73cf151
Teimur Gasanov <teimur@toptal.com>
2020-12-18 18:27:15 +0600

    feat: [FX-1541] add codemod package
    - Added initial codemod example.

---
+++ packages/picasso-codemod/src/v5.0.0/prompt-modal-variants/__testfixtures__/actual.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable */
+// @ts-nocheck
+import React from 'react'
+import { PromptModal } from '@toptal/picasso'
+
+const defaultProps = {
+  title: 'Test title',
+  message: 'Test message',
+  open: true,
+  onSubmit: () => {}
+}
+
+export default () => (
+  <>
+    <PromptModal {...defaultProps} variant='red' />
+    <PromptModal {...defaultProps} variant='green' />
+    <PromptModal {...defaultProps} variant='blue' />
+    <PromptModal {...defaultProps} variant='negative' />
+    <PromptModal {...defaultProps} variant='positive' />
+    <PromptModal {...defaultProps} />
+  </>
+)
---
+++ packages/picasso-codemod/src/v5.0.0/prompt-modal-variants/__testfixtures__/expected.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable */
+// @ts-nocheck
+import React from 'react'
+import { PromptModal } from '@toptal/picasso'
+
+const defaultProps = {
+  title: 'Test title',
+  message: 'Test message',
+  open: true,
+  onSubmit: () => {}
+}
+
+export default () => (
+  <>
+    <PromptModal {...defaultProps} variant='negative' />
+    <PromptModal {...defaultProps} variant='positive' />
+    <PromptModal {...defaultProps} variant='positive' />
+    <PromptModal {...defaultProps} variant='negative' />
+    <PromptModal {...defaultProps} variant='positive' />
+    <PromptModal {...defaultProps} />
+  </>
+)
