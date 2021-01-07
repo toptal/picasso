473b9c5cc7107175b4d49f870e4e71a729e2f668
Teimur Gasanov <teimur@toptal.com>
2021-01-07 23:07:03 +0600

    chore: refactor trigger example

--- packages/picasso/src/Tooltip/story/Trigger.example.tsx
+++ packages/picasso/src/Tooltip/story/Trigger.example.tsx
@@ -5,8 +5,8 @@ import { ClickAwayListener } from '@toptal/picasso/utils'
 const Example = () => {
   const [open, setOpen] = useState(false)
 
-  const handleTooltipOpen = () => setOpen(true)
-  const handleTooltipClose = () => setOpen(false)
+  const toogleTooltipOpen = () => setOpen(!open)
+  const closeTooltip = () => setOpen(false)
 
   return (
     <div style={{ textAlign: 'center' }}>
@@ -15,10 +15,10 @@ const Example = () => {
           <Button>Hover</Button>
         </Tooltip>
       </Container>
-      <ClickAwayListener onClickAway={handleTooltipClose}>
+      <ClickAwayListener onClickAway={closeTooltip}>
         <Container top='large' bottom='large' left='large' right='large' inline>
           <Tooltip open={open} content='Some content...' placement='top'>
-            <Button onClick={handleTooltipOpen}>Click</Button>
+            <Button onClick={toogleTooltipOpen}>Click</Button>
           </Tooltip>
         </Container>
       </ClickAwayListener>
