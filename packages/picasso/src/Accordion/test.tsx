26a73bfc5c06b542c441c4de4000ae7ea15f6e84
Teimur Gasanov <teimur@toptal.com>
2021-01-08 12:08:55 +0600

    test(Accordion): update accordion tests

--- packages/picasso/src/Accordion/test.tsx
+++ packages/picasso/src/Accordion/test.tsx
@@ -4,63 +4,78 @@ import { render, fireEvent } from '@toptal/picasso/test-utils'
 
 import Accordion from './Accordion'
 
-const summaryHeaderText = 'Fryderyk Chopin'
-const Summary = () => <div>{summaryHeaderText}</div>
+const exampleContent =
+  'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta harum explicabo assumenda doloribus voluptatum cum culpa! Molestias voluptatem placeat at velit vero animi, nostrum culpa, unde aliquam magnam libero eius.'
+const exampleSummary = 'Lorem ipsum'
 
-const Details = () => (
-  <div id='details'>
-    Fryderyk Chopin was born in Żelazowa Wola, 46 kilometres west of Warsaw, in
-    what was then the Duchy of Warsaw, a Polish state established by Napoleon.
-    The parish baptismal record gives his birthday as 22 February 1810, and
-    cites his given names in the Latin form Fridericus Franciscus (in Polish, he
-    was Fryderyk Franciszek). However, the composer and his family used the
-    birthdate 1 March, which is now generally accepted as the correct date.
-  </div>
-)
-
-describe('default version for sections', () => {
-  test('should render default version', () => {
+describe('Accordion', () => {
+  test('should render successfully', () => {
     const { container } = render(
-      <Accordion content={<Details />}>
-        <Summary />
-      </Accordion>
+      <Accordion content={exampleContent}>{exampleSummary}</Accordion>
     )
 
     expect(container).toMatchSnapshot()
   })
 
-  test('should render expanded version after click on summary', () => {
-    const { getByText, container } = render(
-      <Accordion content={<Details />}>
-        <Summary />
-      </Accordion>
-    )
-    const summary = getByText(summaryHeaderText)
-
-    fireEvent.click(summary)
-
-    expect(container).toMatchSnapshot()
-  })
-})
-
-describe('controlled version', () => {
-  test('should render expanded version', () => {
-    const { container } = render(
-      <Accordion content={<Details />} expanded>
-        <Summary />
-      </Accordion>
-    )
+  test('should render none when no summary', () => {
+    const { container } = render(<Accordion content={exampleContent} />)
 
     expect(container).toMatchSnapshot()
   })
 
-  test('should render collapsed version', () => {
-    const { container } = render(
-      <Accordion content={<Details />} expanded={false}>
-        <Summary />
+  test('should render expanded initially', async () => {
+    const { getByText } = render(
+      <Accordion content={exampleContent} defaultExpanded>
+        {exampleSummary}
       </Accordion>
     )
 
-    expect(container).toMatchSnapshot()
+    const content = getByText(exampleContent)
+
+    expect(content).toBeVisible()
+  })
+
+  test('should render custom icon when passed', () => {
+    const { getByTestId } = render(
+      <Accordion
+        content={exampleContent}
+        expandIcon={<span data-testid='custom-expand-icon' />}
+      >
+        {exampleSummary}
+      </Accordion>
+    )
+
+    expect(getByTestId('custom-expand-icon')).toBeInTheDocument()
+  })
+
+  test('should be controlled', async () => {
+    const { getByText, rerender } = render(
+      <Accordion content={exampleContent} expanded={false}>
+        {exampleSummary}
+      </Accordion>
+    )
+
+    expect(getByText(exampleContent)).not.toBeVisible()
+
+    rerender(
+      <Accordion content={exampleContent} expanded>
+        {exampleSummary}
+      </Accordion>
+    )
+
+    expect(getByText(exampleContent)).toBeVisible()
+  })
+
+  test('should fire onChange', () => {
+    const handleChange = jest.fn()
+    const { getByText } = render(
+      <Accordion content={exampleContent} onChange={handleChange}>
+        {exampleSummary}
+      </Accordion>
+    )
+
+    fireEvent.click(getByText(exampleSummary))
+
+    expect(handleChange).toBeCalledTimes(1)
   })
 })
