4aa1b43a8d3b0ef55b50cfc49b50c4e52d24be5e
Teimur Gasanov <teimur@toptal.com>
2021-01-08 18:23:31 +0600

    feat(picasso-codemod): add accordion borders codemod (#1831)

---
+++ packages/picasso-codemod/src/v5.0.0/accordion-borders/__testfixtures__/basic.input.tsx
@@ -0,0 +1,19 @@
+// @ts-nocheck
+import React from 'react'
+import { Accordion } from '@toptal/picasso'
+
+export default () => (
+  <>
+    <Accordion content='Accordion content' bordered>
+      Summary
+    </Accordion>
+    {/* eslint-disable-next-line react/jsx-boolean-value */}
+    <Accordion content='Accordion content' bordered={true}>
+      Summary
+    </Accordion>
+    <Accordion content='Accordion content' bordered={false}>
+      Summary
+    </Accordion>
+    <Accordion content='Accordion content'>Summary</Accordion>
+  </>
+)
