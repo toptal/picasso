00174352df67b102a2e01582d836f6780ea2e8e8
Teimur Gasanov <teimur@toptal.com>
2021-01-08 12:08:55 +0600

    test(Accordion): update accordion tests

--- packages/picasso/src/test-utils/index.tsx
+++ packages/picasso/src/test-utils/index.tsx
@@ -11,11 +11,21 @@ const customRender = (
   ui: ReactElement,
   options?: Omit<RenderOptions, 'queries'>,
   picassoConfig?: PicassoConfig
-): RenderResult =>
-  render(
+): RenderResult => {
+  const { rerender, ...rest } = render(
     <TestingPicasso titleCase={picassoConfig?.titleCase}>{ui}</TestingPicasso>,
     options
   )
+
+  const customRerender: typeof rerender = newUi =>
+    rerender(
+      <TestingPicasso titleCase={picassoConfig?.titleCase}>
+        {newUi}
+      </TestingPicasso>
+    )
+
+  return { rerender: customRerender, ...rest }
+}
 
 export * from '@testing-library/react'
 export { customRender as render, TestingPicasso }
