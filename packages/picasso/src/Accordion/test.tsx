a79a06ecda9192a723272e52268a24a0d85cdfa8
Teimur Gasanov <teimur@toptal.com>
2021-01-11 16:05:37 +0600

    chore: add disabled state tests

--- packages/picasso/src/Accordion/test.tsx
+++ packages/picasso/src/Accordion/test.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-multi-comp */
 import React from 'react'
-import { render, fireEvent } from '@toptal/picasso/test-utils'
+import { render, fireEvent, wait } from '@toptal/picasso/test-utils'
 
 import Accordion from './Accordion'
 
@@ -23,16 +23,18 @@ describe('Accordion', () => {
     expect(container).toMatchSnapshot()
   })
 
-  test('renders expanded initially', () => {
+  test('renders expanded initially', async () => {
     const { getByText } = render(
       <Accordion content={exampleContent} defaultExpanded>
         {exampleSummary}
       </Accordion>
     )
 
-    const content = getByText(exampleContent)
+    expect(getByText(exampleContent)).toBeVisible()
 
-    expect(content).toBeVisible()
+    fireEvent.click(getByText(exampleSummary))
+
+    await wait(() => expect(getByText(exampleContent)).not.toBeVisible())
   })
 
   test('renders custom icon when passed', () => {
@@ -66,7 +68,7 @@ describe('Accordion', () => {
     expect(getByText(exampleContent)).toBeVisible()
   })
 
-  test('fires onChange', () => {
+  test('toggles', async () => {
     const handleChange = jest.fn()
     const { getByText, getByTestId } = render(
       <Accordion
@@ -79,8 +81,28 @@ describe('Accordion', () => {
     )
 
     fireEvent.click(getByText(exampleSummary))
+
+    await wait(() => expect(getByText(exampleContent)).toBeVisible())
+
     fireEvent.click(getByTestId('trigger'))
 
+    await wait(() => expect(getByText(exampleContent)).not.toBeVisible())
+
     expect(handleChange).toBeCalledTimes(2)
+  })
+
+  test('does not toggle when disabled', async () => {
+    const handleChange = jest.fn()
+    const { getByText, container } = render(
+      <Accordion content={exampleContent} onChange={handleChange} disabled>
+        {exampleSummary}
+      </Accordion>
+    )
+
+    // Disabled state makes the parent container non-clickable, so click can't be passed to the children
+    // However clicking the children via Javascript will toggle the accordion.
+    fireEvent.click(container)
+    expect(getByText(exampleContent)).not.toBeVisible()
+    expect(handleChange).toBeCalledTimes(0)
   })
 })
