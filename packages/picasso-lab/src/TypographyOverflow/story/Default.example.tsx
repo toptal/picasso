d9479d2f48cdf902a6fc53e3f43df4d28a517d5b
Ilia Nemiro <67374194+inemiro@users.noreply.github.com>
2021-01-13 12:16:25 +0300

    fix(TypographyOverflow): tooltip triggers (#1834)
    * fix: fix Typography overflow tooltip

--- packages/picasso-lab/src/TypographyOverflow/story/Default.example.tsx
+++ packages/picasso-lab/src/TypographyOverflow/story/Default.example.tsx
@@ -3,11 +3,26 @@ import { TypographyOverflow } from '@toptal/picasso-lab'
 
 const Example = () => {
   return (
-    <div style={{ width: 300, marginTop: 100 }}>
-      <TypographyOverflow data-testid='ellipsed-text'>
-        This typography is very long and therefore it overflows.
-      </TypographyOverflow>
-    </div>
+    <>
+      <div style={{ width: 300, marginTop: 100 }}>
+        <TypographyOverflow data-testid='ellipsed-text'>
+          This typography is very long and therefore it overflows.
+        </TypographyOverflow>
+      </div>
+      <div
+        style={{
+          width: '50%',
+          paddingRight: '20px',
+          resize: 'horizontal',
+          overflow: 'auto',
+          borderRight: '3px solid black'
+        }}
+      >
+        <TypographyOverflow data-testid='ellipsed-text-dynamic-width'>
+          Another typography is very long and has dynamic width. Try to resize.
+        </TypographyOverflow>
+      </div>
+    </>
   )
 }
 
