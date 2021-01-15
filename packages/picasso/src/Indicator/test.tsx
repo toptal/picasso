1022428a35320588e5b5ddee44952087f4dade37
Teimur Gasanov <teimur@toptal.com>
2021-01-15 12:24:56 +0600

    chore: revert indicator test

--- packages/picasso/src/Indicator/test.tsx
+++ packages/picasso/src/Indicator/test.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { render } from '@toptal/picasso/test-utils'
 import { OmitInternalProps } from '@toptal/picasso-shared'
 
 import Indicator, { Props } from './Indicator'
 
-const renderIndicator = (props: OmitInternalProps<Props>) => {
+const renderIndicator = (
+  children: ReactNode,
+  props: OmitInternalProps<Props>
+) => {
   const { color } = props
 
-  return render(<Indicator color={color} />)
+  return render(<Indicator color={color}>{children}</Indicator>)
 }
 
 describe('Indicator', () => {
   test('default render', () => {
-    const { container } = renderIndicator({ color: 'red' })
+    const { container } = renderIndicator(null, { color: 'red' })
 
     expect(container).toMatchSnapshot()
   })
