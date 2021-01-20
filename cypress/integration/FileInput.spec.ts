c02af6a167d21142999a8d0754f3e70d7b11941f
OleksandrNechai <oleksandr.nechai@toptal.com>
2021-01-20 19:08:35 +0200

    fix(FileInput): not opening file picker

---
+++ cypress/integration/FileInput.spec.ts
@@ -0,0 +1,21 @@
+describe('FileInput', () => {
+  beforeEach(() => {
+    cy.visit('iframe.html?id=fileinput--default')
+  })
+
+  it('opens file picker', () => {
+    const openFileDialog = cy.stub().as('openFileDialog')
+
+    cy.get('input[type="file"]')
+      .first()
+      .should($input => {
+        $input.on('click', openFileDialog)
+      })
+
+    cy.get('button')
+      .first()
+      .click()
+      // @ts-ignore
+      .should(() => expect(openFileDialog).to.be.called)
+  })
+})
