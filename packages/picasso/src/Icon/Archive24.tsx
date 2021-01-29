0db836f6cb82c2265aa8caa946ead4aca534896e
Ilia Nemiro <67374194+inemiro@users.noreply.github.com>
2021-01-29 15:21:58 +0300

    feat(Icon): add Archive icon (#1873)
    Co-authored-by: Uladzimir Havenchyk <uladzimir@toptal.com>

---
+++ packages/picasso/src/Icon/Archive24.tsx
@@ -0,0 +1,50 @@
+import React, { forwardRef, Ref } from 'react'
+import cx from 'classnames'
+import { makeStyles } from '@material-ui/core/styles'
+import { StandardProps } from '@toptal/picasso-shared'
+
+import kebabToCamelCase from '../utils/kebab-to-camel-case'
+import styles from './styles'
+const BASE_SIZE = 24
+
+type ScaleType = 1 | 2 | 3 | 4
+export interface Props extends StandardProps {
+  scale?: ScaleType
+  color?: string
+  base?: number
+}
+const useStyles = makeStyles(styles, { name: 'PicassoSvgArchive24' })
+const SvgArchive24 = forwardRef(function SvgArchive24(
+  props: Props,
+  ref: Ref<SVGSVGElement>
+) {
+  const { className, style = {}, color, scale, base } = props
+  const classes: Record<string, string> = useStyles(props)
+  const classNames = [classes.root, className]
+  const scaledSize = base || BASE_SIZE * Math.ceil(scale || 1)
+  const colorClassName = kebabToCamelCase(`${color}`)
+
+  if (classes[colorClassName]) {
+    classNames.push(classes[colorClassName])
+  }
+
+  const svgStyle = {
+    minWidth: `${scaledSize}px`,
+    minHeight: `${scaledSize}px`,
+    ...style
+  }
+
+  return (
+    <svg
+      viewBox='0 0 24 24'
+      className={cx(...classNames)}
+      style={svgStyle}
+      ref={ref}
+    >
+      <path d='M24 2v5h-1v15H1V7H0V2h24zm-2 5H2v14h20V7zm-7 4v1H9v-1h6zm8-8H1v3h22V3z' />
+    </svg>
+  )
+})
+
+SvgArchive24.displayName = 'SvgArchive24'
+export default SvgArchive24
