9694489baff8496907209a28e2f749fddb749d38
inemiro <ilia.nemiro@toptal.com>
2021-01-29 12:16:57 +0300

    feat(Icon): add Archive icon

---
+++ packages/picasso/src/Icon/Archive16.tsx
@@ -0,0 +1,50 @@
+import React, { forwardRef, Ref } from 'react'
+import cx from 'classnames'
+import { makeStyles } from '@material-ui/core/styles'
+import { StandardProps } from '@toptal/picasso-shared'
+
+import kebabToCamelCase from '../utils/kebab-to-camel-case'
+import styles from './styles'
+const BASE_SIZE = 16
+
+type ScaleType = 1 | 2 | 3 | 4
+export interface Props extends StandardProps {
+  scale?: ScaleType
+  color?: string
+  base?: number
+}
+const useStyles = makeStyles(styles, { name: 'PicassoSvgArchive16' })
+const SvgArchive16 = forwardRef(function SvgArchive16(
+  props: Props,
+  ref: Ref<SVGSVGElement>
+) {
+  const { className, style = {}, color, scale, base } = props
+  const classes: Record<string, string> = useStyles(props)
+  const classNames = [classes.root, className]
+  const scaledSize = base || BASE_SIZE * Math.ceil(scale || 1)
+  const colorClassName = kebabToCamelCase(`${color}`)
+
+  if (classes[colorClassName]) {
+    classNames.push(classes[colorClassName])
+  }
+
+  const svgStyle = {
+    minWidth: `${scaledSize}px`,
+    minHeight: `${scaledSize}px`,
+    ...style
+  }
+
+  return (
+    <svg
+      viewBox='0 0 16 16'
+      className={cx(...classNames)}
+      style={svgStyle}
+      ref={ref}
+    >
+      <path d='M16 1v4h-1v10H1V5H0V1h16zm-2 4H2v9h12V5zm-4 2v1H6V7h4zm5-5H1v2h14V2z' />
+    </svg>
+  )
+})
+
+SvgArchive16.displayName = 'SvgArchive16'
+export default SvgArchive16
