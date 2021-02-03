c17f4cd94ce5891e94e45129057b3550b13c1d7b
Vova Kulikov <vova.kulikov@toptal.com>
2021-02-03 14:23:23 +0300

    feat(InputAdornment): add stopPropagation prop (#1883)

--- packages/picasso/src/InputAdornment/InputAdornment.tsx
+++ packages/picasso/src/InputAdornment/InputAdornment.tsx
@@ -1,10 +1,17 @@
-import React, { ReactNode, FunctionComponent, HTMLAttributes } from 'react'
+import React, {
+  ReactNode,
+  FunctionComponent,
+  HTMLAttributes,
+  MouseEvent,
+  useCallback
+} from 'react'
 import { makeStyles, Theme } from '@material-ui/core/styles'
 import MUIInputAdornment from '@material-ui/core/InputAdornment'
 import cx from 'classnames'
 import { BaseProps } from '@toptal/picasso-shared'
 
 import styles from './styles'
+import noop from '../utils/noop'
 
 type PositionType = 'start' | 'end'
 
@@ -13,6 +20,7 @@ export interface Props extends BaseProps, HTMLAttributes<HTMLDivElement> {
   position: PositionType
   disabled?: boolean
   disablePointerEvents?: boolean
+  stopPropagation?: boolean
 }
 
 const useStyles = makeStyles<Theme>(styles, {
@@ -27,10 +35,22 @@ const InputAdornment: FunctionComponent<Props> = props => {
     position,
     disabled,
     disablePointerEvents,
+    stopPropagation,
+    onClick = noop,
     ...rest
   } = props
 
   const classes = useStyles()
+  const handleClick = useCallback(
+    (event: MouseEvent<HTMLDivElement>) => {
+      if (stopPropagation) {
+        event.stopPropagation()
+      }
+
+      onClick(event)
+    },
+    [onClick, stopPropagation]
+  )
 
   return (
     <MUIInputAdornment
@@ -45,6 +65,7 @@ const InputAdornment: FunctionComponent<Props> = props => {
       style={style}
       position={position}
       disablePointerEvents={disablePointerEvents}
+      onClick={handleClick}
     >
       {children}
     </MUIInputAdornment>
