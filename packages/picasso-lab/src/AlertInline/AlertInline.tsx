b8705bdde68434a484773855b1886068ee5a927e
Daniel Ostapenko <daniel.ostapenko@toptal.com>
2021-02-04 10:13:26 +0200

    fix(Alert): icon padding for Alert Block

--- packages/picasso-lab/src/AlertInline/AlertInline.tsx
+++ packages/picasso-lab/src/AlertInline/AlertInline.tsx
@@ -1,6 +1,6 @@
 import React, { forwardRef, ReactNode, HTMLAttributes } from 'react'
 import cx from 'classnames'
-import { BaseProps } from '@toptal/picasso-shared'
+import { BaseProps, SizeType } from '@toptal/picasso-shared'
 import {
   Container,
   Typography,
@@ -23,6 +23,7 @@ export interface Props extends BaseProps, HTMLAttributes<HTMLDivElement> {
   children?: ReactNode
   /** Style variant of Alert */
   variant?: VariantType
+  iconPadding?: SizeType<'xsmall' | 'small'> // undocumented prop, only for internal usage
 }
 
 export const renderAlertIcon = (variant?: VariantType) => {
@@ -50,12 +51,12 @@ export const AlertInline = forwardRef<HTMLDivElement, Props>(function Alert(
   ref
 ) {
   const classes = useStyles()
-  const { variant, children, className } = props
+  const { variant, children, className, iconPadding } = props
 
   return (
     <Container inline flex ref={ref} className={classes.root}>
       <Container
-        right='xsmall'
+        right={iconPadding}
         flex
         alignItems='center'
         className={classes.iconWrapper}
@@ -75,7 +76,8 @@ export const AlertInline = forwardRef<HTMLDivElement, Props>(function Alert(
 })
 
 AlertInline.defaultProps = {
-  variant: 'yellow'
+  variant: 'yellow',
+  iconPadding: 'xsmall'
 }
 
 AlertInline.displayName = 'AlertInline'
