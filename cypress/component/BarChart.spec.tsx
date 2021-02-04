db79cc6eea31a1a504371a9b9f6823c4b63802de
Boris Yordanov <boris.yordanov@toptal.com>
2021-02-04 18:37:51 -0400

    test(BarChart): finish refactor

--- cypress/component/BarChart.spec.tsx
+++ cypress/component/BarChart.spec.tsx
@@ -1,10 +1,12 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import React from 'react'
 import { mount } from '@cypress/react'
+import { Container, Paper, Typography } from '@toptal/picasso'
 import { BarChart } from '@toptal/picasso-charts'
 import { TestingPicasso } from '@toptal/picasso/test-utils'
 import { palette } from '@toptal/picasso/utils'
 
-const CHART_DATA = [
+const CHART_DATA_DEFAULT = [
   {
     name: 'Apple',
     value: { 'engineers hired': 500 }
@@ -27,41 +29,85 @@ const CHART_DATA = [
   }
 ]
 
-const TestBarChart = () => (
+const CHART_DATA_CUSTOM_TOOLTIP = [
+  {
+    name: 'Berlin',
+    value: { infected: 4000, recovered: 2400 }
+  },
+  {
+    name: 'Milan',
+    value: { infected: 3000, recovered: 1398 }
+  },
+  {
+    name: 'Moscow',
+    value: { infected: 2000, recovered: 9800 }
+  },
+  {
+    name: 'Los-Angeles',
+    value: { infected: 2780, recovered: 3908 }
+  }
+]
+
+const CustomTooltip = ({ active, payload }: any) => {
+  if (active && payload && payload.length > 0) {
+    const { infected, recovered } = payload[0].payload
+
+    return (
+      <Paper data-testid='tooltip'>
+        <Container padded='xsmall'>
+          <Typography size='medium' color='red'>
+            Infected: {infected}
+          </Typography>
+
+          <Typography size='medium' color='green'>
+            Recovered: {recovered}
+          </Typography>
+        </Container>
+      </Paper>
+    )
+  }
+
+  return null
+}
+
+const TestBarChart = ({ ...rest }) => (
   <TestingPicasso>
     <BarChart
-      data={CHART_DATA}
+      data={CHART_DATA_DEFAULT}
       fillSchema={{
         'engineers hired': palette.blue.main
       }}
       width={720}
       tooltip
+      {...rest}
     />
   </TestingPicasso>
 )
 
 // TODO: Make types for function parameters to work
+
 // @ts-ignore
 const getBar = name => {
   return cy.get(`path[name="${name}"]`).first()
 }
 // @ts-ignore
-const hoverOverBar = name => {
-  return getBar(name).first().trigger('mouseover')
-}
+const hoverOverBar = name => getBar(name).trigger('mousemove')
 // @ts-ignore
 const assertTooltipContent = text => {
-  cy.get('.recharts-default-tooltip').should('be.visible').and('contain', text)
+  cy.get('.recharts-default-tooltip')
+    .should('be.visible')
+    .and('contain', text)
 }
 // @ts-ignore
 const assertCustomTooltipContent = text => {
-  cy.get('[data-testid="tooltip"]').should('be.visible').and('contain', text)
+  cy.get('[data-testid="tooltip"]')
+    .should('be.visible')
+    .and('contain', text)
 }
 
 describe('BarChart', () => {
   it('shows default tooltip on hover', () => {
     mount(<TestBarChart />)
-    cy.wait(1000)
 
     hoverOverBar('Apple')
     assertTooltipContent('Appleengineers hired : 500')
@@ -79,19 +125,33 @@ describe('BarChart', () => {
     assertTooltipContent('Toptalengineers hired : 1000')
   })
 
-  // it('shows custom tooltip on hover', () => {
-  //   cy.visit('iframe.html?id=barchart--customized')
+  it('shows custom tooltip on hover', () => {
+    mount(
+      <TestBarChart
+        data={CHART_DATA_CUSTOM_TOOLTIP}
+        fillSchema={{
+          infected: palette.red.main,
+          recovered: palette.green.main
+        }}
+        labelColorSchema={{
+          infected: palette.red.main,
+          recovered: palette.green.main
+        }}
+        tooltip
+        customTooltip={<CustomTooltip />}
+      />
+    )
 
-  //   hoverOverBar('Berlin')
-  //   assertCustomTooltipContent('Infected: 4000Recovered: 2400')
+    hoverOverBar('Berlin')
+    assertCustomTooltipContent('Infected: 4000Recovered: 2400')
 
-  //   hoverOverBar('Milan')
-  //   assertCustomTooltipContent('Infected: 3000Recovered: 1398')
+    hoverOverBar('Milan')
+    assertCustomTooltipContent('Infected: 3000Recovered: 1398')
 
-  //   hoverOverBar('Moscow')
-  //   assertCustomTooltipContent('Infected: 2000Recovered: 9800')
+    hoverOverBar('Moscow')
+    assertCustomTooltipContent('Infected: 2000Recovered: 9800')
 
-  //   hoverOverBar('Los-Angeles')
-  //   assertCustomTooltipContent('Infected: 2780Recovered: 3908')
-  // })
+    hoverOverBar('Los-Angeles')
+    assertCustomTooltipContent('Infected: 2780Recovered: 3908')
+  })
 })
