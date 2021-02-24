c624620f8ac82b9c65739fa5e4d892e06b890ed6
Teimur Gasanov <teimur@toptal.com>
2021-02-24 16:23:47 +0600

    chore(picasso-forms): add deserialization example (#1922)

---
+++ packages/picasso-forms/src/story/Deserialization.example.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Container } from '@toptal/picasso'
+import { Form } from '@toptal/picasso-forms'
+
+const deserializeValue = <T extends unknown>(value: T) => {
+  if (value === 'true') {
+    return true
+  }
+  if (value === 'false') {
+    return false
+  }
+
+  return value
+}
+
+const Example = () => (
+  <Form
+    onSubmit={values => {
+      console.log('Raw: ', { foo: values.foo })
+      console.log('Deserialized: ', { foo: deserializeValue(values.foo) })
+    }}
+    initialValues={{ foo: 'true' }}
+  >
+    <Form.RadioGroup name='foo' label='Foo'>
+      <Form.Radio label='yes' value='true' />
+      <Form.Radio label='no' value='false' />
+    </Form.RadioGroup>
+    <Container top='small'>
+      <Form.SubmitButton>Submit</Form.SubmitButton>
+    </Container>
+  </Form>
+)
+
+export default Example
