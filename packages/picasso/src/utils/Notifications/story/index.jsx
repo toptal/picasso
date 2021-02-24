6fcedfca89c53c3b94b6aa1579f32e6736d1660f
Uladzimir Havenchyk <uladzimir@toptal.com>
2021-02-24 16:04:37 +0300

    fix(Notification): remove custom variant

--- packages/picasso/src/utils/Notifications/story/index.jsx
+++ packages/picasso/src/utils/Notifications/story/index.jsx
@@ -1,18 +1,17 @@
 import PicassoBook from '~/.storybook/components/PicassoBook'
 
-const chapter = PicassoBook.connectToPage(
-  page =>
-    page
-      .createChapter(
-        'Notifications stream',
-        `In most cases, Notifications shouldn't be used in isolation.
+const chapter = PicassoBook.connectToPage(page =>
+  page
+    .createChapter(
+      'Notifications stream',
+      `In most cases, Notifications shouldn't be used in isolation.
 The 'useNotifications' hook is providing the set of tools for
 operating easily the most common notifications types, like 'info', 'error',
 'warning', 'success' in a single stream of messages.`
-      )
-      .addTextSection('### How to use')
-      .addTextSection(
-        `
+    )
+    .addTextSection('### How to use')
+    .addTextSection(
+      `
 'useNotifications' hook is providing the list of methods to show the notifications
 of the corresponding type:
 
@@ -22,23 +21,23 @@ const { showInfo, showError, showSuccess } = useNotifications()
 
 Each of them has this list of props:
     `
-      )
-      .addDocs([
-        {
-          name: 'content',
-          type: 'string | ReactNode',
-          description: 'The content of notification message'
-        },
-        {
-          name: 'icon',
-          type: 'ReactElement',
-          description: 'The icon for the general notification'
-        },
-        {
-          name: 'options',
-          type: {
-            name: 'OptionsObject',
-            description: `
+    )
+    .addDocs([
+      {
+        name: 'content',
+        type: 'string | ReactNode',
+        description: 'The content of notification message'
+      },
+      {
+        name: 'icon',
+        type: 'ReactElement',
+        description: 'The icon for the general notification'
+      },
+      {
+        name: 'options',
+        type: {
+          name: 'OptionsObject',
+          description: `
 {\n
   preventDuplicate?: boolean\n
   onClose?: (event: React.SyntheticEvent<any>, reason: string) => void\n
@@ -46,111 +45,61 @@ Each of them has this list of props:
   persist?: boolean\n
 }
           `
-          },
-          description: 'Options of the single notification.'
-        }
-      ])
-      .addExample('utils/Notifications/story/Default.example.tsx', {
-        title: 'Default',
-        effect: async (testPage, makeScreenshot) => {
-          testPage.click('[data-testid="trigger"]')
-          await testPage.waitFor(100)
-          await makeScreenshot({
-            isFullScreen: true
-          })
-        }
-      })
-      .addExample('utils/Notifications/story/Variants.example.tsx', {
-        title: 'Variants',
-        effect: async (testPage, makeScreenshot) => {
-          await testPage.click('[data-testid="error-trigger"]')
-          await testPage.waitFor(100)
-
-          await testPage.click('[data-testid="success-trigger"]')
-          await testPage.waitFor(100)
-
-          await makeScreenshot({
-            isFullScreen: true
-          })
-        }
-      })
-      .addExample(
-        'utils/Notifications/story/GeneralNotifications.example.tsx',
-        {
-          title: 'General Notifications',
-          effect: async (testPage, makeScreenshot) => {
-            testPage.click('[data-testid="trigger"]')
-            await testPage.waitFor(100)
-            await makeScreenshot({
-              isFullScreen: true
-            })
-          }
-        }
-      )
-      .addExample('utils/Notifications/story/Options.example.tsx', {
-        title: 'Options',
-        effect: async (testPage, makeScreenshot) => {
-          testPage.click('[data-testid="trigger"]')
-          await testPage.waitFor(100)
-          await makeScreenshot({
-            isFullScreen: true
-          })
-
-          await testPage.waitFor(1000)
-
-          // by now notification should disappear
-          await makeScreenshot({
-            isFullScreen: true
-          })
-        }
-      })
-      .addTextSection(
-        `
-Additionally for custom notifications 'useNotifications' hook is providing the special methods:
-
-\`\`\`javascript
-const { showCustomNotification, closeNotification } = useNotifications()
-\`\`\`
-
-With the list of props:
-      `
-      )
-      .addDocs([
-        {
-          name: 'Content',
-          type: 'ReactElement',
-          description: 'The custom notification component'
         },
-        {
-          name: 'position',
-          type: {
-            name: 'SnackbarOrigin',
-            description: `
-{\n
-  horizontal: 'left' | 'center' | 'right'\n
-  vertical: 'top' | 'bottom'\n
-}
-            `
-          },
-          description: 'Position of the single notification.'
-        },
-        {
-          name: 'options',
-          type: {
-            name: 'OptionsObject',
-            description: `
-{\n
-  preventDuplicate?: boolean\n
-  onClose?: (event: React.SyntheticEvent<any>, reason: string) => void\n
-  autoHideDuration?: number\n
-  persist?: boolean\n
-}
-          `
-          },
-          description: 'Options of the single notification.'
-        }
-      ])
-      .addExample('utils/Notifications/story/Custom.example.tsx', 'Custom') // picasso-skip-visuals
+        description: 'Options of the single notification.'
+      }
+    ])
+    .addExample('utils/Notifications/story/Default.example.tsx', {
+      title: 'Default',
+      effect: async (testPage, makeScreenshot) => {
+        testPage.click('[data-testid="trigger"]')
+        await testPage.waitFor(100)
+        await makeScreenshot({
+          isFullScreen: true
+        })
+      }
+    })
+    .addExample('utils/Notifications/story/Variants.example.tsx', {
+      title: 'Variants',
+      effect: async (testPage, makeScreenshot) => {
+        await testPage.click('[data-testid="error-trigger"]')
+        await testPage.waitFor(100)
+
+        await testPage.click('[data-testid="success-trigger"]')
+        await testPage.waitFor(100)
+
+        await makeScreenshot({
+          isFullScreen: true
+        })
+      }
+    })
+    .addExample('utils/Notifications/story/GeneralNotifications.example.tsx', {
+      title: 'General Notifications',
+      effect: async (testPage, makeScreenshot) => {
+        testPage.click('[data-testid="trigger"]')
+        await testPage.waitFor(100)
+        await makeScreenshot({
+          isFullScreen: true
+        })
+      }
+    })
+    .addExample('utils/Notifications/story/Options.example.tsx', {
+      title: 'Options',
+      effect: async (testPage, makeScreenshot) => {
+        testPage.click('[data-testid="trigger"]')
+        await testPage.waitFor(100)
+        await makeScreenshot({
+          isFullScreen: true
+        })
+
+        await testPage.waitFor(1000)
+
+        // by now notification should disappear
+        await makeScreenshot({
+          isFullScreen: true
+        })
+      }
+    })
 )
 
 export default {
