3653391341af0c76a73019b824e78451d84e66e7
Andrzej Poniedzialek <andrzej.poniedzialek@toptal.com>
2021-02-26 09:46:36 +0100

    chore(pause16): add data-testid

--- packages/picasso/src/Icon/Pause16.tsx
+++ packages/picasso/src/Icon/Pause16.tsx
@@ -40,6 +40,7 @@ const SvgPause16 = forwardRef(function SvgPause16(
       className={cx(...classNames)}
       style={svgStyle}
       ref={ref}
+      data-testid='icon-pause16'
     >
       <path d='M8 0a8 8 0 110 16A8 8 0 018 0zm0 1a7 7 0 100 14A7 7 0 008 1zM7 5v6H6V5h1zm3 0v6H9V5h1z' />
     </svg>
