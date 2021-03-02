aafbf63b384b13948697f0b97cbb69d9831a2247
Daniel Ostapenko <daniel.ostapenko@toptal.com>
2021-03-02 10:55:41 +0200

    fix(PageArticle): make smaller padding for smaller screens

--- packages/picasso/src/PageArticle/styles.ts
+++ packages/picasso/src/PageArticle/styles.ts
@@ -1,9 +1,13 @@
 import { createStyles, Theme } from '@material-ui/core/styles'
 
-export default ({ layout }: Theme) =>
+export default ({ screens, layout }: Theme) =>
   createStyles({
     root: {
       flex: 1,
-      margin: `0 ${layout.contentPaddingHorizontal}`
+      margin: `0 ${layout.contentPaddingHorizontal}`,
+
+      [screens('small', 'medium')]: {
+        margin: `0 ${layout.contentMobilePaddingHorizontal}`
+      }
     }
   })
--- packages/shared/src/Picasso/config/layout.ts
+++ packages/shared/src/Picasso/config/layout.ts
@@ -3,12 +3,14 @@ export interface Layout {
   contentWidth: string
   contentMinWidth?: string
   contentPaddingHorizontal: string
+  contentMobilePaddingHorizontal: string
 }
 
 const layout: Layout = {
   contentWidthWide: '90em',
   contentWidth: '75em',
-  contentPaddingHorizontal: '2em'
+  contentPaddingHorizontal: '2em',
+  contentMobilePaddingHorizontal: '1em'
 }
 
 export default layout
