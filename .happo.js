c71e5efda327608d7973fcf8c3724bae946bf4a8
Alexander Polyankin <alexander.polyankin@toptal.com>
2021-03-03 13:20:49 +0300

    chore(happo): run tests on master (#1940)

--- .happo.js
+++ .happo.js
@@ -2,18 +2,12 @@ const { RemoteBrowserTarget } = require('happo.io')
 const happoStorybookPlugin = require('happo-plugin-storybook')
 
 module.exports = {
-  project: process.env.HAPPO_PROJECT,
+  project: 'Picasso',
   apiKey: process.env.HAPPO_API_KEY,
   apiSecret: process.env.HAPPO_API_SECRET,
 
   targets: {
     'chrome-desktop': new RemoteBrowserTarget('chrome', {
-      viewport: '1280x1024'
-    }),
-    'firefox-desktop': new RemoteBrowserTarget('firefox', {
-      viewport: '1280x1024'
-    }),
-    'safari-desktop': new RemoteBrowserTarget('safari', {
       viewport: '1280x1024'
     })
   },
