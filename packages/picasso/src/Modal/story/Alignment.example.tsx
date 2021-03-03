1722abeaaca9633679f16afc9783d56be56a682c
Boris Yordanov <boris.yordanov@toptal.com>
2021-03-03 17:23:44 +0200

    docs: improve alignment example

--- packages/picasso/src/Modal/story/Alignment.example.tsx
+++ packages/picasso/src/Modal/story/Alignment.example.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Modal, Button } from '@toptal/picasso'
+import { Modal, Button, Typography } from '@toptal/picasso'
 import { useModal } from '@toptal/picasso/utils'
 
 const ModalDialog = ({
@@ -22,25 +22,9 @@ const ModalDialog = ({
     >
       <Modal.Title>Alignment is "{align}"</Modal.Title>
       <Modal.Content>
-        <h4>This Window is aligned as "{align}".</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-          mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua.
-        </p>
+        <Typography variant='heading'>
+          This Window is aligned as "{align}".
+        </Typography>
       </Modal.Content>
       <Modal.Actions>
         <Button disabled={isLoading} variant='secondary' onClick={onClose}>
