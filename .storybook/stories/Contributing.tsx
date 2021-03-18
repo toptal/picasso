262f03e1b7e6390af3298e2f79bba3e9dedff1d8
Boris Yordanov <boris.yordanov@toptal.com>
2021-03-18 10:27:17 +0200

    chore: cleanup

--- .storybook/stories/Contributing.tsx
+++ .storybook/stories/Contributing.tsx
@@ -1,24 +1,18 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// eslint-disable-file @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { doc } from 'storybook-readme'
 import { storiesOf } from '@storybook/react'
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import CSS_NAMING from '../../docs/contribution/css-naming.md'
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import NEW_COMPONENT from '../../docs/contribution/new-component-creation.md'
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import GITHUB_WORKFLOW from '../../docs/contribution/github-workflow.md'
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import JSS_ONBOARDING from '../../docs/contribution/jss-onboarding.md'
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import CREATING_EXAMPLES from '../../docs/contribution/creating-examples.md'
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import VISUAL_SNAPSHOTS from '../../docs/contribution/visual-testing.md'
 
--- .storybook/stories/Picasso.tsx
+++ .storybook/stories/Picasso.tsx
@@ -1,18 +1,14 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// eslint-disable-file @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { doc } from 'storybook-readme'
 import { storiesOf } from '@storybook/react'
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import README from '../../README.md'
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import CHANGELOG from '../../packages/picasso/CHANGELOG.md'
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import SUPPORT from '../../docs/SUPPORT.md'
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import API_PRINCIPLES from '../../docs/api-principles.md'
 
