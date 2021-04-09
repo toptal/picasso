a08d4eb577fccc12218099d6d76a2aa14a651950
OleksandrNechai <oleksandr.nechai@toptal.com>
2021-04-09 10:38:28 +0300

    fix: make things pretty and fix visual

--- cypress/component/Section.spec.tsx
+++ cypress/component/Section.spec.tsx
@@ -1,24 +1,35 @@
 import React from 'react'
 import { mount } from '@cypress/react'
-import { Button, Typography } from '@toptal/picasso'
+import { Button, Typography, Container } from '@toptal/picasso'
 import { Section } from '@toptal/picasso-lab'
 import { TestingPicasso } from '@toptal/picasso/test-utils'
 
 const SectionExample = () => {
   return (
     <TestingPicasso>
-      <Section title='Title'>
-        <Typography>Title only</Typography>
-      </Section>
-      <Section title='Title' subtitle='Subtitle'>
-        <Typography>Title and subtitle</Typography>
-      </Section>
-      <Section actions={<Button size='small'>Action</Button>}>
-        <Typography>Actions only</Typography>
-      </Section>
-      <Section title='Title' actions={<Button size='small'>Action</Button>}>
-        <Typography>Title and actions</Typography>
-      </Section>
+      <Container bottom='small'>
+        <Section title='Title'>
+          <Typography>Title only</Typography>
+        </Section>
+      </Container>
+      <Container bottom='small'>
+        <Section title='Title' subtitle='Subtitle'>
+          <Typography>Title and subtitle</Typography>
+        </Section>
+      </Container>
+
+      <Container bottom='small'>
+        <Section actions={<Button size='small'>Action</Button>}>
+          <Typography>Actions only</Typography>
+        </Section>
+      </Container>
+
+      <Container bottom='small'>
+        <Section title='Title' actions={<Button size='small'>Action</Button>}>
+          <Typography>Title and actions</Typography>
+        </Section>
+      </Container>
+
       <Section
         title='Title'
         subtitle='Subtitle'
