6a29e0798da5f550e7555629204b5533c97642c5
Alexander Polyankin <alexander.polyankin@toptal.com>
2021-04-12 15:49:05 +0300

    chore(menu): extract ui of menu item

--- packages/picasso/src/SelectListItem/index.ts
+++ packages/picasso/src/SelectListItem/index.ts
@@ -1,6 +1,7 @@
 import { OmitInternalProps } from '@toptal/picasso-shared'
 
-import { Props } from './SelectListItem'
+import { Attributes, Props } from './SelectListItem'
 
 export { default } from './SelectListItem'
 export type SelectListItemProps = OmitInternalProps<Props>
+export type SelectListItemAttributes = Attributes
