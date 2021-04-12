131190acd39c00e8cf0742c0cf1d77aba7277223
Alexander Polyankin <alexander.polyankin@toptal.com>
2021-04-12 16:29:29 +0300

    chore(menu): update snapshots

--- packages/picasso/src/Menu/index.ts
+++ packages/picasso/src/Menu/index.ts
@@ -3,5 +3,5 @@ import { OmitInternalProps } from '@toptal/picasso-shared'
 import { Props } from './Menu'
 
 export { default } from './Menu'
+export type { ListNativeProps } from './Menu'
 export type MenuProps = OmitInternalProps<Props>
-export * from './Menu'
