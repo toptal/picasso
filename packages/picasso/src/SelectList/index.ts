dc8c5350cfb138e80d910745d647ff2ae640d7a7
Alexander Polyankin <alexander.polyankin@toptal.com>
2021-04-12 16:21:23 +0300

    chore(menu): update snapshots

--- packages/picasso/src/SelectList/index.ts
+++ packages/picasso/src/SelectList/index.ts
@@ -3,5 +3,4 @@ import { OmitInternalProps } from '@toptal/picasso-shared'
 import { Props } from './SelectList'
 
 export { default } from './SelectList'
-export type { SelectListNativeProps } from './SelectList'
 export type SelectListProps = OmitInternalProps<Props>
