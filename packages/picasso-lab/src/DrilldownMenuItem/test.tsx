3670e461957ca12f91e691336d0c20ed303e91f3
Alexander Polyankin <alexander.polyankin@toptal.com>
2021-04-14 23:09:55 +0300

    chore(drilldown): add missing

--- packages/picasso-lab/src/DrilldownMenuItem/test.tsx
+++ packages/picasso-lab/src/DrilldownMenuItem/test.tsx
@@ -1,12 +1,23 @@
-import { render, TestingPicasso } from '@toptal/picasso/test-utils'
-import React from 'react'
+import React, { useState } from 'react'
+import { fireEvent, render, TestingPicasso } from '@toptal/picasso/test-utils'
 
 import DrilldownMenuItem from './'
+import DrilldownMenuContext from '../DrilldownMenu/DrilldownMenuContext'
 
 const TestDrilldownMenuItem = () => {
+  const [activeMenuKey, setActiveMenuKey] = useState<string | undefined>(
+    undefined
+  )
+
   return (
     <TestingPicasso>
-      <DrilldownMenuItem data-testid='item'>Item</DrilldownMenuItem>
+      <DrilldownMenuContext.Provider
+        value={{ activeMenuKey, setActiveMenuKey }}
+      >
+        <DrilldownMenuItem menu={<div data-testid='menu' />} data-testid='item'>
+          Item
+        </DrilldownMenuItem>
+      </DrilldownMenuContext.Provider>
     </TestingPicasso>
   )
 }
@@ -16,5 +27,22 @@ describe('DrilldownMenuItem', () => {
     const { container } = render(<TestDrilldownMenuItem />)
 
     expect(container).toMatchSnapshot()
+  })
+
+  it('sets active menu key when clicked', () => {
+    const { getByTestId, queryByTestId } = render(<TestDrilldownMenuItem />)
+
+    fireEvent.click(getByTestId('item'))
+
+    expect(queryByTestId('menu')).toBeInTheDocument()
+  })
+
+  it('clears active menu key when clicked outside of the inner menu', () => {
+    const { getByTestId, queryByTestId } = render(<TestDrilldownMenuItem />)
+
+    fireEvent.click(getByTestId('item'))
+    fireEvent.click(document.body)
+
+    expect(queryByTestId('menu')).not.toBeInTheDocument()
   })
 })
