01bd6ab8c5057461bf84c9d7bacfd17355cd938c
Alexander Polyankin <alexander.polyankin@toptal.com>
2021-04-19 13:56:20 +0300

    feat(menu): extract to flat menu

--- packages/picasso/src/FlatMenu/test.tsx
+++ packages/picasso/src/FlatMenu/test.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import { render } from '@toptal/picasso/test-utils'
+import { fireEvent, render } from '@toptal/picasso/test-utils'
 
 import FlatMenu from '../FlatMenu'
 
 const TestFlatMenu = () => {
+  const menu = (
+    <FlatMenu>
+      <FlatMenu.Item>Item A1</FlatMenu.Item>
+    </FlatMenu>
+  )
+
   return (
     <FlatMenu>
-      <FlatMenu.Item>Item A</FlatMenu.Item>
+      <FlatMenu.Item menu={menu} data-testid='item-a'>
+        Item A
+      </FlatMenu.Item>
       <FlatMenu.Item>Item B</FlatMenu.Item>
     </FlatMenu>
   )
@@ -15,6 +23,14 @@ const TestFlatMenu = () => {
 describe('FlatMenu', () => {
   it('renders', () => {
     const { container } = render(<TestFlatMenu />)
+
+    expect(container).toMatchSnapshot()
+  })
+
+  it('has back button when in submenu', () => {
+    const { container, getByTestId } = render(<TestFlatMenu />)
+
+    fireEvent.click(getByTestId('item-a'))
 
     expect(container).toMatchSnapshot()
   })
