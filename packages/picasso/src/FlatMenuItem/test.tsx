020adf93d00beb8234d943d635308d501f1a88ca
Alexander Polyankin <alexander.polyankin@toptal.com>
2021-04-19 14:44:22 +0300

    feat(menu): extract to flat menu

--- packages/picasso/src/FlatMenuItem/test.tsx
+++ packages/picasso/src/FlatMenuItem/test.tsx
@@ -1,18 +1,43 @@
 import React from 'react'
-import { render } from '@toptal/picasso/test-utils'
+import { fireEvent, render } from '@toptal/picasso/test-utils'
 
-import FlatMenuItem, { FlatMenuItemProps } from '../FlatMenuItem'
-
-jest.mock('ap-style-title-case')
-
-const TestFlatMenuItem = (props: FlatMenuItemProps) => {
-  return <FlatMenuItem {...props} />
-}
+import FlatMenuItem from '../FlatMenuItem'
+import FlatMenuContext from '../FlatMenu/FlatMenuContext'
 
 describe('FlatMenuItem', () => {
   it('renders', () => {
-    const { container } = render(<TestFlatMenuItem>Item</TestFlatMenuItem>)
+    const { container } = render(<FlatMenuItem>Item</FlatMenuItem>)
 
     expect(container).toMatchSnapshot()
+  })
+
+  it('delegates menu element', () => {
+    const menu = <div />
+    const refresh = jest.fn()
+
+    render(
+      <FlatMenuContext.Provider value={{ refresh }}>
+        <FlatMenuItem menu={menu}>Item</FlatMenuItem>
+      </FlatMenuContext.Provider>
+    )
+
+    expect(refresh).toHaveBeenCalledWith(expect.any(String), menu)
+  })
+
+  it('delegates clicks for toggling a menu', () => {
+    const menu = <div />
+    const push = jest.fn()
+
+    const { getByTestId } = render(
+      <FlatMenuContext.Provider value={{ push }}>
+        <FlatMenuItem menu={menu} data-testid='item'>
+          Item
+        </FlatMenuItem>
+      </FlatMenuContext.Provider>
+    )
+
+    fireEvent.click(getByTestId('item'))
+
+    expect(push).toHaveBeenCalledWith(expect.any(String), menu)
   })
 })
