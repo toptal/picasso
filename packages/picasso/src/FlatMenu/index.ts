e6dbdc59c9fc9cf5bebb5f596c556f78195eaba4
Alexander Polyankin <alexander.polyankin@toptal.com>
2021-04-19 12:14:36 +0300

    feat(menu): extract to flat menu

---
+++ packages/picasso/src/FlatMenu/index.ts
@@ -0,0 +1,6 @@
+import { OmitInternalProps } from '@toptal/picasso-shared'
+
+import { Props } from './FlatMenu'
+
+export { default } from './FlatMenu'
+export type FlatMenuProps = OmitInternalProps<Props>
---
+++ packages/picasso/src/FlatMenuItem/FlatMenuItem.tsx
@@ -0,0 +1,73 @@
+import { OverridableComponent } from '@toptal/picasso-shared'
+import React, {
+  forwardRef,
+  useCallback,
+  useContext,
+  useEffect,
+  useState
+} from 'react'
+
+import MenuContext, { MenuContextProps } from '../Menu/MenuContext'
+import MenuListItem, { MenuListItemProps } from '../MenuListItem'
+import { noop } from '../utils'
+
+export type VariantType = 'light' | 'dark'
+
+export type Props = Omit<MenuListItemProps, 'contentRef'>
+
+const generateKey = (() => {
+  let count = 0
+
+  return () => String(++count)
+})()
+
+export const FlatMenuItem: OverridableComponent<Props> = forwardRef<
+  HTMLElement,
+  Props
+>(function FlatMenuItem (props, ref) {
+  const { className, style, menu, onClick, ...rest } = props
+  const [key] = useState(generateKey)
+  const { push, refresh } = useContext<MenuContextProps>(MenuContext)
+
+  useEffect(() => {
+    if (menu && refresh) {
+      refresh(key, menu)
+    }
+  }, [key, menu, refresh])
+
+  const handleClick = useCallback(
+    (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+      if (menu && push) {
+        event.stopPropagation()
+        push(key, menu)
+      }
+
+      if (onClick) {
+        onClick(event)
+      }
+    },
+    [key, menu, push, onClick]
+  )
+
+  return (
+    <MenuListItem
+      {...rest}
+      ref={ref}
+      className={className}
+      style={style}
+      arrow={Boolean(menu)}
+      onClick={handleClick}
+    />
+  )
+})
+
+FlatMenuItem.defaultProps = {
+  as: 'li',
+  onClick: noop,
+  variant: 'light',
+  nonSelectable: false
+}
+
+FlatMenuItem.displayName = 'FlatMenuItem'
+
+export default FlatMenuItem
