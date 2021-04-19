b9fe071196475fc4c114eb7d4cceb2a7f74b02e8
Alexander Polyankin <alexander.polyankin@toptal.com>
2021-04-19 13:50:16 +0300

    feat(menu): extract to flat menu

--- packages/picasso/src/FlatMenu/test.tsx
+++ packages/picasso/src/FlatMenu/test.tsx
@@ -1,42 +1,20 @@
-import React, { FunctionComponent } from 'react'
-import { render, fireEvent } from '@toptal/picasso/test-utils'
-import { OmitInternalProps } from '@toptal/picasso-shared'
+import React from 'react'
+import { render } from '@toptal/picasso/test-utils'
 
-import FlatMenu, { FlatMenuProps } from '../FlatMenu'
+import FlatMenu from '../FlatMenu'
 
-const TestFlatMenu: FunctionComponent<OmitInternalProps<FlatMenuProps>> = ({
-  children
-}) => <FlatMenu>{children}</FlatMenu>
+const TestFlatMenu = () => {
+  return (
+    <FlatMenu>
+      <FlatMenu.Item>Item A</FlatMenu.Item>
+      <FlatMenu.Item>Item B</FlatMenu.Item>
+    </FlatMenu>
+  )
+}
 
 describe('FlatMenu', () => {
   it('renders', () => {
-    const { container } = render(
-      <TestFlatMenu>
-        <FlatMenu.Item>Item 1</FlatMenu.Item>
-        <FlatMenu.Item>Item 2</FlatMenu.Item>
-        <FlatMenu.Item>Item 3</FlatMenu.Item>
-      </TestFlatMenu>
-    )
-
-    expect(container).toMatchSnapshot()
-  })
-
-  it('has back button when in submenu', () => {
-    const { container, getByText } = render(
-      <TestFlatMenu>
-        <FlatMenu.Item
-          menu={
-            <TestFlatMenu>
-              <FlatMenu.Item>Submenu Item</FlatMenu.Item>
-            </TestFlatMenu>
-          }
-        >
-          Item 1
-        </FlatMenu.Item>
-      </TestFlatMenu>
-    )
-
-    fireEvent.click(getByText('Item 1'))
+    const { container } = render(<TestFlatMenu />)
 
     expect(container).toMatchSnapshot()
   })
