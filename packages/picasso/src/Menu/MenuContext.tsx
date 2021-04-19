c2544d0dab0c73fcb817adb7d083c3803f86a6a0
Alexander Polyankin <alexander.polyankin@toptal.com>
2021-04-19 15:40:33 +0300

    feat(menu): extract to flat menu

--- packages/picasso/src/Menu/MenuContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react'
-
-import { ModeType } from './Menu'
-
-export interface MenuContextProps {
-  mode?: ModeType
-}
-
-export default createContext<MenuContextProps>({})
