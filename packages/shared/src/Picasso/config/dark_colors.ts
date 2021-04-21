aa197275b5f8df7cf3ea0f741743c7e814c8e2e4
Piotr Niełacny <piotr.nielacny@gmail.com>
2021-04-21 11:02:58 +0200

    Extract colors from desings
    In order to create dark mode we need to extract colors from designs.
    After that it should be easy to use in the code.
    
    Desings: https://app.abstract.com/projects/4039be9b-a18a-40bb-b929-40ab7d49de6a/branches/972f17ba-dfc1-4174-87ac-34389f12ad31/files/ff6ece7a-47d6-400f-989c-9122b545459a?filter=changed

---
+++ packages/shared/src/Picasso/config/dark_colors.ts
@@ -0,0 +1,28 @@
+export const colors = {
+  background: {
+    darker: '#131518',
+    dark: '#181a1e',
+    light: '#1b1d21',
+    lighter: '#272d3c'
+  },
+
+  text: {
+    light: '#ffffff',
+    primary: '#c4c6ca',
+    dark: '#c0c2c6',
+    link: '#50a9e9',
+    disabled: '#455065'
+  },
+
+  button: {
+    border: '#84888e',
+    fill: '#262d3d',
+    text: '#ffffff'
+  },
+
+  pill: {
+    fill: '#262d3d',
+    border: '#84888e',
+    text: '#c4c6ca'
+  }
+}
