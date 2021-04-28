4b4a14714e824f3d7f02afc20951ecb9430138f6
Alexander Polyankin <alexander.polyankin@toptal.com>
2021-04-28 16:15:43 +0300

    feat(button): make children required
    BREAKING CHANGE:

--- packages/picasso/src/ButtonGroup/ButtonGroup.tsx
+++ packages/picasso/src/ButtonGroup/ButtonGroup.tsx
@@ -1,11 +1,17 @@
+import React, {
+  ReactNode,
+  HTMLAttributes,
+  forwardRef,
+  ElementType,
+  useMemo
+} from 'react'
 import { makeStyles, Theme } from '@material-ui/core/styles'
+import cx from 'classnames'
 import {
-  addClassesToChildren,
   BaseProps,
+  addClassesToChildren,
   Classes
 } from '@toptal/picasso-shared'
-import cx from 'classnames'
-import React, { forwardRef, HTMLAttributes, ReactNode, useMemo } from 'react'
 
 import Button from '../Button'
 import styles from './styles'
@@ -19,33 +25,33 @@ const useStyles = makeStyles<Theme>(styles, {
   name: 'PicassoButtonGroup'
 })
 
-const useChildrenWithClasses = (classes: Classes, children?: ReactNode) => {
-  return useMemo(
-    () =>
-      addClassesToChildren({
-        children,
-        classes,
-        config: () => [
-          [
-            Button,
-            {
-              root: classes.button,
-              active: classes.active,
-              focused: classes.focused,
-              hovered: classes.hovered
-            }
-          ]
-        ]
-      }),
-    [classes, children]
-  )
-}
+const getChildrenClassesConfig = (
+  classes: Classes
+): [ElementType, Classes][] => [
+  [
+    Button,
+    {
+      root: classes.button,
+      active: classes.active,
+      focused: classes.focused,
+      hovered: classes.hovered
+    }
+  ]
+]
 
 export const ButtonGroup = forwardRef<HTMLDivElement, Props>(
   function ButtonGroup (props, ref) {
     const { children, className, style, ...rest } = props
     const classes = useStyles()
-    const childrenWithClasses = useChildrenWithClasses(classes, children)
+    const childrenWithClasses = useMemo(
+      () =>
+        addClassesToChildren({
+          children,
+          classes,
+          config: getChildrenClassesConfig
+        }),
+      [children, classes]
+    )
 
     return (
       <div
