b38f21373bf17361c9858aa12078d9dad66c993c
Daniel Ostapenko <daniel.ostapenko@toptal.com>
2021-04-29 17:24:19 +0300

    test: [FX-1872] Add more tests

--- packages/picasso/src/ApplicationUpdateNotification/test.tsx
+++ packages/picasso/src/ApplicationUpdateNotification/test.tsx
@@ -1,12 +1,42 @@
 import React from 'react'
 import { render } from '@toptal/picasso/test-utils'
+import { screen } from '@testing-library/react'
+import { OmitInternalProps } from '@toptal/picasso-shared'
 
-import ApplicationUpdateNotification from './ApplicationUpdateNotification'
+import ApplicationUpdateNotification, {
+  Props
+} from './ApplicationUpdateNotification'
+
+const renderApplicationUpdateNotification = (
+  props: OmitInternalProps<Props> = {}
+) => render(<ApplicationUpdateNotification {...props} />)
 
 describe('ApplicationUpdateNotification', () => {
   it('renders', () => {
-    const { container } = render(<ApplicationUpdateNotification />)
+    const { container } = renderApplicationUpdateNotification()
 
     expect(container).toMatchSnapshot()
+  })
+
+  it('when click on "Update Later" the corresponding callback is called', () => {
+    const onCloseMock = jest.fn()
+
+    renderApplicationUpdateNotification({
+      onClose: onCloseMock
+    })
+
+    screen.getByText('Update Later').click()
+    expect(onCloseMock).toHaveBeenCalled()
+  })
+
+  it('when click on "Reload Now" the corresponding callback is called', () => {
+    const onReloadClickonCloseMock = jest.fn()
+
+    renderApplicationUpdateNotification({
+      onReloadClick: onReloadClickonCloseMock
+    })
+
+    screen.getByText('Reload Now').click()
+    expect(onReloadClickonCloseMock).toHaveBeenCalled()
   })
 })
