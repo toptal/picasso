f4541c8c1d80111062df479375821b6979c2affd
Daniel Ostapenko <daniel.ostapenko@toptal.com>
2021-04-29 16:59:10 +0300

    chore: [FX-1872] Remove visual tests, use happo instead

--- packages/picasso/src/ApplicationUpdateNotification/story/index.jsx
+++ packages/picasso/src/ApplicationUpdateNotification/story/index.jsx
@@ -9,7 +9,7 @@ page
   .createChapter()
   .addExample('ApplicationUpdateNotification/story/Default.example.tsx', {
     title: 'Default'
-  })
+  }) // picasso-skip-visuals
   .addExample('ApplicationUpdateNotification/story/InAction.example.tsx', {
     title: 'In Action'
-  })
+  }) // picasso-skip-visuals
