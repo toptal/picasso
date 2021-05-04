030360d593319474a049b84bd2877bb6b8e6d241
Alexander Polyankin <alexander.polyankin@toptal.com>
2021-05-04 14:29:25 +0300

    fix(TypographyOverflow): fix visual tests

--- packages/picasso-lab/src/TypographyOverflow/styles.ts
+++ packages/picasso-lab/src/TypographyOverflow/styles.ts
@@ -15,6 +15,16 @@ export default () =>
       '-webkit-line-clamp': lines,
       '-webkit-box-orient': 'vertical',
       overflow: 'hidden',
-      textOverflow: 'ellipsis'
+      textOverflow: 'ellipsis',
+      /**
+       * Workaround for `textOverflow: ellipsis`.
+       * Browser can show ellipsis at the end, even if it's not required -
+       * it's possible if the text has the very same width as the container
+       * (ellipsis width will be the same as the width of the text hidden by these ellipsis).
+       * It could be also affected by the font itself, when it has wider render bouncing borders.
+       * So to be sure, that text container in slightly wider than parent,
+       * we have to to add some super-minor space at the end.
+       */
+      paddingRight: '0.9px'
     })
   })
