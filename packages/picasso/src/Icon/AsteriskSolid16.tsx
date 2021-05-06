830444fce36f2c76312fbce2ce2c44a155e7a3f5
Sacha Lifszyc <sacha.lifszyc@toptal.com>
2021-05-06 04:10:34 -0300

    feat(Icon): add asterisk solid icon (#2059)

---
+++ packages/picasso/src/Icon/AsteriskSolid16.tsx
@@ -0,0 +1,60 @@
+import React, { forwardRef, Ref } from 'react'
+import cx from 'classnames'
+import { makeStyles } from '@material-ui/core/styles'
+import { StandardProps } from '@toptal/picasso-shared'
+
+import kebabToCamelCase from '../utils/kebab-to-camel-case'
+import styles from './styles'
+const BASE_SIZE = 16
+
+type ScaleType = 1 | 2 | 3 | 4
+export interface Props extends StandardProps {
+  scale?: ScaleType
+  color?: string
+  base?: number
+}
+const useStyles = makeStyles(styles, { name: 'PicassoSvgAsteriskSolid16' })
+const SvgAsteriskSolid16 = forwardRef(function SvgAsteriskSolid16 (
+  props: Props,
+  ref: Ref<SVGSVGElement>
+) {
+  const {
+    className,
+    style = {},
+    color,
+    scale,
+    base,
+    'data-testid': testId
+  } = props
+  const classes: Record<string, string> = useStyles(props)
+  const classNames = [classes.root, className]
+  const scaledSize = base || BASE_SIZE * Math.ceil(scale || 1)
+  const colorClassName = kebabToCamelCase(`${color}`)
+
+  if (classes[colorClassName]) {
+    classNames.push(classes[colorClassName])
+  }
+
+  const svgStyle = {
+    minWidth: `${scaledSize}px`,
+    minHeight: `${scaledSize}px`,
+    ...style
+  }
+
+  return (
+    <svg
+      viewBox='0 0 16 16'
+      xmlns='http://www.w3.org/2000/svg'
+      xmlnsXlink='http://www.w3.org/1999/xlink'
+      className={cx(...classNames)}
+      style={svgStyle}
+      ref={ref}
+      data-testid={testId}
+    >
+      <path d='M9.053 0l-.001 5.229 4.395-2.615L15 5.386 10.605 8 15 10.614l-1.553 2.772-4.395-2.616V16H5.948v-5.23l-4.394 2.616L0 10.614 4.395 8 0 5.386l1.553-2.772L5.947 5.23V0h3.106z' />
+    </svg>
+  )
+})
+
+SvgAsteriskSolid16.displayName = 'SvgAsteriskSolid16'
+export default SvgAsteriskSolid16
