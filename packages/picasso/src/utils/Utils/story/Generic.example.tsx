f28da356fe22a76a62e0070421ecc7baf4c4dec3
Daniel Ostapenko <daniel.ostapenko@toptal.com>
2021-05-13 12:39:43 +0300

    chore: fix code style of examples

--- packages/picasso/src/utils/Utils/story/Generic.example.tsx
+++ packages/picasso/src/utils/Utils/story/Generic.example.tsx
@@ -1,32 +1,30 @@
 import { Table } from '@toptal/picasso'
 import React from 'react'
 
-const Example = () => {
-  const data = {
-    noop: `A function that does nothing, a replacement for () => {}`,
-    isBoolean: `Checks if the provided value is a boolean`,
-    isNumber: `Checks if the provided value is a number`,
-    isString: `Checks if the provided value is a string`
-  }
-
-  return (
-    <Table>
-      <Table.Head>
-        <Table.Row>
-          <Table.Cell>Name</Table.Cell>
-          <Table.Cell>Description</Table.Cell>
-        </Table.Row>
-      </Table.Head>
-      <Table.Body>
-        {Object.entries(data).map(([name, description], index) => (
-          <Table.Row key={index}>
-            <Table.Cell>{name}</Table.Cell>
-            <Table.Cell>{description}</Table.Cell>
-          </Table.Row>
-        ))}
-      </Table.Body>
-    </Table>
-  )
+const data = {
+  noop: `A function that does nothing, a replacement for () => {}`,
+  isBoolean: `Checks if the provided value is a boolean`,
+  isNumber: `Checks if the provided value is a number`,
+  isString: `Checks if the provided value is a string`
 }
+
+const Example = () => (
+  <Table>
+    <Table.Head>
+      <Table.Row>
+        <Table.Cell>Name</Table.Cell>
+        <Table.Cell>Description</Table.Cell>
+      </Table.Row>
+    </Table.Head>
+    <Table.Body>
+      {Object.entries(data).map(([name, description]) => (
+        <Table.Row key={name}>
+          <Table.Cell>{name}</Table.Cell>
+          <Table.Cell>{description}</Table.Cell>
+        </Table.Row>
+      ))}
+    </Table.Body>
+  </Table>
+)
 
 export default Example
--- packages/picasso/src/utils/Utils/story/React.example.tsx
+++ packages/picasso/src/utils/Utils/story/React.example.tsx
@@ -1,34 +1,32 @@
 import { Table } from '@toptal/picasso'
 import React from 'react'
 
-const Example = () => {
-  const data = {
-    forwardRef: `Wrapper around React.forwardRef that preserves genericity of the passed component`,
-    useSafeState: `Wrapper around React.useState that ignores state updates when the component is unmounted`,
-    useCombinedRefs: `Combines multiple refs into one`,
-    useWidthOf: `Calculates width of an element`,
-    disableUnsupportedProps: `Removes props that are not supported for passed props values`,
-    ClickAwayListener: `Listen for click events that occur somewhere in the document, outside of the element itself`
-  }
-
-  return (
-    <Table>
-      <Table.Head>
-        <Table.Row>
-          <Table.Cell>Name</Table.Cell>
-          <Table.Cell>Description</Table.Cell>
-        </Table.Row>
-      </Table.Head>
-      <Table.Body>
-        {Object.entries(data).map(([name, description], index) => (
-          <Table.Row key={index}>
-            <Table.Cell>{name}</Table.Cell>
-            <Table.Cell>{description}</Table.Cell>
-          </Table.Row>
-        ))}
-      </Table.Body>
-    </Table>
-  )
+const data = {
+  forwardRef: `Wrapper around React.forwardRef that preserves genericity of the passed component`,
+  useSafeState: `Wrapper around React.useState that ignores state updates when the component is unmounted`,
+  useCombinedRefs: `Combines multiple refs into one`,
+  useWidthOf: `Calculates width of an element`,
+  disableUnsupportedProps: `Removes props that are not supported for passed props values`,
+  ClickAwayListener: `Listen for click events that occur somewhere in the document, outside of the element itself`
 }
+
+const Example = () => (
+  <Table>
+    <Table.Head>
+      <Table.Row>
+        <Table.Cell>Name</Table.Cell>
+        <Table.Cell>Description</Table.Cell>
+      </Table.Row>
+    </Table.Head>
+    <Table.Body>
+      {Object.entries(data).map(([name, description]) => (
+        <Table.Row key={name}>
+          <Table.Cell>{name}</Table.Cell>
+          <Table.Cell>{description}</Table.Cell>
+        </Table.Row>
+      ))}
+    </Table.Body>
+  </Table>
+)
 
 export default Example
