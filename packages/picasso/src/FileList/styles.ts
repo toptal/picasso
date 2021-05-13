7b303b6fa4f70f9ed2b0e305b8bd399df1ea814c
Ademir Besirovic <ademir.besirovic@toptal.com>
2021-05-13 11:23:13 +0200

    refactor(FileList): move FileListItem to separate file

--- packages/picasso/src/FileList/styles.ts
+++ packages/picasso/src/FileList/styles.ts
@@ -1,14 +1,8 @@
 import { Theme, createStyles } from '@material-ui/core/styles'
-import { rem } from '@toptal/picasso-shared'
 
 export default ({ palette }: Theme) =>
   createStyles({
     root: {
       borderTop: `1px solid ${palette.grey.light2}`
-    },
-    fileListItem: {
-      borderBottom: `1px solid ${palette.grey.light2}`,
-      paddingTop: rem('4px'),
-      paddingBottom: rem('4px')
     }
   })
---
+++ packages/picasso/src/FileListItem/test.tsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import { OmitInternalProps } from '@toptal/picasso-shared'
+import { render, fireEvent } from '@toptal/picasso/test-utils'
+
+import FileListItem, { Props } from './FileListItem'
+
+const renderFileListItem = (props: OmitInternalProps<Props>) =>
+  render(<FileListItem {...props} />)
+
+describe('FileListItem', () => {
+  const file = {
+    name: 'user-profile-picture.png',
+    uploading: false,
+    progress: 0,
+    error: undefined
+  }
+
+  it('renders', () => {
+    const { container } = renderFileListItem({
+      file,
+      onRemove: jest.fn()
+    })
+
+    expect(container).toMatchSnapshot()
+  })
+
+  it('fires callback on remove', () => {
+    const handleRemove = jest.fn()
+
+    const { getByRole } = renderFileListItem({
+      file,
+      onRemove: handleRemove
+    })
+
+    fireEvent.click(getByRole('button'))
+
+    expect(handleRemove).toHaveBeenCalled()
+  })
+
+  describe('when file is uploading', () => {
+    it(`renders 'Uploading...' label and progress bar`, () => {
+      const { queryByText, queryByTestId } = renderFileListItem({
+        file: { ...file, uploading: true, progress: 30 }
+      })
+
+      expect(queryByText('Uploading...')).toBeInTheDocument()
+      expect(queryByTestId('file-list-item-progressbar')).toBeInTheDocument()
+    })
+
+    describe('when error exists', () => {
+      it('renders file name and error message', () => {
+        const { queryByText, queryByTestId } = renderFileListItem({
+          file: {
+            ...file,
+            uploading: true,
+            progress: 30,
+            error: 'File is too large'
+          }
+        })
+
+        expect(queryByText('Uploading...')).not.toBeInTheDocument()
+        expect(
+          queryByTestId('file-list-item-progressbar')
+        ).not.toBeInTheDocument()
+
+        expect(queryByText('user-profile-picture.png')).toBeInTheDocument()
+        expect(queryByText('File is too large')).toBeInTheDocument()
+      })
+    })
+  })
+
+  describe('when error exists', () => {
+    it('renders file name and error message', () => {
+      const { queryByText } = renderFileListItem({
+        file: {
+          ...file,
+          uploading: true,
+          progress: 30,
+          error: 'File is too large'
+        }
+      })
+
+      expect(queryByText('user-profile-picture.png')).toBeInTheDocument()
+      expect(queryByText('File is too large')).toBeInTheDocument()
+    })
+  })
+})
