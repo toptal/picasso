3472f1629784fed2bd2f8650335b6b84bd40467c
Uladzimir Havenchyk <uladzimir@toptal.com>
2021-05-20 16:55:52 +0300

    feat(Page): rename Head to Helmet (#2079)

--- packages/picasso/src/PageHead/index.ts
+++
@@ -1,7 +0,0 @@
-import { OmitInternalProps } from '@toptal/picasso-shared'
-
-import { Props } from './PageHead'
-
-export { default } from './PageHead'
-export type PageHeadProps = OmitInternalProps<Props>
-export * from './PageHead'
