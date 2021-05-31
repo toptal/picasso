0ebc3c9555f03ae718984f0efe650be61eb80c32
OleksandrNechai <oleksandr.nechai@toptal.com>
2021-05-31 17:08:46 +0300

    fix: visual tests

--- packages/picasso/src/TagSelectorLabel/styles.ts
+++ packages/picasso/src/TagSelectorLabel/styles.ts
@@ -1,11 +1,13 @@
 import { createStyles } from '@material-ui/core/styles'
+import { rem } from '@toptal/picasso-shared'
 
 import { TAG_SELECTOR_INPUT_GUTTER_SIZE } from '../TagSelectorInput'
+export const TAG_SELECTOR_HORIZONTAL_GUTTER_SIZE = rem('8px')
 
 export default () =>
   createStyles({
     label: {
-      marginRight: TAG_SELECTOR_INPUT_GUTTER_SIZE,
+      marginRight: TAG_SELECTOR_HORIZONTAL_GUTTER_SIZE,
       marginBottom: TAG_SELECTOR_INPUT_GUTTER_SIZE
     }
   })
