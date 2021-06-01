bc3e3cb390807eac95119b4df1657775626a293a
OleksandrNechai <oleksandr.nechai@toptal.com>
2021-06-01 23:08:32 +0300

    fix: review comments

--- packages/picasso/src/TagSelectorLabel/styles.ts
+++ packages/picasso/src/TagSelectorLabel/styles.ts
@@ -3,12 +3,10 @@ import { rem } from '@toptal/picasso-shared'
 
 import { TAG_SELECTOR_INPUT_GUTTER_SIZE } from '../TagSelectorInput'
 
-const TAG_SELECTOR_HORIZONTAL_GUTTER_SIZE = rem('8px')
-
 export default () =>
   createStyles({
     label: {
-      marginRight: TAG_SELECTOR_HORIZONTAL_GUTTER_SIZE,
+      marginRight: rem('8px'),
       marginBottom: TAG_SELECTOR_INPUT_GUTTER_SIZE
     }
   })
