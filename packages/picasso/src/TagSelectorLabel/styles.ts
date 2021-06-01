339172ffea8cb0f9330ef705f9c4e8c192e90ed1
Oleksandr Nechai <oleksandr.nechai@toptal.com>
2021-06-02 00:14:24 +0300

    fix(TagSelector): reduce height (#2091)

--- packages/picasso/src/TagSelectorLabel/styles.ts
+++ packages/picasso/src/TagSelectorLabel/styles.ts
@@ -1,11 +1,12 @@
 import { createStyles } from '@material-ui/core/styles'
+import { rem } from '@toptal/picasso-shared'
 
 import { TAG_SELECTOR_INPUT_GUTTER_SIZE } from '../TagSelectorInput'
 
 export default () =>
   createStyles({
     label: {
-      marginRight: TAG_SELECTOR_INPUT_GUTTER_SIZE,
+      marginRight: rem('8px'),
       marginBottom: TAG_SELECTOR_INPUT_GUTTER_SIZE
     }
   })
