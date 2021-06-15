ff041cf35e8b588fe1db77e988579d3f11f36b4e
Rafał Kwaśniewski <rafal.kwasniewski@toptal.com>
2021-06-15 15:13:25 +0700

    style: shades fix for modal max-height variant

--- packages/picasso/src/ModalContent/styles.ts
+++ packages/picasso/src/ModalContent/styles.ts
@@ -1,18 +1,45 @@
 import { createStyles } from '@material-ui/core'
 
-const shadeStyles = {
-  left: 0,
-  right: 0,
+const WRAPPER_PADDING = '2em'
+const SHADE_HEIGHT = '80px'
+const BACKGROUND_STARTING_COLOR = '#fff'
+const BACKGROUND_FINISHING_COLOR = 'rgba(255, 255, 255, 0)'
+
+const shadeCommonStyles = {
   zIndex: 1,
   position: 'absolute' as const,
-  pointerEvents: 'none' as const,
-  height: 80
+  pointerEvents: 'none' as const
+}
+
+const shadeStyles = {
+  ...shadeCommonStyles,
+  right: WRAPPER_PADDING,
+  left: WRAPPER_PADDING,
+  height: `calc(${SHADE_HEIGHT} + ${WRAPPER_PADDING})`
+}
+
+const shadePseudoStyles = {
+  ...shadeCommonStyles,
+  right: 0,
+  left: 0,
+  content: '""'
+}
+
+const shadePseudoBeforeStyles = {
+  ...shadePseudoStyles,
+  backgroundColor: BACKGROUND_STARTING_COLOR,
+  height: WRAPPER_PADDING
+}
+
+const shadePseudoAfterStyles = {
+  ...shadePseudoStyles,
+  height: SHADE_HEIGHT
 }
 
 export default () =>
   createStyles({
     modalContent: {
-      padding: '2em',
+      padding: WRAPPER_PADDING,
       overflow: 'auto',
       flex: '1 1 auto'
     },
@@ -25,11 +52,27 @@ export default () =>
     topShade: {
       ...shadeStyles,
       top: 0,
-      background: `linear-gradient(180deg, white 0%, rgba(255,255,255,0) 5rem)`
+      '&:before': {
+        ...shadePseudoBeforeStyles,
+        top: 0
+      },
+      '&:after': {
+        ...shadePseudoAfterStyles,
+        top: WRAPPER_PADDING,
+        background: `linear-gradient(180deg, ${BACKGROUND_STARTING_COLOR} 0%, ${BACKGROUND_FINISHING_COLOR} 5rem)`
+      }
     },
     bottomShade: {
       ...shadeStyles,
       bottom: 0,
-      background: `linear-gradient(0deg, white 0%, rgba(255,255,255,0) 5rem)`
+      '&:before': {
+        ...shadePseudoBeforeStyles,
+        bottom: 0
+      },
+      '&:after': {
+        ...shadePseudoAfterStyles,
+        bottom: WRAPPER_PADDING,
+        background: `linear-gradient(0deg, ${BACKGROUND_STARTING_COLOR} 0%, ${BACKGROUND_FINISHING_COLOR} 5rem)`
+      }
     }
   })
