abcb0b50d57e092a4e5e8921e8e930dc7539fe52
Rafał Kwaśniewski <rafal.kwasniewski@toptal.com>
2021-06-17 21:48:27 +0700

    style: cleaning & use variable in background linear

--- packages/picasso/src/ModalContent/styles.ts
+++ packages/picasso/src/ModalContent/styles.ts
@@ -3,12 +3,6 @@ import { createStyles, hexToRgb, Theme } from '@material-ui/core'
 const WRAPPER_PADDING = '2em'
 const SHADE_HEIGHT = '80px'
 
-// const shadeCommonStyles = {
-//   zIndex: 1,
-//   position: 'absolute' as const,
-//   pointerEvents: 'none' as const
-// }
-
 const shadeStyles = {
   zIndex: 1,
   position: 'absolute' as const,
@@ -17,23 +11,6 @@ const shadeStyles = {
   left: WRAPPER_PADDING,
   height: `calc(${SHADE_HEIGHT} + ${WRAPPER_PADDING})`
 }
-
-// const shadePseudoStyles = {
-//   ...shadeCommonStyles,
-//   right: 0,
-//   left: 0,
-//   content: '""'
-// }
-//
-// const shadePseudoBeforeStyles = {
-//   ...shadePseudoStyles,
-//   height: WRAPPER_PADDING
-// }
-//
-// const shadePseudoAfterStyles = {
-//   ...shadePseudoStyles,
-//   height: SHADE_HEIGHT
-// }
 
 export default ({ palette }: Theme) => {
   const BACKGROUND_STARTING_COLOR = palette.background.default
@@ -54,32 +31,12 @@ export default ({ palette }: Theme) => {
     topShade: {
       ...shadeStyles,
       top: 0,
-      background: `linear-gradient(180deg, ${BACKGROUND_STARTING_COLOR} 0%, ${BACKGROUND_STARTING_COLOR} 2rem, ${BACKGROUND_FINISHING_COLOR} 5rem)`
-      // '&:before': {
-      //   ...shadePseudoBeforeStyles,
-      //   backgroundColor: BACKGROUND_STARTING_COLOR,
-      //   top: 0
-      // },
-      // '&:after': {
-      //   ...shadePseudoAfterStyles,
-      //   top: WRAPPER_PADDING,
-      //   background: `linear-gradient(180deg, ${BACKGROUND_STARTING_COLOR} 0%, ${BACKGROUND_FINISHING_COLOR} 5rem)`
-      // }
+      background: `linear-gradient(180deg, ${BACKGROUND_STARTING_COLOR} 0%, ${BACKGROUND_STARTING_COLOR} ${WRAPPER_PADDING}, ${BACKGROUND_FINISHING_COLOR} 100%)`
     },
     bottomShade: {
       ...shadeStyles,
       bottom: 0,
-      background: `linear-gradient(0deg, ${BACKGROUND_STARTING_COLOR} 0%, ${BACKGROUND_STARTING_COLOR} 2rem, ${BACKGROUND_FINISHING_COLOR} 5rem)`
-      // '&:before': {
-      //   ...shadePseudoBeforeStyles,
-      //   backgroundColor: BACKGROUND_STARTING_COLOR,
-      //   bottom: 0
-      // },
-      // '&:after': {
-      //   ...shadePseudoAfterStyles,
-      //   bottom: WRAPPER_PADDING,
-      //   background: `linear-gradient(0deg, ${BACKGROUND_STARTING_COLOR} 0%, ${BACKGROUND_FINISHING_COLOR} 5rem)`
-      // }
+      background: `linear-gradient(0deg, ${BACKGROUND_STARTING_COLOR} 0%, ${BACKGROUND_STARTING_COLOR} ${WRAPPER_PADDING}, ${BACKGROUND_FINISHING_COLOR} 100%)`
     }
   })
 }
