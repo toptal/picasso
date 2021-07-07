342dd6641106c5a6b68704f8dc3db391aab1c6fb
inemiro <ilia.nemiro@toptal.com>
2021-07-07 11:30:10 +0300

    feat(NumberInput): add Int validation for Form.NumberInput

--- packages/picasso-forms/src/NumberInput/NumberInput.tsx
+++ packages/picasso-forms/src/NumberInput/NumberInput.tsx
@@ -3,18 +3,40 @@ import {
   NumberInput as PicassoNumberInput,
   NumberInputProps
 } from '@toptal/picasso'
+import { FieldValidator } from 'final-form'
 
+import { validators } from '../utils'
 import FieldWrapper, { FieldProps } from '../FieldWrapper'
 
 export type Props = NumberInputProps & FieldProps<NumberInputProps['value']>
 
-export const NumberInput = (props: Props) => (
-  <FieldWrapper<NumberInputProps> {...props}>
-    {(inputProps: NumberInputProps) => {
-      return <PicassoNumberInput {...inputProps} />
-    }}
-  </FieldWrapper>
-)
+const MIN = -2147483648
+const MAX = 2147483647
+
+const { composeValidators } = validators
+
+export const NumberInput = (props: Props) => {
+  const { min = MIN, max = MAX, validate } = props
+  const validateNumberLimits: FieldValidator<NumberInputProps['value']> = value => {
+    if (Number(value) > max) {
+      return `Must be less than or equal to ${max}.`
+    }
+    if (Number(value) < min) {
+      return `Must be greater than or equal to ${min}.`
+    }
+  }
+
+  return (
+    <FieldWrapper<NumberInputProps>
+      {...props}
+      validate={composeValidators([validateNumberLimits, validate])}
+    >
+      {(inputProps: NumberInputProps) => {
+        return <PicassoNumberInput {...inputProps} />
+      }}
+    </FieldWrapper>
+  )
+}
 
 NumberInput.defaultProps = {}
 
