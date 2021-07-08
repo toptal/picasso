8e303dc19b40c97efaf5145c2f1600ebf5b93ad7
George Aidonidis <george.aidonidis@toptal.com>
2021-07-08 09:45:46 +0300

    fix(TagSelector): compare by value
    * Change `isIncluded` function to compare items by value not reference

--- packages/picasso/src/TagSelector/TagSelector.tsx
+++ packages/picasso/src/TagSelector/TagSelector.tsx
@@ -22,8 +22,11 @@ export interface Item extends AutocompleteItem {
 
 const EMPTY_INPUT_VALUE = ''
 
-const isIncluded = (items: Item[], currentItem: Item) =>
-  items.some(item => item === currentItem)
+export const isIncluded = (items: Item[], currentItem: Item) =>
+  items.some(
+    ({ text, value }) =>
+      text === currentItem.text && value === currentItem.value
+  )
 
 const getItemText = (item: Item | null) =>
   (item && item.text) || EMPTY_INPUT_VALUE
