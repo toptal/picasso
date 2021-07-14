fe62548f4c29d35a67a255389fd355fff266e4e7
Dmitry Maklygin <dmitry.maklygin@toptal.com>
2021-07-14 14:33:50 +0300

    fix(Popper): fix popper shaking on each render

--- packages/picasso/src/Popper/test.tsx
+++ packages/picasso/src/Popper/test.tsx
@@ -1,63 +1,159 @@
-import React, { useState, FC, ReactNode, forwardRef } from 'react'
-import Picasso from '@toptal/picasso-provider'
-import { render, act, fireEvent } from '@toptal/picasso/test-utils'
+import React from 'react'
+import MUIPopper from '@material-ui/core/Popper'
+import { usePicassoRoot } from '@toptal/picasso-provider'
+import { makeStyles } from '@material-ui/core/styles'
+import { render } from '@testing-library/react'
 
-import Popper from './Popper'
+import Popper, { Props, getPopperOptions } from './Popper'
+import styles from './styles'
 
-// eslint-disable-next-line react/display-name
-const FakeRootNode = forwardRef<HTMLDivElement, { children?: ReactNode }>(
-  (props, ref) => {
-    const { children } = props
+jest.mock('@material-ui/core/Popper', () => jest.fn(() => null))
+jest.mock('@toptal/picasso-provider', () => ({
+  usePicassoRoot: jest.fn()
+}))
+jest.mock('@material-ui/core/styles', () => ({
+  makeStyles: jest.fn(() => () => ({ root: 'TEST_CLASS_NAME+1' }))
+}))
+jest.mock('../utils', () => ({
+  useBreakpoint: () => true,
+  useWidthOf: () => '300px'
+}))
 
-    return (
-      <div ref={ref} role='root'>
-        {children}
-      </div>
-    )
-  }
-)
+const mockedUsePicassoRoot = usePicassoRoot as jest.Mock<
+  ReturnType<typeof usePicassoRoot>
+>
+const mockedMakeStyles = makeStyles as jest.Mock<ReturnType<typeof makeStyles>>
+const mockedMUIPopper = MUIPopper as jest.Mock<ReturnType<typeof MUIPopper>>
 
-const PicassoWithFakeRootNode: FC = ({ children }) => {
-  return (
-    <Picasso
-      loadFonts={false}
-      loadFavicon={false}
-      fixViewport={false}
-      RootComponent={FakeRootNode}
-    >
-      {children}
-    </Picasso>
-  )
+const rootDiv = document.createElement('div')
+
+rootDiv.setAttribute('id', 'root')
+
+const anchorEl = document.body
+const children = 'some children'
+const className = 'TEST_CLASS_NAME+1'
+const disablePortal = false
+const open = true
+const placement = 'top'
+const defaultPopperProps = {
+  container: rootDiv,
+  anchorEl,
+  children,
+  className,
+  disablePortal,
+  open,
+  placement,
+  popperRef: null,
+  style: {
+    width: '300px'
+  },
+  popperOptions: getPopperOptions({})
 }
 
-const PopperRenderer = () => {
-  const [popoverIsOpen, setPopoverIsOpen] = useState(false)
-
-  return (
-    <>
-      <button onClick={() => setPopoverIsOpen(true)} role='action'>
-        Click
-      </button>
-      <Popper open={popoverIsOpen} anchorEl={document.body}>
-        some children
+const renderComponent = (props: Partial<Props> = {}) => {
+  return render(
+    <div id='root'>
+      <Popper
+        open={open}
+        anchorEl={anchorEl}
+        disablePortal={disablePortal}
+        placement={placement}
+        {...props}
+      >
+        {children}
       </Popper>
-    </>
+    </div>,
+    {
+      container: document.body.appendChild(rootDiv)
+    }
   )
 }
 
 describe('Popper', () => {
-  it('renders', () => {
-    const { getByRole } = render(<PopperRenderer />, {
-      wrapper: PicassoWithFakeRootNode
+  beforeEach(() => {
+    mockedMUIPopper.mockClear()
+    mockedUsePicassoRoot.mockReturnValue(rootDiv)
+  })
+
+  it('creates useStyle hook properly', () => {
+    expect(mockedMakeStyles).toHaveBeenCalledTimes(1)
+    expect(mockedMakeStyles).toHaveBeenCalledWith(styles, {
+      name: 'PicassoPopper'
     })
+  })
 
-    act(() => {
-      fireEvent.click(getByRole('action'))
+  describe('when container prop is passed', () => {
+    it('calls MUIPopper with passed container', () => {
+      const container = document.createElement('div')
+
+      renderComponent({
+        container
+      })
+
+      expect(mockedMUIPopper).toHaveBeenCalledTimes(1)
+      expect(mockedMUIPopper).toHaveBeenCalledWith(
+        {
+          ...defaultPopperProps,
+          container
+        },
+        {}
+      )
     })
+  })
 
-    const popper = getByRole('tooltip')
-    const root = getByRole('root')
+  describe('when container prop is NOT passed', () => {
+    it('calls MUIPopper with default root container', () => {
+      renderComponent({
+        container: undefined
+      })
 
-    expect(root).toContainElement(popper)
+      expect(mockedMUIPopper).toHaveBeenCalledTimes(1)
+      expect(mockedMUIPopper).toHaveBeenCalledWith(
+        {
+          ...defaultPopperProps,
+          container: rootDiv
+        },
+        {}
+      )
+    })
+  })
+
+  describe('when custom width prop is passed', () => {
+    it('calls MUIPopper with custom width style', () => {
+      renderComponent({
+        width: '400px'
+      })
+
+      expect(mockedMUIPopper).toHaveBeenCalledTimes(1)
+      expect(mockedMUIPopper).toHaveBeenCalledWith(
+        {
+          ...defaultPopperProps,
+          style: {
+            width: '400px'
+          }
+        },
+        {}
+      )
+    })
+  })
+
+  describe('when custom width prop is NOT passed', () => {
+    describe('when autoWidth prop is false', () => {
+      it('calls MUIPopper without assigning width style', () => {
+        renderComponent({
+          width: undefined,
+          autoWidth: false
+        })
+
+        expect(mockedMUIPopper).toHaveBeenCalledTimes(1)
+        expect(mockedMUIPopper).toHaveBeenCalledWith(
+          {
+            ...defaultPopperProps,
+            style: {}
+          },
+          {}
+        )
+      })
+    })
   })
 })
