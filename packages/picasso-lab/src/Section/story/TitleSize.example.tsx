d0410af67773215bd5ba99f3490783adddb1bb05
Sergiu Butnarasu <sergiu.butnarasu@toptal.com>
2021-08-09 11:13:12 +0300

    chore: update section example

--- packages/picasso-lab/src/Section/story/TitleSize.example.tsx
+++ packages/picasso-lab/src/Section/story/TitleSize.example.tsx
@@ -1,17 +1,33 @@
 import React from 'react'
-import { List } from '@toptal/picasso'
+import { Grid, List, Typography } from '@toptal/picasso'
 import { Section } from '@toptal/picasso-lab'
 
 const Example = () => {
   return (
-    <Section variant='bordered' title='Quotes' titleSize='small'>
-      <List variant='unordered'>
-        <List.Item>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </List.Item>
-        <List.Item>In nec cursus lectus, nec malesuada tellus.</List.Item>
-      </List>
-    </Section>
+    <Grid>
+      <Grid.Item small={6}>
+        <Typography>Default title size</Typography>
+        <Section title='Quotes'>
+          <List variant='unordered'>
+            <List.Item>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </List.Item>
+            <List.Item>In nec cursus lectus, nec malesuada tellus.</List.Item>
+          </List>
+        </Section>
+      </Grid.Item>
+      <Grid.Item small={6}>
+        <Typography>Small title size</Typography>
+        <Section title='Quotes' titleSize='small'>
+          <List variant='unordered'>
+            <List.Item>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </List.Item>
+            <List.Item>In nec cursus lectus, nec malesuada tellus.</List.Item>
+          </List>
+        </Section>
+      </Grid.Item>
+    </Grid>
   )
 }
 
