200864d127863cf378cddd76a825cdf4949a05bc
Nikola Đuza <nikola.duza@toptal.com>
2021-08-12 09:32:52 +0200

    feat(Icon): add 24px version of the Player icon (#2132)

---
+++ packages/picasso/src/Icon/Player24.tsx
@@ -0,0 +1,60 @@
+import React, { forwardRef, Ref } from 'react'
+import cx from 'classnames'
+import { makeStyles } from '@material-ui/core/styles'
+import { StandardProps } from '@toptal/picasso-shared'
+
+import kebabToCamelCase from '../utils/kebab-to-camel-case'
+import styles from './styles'
+const BASE_SIZE = 24
+
+type ScaleType = 1 | 2 | 3 | 4
+export interface Props extends StandardProps {
+  scale?: ScaleType
+  color?: string
+  base?: number
+}
+const useStyles = makeStyles(styles, { name: 'PicassoSvgPlayer24' })
+const SvgPlayer24 = forwardRef(function SvgPlayer24 (
+  props: Props,
+  ref: Ref<SVGSVGElement>
+) {
+  const {
+    className,
+    style = {},
+    color,
+    scale,
+    base,
+    'data-testid': testId
+  } = props
+  const classes: Record<string, string> = useStyles(props)
+  const classNames = [classes.root, className]
+  const scaledSize = base || BASE_SIZE * Math.ceil(scale || 1)
+  const colorClassName = kebabToCamelCase(`${color}`)
+
+  if (classes[colorClassName]) {
+    classNames.push(classes[colorClassName])
+  }
+
+  const svgStyle = {
+    minWidth: `${scaledSize}px`,
+    minHeight: `${scaledSize}px`,
+    ...style
+  }
+
+  return (
+    <svg
+      viewBox='0 0 24 24'
+      xmlns='http://www.w3.org/2000/svg'
+      xmlnsXlink='http://www.w3.org/1999/xlink'
+      className={cx(...classNames)}
+      style={svgStyle}
+      ref={ref}
+      data-testid={testId}
+    >
+      <path d='M24 3v18H0V3h24zm-1 1H1v16h22V4zM9 8l7 4-7 4V8zm1 1.723v4.553L13.984 12 10 9.723z' />
+    </svg>
+  )
+})
+
+SvgPlayer24.displayName = 'SvgPlayer24'
+export default SvgPlayer24
