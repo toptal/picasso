7911a83daad251ad4a66e11631110ecacc6eb7a2
Tomáš Sláma <tomas.slama@toptal.com>
2021-08-23 11:02:41 +0200

    test(Stepper): update visual tests to happo

--- packages/picasso/src/Stepper/story/index.jsx
+++ packages/picasso/src/Stepper/story/index.jsx
@@ -5,7 +5,7 @@ const page = PicassoBook.section('Components').createPage(
   'Stepper',
   `
     Stepper component display progress through a sequence of steps.
-    
+
     ${PicassoBook.createBaseDocsLink(
       'https://share.goabstract.com/fe19eb54-183d-4359-95f2-43f93bad961c?collectionLayerId=903f1c63-750f-495e-8d0f-cdd889851fa2&mode=design&present=true'
     )}
@@ -18,6 +18,6 @@ page
 
 page
   .createChapter()
-  .addExample('Stepper/story/Default.example.tsx', 'Default')
-  .addExample('Stepper/story/Variants.example.tsx', 'Variants')
-  .addExample('Stepper/story/FullWidth.example.tsx', 'Full width')
+  .addExample('Stepper/story/Default.example.tsx', 'Default') // picasso-skip-visuals
+  .addExample('Stepper/story/Variants.example.tsx', 'Variants') // picasso-skip-visuals
+  .addExample('Stepper/story/FullWidth.example.tsx', 'Full width') // picasso-skip-visuals
