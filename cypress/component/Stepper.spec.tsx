a0341e8a18836b36ddd5a4e5088841d517f25817
Tomáš Sláma <tomas.slama@toptal.com>
2021-08-24 18:23:28 +0200

    test(Stepper): fix typescript issues

--- cypress/component/Stepper.spec.tsx
+++ cypress/component/Stepper.spec.tsx
@@ -3,7 +3,7 @@ import { mount } from '@cypress/react'
 import { Stepper, Container } from '@toptal/picasso'
 import { TestingPicasso } from '@toptal/picasso/test-utils'
 
-const Example = props => (
+const Example: React.FC<{ hideLabels?: boolean }> = props => (
   <TestingPicasso>
     <Container padded='medium'>
       <Stepper steps={['Step 1', 'Step 2', 'Step 3', 'Step 4']} {...props} />
