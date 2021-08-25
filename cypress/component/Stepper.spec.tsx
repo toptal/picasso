5cd2d5e06d6db4047c8652d1bfb88f8d9644f513
Tomáš Sláma <tomas.slama@toptal.com>
2021-08-25 12:55:30 +0200

    style(Stepper): [FX-1108] update to BASE 2.0 (#2143)
    * style(Stepper): update color and icon to BASE 2.0
    
    * test(Stepper): update snapshots
    
    * test(Stepper): update visual tests to happo
    
    * test(Stepper): fix issues in tests
    
    * test(Stepper): update image snapshots
    
    * test(Stepper): fix typescript issues

---
+++ cypress/component/Stepper.spec.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { mount } from '@cypress/react'
+import { Stepper, Container } from '@toptal/picasso'
+import { TestingPicasso } from '@toptal/picasso/test-utils'
+
+const Example: React.FC<{ hideLabels?: boolean }> = props => (
+  <TestingPicasso>
+    <Container padded='medium'>
+      <Stepper steps={['Step 1', 'Step 2', 'Step 3', 'Step 4']} {...props} />
+    </Container>
+    <Container top='small' padded='medium'>
+      <Stepper
+        active={1}
+        steps={['Step 1', 'Step 2', 'Step 3', 'Step 4']}
+        {...props}
+      />
+    </Container>
+    <Container top='small' padded='medium'>
+      <Stepper
+        active={3}
+        steps={['Step 1', 'Step 2', 'Step 3', 'Step 4']}
+        {...props}
+      />
+    </Container>
+    <Container top='small' padded='medium'>
+      <Stepper
+        active={4}
+        steps={['Step 1', 'Step 2', 'Step 3', 'Step 4']}
+        {...props}
+      />
+    </Container>
+  </TestingPicasso>
+)
+
+describe('Stepper', () => {
+  it('renders with label', () => {
+    mount(<Example />)
+    cy.get('body').happoScreenshot()
+  })
+
+  it('renders full width', () => {
+    mount(
+      <TestingPicasso>
+        <div style={{ width: '40rem' }}>
+          <Stepper fullWidth steps={['Step 1', 'Step 2', 'Step 3', 'Step 4']} />
+        </div>
+      </TestingPicasso>
+    )
+
+    cy.get('body').happoScreenshot()
+  })
+
+  it('renders without label', () => {
+    mount(
+      <TestingPicasso>
+        <Example hideLabels />
+      </TestingPicasso>
+    )
+    cy.get('body').happoScreenshot()
+  })
+})
