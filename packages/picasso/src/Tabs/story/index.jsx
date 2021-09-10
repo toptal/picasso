b60f92d065d72d3412d905c2eaf5c5f66bd8c7cf
Teimur Gasanov <teimur@toptal.com>
2021-09-10 15:06:52 +0600

    chore: add abstract link

--- packages/picasso/src/Tabs/story/index.jsx
+++ packages/picasso/src/Tabs/story/index.jsx
@@ -4,7 +4,13 @@ import PicassoBook from '~/.storybook/components/PicassoBook'
 
 const page = PicassoBook.section('Layout').createPage(
   'Tabs',
-  'Tabs allow to switch between content sections'
+  `
+  Tabs allow to switch between content sections
+
+  ${PicassoBook.createBaseDocsLink(
+    'https://share.goabstract.com/7263b996-174b-4d0c-b5aa-22344bfba249?mode=build&sha=baf4205fe8e730f6bc50c064446103ccc8c988f1'
+  )}
+  `
 )
 
 page
