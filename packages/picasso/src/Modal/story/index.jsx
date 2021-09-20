695c08de591201d5ada8782a142776b728b9fcc1
Teimur Gasanov <teimur@toptal.com>
2021-09-20 09:34:21 +0300

    feat(Modal): [FX-2054] update title size (#2168)

--- packages/picasso/src/Modal/story/index.jsx
+++ packages/picasso/src/Modal/story/index.jsx
@@ -30,80 +30,19 @@ page
 To show the 'Modal' component you should use 'useModal' hook. And if you need
 any additional logic inside the 'Modal' component you should create a wrapper 
 component and manage the internal state there.
-`,
-    effect: async (testPage, makeScreenshot) => {
-      await testPage.click('[data-testid="open"]')
-      await testPage.waitFor(100)
-      await makeScreenshot({
-        isFullScreen: true
-      })
-
-      await testPage.click('[data-testid="close"]')
-      await testPage.waitFor(100)
-      await makeScreenshot({
-        isFullScreen: true
-      })
-
-      await testPage.waitFor(1000)
-      await makeScreenshot()
-    }
-  })
+`
+  }) // picasso-skip-visuals
   .addExample('Modal/story/Sizes.example.tsx', {
-    title: 'Sizes',
-    effect: async (testPage, makeScreenshot) => {
-      await testPage.click('[data-testid="trigger-small"]')
-      await testPage.waitFor('[data-testid="cancel"]')
-      await makeScreenshot({
-        isFullScreen: true
-      })
-
-      await testPage.click('[data-testid="cancel"]')
-
-      await testPage.click('[data-testid="trigger-medium"]')
-      await testPage.waitFor('[data-testid="cancel"]')
-      await makeScreenshot({
-        isFullScreen: true
-      })
-
-      await testPage.click('[data-testid="cancel"]')
-
-      await testPage.click('[data-testid="trigger-large"]')
-      await testPage.waitFor('[data-testid="cancel"]')
-      await makeScreenshot({
-        isFullScreen: true
-      })
-
-      await testPage.click('[data-testid="cancel"]')
-
-      await testPage.click('[data-testid="trigger-full-screen"]')
-      await testPage.waitFor('[data-testid="cancel"]')
-      await makeScreenshot({
-        isFullScreen: true
-      })
-    }
-  })
+    title: 'Sizes'
+  }) // picasso-skip-visuals
   .addExample('Modal/story/MaxHeight.example.tsx', {
-    title: 'Max Height',
-    effect: async (testPage, makeScreenshot) => {
-      await testPage.click('[data-testid="trigger"]')
-      await testPage.waitFor('[data-testid="cancel"]')
-      await makeScreenshot({
-        isFullScreen: true
-      })
-    }
-  })
+    title: 'Max Height'
+  }) // picasso-skip-visuals
   .addExample('Modal/story/Tooltips.example.tsx', {
     // TODO title should be 1-2 words, title -> descripion
     title: 'Two tooltips on the page'
   }) // picasso-skip-visuals
   .addExample('Modal/story/Alignment.example.tsx', {
     title: 'Alignment',
-    description: 'Demonstrate how `align` prop works',
-    effect: async (testPage, makeScreenshot) => {
-      await testPage.click('[data-testid="align-top-open"]')
-      await testPage.waitFor(100)
-      await makeScreenshot({
-        isFullScreen: true
-      })
-    }
-  })
+    description: 'Demonstrate how `align` prop works'
+  }) // picasso-skip-visuals
