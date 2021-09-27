342e06f06d9ebda6dcdc2d99e694e67aacfb34ae
Tomáš Sláma <tomas.slama@toptal.com>
2021-09-27 14:11:00 +0200

    style(ButtonGroup): fix border color of active state

--- packages/picasso/src/ButtonGroupItem/styles.ts
+++ packages/picasso/src/ButtonGroupItem/styles.ts
@@ -5,6 +5,11 @@ import { createOutlineCommons, activeGroup } from '../Button/styles'
 export default (theme: Theme) =>
   createStyles({
     root: {
+      '&:active, &$active, &:hover, &$hovered, &:focus, &$focused': {
+        // border overlap to keep proper border width, but on state change
+        // we need to move up overlapped border
+        zIndex: 1
+      },
       '&$group': {
         ...createOutlineCommons(theme),
         '&:active, &$active': activeGroup(theme)
