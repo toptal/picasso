a97fae355968fbc651939e8288f00b3b54aef5e8
Viktor Shyrokov <viktor.shyrokov@toptal.com>
2021-09-27 12:49:49 +0300

    feat(Icon): [SPB-2461] add transfer icon

---
+++ packages/picasso/src/Icon/Transfer24.tsx
@@ -0,0 +1,60 @@
+import React, { forwardRef, Ref } from 'react'
+import cx from 'classnames'
+import { makeStyles } from '@material-ui/core/styles'
+import { StandardProps } from '@toptal/picasso-shared'
+
+import kebabToCamelCase from '../utils/kebab-to-camel-case'
+import styles from './styles'
+const BASE_SIZE = 24
+
+type ScaleType = 1 | 2 | 3 | 4
+export interface Props extends StandardProps {
+  scale?: ScaleType
+  color?: string
+  base?: number
+}
+const useStyles = makeStyles(styles, { name: 'PicassoSvgTransfer24' })
+const SvgTransfer24 = forwardRef(function SvgTransfer24(
+  props: Props,
+  ref: Ref<SVGSVGElement>
+) {
+  const {
+    className,
+    style = {},
+    color,
+    scale,
+    base,
+    'data-testid': testId
+  } = props
+  const classes: Record<string, string> = useStyles(props)
+  const classNames = [classes.root, className]
+  const scaledSize = base || BASE_SIZE * Math.ceil(scale || 1)
+  const colorClassName = kebabToCamelCase(`${color}`)
+
+  if (classes[colorClassName]) {
+    classNames.push(classes[colorClassName])
+  }
+
+  const svgStyle = {
+    minWidth: `${scaledSize}px`,
+    minHeight: `${scaledSize}px`,
+    ...style
+  }
+
+  return (
+    <svg
+      viewBox='0 0 24 24'
+      xmlns='http://www.w3.org/2000/svg'
+      xmlnsXlink='http://www.w3.org/1999/xlink'
+      className={cx(...classNames)}
+      style={svgStyle}
+      ref={ref}
+      data-testid={testId}
+    >
+      <path d='M22 14v1H4.707l3.5 3.5-.707.707L2.293 14H22zm-5.5-9.207L21.707 10H2V9h17.293l-3.5-3.5.707-.707z' />
+    </svg>
+  )
+})
+
+SvgTransfer24.displayName = 'SvgTransfer24'
+export default SvgTransfer24
