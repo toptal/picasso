704c2e11a896dfb6ecc00da3b921f55369811fdf
Dmitry Maklygin <dmitry.maklygin@toptal.com>
2021-10-19 16:31:32 +0300

    fix(Autocomplete): add inputProps type to the definition of Autocomplete props (#2114)

--- packages/picasso/src/Autocomplete/index.ts
+++ packages/picasso/src/Autocomplete/index.ts
@@ -3,6 +3,6 @@ import { OmitInternalProps } from '@toptal/picasso-shared'
 import { Props as OuterProps } from './Autocomplete'
 export { default } from './Autocomplete'
 export * from './types'
-export type AutocompleteProps = OmitInternalProps<OuterProps, 'inputProps'>
+export type AutocompleteProps = OmitInternalProps<OuterProps>
 /** @deprecated Use AutocompleteProps instead */
 export type Props = AutocompleteProps
