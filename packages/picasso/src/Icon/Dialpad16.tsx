d64c0a1657f171e8167d0d2817d203985cc20bf9
Yoav Nino <yoav.nino@toptal.com>
2021-11-05 15:39:59 -0300

    feat(icon): add dialpad16 icon

---
+++ packages/picasso/src/Icon/Dialpad16.tsx
@@ -0,0 +1,58 @@
+import React, { forwardRef, Ref } from 'react'
+import cx from 'classnames'
+import { makeStyles } from '@material-ui/core/styles'
+import { StandardProps } from '@toptal/picasso-shared'
+
+import kebabToCamelCase from '../utils/kebab-to-camel-case'
+import styles from './styles'
+const BASE_SIZE = 16
+
+type ScaleType = 1 | 2 | 3 | 4
+export interface Props extends StandardProps {
+  scale?: ScaleType;
+  color?: string;
+  base?: number;
+}
+const useStyles = makeStyles(styles, { name: 'PicassoSvgDialpad16' })
+const SvgDialpad16 = forwardRef(function SvgDialpad16(
+  props: Props,
+  ref: Ref<SVGSVGElement>
+) {
+  const {
+    className,
+    style = {},
+    color,
+    scale,
+    base,
+    'data-testid': testId
+  } = props
+  const classes: Record<string, string> = useStyles(props)
+  const classNames = [classes.root, className]
+  const scaledSize = base || BASE_SIZE * Math.ceil(scale || 1)
+  const colorClassName = kebabToCamelCase(`${color}`)
+
+  if (classes[colorClassName]) {
+    classNames.push(classes[colorClassName])
+  }
+
+  const svgStyle = {
+    minWidth: `${scaledSize}px`,
+    minHeight: `${scaledSize}px`,
+    ...style
+  }
+
+  return (
+    <svg
+      viewBox='0 0 16 16'
+      className={cx(...classNames)}
+      style={svgStyle}
+      ref={ref}
+      data-testid={testId}
+    >
+      <path d='M7.5 12a1.5 1.5 0 110 3 1.5 1.5 0 010-3zm0 1a.5.5 0 100 1 .5.5 0 000-1zm-5-5a1.5 1.5 0 110 3 1.5 1.5 0 010-3zm5 0a1.5 1.5 0 110 3 1.5 1.5 0 010-3zm5 0a1.5 1.5 0 110 3 1.5 1.5 0 010-3zm-10 1a.5.5 0 100 1 .5.5 0 000-1zm5 0a.5.5 0 100 1 .5.5 0 000-1zm5 0a.5.5 0 100 1 .5.5 0 000-1zm-10-5a1.5 1.5 0 110 3 1.5 1.5 0 010-3zm5 0a1.5 1.5 0 110 3 1.5 1.5 0 010-3zm5 0a1.5 1.5 0 110 3 1.5 1.5 0 010-3zm-10 1a.5.5 0 100 1 .5.5 0 000-1zm5 0a.5.5 0 100 1 .5.5 0 000-1zm5 0a.5.5 0 100 1 .5.5 0 000-1zm-10-5a1.5 1.5 0 110 3 1.5 1.5 0 010-3zm5 0a1.5 1.5 0 110 3 1.5 1.5 0 010-3zm5 0a1.5 1.5 0 110 3 1.5 1.5 0 010-3zm-10 1a.5.5 0 100 1 .5.5 0 000-1zm5 0a.5.5 0 100 1 .5.5 0 000-1zm5 0a.5.5 0 100 1 .5.5 0 000-1z' />
+    </svg>
+  )
+})
+
+SvgDialpad16.displayName = 'SvgDialpad16'
+export default SvgDialpad16
