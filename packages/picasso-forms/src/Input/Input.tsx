98b4010f5283206457aadeddc20dcf491e61dc5e
Huy Tran <tran.huy@toptal.com>
2021-11-08 23:02:02 +0700

    Revert "fix: bring back autocomplete to input (#2207)"
    This reverts commit 86954059806a0d637802e39dafa65f4bb4cb6ceb.

--- packages/picasso-forms/src/Input/Input.tsx
+++ packages/picasso-forms/src/Input/Input.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Input as PicassoInput, InputProps } from '@toptal/picasso'
 
 import FieldWrapper, { FieldProps } from '../FieldWrapper'
-import getInputName from './utils/get-input-name'
 
 export type FormInputProps = Omit<InputProps, 'onResetClick'> & {
   /** Callback invoked when reset button was clicked */
@@ -12,11 +11,9 @@ export type Props = FormInputProps & FieldProps<InputProps['value']>
 
 export const Input = React.forwardRef<HTMLInputElement, Props>((props, ref) => (
   <FieldWrapper<FormInputProps> {...props}>
-    {({ name, ...inputProps }: InputProps) => (
-      // TODO: remove getInputName completely when Chrome fixes autocomplete issue
-      // Link to the issue: https://bugs.chromium.org/p/chromium/issues/detail?id=1255609
-      <PicassoInput name={getInputName(name)} {...inputProps} ref={ref} />
-    )}
+    {(inputProps: InputProps) => {
+      return <PicassoInput {...inputProps} ref={ref} />
+    }}
   </FieldWrapper>
 ))
 
