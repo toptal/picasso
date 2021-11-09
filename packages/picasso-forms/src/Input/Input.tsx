ca8edd11dd837840534b7d8f0507e73f8f67badf
Tran Tan Huy <tran.huy@toptal.com>
2021-11-09 10:08:20 +0700

    fix: add warning about autocomplete

--- packages/picasso-forms/src/Input/Input.tsx
+++ packages/picasso-forms/src/Input/Input.tsx
@@ -9,9 +9,26 @@ export type FormInputProps = Omit<InputProps, 'onResetClick'> & {
 }
 export type Props = FormInputProps & FieldProps<InputProps['value']>
 
+const warnAutocompleteDisabledInput = (name?: string) => {
+  const autocompleteDisabled =
+    name && /^(((field|input)(_|-)?\d+)|tan|otp|title|captcha)$/.test(name)
+
+  if (autocompleteDisabled) {
+    console.warn(`
+In Chrome browser, autocomplete will be disabled for input[name="${name}"].
+Known bug: https://bugs.chromium.org/p/chromium/issues/detail?id=1255609
+    `)
+  }
+}
+
 export const Input = React.forwardRef<HTMLInputElement, Props>((props, ref) => (
   <FieldWrapper<FormInputProps> {...props}>
     {(inputProps: InputProps) => {
+      // TODO: remove this console.warn completely when Chrome fixes autocomplete issue
+      // Regex is taken from https://source.chromium.org/chromium/chromium/src/+/main:components/autofill/core/browser/autocomplete_history_manager.cc;l=53;drc=1d2260f9ed19c755db1631b7fb9b1ba216b323dc
+      // according to https://bugs.chromium.org/p/chromium/issues/detail?id=1255609
+      warnAutocompleteDisabledInput(inputProps.name)
+
       return <PicassoInput {...inputProps} ref={ref} />
     }}
   </FieldWrapper>
