91e0f6f997ca380ea852985acfbe74d737862e49
Tran Tan Huy <tran.huy@toptal.com>
2021-11-09 10:36:46 +0700

    fix: wrap it in useEffect to call once only

--- packages/picasso-forms/src/Input/Input.tsx
+++ packages/picasso-forms/src/Input/Input.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Input as PicassoInput, InputProps } from '@toptal/picasso'
 
 import FieldWrapper, { FieldProps } from '../FieldWrapper'
@@ -15,24 +15,26 @@ const warnAutocompleteDisabledInput = (name?: string) => {
 
   if (autocompleteDisabled) {
     console.warn(`
-In Chrome browser, autocomplete will be disabled for input[name="${name}"].
+In Chromium-based browsers, autocomplete might be disabled for input[name="${name}"].
 Known bug: https://bugs.chromium.org/p/chromium/issues/detail?id=1255609
     `)
   }
 }
 
-export const Input = React.forwardRef<HTMLInputElement, Props>((props, ref) => (
-  <FieldWrapper<FormInputProps> {...props}>
-    {(inputProps: InputProps) => {
-      // TODO: remove this console.warn completely when Chrome fixes autocomplete issue
-      // Regex is taken from https://source.chromium.org/chromium/chromium/src/+/main:components/autofill/core/browser/autocomplete_history_manager.cc;l=53;drc=1d2260f9ed19c755db1631b7fb9b1ba216b323dc
-      // according to https://bugs.chromium.org/p/chromium/issues/detail?id=1255609
-      warnAutocompleteDisabledInput(inputProps.name)
+export const Input = React.forwardRef<HTMLInputElement, Props>((props, ref) => {
+  useEffect(() => {
+    // TODO: remove this console.warn completely when Chrome fixes autocomplete issue
+    // Regex is taken from https://source.chromium.org/chromium/chromium/src/+/main:components/autofill/core/browser/autocomplete_history_manager.cc;l=53;drc=1d2260f9ed19c755db1631b7fb9b1ba216b323dc
+    // according to https://bugs.chromium.org/p/chromium/issues/detail?id=1255609
+    warnAutocompleteDisabledInput(props.name)
+  }, [props.name])
 
-      return <PicassoInput {...inputProps} ref={ref} />
-    }}
-  </FieldWrapper>
-))
+  return (
+    <FieldWrapper<FormInputProps> {...props}>
+      {(inputProps: InputProps) => <PicassoInput {...inputProps} ref={ref} />}
+    </FieldWrapper>
+  )
+})
 
 Input.defaultProps = {}
 
