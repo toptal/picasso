b2332734ba730e9c001638bee47d6ce1f3b87a22
Michał Bednarz <michal.bednarz@toptal.com>
2021-11-16 13:16:11 +0100

    feat(icon): add new icon (#2236)

---
+++ packages/picasso/src/Icon/New16.tsx
@@ -0,0 +1,63 @@
+import React, { forwardRef, Ref } from 'react'
+import cx from 'classnames'
+import { makeStyles } from '@material-ui/core/styles'
+import { StandardProps } from '@toptal/picasso-shared'
+
+import kebabToCamelCase from '../utils/kebab-to-camel-case'
+import styles from './styles'
+const BASE_SIZE = 16
+
+type ScaleType = 1 | 2 | 3 | 4
+export interface Props extends StandardProps {
+  scale?: ScaleType
+  color?: string
+  base?: number
+}
+const useStyles = makeStyles(styles, { name: 'PicassoSvgNew16' })
+const SvgNew16 = forwardRef(function SvgNew16(
+  props: Props,
+  ref: Ref<SVGSVGElement>
+) {
+  const {
+    className,
+    style = {},
+    color,
+    scale,
+    base,
+    'data-testid': testId
+  } = props
+  const classes: Record<string, string> = useStyles(props)
+  const classNames = [classes.root, className]
+  const scaledSize = base || BASE_SIZE * Math.ceil(scale || 1)
+  const colorClassName = kebabToCamelCase(`${color}`)
+
+  if (classes[colorClassName]) {
+    classNames.push(classes[colorClassName])
+  }
+
+  const svgStyle = {
+    minWidth: `${scaledSize}px`,
+    minHeight: `${scaledSize}px`,
+    ...style
+  }
+
+  return (
+    <svg
+      viewBox='0 0 16 16'
+      fill='none'
+      className={cx(...classNames)}
+      style={svgStyle}
+      ref={ref}
+      data-testid={testId}
+    >
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M8 1v1H2v12h12V8h1v7H1V1h7zm4 0l3 3-6 6H6V7l6-6zM7 7.414l5-4.999L13.585 4 8.584 9H7V7.414z'
+      />
+    </svg>
+  )
+})
+
+SvgNew16.displayName = 'SvgNew16'
+export default SvgNew16
