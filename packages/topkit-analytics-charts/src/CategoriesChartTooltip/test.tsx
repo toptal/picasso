0d5f96fb4ca1e50ee2e301d74f8774db855623ca
Jini <lasha.jini@toptal.com>
2021-11-17 19:42:38 +0400

    refactor: revert missing data-testid

--- packages/topkit-analytics-charts/src/CategoriesChartTooltip/test.tsx
+++ packages/topkit-analytics-charts/src/CategoriesChartTooltip/test.tsx
@@ -3,6 +3,10 @@ import { render, screen, TestingPicasso } from '@toptal/picasso/test-utils'
 
 import CategoriesChartTooltip, { Props } from './CategoriesChartTooltip'
 
+const testIds = {
+  paper: 'tooltip-content'
+}
+
 const renderTooltip = (props: Partial<Props>) => {
   const defaultProps = {
     active: true,
@@ -23,5 +27,24 @@ describe('CategoriesChartTooltip', () => {
     renderTooltip({ active: false })
 
     expect(screen.queryByTestId('tooltip')).not.toBeInTheDocument()
+  })
+
+  it('renders tooltip when active and has payload', () => {
+    const { getByTestId } = renderTooltip({
+      active: true,
+      originalData: [
+        {
+          id: 'payload-name',
+          values: [
+            { id: 'value1', values: [] },
+            { id: 'value2', values: [] }
+          ]
+        }
+      ],
+      payload: [{ payload: { name: 'payload-name', team: 0, user: 0 } }],
+      testIds: { paper: testIds.paper }
+    })
+
+    expect(getByTestId(testIds.paper)).not.toBeNull()
   })
 })
