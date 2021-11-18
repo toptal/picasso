37d2b07e9b5fdaed013ca96f43bc744374f1839f
Elvio Cavalcante <elvio.cavalcante@toptal.com>
2021-11-18 15:29:05 -0300

    feat(Icon): add new arrow subdirectory icon

---
+++ packages/picasso/src/Icon/ArrowSubdirectory24.tsx
@@ -0,0 +1,58 @@
+import React, { forwardRef, Ref } from 'react'
+import cx from 'classnames'
+import { makeStyles } from '@material-ui/core/styles'
+import { StandardProps } from '@toptal/picasso-shared'
+
+import kebabToCamelCase from '../utils/kebab-to-camel-case'
+import styles from './styles'
+const BASE_SIZE = 24
+
+type ScaleType = 1 | 2 | 3 | 4
+export interface Props extends StandardProps {
+  scale?: ScaleType
+  color?: string
+  base?: number
+}
+const useStyles = makeStyles(styles, { name: 'PicassoSvgArrowSubdirectory24' })
+const SvgArrowSubdirectory24 = forwardRef(function SvgArrowSubdirectory24(
+  props: Props,
+  ref: Ref<SVGSVGElement>
+) {
+  const {
+    className,
+    style = {},
+    color,
+    scale,
+    base,
+    'data-testid': testId
+  } = props
+  const classes: Record<string, string> = useStyles(props)
+  const classNames = [classes.root, className]
+  const scaledSize = base || BASE_SIZE * Math.ceil(scale || 1)
+  const colorClassName = kebabToCamelCase(`${color}`)
+
+  if (classes[colorClassName]) {
+    classNames.push(classes[colorClassName])
+  }
+
+  const svgStyle = {
+    minWidth: `${scaledSize}px`,
+    minHeight: `${scaledSize}px`,
+    ...style
+  }
+
+  return (
+    <svg
+      viewBox='0 0 24 24'
+      className={cx(...classNames)}
+      style={svgStyle}
+      ref={ref}
+      data-testid={testId}
+    >
+      <path d='M15.5 18.207l-.707-.707 4.499-4.5H3V6h1v6h15.293l-4.5-4.5.707-.707 5 5 .707.707-5.707 5.707z' />
+    </svg>
+  )
+})
+
+SvgArrowSubdirectory24.displayName = 'SvgArrowSubdirectory24'
+export default SvgArrowSubdirectory24
