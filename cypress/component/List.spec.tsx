79798b2d5639cba6cd0b6a099d79f9759b977e3b
Jini <lasha.jini@toptal.com>
2021-12-03 14:17:14 +0400

    chore: re-define helper function

--- cypress/component/List.spec.tsx
+++ cypress/component/List.spec.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 import { List, Referrals16 } from '@toptal/picasso'
 import { mount } from '@cypress/react'
 import { TestingPicasso } from '@toptal/picasso/test-utils'
-import { generateListItems } from '@toptal/picasso/src/List/utils'
+
+/* eslint-disable react/no-array-index-key */
+const generateListItems = (total: number, listItemProps?: any) =>
+  Array(total)
+    .fill(0)
+    .map((_, index) => (
+      <List.Item key={index} {...listItemProps}>
+        {`list item N${index + 1}`}
+      </List.Item>
+    ))
 
 describe('List', () => {
   describe('Unordered', () => {
