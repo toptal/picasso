72775c75ef35badf94e881c9e1641aa6cdc2df0f
Jini <lasha.jini@toptal.com>
2021-12-06 17:30:49 +0400

    chore: update cypress tests

--- cypress/component/Container.spec.tsx
+++ cypress/component/Container.spec.tsx
@@ -2,6 +2,16 @@ import React from 'react'
 import { mount } from '@cypress/react'
 import { Container, ContainerProps } from '@toptal/picasso'
 import { TestingPicasso } from '@toptal/picasso/test-utils'
+import { VariantType } from '@toptal/picasso/src/Container'
+
+const colors: VariantType[] = [
+  'red',
+  'green',
+  'white',
+  'yellow',
+  'blue',
+  'grey'
+]
 
 const TestContainer = (props: ContainerProps) => (
   <Container {...props}>{props.children}</Container>
@@ -15,5 +25,60 @@ describe('Container', () => {
       </TestingPicasso>
     )
     cy.get('body').happoScreenshot()
+  })
+
+  describe('colored variants', () => {
+    it('renders all variants', () => {
+      mount(
+        <TestingPicasso>
+          {colors.map(color => (
+            <TestContainer
+              padded='medium'
+              bottom='small'
+              top='small'
+              variant={color}
+              key={color}
+            >
+              {color} variant
+            </TestContainer>
+          ))}
+        </TestingPicasso>
+      )
+      cy.get('body').happoScreenshot()
+    })
+
+    it('renders all variants with bordered prop', () => {
+      mount(
+        <TestingPicasso>
+          {colors.map(color => (
+            <TestContainer
+              bordered
+              padded='medium'
+              bottom='small'
+              top='small'
+              variant={color}
+              data-testid={`${color}-container`}
+              key={color}
+            >
+              {color} variant
+            </TestContainer>
+          ))}
+        </TestingPicasso>
+      )
+
+      colors.forEach(color => {
+        /* eslint-disable cypress/no-assigning-return-values */
+        const container = cy.get(`[data-testid="${color}-container"`)
+
+        container.invoke('attr', 'class').should('contain', `${color}Variant`)
+
+        if (color !== 'white') {
+          container.should('not.contain', 'bordered')
+        } else {
+          container.should('contain', 'bordered')
+        }
+      })
+      cy.get('body').happoScreenshot()
+    })
   })
 })
