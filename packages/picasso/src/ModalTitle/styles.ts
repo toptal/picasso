99e3f2f59124d75dddc57bba71742f6f20fab88f
Tomas Tamagnone <tomas.tamagnone@toptal.com>
2021-12-07 18:09:48 -0300

    feat(modals): add right padding to modal title

--- packages/picasso/src/ModalTitle/styles.ts
+++ packages/picasso/src/ModalTitle/styles.ts
@@ -3,6 +3,7 @@ import { createStyles } from '@material-ui/core/styles'
 export default () =>
   createStyles({
     root: {
-      margin: '2em 2em 0'
+      margin: '2em 2em 0',
+      paddingRight: '1.5em'
     }
   })
