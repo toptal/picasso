89d381106fe7bbfbac766a8692fc54d18f259259
Yusuf Yilmaz <yusuf.yilmaz@toptal.com>
2021-12-10 14:08:49 +0100

    fix: add fix for lint error

--- packages/picasso/src/NumberInput/NumberInput.tsx
+++ packages/picasso/src/NumberInput/NumberInput.tsx
@@ -47,8 +47,8 @@ type NumberAdornmentProps = {
   disabled?: boolean
 }
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const nativeInputValueSetter = isBrowser()
-  /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */
   ? (Object.getOwnPropertyDescriptor(
       window.HTMLInputElement.prototype,
       'value'
