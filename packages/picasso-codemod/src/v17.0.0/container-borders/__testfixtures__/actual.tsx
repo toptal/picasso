9b5ece695dedad1d6b66364b10e85acfa337dd90
Jini <lasha.jini@toptal.com>
2021-12-13 16:42:43 +0400

    chore: add codemod

---
+++ packages/picasso-codemod/src/v17.0.0/container-borders/__testfixtures__/actual.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Container } from '@toptal/picasso'
+
+export default () => (
+  <>
+    <Container>text</Container>
+    <Container variant='red'>text</Container>
+    <Container variant='white'>text</Container>
+    <Container variant='red' bordered>
+      text
+    </Container>
+    <Container variant='white' bordered>
+      text
+    </Container>
+    <Container variant='transparent' bordered>
+      text
+    </Container>
+    <Container variant={undefined} bordered>
+      text
+    </Container>
+    <Container bordered>text</Container>
+  </>
+)
