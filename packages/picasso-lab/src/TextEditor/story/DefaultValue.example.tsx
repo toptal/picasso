390957edc918c086021180b1e86f3c2d93e581a6
Tomáš Sláma <tomas.slama@toptal.com>
2021-12-19 16:57:25 +0100

    style(TextEditor): add lists support to texteditor component (#2334)

--- packages/picasso-lab/src/TextEditor/story/DefaultValue.example.tsx
+++ packages/picasso-lab/src/TextEditor/story/DefaultValue.example.tsx
@@ -4,7 +4,7 @@ import { Container } from '@toptal/picasso'
 
 const Example = () => {
   const [value, setValue] = useState<string | undefined>(
-    '<p>Look at my test, my test is <strong>amazing!</strong></p>'
+    '<h3>Position Description</h3><p>We’re looking for hardworking, self-starting Designers for our Product Design team to help us define how talent interacts with Toptal. You’ll build beautiful and inspiring design experiences that help users discover and connect with resources they need in truly innovative ways.</p><p>We’re looking for hardworking, self-starting Designers for our Product Design team to help us define how talent interacts with Toptal. You’ll build beautiful and inspiring design experiences that help users discover and connect with resources they need in truly innovative ways.</p><h3>Requirements</h3><ol><li>Collaborate with PMs and other designers to ship your first product features.</li><li>Learn about our design system.</li></ol><h3>Requirements</h3><ul><li>Proficiency with various design and prototyping tools (such as Sketch, Abstract, Marvel, Principle, Figma), as well as knowledge of HTML and CSS.</li><li>An understanding that phenomenal experiences come from collaborative decision-making with front-end developers, engineers, researchers, content strategists, and other disciplines.</li></ul>'
   )
 
   const handleChange: TextEditorChangeHandler = newValue => setValue(newValue)
--- packages/picasso-lab/src/TextEditor/styles.ts
+++ packages/picasso-lab/src/TextEditor/styles.ts
@@ -1,8 +1,66 @@
 import { createStyles } from '@material-ui/core/styles'
+import { rem } from '@toptal/picasso-shared/styles'
+
+const margins = {
+  '& p': {
+    margin: '0.5rem 0'
+  },
+  '& h3': {
+    margin: '1rem 0 0.5rem'
+  },
+  '& p:first-child, & h3:first-child': {
+    margin: '0 0 0.5rem'
+  },
+  '& li:not(:last-child)': {
+    margin: '0 0 0.5rem'
+  },
+  '& ol, & ul': {
+    padding: 0,
+    margin: '0.5rem 0'
+  }
+}
+
+const listStyles = {
+  '& li': {
+    listStyleType: 'none',
+    paddingLeft: '2rem',
+    position: 'relative',
+    '&:before': {
+      display: 'inline-block',
+      position: 'absolute',
+      left: 0,
+      whiteSpace: 'nowrap',
+      width: '1rem'
+    }
+  },
+  '& ol li': {
+    counterIncrement: 'list-0',
+    '&:before': {
+      content: 'counter(list-0, decimal) "."'
+    }
+  },
+  '& *:not(li)': {
+    counterReset: 'list-0'
+  },
+  '& ul li': {
+    '&:before': {
+      content: '""',
+      backgroundImage:
+        "url(\"data:image/svg+xml,%3Csvg width='16' height='16' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'%3E%3Cdefs%3E%3Cpath d='M8 6c1.1046 0 2 .8954 2 2s-.8954 2-2 2-2-.8954-2-2 .8954-2 2-2Z' id='a'/%3E%3C/defs%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cmask id='b' fill='%23fff'%3E%3Cuse xlink:href='%23a'/%3E%3C/mask%3E%3Cuse fill='%23979797' fill-rule='nonzero' xlink:href='%23a'/%3E%3Cg mask='url(%23b)' fill='%23455065'%3E%3Cpath d='M0 0h16v16H0z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E\")",
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      height: rem('22px'),
+      width: '1rem'
+    }
+  }
+}
 
 export default () =>
   createStyles({
     root: {
-      height: 'calc(100% - 43px)' // TODO proper style editor
+      height: 'calc(100% - 43px)',
+
+      ...listStyles,
+      ...margins
     }
   })
