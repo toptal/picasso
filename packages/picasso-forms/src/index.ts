36f8c3294f71fdeced61bfc3c5449ca63d204066
Timofei Kachalov <timofei.kachalov@toptal.com>
2021-12-23 15:22:21 +0300

    chore(picasso-forms): add additional reexports from final-form packages

--- packages/picasso-forms/src/index.ts
+++ packages/picasso-forms/src/index.ts
@@ -1,9 +1,9 @@
 
 // Final Form exports
-export type { FormApi, MutableState, AnyObject, FieldValidator, SubmissionErrors, Config } from 'final-form'
+export type { FormApi, FormState, FieldState, MutableState, AnyObject, FieldValidator, SubmissionErrors, Config } from 'final-form'
 export { FORM_ERROR, setIn } from 'final-form'
 export { useForm, useField, useFormState, FormSpy, Form as FinalForm, Field as FinalField } from 'react-final-form'
-export type { FieldMetaState, FieldRenderProps, FormRenderProps, FieldProps, FormProps, FieldInputProps } from 'react-final-form'
+export type { FieldMetaState, FieldRenderProps, FormRenderProps, FormSpyRenderProps, FieldProps, FormProps, FieldInputProps } from 'react-final-form'
 export { default as arrayMutators } from 'final-form-arrays'
 export { useFieldArray, FieldArray } from 'react-final-form-arrays'
 export type { FieldArrayProps, FieldArrayRenderProps } from 'react-final-form-arrays'
