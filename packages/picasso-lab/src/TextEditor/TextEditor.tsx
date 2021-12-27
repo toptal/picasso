cd2af1b23a6620a6b2e5729da0ea8e93629f0c32
Jini <lasha.jini@toptal.com>
2021-12-27 14:22:44 +0400

    chore: remove comment

--- packages/picasso-lab/src/TextEditor/TextEditor.tsx
+++ packages/picasso-lab/src/TextEditor/TextEditor.tsx
@@ -117,9 +117,6 @@ export const TextEditor = forwardRef<HTMLDivElement, Props>(function TextEditor(
     <Container className={classes.editorWrapper}>
       <Container id={`${id}toolbar`} className={classes.qlToolbar}>
         <Container className={classes.qlFormats}>
-          {
-            // TODO: add styling to options
-          }
           <Select
             onChange={handleChange}
             options={OPTIONS}
