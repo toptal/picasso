6d4ee46110aca98889a6c6c1dcb5b49e66f6be0e
Daniel Ostapenko <daniel.ostapenko@toptal.com>
2021-12-30 12:34:47 +0200

    chore: fix typecheck for fixtures

--- packages/picasso-codemod/src/v17.0.0/container-borders/__testfixtures__/actual.tsx
+++ packages/picasso-codemod/src/v17.0.0/container-borders/__testfixtures__/actual.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import React from 'react'
 import { Container } from '@toptal/picasso'
 
@@ -6,10 +8,18 @@ export default () => (
     <Container>text</Container>
     <Container variant='red'>text</Container>
     <Container variant='white'>text</Container>
-    <Container variant='red' bordered>text</Container>
-    <Container variant='white' bordered>text</Container>
-    <Container variant='transparent' bordered>text</Container>
-    <Container variant={undefined} bordered>text</Container>
+    <Container variant='red' bordered>
+      text
+    </Container>
+    <Container variant='white' bordered>
+      text
+    </Container>
+    <Container variant='transparent' bordered>
+      text
+    </Container>
+    <Container variant={undefined} bordered>
+      text
+    </Container>
     <Container bordered>text</Container>
   </>
 )
--- packages/picasso-codemod/src/v17.0.0/container-borders/__testfixtures__/expected.tsx
+++ packages/picasso-codemod/src/v17.0.0/container-borders/__testfixtures__/expected.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import React from 'react'
 import { Container } from '@toptal/picasso'
 
@@ -7,9 +9,15 @@ export default () => (
     <Container variant='red'>text</Container>
     <Container variant='white'>text</Container>
     <Container variant='red'>text</Container>
-    <Container variant='white' bordered>text</Container>
-    <Container variant='transparent' bordered>text</Container>
-    <Container variant={undefined} bordered>text</Container>
+    <Container variant='white' bordered>
+      text
+    </Container>
+    <Container variant='transparent' bordered>
+      text
+    </Container>
+    <Container variant={undefined} bordered>
+      text
+    </Container>
     <Container bordered>text</Container>
   </>
 )
