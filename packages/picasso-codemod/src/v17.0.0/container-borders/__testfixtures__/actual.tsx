17f60eb618ce33b72449d0b16d4c8dc8b2a5cb2d
Daniel Ostapenko <daniel.ostapenko@toptal.com>
2021-12-30 13:06:29 +0200

    chore: fix tests

--- packages/picasso-codemod/src/v17.0.0/container-borders/__testfixtures__/actual.tsx
+++ packages/picasso-codemod/src/v17.0.0/container-borders/__testfixtures__/actual.tsx
@@ -1,3 +1,5 @@
+// prettier-ignore
+
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 import React from 'react'
@@ -8,18 +10,10 @@ export default () => (
     <Container>text</Container>
     <Container variant='red'>text</Container>
     <Container variant='white'>text</Container>
-    <Container variant='red' bordered>
-      text
-    </Container>
-    <Container variant='white' bordered>
-      text
-    </Container>
-    <Container variant='transparent' bordered>
-      text
-    </Container>
-    <Container variant={undefined} bordered>
-      text
-    </Container>
+    <Container variant='red' bordered>text</Container>
+    <Container variant='white' bordered>text</Container>
+    <Container variant='transparent' bordered>text</Container>
+    <Container variant={undefined} bordered>text</Container>
     <Container bordered>text</Container>
   </>
 )
--- packages/picasso-codemod/src/v17.0.0/container-borders/__testfixtures__/expected.tsx
+++ packages/picasso-codemod/src/v17.0.0/container-borders/__testfixtures__/expected.tsx
@@ -9,15 +9,9 @@ export default () => (
     <Container variant='red'>text</Container>
     <Container variant='white'>text</Container>
     <Container variant='red'>text</Container>
-    <Container variant='white' bordered>
-      text
-    </Container>
-    <Container variant='transparent' bordered>
-      text
-    </Container>
-    <Container variant={undefined} bordered>
-      text
-    </Container>
+    <Container variant='white' bordered>text</Container>
+    <Container variant='transparent' bordered>text</Container>
+    <Container variant={undefined} bordered>text</Container>
     <Container bordered>text</Container>
   </>
 )
