9e93968a0cefecef493e89f64abf8065b3e81ad8
Tomáš Sláma <tomas.slama@toptal.com>
2022-01-12 15:15:56 +0100

    refactor(TextEditor): cleanup useAutofocus hook and add test (#2378)

---
+++ packages/picasso-lab/src/TextEditor/hooks/useDisabledEditor/test.ts
@@ -0,0 +1,44 @@
+import { renderHook } from '@testing-library/react-hooks'
+
+import useDisabledEditor from './useDisabledEditor'
+
+describe('useDisabledEditor', () => {
+  describe('when disabled is off', () => {
+    it('does not disable the editor', () => {
+      const disabled = false
+      const ref = {
+        current: {
+          enable: jest.fn()
+        }
+      }
+
+      renderHook(() => useDisabledEditor({ disabled, ref }))
+
+      expect(ref.current.enable).not.toHaveBeenCalled()
+    })
+  })
+
+  describe('when disabled is on', () => {
+    it('disables the editor', () => {
+      const ref = {
+        current: {
+          enable: jest.fn()
+        }
+      }
+
+      const { rerender } = renderHook(
+        ({ disabled }) => useDisabledEditor({ disabled, ref }),
+        {
+          initialProps: {
+            disabled: true
+          }
+        }
+      )
+
+      expect(ref.current.enable).toHaveBeenCalledWith(false)
+      rerender({ disabled: false })
+      expect(ref.current.enable).toHaveBeenCalledWith(true)
+      expect(ref.current.enable).toHaveBeenCalledTimes(2)
+    })
+  })
+})
---
+++ packages/picasso-lab/src/TextEditor/hooks/useQuillInstance/useQuillInstance.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useRef } from 'react'
+import Quill, { QuillOptionsStatic } from 'quill'
+
+import {
+  useTypographyClasses,
+  makeHeaderFormat,
+  makeBoldFormat
+} from '../../formats'
+import { EditorRefType } from '../../types'
+import { Props } from '../../TextEditor'
+
+type EditorOptionsType = {
+  id: Props['id']
+  placeholder?: Props['placeholder']
+}
+
+const getModules = (
+  id: EditorOptionsType['id']
+): QuillOptionsStatic['modules'] => {
+  return {
+    // tools we provide to format text
+    // https://quilljs.com/docs/modules/toolbar/
+    toolbar: {
+      // There is issue in quill which does not allow us to have multiple
+      // editors with single toolbar on the page:
+      // https://github.com/quilljs/quill/issues/633 So, I've added unique id
+      // for each of them (using class selector does not work either, custom
+      // toolbar is still applied on the first editor)
+      container: `#${id}toolbar`
+    },
+    clipboard: {
+      matchVisual: false
+    }
+  }
+}
+
+/**
+ * Formats we allow to paste into editor
+ *
+ * This is separate from adding a control in the Toolbar.
+ * For example, you can configure Quill to allow bolded
+ * content to be pasted into an editor that has no bold
+ * button in the toolbar. */
+const formats: QuillOptionsStatic['formats'] = [
+  'bold',
+  'italic',
+  'header',
+  'list'
+]
+
+const useQuillInstance = ({
+  id,
+  placeholder
+}: EditorOptionsType): EditorRefType => {
+  const ref = useRef<Quill>()
+  const typographyClasses = useTypographyClasses()
+
+  useEffect(() => {
+    Quill.register(makeHeaderFormat(typographyClasses), true)
+    Quill.register(makeBoldFormat(typographyClasses), true)
+
+    ref.current = new Quill(`#${id}`, {
+      modules: getModules(id),
+      formats,
+      placeholder
+    })
+  }, [id, placeholder, ref, typographyClasses])
+
+  return ref
+}
+
+export default useQuillInstance
