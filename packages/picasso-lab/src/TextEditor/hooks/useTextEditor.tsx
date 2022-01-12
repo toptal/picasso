23145d2800dd7785a48974913ae5f8a7cdf28db8
Tomáš Sláma <tomas.slama@toptal.com>
2022-01-12 15:51:13 +0100

    refactor(TextEditor): add loose focus fix for toolbar (#2379)

--- packages/picasso-lab/src/TextEditor/hooks/useTextEditor.tsx
+++ packages/picasso-lab/src/TextEditor/hooks/useTextEditor.tsx
@@ -2,6 +2,8 @@ import { Props as TextEditorProps } from '../TextEditor'
 import useAutofocus from './useAutofocus'
 import useQuillInstance from './useQuillInstance'
 import useDisabledEditor from './useDisabledEditor'
+import useEditorLoseFocusFix from './useEditorLoseFocusFix'
+import useEditorLoseFocusFixHandler from './useEditorLoseFocusFixHandler'
 
 type Props = {
   id: TextEditorProps['id']
@@ -18,6 +20,17 @@ const useTextEditor = ({ autofocus, disabled, id, placeholder }: Props) => {
 
   useDisabledEditor({ ref: quillInstanceRef, disabled })
   useAutofocus({ ref: quillInstanceRef, autofocus })
+
+  // common issue of custom toolbar
+  // https://github.com/quilljs/quill/issues/1290
+  // when clicking anywhere quill loses focus, we need
+  // to prevent it when clicking inside toolbar
+  const { preventDefaultHandler } = useEditorLoseFocusFixHandler()
+
+  useEditorLoseFocusFix({
+    ref: quillInstanceRef,
+    handler: preventDefaultHandler
+  })
 }
 
 export default useTextEditor
