cd5d94ffe74345759b324bcbc81f0a1fb1062efe
Tomáš Sláma <tomas.slama@toptal.com>
2022-01-13 13:41:41 +0100

    refactor(TextEditor): simplify test

--- packages/picasso-lab/src/TextEditor/hooks/useTextChange/test.ts
+++ packages/picasso-lab/src/TextEditor/hooks/useTextChange/test.ts
@@ -1,44 +1,29 @@
-import { MutableRefObject } from 'react'
-import Quill, { EventEmitter, TextChangeHandler } from 'quill'
+import Quill from 'quill'
 import { renderHook } from '@testing-library/react-hooks'
-import { createEvent, fireEvent } from '@toptal/picasso/test-utils'
 
-import useTextChange from '.'
+import useTextChange from './useTextChange'
 
 describe('useTextChange', () => {
-  let mockElement: Element
-
-  beforeAll(() => {
-    mockElement = document.createElement('div')
-    jest.spyOn(mockElement, 'addEventListener')
-    jest.spyOn(mockElement, 'removeEventListener')
-  })
-
-  it('calls handler on editors text-change event', () => {
+  it('subscribes handler to quills event', () => {
     const handler = jest.fn()
-    const ref: MutableRefObject<Quill> = {
-      current: {
-        on: (type: 'text-change', cb: TextChangeHandler) => {
-          mockElement.addEventListener(
-            type,
-            (cb as unknown) as (this: Element, event: Event) => void
-          )
-
-          return (ref.current.on as unknown) as EventEmitter
-        },
-        off: (type: 'text-change', cb: TextChangeHandler) => {
-          mockElement.removeEventListener(
-            type,
-            (cb as unknown) as (this: Element, event: Event) => void
-          )
-        }
-      } as Quill
+    const ref = {
+      current: ({
+        on: jest.fn(),
+        off: jest.fn()
+      } as unknown) as Quill
     }
 
-    renderHook(() => useTextChange({ ref, handler }))
+    const { unmount } = renderHook(() => useTextChange({ ref, handler }))
 
-    fireEvent(mockElement, createEvent('text-change', mockElement))
+    const { on, off } = ref.current
 
-    expect(handler).toHaveBeenCalledTimes(1)
+    expect(on).toHaveBeenCalledTimes(1)
+    expect(on).toHaveBeenCalledWith('text-change', handler)
+    expect(off).not.toHaveBeenCalled()
+
+    unmount()
+
+    expect(off).toHaveBeenCalledTimes(1)
+    expect(off).toHaveBeenCalledWith('text-change', handler)
   })
 })
