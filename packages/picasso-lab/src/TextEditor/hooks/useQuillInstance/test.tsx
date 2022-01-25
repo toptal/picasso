29515d5b729c194d267fda629791c4dea8c281d2
Tomáš Sláma <tomas.slama@toptal.com>
2022-01-25 17:50:11 +0100

    test(TextEditor): test usage of Quill constructor (#2405)

--- packages/picasso-lab/src/TextEditor/hooks/useQuillInstance/test.tsx
+++ packages/picasso-lab/src/TextEditor/hooks/useQuillInstance/test.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import { TestingPicasso, render } from '@toptal/picasso/test-utils'
 import { renderHook } from '@testing-library/react-hooks'
+import Quill from 'quill'
 
 import useQuillInstance from './useQuillInstance'
 import { ActionCreatorsType } from '../../types'
+
+type QuilMockType = jest.Mock & {
+  import: jest.Mock
+  register: jest.Mock
+}
+
+jest.mock('quill', () => {
+  const QuillMock = jest.fn() as QuilMockType
+
+  QuillMock.import = jest.fn().mockImplementation(() => null)
+  QuillMock.register = jest.fn()
+
+  return {
+    __esModule: true,
+    default: QuillMock
+  }
+})
 
 describe('useQuillInstance', () => {
   it('returns quill instance in ref', () => {
@@ -32,5 +50,6 @@ describe('useQuillInstance', () => {
     })
 
     expect(result.current).toBeTruthy()
+    expect(Quill).toHaveBeenCalledWith(`#${id}`, expect.any(Object))
   })
 })
