08b0535f917b117af73172e882204a7124d88dac
Jini <lasha.jini@toptal.com>
2022-01-31 16:27:32 +0400

    fix: codemod

--- packages/picasso-codemod/src/v17.0.0/picasso-lab/picasso-lab.ts
+++ packages/picasso-codemod/src/v17.0.0/picasso-lab/picasso-lab.ts
@@ -1,12 +1,12 @@
 import { Transform } from 'jscodeshift'
+
 const transform: Transform = (file, api) => {
   const j = api.jscodeshift
 
   return j(file.source)
     .find(j.StringLiteral)
-    .forEach(path => path.value.value.includes('picasso-lab'))
     .map(path => {
-      path.value.value.replace('picasso-lab', 'picasso')
+      path.value.value = path.value.value.replace('picasso-lab', 'picasso')
 
       return path
     })
