352188d852624be82830af4fa4b017f4ca2e7fea
Daniel Ostapenko <daniel.ostapenko@toptal.com>
2022-02-10 10:38:14 +0200

    chore: add eslintrc file in the fixtures folder

---
+++ packages/picasso-codemod/src/v18.0.0/picasso-lab/__testfixtures__/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: '../../../../../../.eslintrc.js',
+  rules: {
+    'import/order': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'import/no-duplicates': 'off',
+    'import/no-named-default': 'off'
+  }
+}
