96e0df4315e83aa6d3514f72e037f1b6a9eaac02
Jini <lasha.jini@toptal.com>
2022-02-10 15:30:46 +0400

    chore: update test

--- packages/picasso-codemod/src/v18.0.0/picasso-lab/__testfixtures__/basic.output.tsx
+++ packages/picasso-codemod/src/v18.0.0/picasso-lab/__testfixtures__/basic.output.tsx
@@ -1,12 +1,12 @@
 /* TOP-LEVEL comment */
 
 /* still TOP-LEVEL comment */
-import { _MUI } from 'mui'
-import { _TestUtils } from '@toptal/picasso/test-utils'
-import { _Utils } from '@toptal/picasso/utils'
+import { _MUI } from 'mui';
+import { _TestUtils } from '@toptal/picasso/test-utils';
+import { _Utils } from '@toptal/picasso/utils';
 
 /* comment 1 */
-import { _Icon } from '@toptal/picasso/Icon'
+import { _Icon } from '@toptal/picasso/Icon';
 
 // comment 2
 /* comment 3 */
@@ -24,18 +24,18 @@ import _Picasso2, {
  _Component1Type,
  default as _Component5Default,
  Component6 as _CustomName1
-} from '@toptal/picasso'
+} from '@toptal/picasso';
 
 /* @ts-rule */
-import { _useState } from 'react'
+import { _useState } from 'react';
 
-import * as _PicassoLab from '@toptal/picasso'
-import * as _Picasso1 from '@toptal/picasso'
+import * as _PicassoLab from '@toptal/picasso';
+import * as _Picasso1 from '@toptal/picasso';
 
-import { _NOT_EFFECTED1 } from '@toptal/picasso-shared'
+import { _NOT_EFFECTED1 } from '@toptal/picasso-shared';
 
-import { _NOT_EFFECTED2 } from '../RelativePath'
+import { _NOT_EFFECTED2 } from '../RelativePath';
 
-const foo = (res: string) => res
+const foo = (res: string) => res;
 
-foo('@toptal/picasso')
+foo('@toptal/picasso');
