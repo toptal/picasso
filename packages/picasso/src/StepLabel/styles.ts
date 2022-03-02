cd39e12b96e88d870e7470310291cbb7b9a89ce3
George Dulishkovych <george.dulishkovych@toptal.com>
2022-03-02 17:46:19 +0200

    feat(Stepper): make component responsive for small screens (#2478)

--- packages/picasso/src/StepLabel/styles.ts
+++ packages/picasso/src/StepLabel/styles.ts
@@ -1,6 +1,7 @@
 import { Theme, createStyles } from '@material-ui/core/styles'
 import { rem } from '@toptal/picasso-shared'
 import { PicassoProvider } from '@toptal/picasso-provider'
+import { breakpoints } from '@toptal/picasso/utils'
 
 PicassoProvider.override(() => ({
   MuiStepLabel: {
@@ -17,7 +18,7 @@ PicassoProvider.override(() => ({
   }
 }))
 
-export default ({ palette }: Theme) =>
+export default ({ palette, breakpoints: { down } }: Theme) =>
   createStyles({
     hidden: {
       display: 'none'
@@ -29,6 +30,9 @@ export default ({ palette }: Theme) =>
       fontSize: rem('11px'),
       fontWeight: 600,
       lineHeight: '1em',
-      color: palette.grey.dark
+      color: palette.grey.dark,
+      [down(breakpoints.small)]: {
+        display: 'none'
+      }
     }
   })
