f521bf21e23f9d9833f0fa89e9e47498356e5162
Augusto Moura <augusto.moura@toptal.com>
2022-03-02 21:01:11 -0300

    feat: rename rating-stars codemod to rename-rating-stars

--- packages/picasso-codemod/src/v19.0.0/rating-stars/__tests__/test.ts
+++
@@ -1,4 +0,0 @@
-import { defineTest } from 'jscodeshift/src/testUtils'
-
-defineTest(__dirname, 'rating-stars', {}, 'basic', { parser: 'tsx' })
-defineTest(__dirname, 'rating-stars', {}, 'forms', { parser: 'tsx' })
