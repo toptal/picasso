db1e50f7ec90985c5a4958fbe0d61695e022d579
Ozgur Kececioglu <ozgur.kececioglu@toptal.com>
2022-03-22 13:53:47 +0300

    fix: simplify TagSelector example

--- packages/picasso/src/TagSelector/story/Status.example.tsx
+++ packages/picasso/src/TagSelector/story/Status.example.tsx
@@ -1,98 +1,26 @@
-import React, { useState } from 'react'
-import { TagSelector, AutocompleteItem, Container } from '@toptal/picasso'
-import { isSubstring } from '@toptal/picasso/utils'
+import React from 'react'
+import { Container, TagSelector, Typography } from '@toptal/picasso'
 
-const allOptions = [
+const value = [
   { value: 'AF', text: 'Afghanistan' },
   { value: 'AI', text: 'Aland Islands' },
-  { value: 'ALB', text: 'Albania' },
-  { value: 'ALG', text: 'Algeria' },
-  { value: 'BY', text: 'Belarus' },
-  { value: 'HR', text: 'Croatia' },
-  { value: 'LU', text: 'Lithuania' },
-  { value: 'SK', text: 'Slovakia' },
-  { value: 'SP', text: 'Spain' },
-  { value: 'UA', text: 'Ukraine' }
+  { value: 'ALB', text: 'Albania' }
 ]
 
-const EMPTY_INPUT_VALUE = ''
-const getDisplayValue = (item: AutocompleteItem | null) =>
-  (item && item.text) || EMPTY_INPUT_VALUE
-const filterOptions = (str = '') => {
-  if (str === '') {
-    return allOptions
-  }
-
-  const result = allOptions.filter(option =>
-    isSubstring(str, getDisplayValue(option))
-  )
-
-  return result.length > 0 ? result : null
-}
-
 const Example = () => {
-  const [options, setOptions] = useState<AutocompleteItem[] | null>(allOptions)
-  const [value, setValue] = useState<AutocompleteItem[]>([])
-  const [inputValue, setInputValue] = useState(EMPTY_INPUT_VALUE)
-
   return (
     <Container flex direction='column'>
       <Container padded='small'>
-        <TagSelector
-          options={options}
-          placeholder='default'
-          value={value}
-          inputValue={inputValue}
-          getDisplayValue={getDisplayValue}
-          onChange={selectedValues => {
-            window.console.log('onChange values: ', selectedValues)
-            setValue(selectedValues)
-          }}
-          onInputChange={newInputValue => {
-            window.console.log('onInputChange value: ', newInputValue)
-            setInputValue(newInputValue)
-            setOptions(filterOptions(newInputValue))
-          }}
-          status='default'
-        />
+        <Typography>Default</Typography>
+        <TagSelector placeholder='default' status='default' value={value} />
       </Container>
       <Container padded='small'>
-        <TagSelector
-          options={options}
-          placeholder='error'
-          value={value}
-          inputValue={inputValue}
-          getDisplayValue={getDisplayValue}
-          onChange={selectedValues => {
-            window.console.log('onChange values: ', selectedValues)
-            setValue(selectedValues)
-          }}
-          onInputChange={newInputValue => {
-            window.console.log('onInputChange value: ', newInputValue)
-            setInputValue(newInputValue)
-            setOptions(filterOptions(newInputValue))
-          }}
-          status='error'
-        />
+        <Typography>Error</Typography>
+        <TagSelector placeholder='error' status='error' value={value} />
       </Container>
       <Container padded='small'>
-        <TagSelector
-          options={options}
-          placeholder='success'
-          value={value}
-          inputValue={inputValue}
-          getDisplayValue={getDisplayValue}
-          onChange={selectedValues => {
-            window.console.log('onChange values: ', selectedValues)
-            setValue(selectedValues)
-          }}
-          onInputChange={newInputValue => {
-            window.console.log('onInputChange value: ', newInputValue)
-            setInputValue(newInputValue)
-            setOptions(filterOptions(newInputValue))
-          }}
-          status='success'
-        />
+        <Typography>Success</Typography>
+        <TagSelector placeholder='success' status='success' value={value} />
       </Container>
     </Container>
   )
