381fef93d9e1c5a0cd9b6279ae9dad15d7d956e5
Daniel O <daniel.ostapenko@toptal.com>
2022-03-29 10:48:53 +0300

    fix: return back old field wrapper (#2553)

---
+++ packages/picasso-forms/src/FieldWrapper/index.ts
@@ -0,0 +1,6 @@
+export { default } from './FieldWrapper'
+export * from './FieldWrapper'
+
+// this is done to keep backwards compatibility
+// when FieldWrapper was refactored and moved to Field component
+export { FieldProps } from '../Field'
