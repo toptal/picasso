883cdd6be43cf0f433808802a33b6ac64a44b166
Augusto Moura <augusto.moura@toptal.com>
2022-03-30 23:23:39 -0300

    feat: rename Sidebar to PageSidebar & remove its export
    BREAKING CHANGE: Sidebar is not being exported on it's own by picasso
    anymore, it should be used as a property of Page. The type SidebarProps
    now it's being exported as PageSidebarProps

--- packages/picasso/src/SidebarItem/story/Disabled.example.tsx
+++ packages/picasso/src/SidebarItem/story/Disabled.example.tsx
@@ -1,31 +1,31 @@
 import React from 'react'
-import { Sidebar } from '@toptal/picasso'
+import { Page } from '@toptal/picasso'
 import { Referrals16, Overview16 } from '@toptal/picasso/Icon'
 
 const Example = () => (
-  <Sidebar>
-    <Sidebar.Menu>
-      <Sidebar.Item icon={<Overview16 />} disabled>
+  <Page.Sidebar>
+    <Page.Sidebar.Menu>
+      <Page.Sidebar.Item icon={<Overview16 />} disabled>
         Overview
-      </Sidebar.Item>
-      <Sidebar.Item
+      </Page.Sidebar.Item>
+      <Page.Sidebar.Item
         collapsible
         disabled
         icon={<Referrals16 />}
         menu={
-          <Sidebar.Menu>
-            <Sidebar.Item>Share Online</Sidebar.Item>
-            <Sidebar.Item>Referred Users</Sidebar.Item>
-            <Sidebar.Item>Commissions</Sidebar.Item>
-            <Sidebar.Item>Payment Options</Sidebar.Item>
-            <Sidebar.Item>Expected Commissions</Sidebar.Item>
-          </Sidebar.Menu>
+          <Page.Sidebar.Menu>
+            <Page.Sidebar.Item>Share Online</Page.Sidebar.Item>
+            <Page.Sidebar.Item>Referred Users</Page.Sidebar.Item>
+            <Page.Sidebar.Item>Commissions</Page.Sidebar.Item>
+            <Page.Sidebar.Item>Payment Options</Page.Sidebar.Item>
+            <Page.Sidebar.Item>Expected Commissions</Page.Sidebar.Item>
+          </Page.Sidebar.Menu>
         }
       >
         Referrals
-      </Sidebar.Item>
-    </Sidebar.Menu>
-  </Sidebar>
+      </Page.Sidebar.Item>
+    </Page.Sidebar.Menu>
+  </Page.Sidebar>
 )
 
 export default Example
