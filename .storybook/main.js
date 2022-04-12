5f35a317d58adb2e091759f451e19902d101f2e0
Tomáš Sláma <tomas.slama@toptal.com>
2022-04-12 13:06:43 +0200

    chore: update storybook (#2624)

--- .storybook/main.js
+++ .storybook/main.js
@@ -24,6 +24,7 @@ module.exports = {
     './addons/anchor-link-handler/register',
     './addons/document-title/register'
   ],
+  staticDirs: ['./public'],
   stories: [path.join(__dirname, './load-stories.js')],
   typescript: {
     check: isDevelopment,
