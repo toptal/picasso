474e8042abd6f06d7cea1c69a327066b75d4e24a
Augusto Moura <augusto.moura@toptal.com>
2022-04-14 12:43:26 -0300

    chore: add license (#2640)
    * chore: add LICENSE notice
    
    * chore: minor details in the license file
    
    * feat: use packageDir instead of __dirname

--- bin/utils/index.js
+++ bin/utils/index.js
@@ -1,11 +1,13 @@
 const { log } = require('./log')
 const { copyPackageJson } = require('./copy-package-json')
 const { copyReadme } = require('./copy-readme')
+const { copyLicense } = require('./copy-license')
 const { safeExec } = require('./safe-exec')
 
 module.exports = {
   log,
   copyPackageJson,
   copyReadme,
+  copyLicense,
   safeExec
 }
