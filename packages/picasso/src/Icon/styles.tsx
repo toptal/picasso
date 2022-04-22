6231a9dfe85a6eacce5557ee134b150e251aad8d
Ozgur Kececioglu <ozgur.kececioglu@toptal.com>
2022-04-22 10:16:59 +0300

    fix: fix semicolons after codemod

--- packages/picasso/src/Icon/styles.tsx
+++ packages/picasso/src/Icon/styles.tsx
@@ -1,6 +1,6 @@
-import { Theme } from '@mui/material';
+import { Theme } from '@mui/material'
 
-import createStyles from '@mui/styles/createStyles';
+import createStyles from '@mui/styles/createStyles'
 
 export default ({ palette }: Theme) =>
   createStyles({
