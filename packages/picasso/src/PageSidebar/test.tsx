ea49852b78f34a505cb44e71ee83a9441d7dc47b
Augusto Moura <augusto.moura@toptal.com>
2022-04-26 16:41:55 -0300

    test: add test for setting expanded item key

--- packages/picasso/src/PageSidebar/test.tsx
+++ packages/picasso/src/PageSidebar/test.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 /* eslint-disable-next-line */
-import { render } from '@toptal/picasso/test-utils'
+import { render, fireEvent, act } from '@toptal/picasso/test-utils'
 import { OmitInternalProps } from '@toptal/picasso-shared'
 
 import PageSidebar, { Props } from './PageSidebar'
+import {
+  SidebarContextProvider,
+  useSidebarContext
+} from './SidebarContextProvider'
 
 const TestSidebar = ({ children }: OmitInternalProps<Props>) => {
   return <PageSidebar>{children}</PageSidebar>
@@ -35,5 +39,41 @@ describe('PageSidebar', () => {
     )
 
     expect(container).toMatchSnapshot()
+  })
+
+  describe('when you set a expandedItemKey', () => {
+    it('reflects on its state', async () => {
+      const NEW_EXPANDED_KEY = 42
+
+      const Example = () => {
+        const { setExpandedItemKey, expandedItemKey } = useSidebarContext()
+
+        return (
+          <div>
+            <button onClick={() => setExpandedItemKey(NEW_EXPANDED_KEY)}>
+              Change it
+            </button>
+            <span role='log'>{expandedItemKey}</span>
+          </div>
+        )
+      }
+
+      const { getByRole } = render(
+        <SidebarContextProvider isHovered={false} isCollapsed={false}>
+          <Example />
+        </SidebarContextProvider>
+      )
+
+      const button = getByRole('button')
+      const text = getByRole('log')
+
+      expect(text).not.toHaveTextContent(NEW_EXPANDED_KEY.toString())
+
+      act(() => {
+        fireEvent.click(button)
+      })
+
+      expect(text).toHaveTextContent(NEW_EXPANDED_KEY.toString())
+    })
   })
 })
