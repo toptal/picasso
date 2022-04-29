a9e53294396c969a296f512da8311829e5052911
Ozgur Kececioglu <ozgur.kececioglu@toptal.com>
2022-04-29 14:39:58 +0300

    chore: add Shadows type back

--- packages/picasso-provider/src/Picasso/config/shadows.ts
+++ packages/picasso-provider/src/Picasso/config/shadows.ts
@@ -1,4 +1,5 @@
 import { createTheme } from '@mui/material/styles'
+import { Shadows } from '@mui/material/styles/shadows'
 
 const muiShadows = createTheme().shadows
 const shadows = [
@@ -21,6 +22,6 @@ const shadows = [
     '0 0 0 1px rgba(0, 0, 0, 0.04), 0 8px 12px -3px rgba(0, 0, 0, 0.08), 0 4px 8px -2px rgba(0, 0, 0, 0.04)'
   ],
   ...muiShadows.slice(6)
-] as typeof muiShadows
+] as Shadows
 
 export default shadows
