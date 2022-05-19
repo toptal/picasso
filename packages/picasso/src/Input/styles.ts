acb4d656edb14dbec5e6c38625889d5a4d63916b
Ivan Atanasov <ivan.atanasov@toptal.com>
2022-05-19 13:58:14 +0300

    fix: use rems

--- packages/picasso/src/Input/styles.ts
+++ packages/picasso/src/Input/styles.ts
@@ -23,7 +23,7 @@ export default ({ palette }: Theme) =>
       resize: 'vertical',
       overflow: 'hidden',
       '&::-webkit-resizer': {
-        backgroundSize: '8px 8px',
+        backgroundSize: '0.5rem 0.5rem',
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center center',
         backgroundImage: `url("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0nOCcgaGVpZ2h0PSc4JyB2aWV3Qm94PScwIDAgOCA4JyBmaWxsPSdub25lJyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnPjxwYXRoIGZpbGwtcnVsZT0nZXZlbm9kZCcgY2xpcC1ydWxlPSdldmVub2RkJyBkPSdNNi42NDYzNiAwLjY0NjQ4NEw3LjM1MzQ3IDEuMzUzNTlMMS4zNTM0NyA3LjM1MzU5TDAuNjQ2MzYyIDYuNjQ2NDhMNi42NDYzNiAwLjY0NjQ4NFpNNy4zNTM0NyA0LjM1MzU5TDQuMzUzNDcgNy4zNTM1OUwzLjY0NjM2IDYuNjQ2NDhMNi42NDYzNiAzLjY0NjQ4TDcuMzUzNDcgNC4zNTM1OVonIGZpbGw9JyNEOEQ5REMnLz48L3N2Zz4=")`
