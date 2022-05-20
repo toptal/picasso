7c1199fa7218ab9699b8b2127cca58375f4f6ddc
George Asfour <george.asfour@toptal.com>
2022-05-20 15:39:14 +0300

    [CRT-no-jira] Add Cube icon

---
+++ packages/picasso/src/Icon/Cube16.tsx
@@ -0,0 +1,60 @@
+import React, { forwardRef, Ref } from 'react'
+import cx from 'classnames'
+import { makeStyles } from '@material-ui/core/styles'
+import { StandardProps } from '@toptal/picasso-shared'
+
+import kebabToCamelCase from '../utils/kebab-to-camel-case'
+import styles from './styles'
+const BASE_SIZE = 16
+
+type ScaleType = 1 | 2 | 3 | 4
+export interface Props extends StandardProps {
+  scale?: ScaleType
+  color?: string
+  base?: number
+}
+const useStyles = makeStyles(styles, {
+  name: 'PicassoSvgCube16'
+})
+const SvgCube16 = forwardRef(function SvgCube16(
+  props: Props,
+  ref: Ref<SVGSVGElement>
+) {
+  const {
+    className,
+    style = {},
+    color,
+    scale,
+    base,
+    'data-testid': testId
+  } = props
+  const classes: Record<string, string> = useStyles(props)
+  const classNames = [classes.root, className]
+  const scaledSize = base || BASE_SIZE * Math.ceil(scale || 1)
+  const colorClassName = kebabToCamelCase(`${color}`)
+
+  if (classes[colorClassName]) {
+    classNames.push(classes[colorClassName])
+  }
+
+  const svgStyle = {
+    minWidth: `${scaledSize}px`,
+    minHeight: `${scaledSize}px`,
+    ...style
+  }
+
+  return (
+    <svg
+      viewBox='0 0 16 16'
+      className={cx(...classNames)}
+      style={svgStyle}
+      ref={ref}
+      data-testid={testId}
+    >
+      <path d='m10.5.997 10.505 5.002v11.997L10.5 23.002 0 17.996V5.999L10.5.997ZM1 7.029v10.336l9.002 4.292V11.316L1 7.029Zm19.004 0-9.002 4.286v10.34l9.002-4.291V7.029ZM10.5 2.104 1.744 6.276l8.757 4.17 8.758-4.17L10.5 2.104Z' />
+    </svg>
+  )
+})
+
+SvgCube16.displayName = 'SvgCube16'
+export default SvgCube16
