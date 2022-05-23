5469e6e175b59664a5e929fdb6ecfe721ab4e725
Ivan Atanasov <ivan.atanasov@toptal.com>
2022-05-23 14:41:21 +0300

    feat: add new `buttonLabel` prop to `FileInput`

--- packages/picasso/src/FileInput/test.tsx
+++ packages/picasso/src/FileInput/test.tsx
@@ -2,14 +2,21 @@ import React from 'react'
 import { render } from '@toptal/picasso/test-utils'
 
 import FileInput from './FileInput'
+import { FileInputProps } from '.'
 
-const TestFileInput = () => {
-  return <FileInput />
+const TestFileInput = (props: FileInputProps = {}) => {
+  return <FileInput {...props} />
 }
 
 describe('FileInput', () => {
   it('renders', () => {
     const { container } = render(<TestFileInput />)
+
+    expect(container).toMatchSnapshot()
+  })
+
+  it('can change label', () => {
+    const { container } = render(<TestFileInput buttonLabel='Upload File' />)
 
     expect(container).toMatchSnapshot()
   })
