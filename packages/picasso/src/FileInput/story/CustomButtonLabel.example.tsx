757ab7b695754d58af409933cbe6910c0f1328c9
Ivan Atanasov <ivan.atanasov@toptal.com>
2022-05-25 12:56:50 +0300

    fix: remove unneeded div

--- packages/picasso/src/FileInput/story/CustomButtonLabel.example.tsx
+++ packages/picasso/src/FileInput/story/CustomButtonLabel.example.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 import { FileInput } from '@toptal/picasso'
 
 const Example = () => (
-  <div>
-    <FileInput buttonLabel='Upload File' hint='Max file size: 25MB' />
-  </div>
+  <FileInput buttonLabel='Upload File' hint='Max file size: 25MB' />
 )
 
 export default Example
