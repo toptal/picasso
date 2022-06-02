4a93799593f45b3f569f284c4ef5caf685a8b260
Ozgur Kececioglu <ozgur.kececioglu@toptal.com>
2022-06-02 13:37:06 +0300

    chore: split test data from components files

--- cypress/component/BarChart.spec.tsx
+++ cypress/component/BarChart.spec.tsx
@@ -6,47 +6,10 @@ import { BarChart } from '@toptal/picasso-charts'
 import { TestingPicasso } from '@toptal/picasso/test-utils'
 import { palette } from '@toptal/picasso/utils'
 
-const CHART_DATA_DEFAULT = [
-  {
-    name: 'Apple',
-    value: { 'engineers hired': 500 }
-  },
-  {
-    name: 'Google',
-    value: { 'engineers hired': 700 }
-  },
-  {
-    name: 'Facebook',
-    value: { 'engineers hired': 600 }
-  },
-  {
-    name: 'Amazon',
-    value: { 'engineers hired': 400 }
-  },
-  {
-    name: 'Toptal',
-    value: { 'engineers hired': 1000 }
-  }
-]
-
-const CHART_DATA_CUSTOM_TOOLTIP = [
-  {
-    name: 'Berlin',
-    value: { infected: 4000, recovered: 2400 }
-  },
-  {
-    name: 'Milan',
-    value: { infected: 3000, recovered: 1398 }
-  },
-  {
-    name: 'Moscow',
-    value: { infected: 2000, recovered: 9800 }
-  },
-  {
-    name: 'Los-Angeles',
-    value: { infected: 2780, recovered: 3908 }
-  }
-]
+import {
+  CHART_DATA_CUSTOM_TOOLTIP,
+  CHART_DATA_DEFAULT
+} from '../test-data/BarChart.data.json'
 
 const CustomTooltip = ({ active, payload }: any) => {
   if (active && payload && payload.length > 0) {
