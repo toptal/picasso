d2caf7f586a0b507288e59b1cecaaba54678bac8
Ozgur Kececioglu <ozgur.kececioglu@toptal.com>
2022-06-07 08:37:24 +0300

    chore(Radio): migrate stories with effects on cypress side

--- cypress/component/Radio.spec.tsx
+++ cypress/component/Radio.spec.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Container, Radio, Tooltip } from '@toptal/picasso'
 
-const RadioTooltipExample = () => {
+const RadioExample = () => {
+  return (
+    <TestingPicasso>
+      <Container padded='medium'>
+        <Radio label='Radio 1' value='radio1' data-testid='trigger' />
+      </Container>
+    </TestingPicasso>
+  )
+}
+
+const TestRadioWithTooltip = () => {
   const tooltipContent = <span data-testid='tooltip-content'>Content</span>
 
   return (
-    <Container style={{ marginTop: '200px', marginLeft: '200px' }}>
+    <Container padded='medium'>
       <Tooltip content={tooltipContent}>
         <Radio label='Radio 1' value='radio1' data-testid='trigger' />
       </Tooltip>
@@ -13,11 +23,11 @@ const RadioTooltipExample = () => {
   )
 }
 
-const DisabledRadioTooltipExample = () => {
+const TestDisabledRadioWithTooltip = () => {
   const tooltipContent = <span data-testid='tooltip-content'>Content</span>
 
   return (
-    <Container style={{ marginTop: '200px', marginLeft: '200px' }}>
+    <Container padded='medium'>
       <Tooltip content={tooltipContent}>
         <Container as='span'>
           <Radio
@@ -32,20 +42,42 @@ const DisabledRadioTooltipExample = () => {
   )
 }
 
+const COMPONENT = 'Radio'
+
 describe('Radio', () => {
+  it('renders Radio with hovered and focused state', () => {
+    mount(<RadioExample />)
+
+    // happo doesn't retain hover state but it has a workaround
+    // "data-happo-hover" is being added and removed to mimic the state
+    cy.getByTestId('trigger')
+      .invoke('attr', 'data-happo-hover', true)
+      .get('body')
+      .happoScreenshot({
+        component: COMPONENT,
+        variant: 'default/after-hovered',
+      })
+    cy.getByTestId('trigger').invoke('removeAttr', 'data-happo-hover')
+
+    cy.get('input').focus().get('body').happoScreenshot({
+      component: COMPONENT,
+      variant: 'default/after-focused',
+    })
+  })
+
   it('shows the tooltip when hover a radio button', () => {
-    cy.mount(<RadioTooltipExample />)
+    cy.mount(<TestRadioWithTooltip />)
 
     // hover the radio button itself (not the label)
-    cy.get('[data-testid="trigger"]').realHover()
-    cy.get('[data-testid="tooltip-content"]').should('be.visible')
+    cy.getByTestId('trigger').realHover()
+    cy.getByTestId('tooltip-content').should('be.visible')
   })
 
   it('shows the tooltip when hover a disabled radio button', () => {
-    cy.mount(<DisabledRadioTooltipExample />)
+    cy.mount(<TestDisabledRadioWithTooltip />)
 
     // hover the radio button itself (not the label)
-    cy.get('[data-testid="trigger"]').realHover()
-    cy.get('[data-testid="tooltip-content"]').should('be.visible')
+    cy.getByTestId('trigger').realHover()
+    cy.getByTestId('tooltip-content').should('be.visible')
   })
 })
