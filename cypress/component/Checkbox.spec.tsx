d4c9bc1d640c0bce468c6685d11773a5ecd7f88d
Ozgur Kececioglu <ozgur.kececioglu@toptal.com>
2022-06-09 08:35:44 +0300

    chore(Checkbox): add workaround for hover state

--- cypress/component/Checkbox.spec.tsx
+++ cypress/component/Checkbox.spec.tsx
@@ -22,10 +22,22 @@ describe('Checkbox', () => {
   it('renders', () => {
     mount(<TestCheckbox />)
 
-    // looks like happo doesn't work with hover pseudo-class properly
-    // these happo screenshots disabled for now until we get helped by happo side
-    // cy.getByTestId('checkbox-unchecked').realHover().get('body').happoScreenshot()
-    // cy.getByTestId('checkbox-checked').realHover().get('body').happoScreenshot()
+    // happo doesn't retain hover state but it has a workaround
+    // "data-happo-hover" is being added and removed to mimic the state
+    cy.getByTestId('checkbox-unchecked')
+      .invoke('attr', 'data-happo-hover', true)
+      .get('body')
+      .happoScreenshot()
+    cy.getByTestId('checkbox-unchecked').invoke(
+      'removeAttr',
+      'data-happo-hover'
+    )
+
+    cy.getByTestId('checkbox-checked')
+      .invoke('attr', 'data-happo-hover', true)
+      .get('body')
+      .happoScreenshot()
+    cy.getByTestId('checkbox-checked').invoke('removeAttr', 'data-happo-hover')
 
     // our data-testid's are not being passed to the input
     cy.get('input').first().focus().get('body').happoScreenshot()
