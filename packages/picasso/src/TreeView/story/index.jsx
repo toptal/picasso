7d52645c9429e0faa9b06de692d836f104a1a105
Augusto Moura <augusto.moura@toptal.com>
2022-06-15 22:52:13 -0300

    feat: add effect test on cypress happo

--- packages/picasso/src/TreeView/story/index.jsx
+++ packages/picasso/src/TreeView/story/index.jsx
@@ -16,41 +16,29 @@ page
   .createChapter()
   .addExample('TreeView/story/Default.example.tsx', {
     title: 'Default',
-    takeScreenshot: false,
   })
   .addExample('TreeView/story/Selected.example.tsx', {
     title: 'With selected node',
     description:
       "To set the particular node selected, you need to set `node`'s attribute *selected* to `true`. Also there is additional attribute `selectedOffset` for adding an scroll offset for particular node",
-    takeScreenshot: false,
   })
   .addExample('TreeView/story/Modal.example.tsx', {
     title: 'With Modal',
-    effect: async (testPage, makeScreenshot) => {
-      await testPage.click('[data-testid="open"]')
-      await testPage.waitFor('[data-testid="tree-dialog-content"]')
-      await makeScreenshot({
-        isFullScreen: true,
-      })
-    },
+    takeScreenshot: false,
   })
   .addExample('TreeView/story/CustomZoom.example.tsx', {
     title: 'Custom Zoom',
-    takeScreenshot: false,
   })
   .addExample('TreeView/story/AvatarSize.example.tsx', {
     title: 'Avatar Size',
-    takeScreenshot: false,
   })
   .addExample('TreeView/story/Horizontal.example.tsx', {
     title: 'Horizontal Direction',
-    takeScreenshot: false,
   })
   .addExample('TreeView/story/Compact.example.tsx', {
     title: 'Compact tree',
     description:
       'Example of a compact tree - one with only single node on each depth that has children',
-    takeScreenshot: false,
   })
   .addExample('TreeView/story/TreeNodeAvatar.example.tsx', {
     title: 'TreeNodeAvatar',
