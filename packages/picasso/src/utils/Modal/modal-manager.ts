cfb29b0661d87f825570d06014b761298fb4ca97
Alexander Shahorsky <alexander.shahorsky@toptal.com>
2022-06-17 11:49:35 +0200

    feat(Modal): improve body scroll lock to properly handle multiple open modals

--- packages/picasso/src/utils/Modal/modal-manager.ts
+++ packages/picasso/src/utils/Modal/modal-manager.ts
@@ -1,5 +1,8 @@
+import { isBrowser } from '@toptal/picasso-shared'
+
 export class ModalManager {
   modals: number[] = []
+  scrollLock: { prevBodyOverflow: string } | undefined
 
   add(modalId: number) {
     if (this.modals.includes(modalId)) {
@@ -7,6 +10,7 @@ export class ModalManager {
     }
 
     this.modals.push(modalId)
+    this.dropScrollLock()
   }
 
   remove(modalId: number) {
@@ -17,11 +21,36 @@ export class ModalManager {
     }
 
     this.modals.splice(modalIndex, 1)
+    this.liftScrollLock()
   }
 
   isTopModal(modalId: number) {
     return (
       this.modals.length > 0 && this.modals[this.modals.length - 1] === modalId
     )
+  }
+
+  private dropScrollLock() {
+    if (!isBrowser()) {
+      return
+    }
+
+    if (!this.scrollLock) {
+      this.scrollLock = {
+        prevBodyOverflow: window.getComputedStyle(document.body).overflow,
+      }
+      document.body.style.overflow = 'hidden'
+    }
+  }
+
+  private liftScrollLock() {
+    if (!isBrowser()) {
+      return
+    }
+
+    if (this.scrollLock && this.modals.length === 0) {
+      document.body.style.overflow = this.scrollLock.prevBodyOverflow
+      this.scrollLock = undefined
+    }
   }
 }
