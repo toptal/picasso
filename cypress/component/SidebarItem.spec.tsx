8f3292d7c65cc920e75b4251e004d013fac90d0f
TomasSlama <tomas.slama@toptal.com>
2022-06-17 09:32:37 +0200

    chore: apply suggestions

--- cypress/component/SidebarItem.spec.tsx
+++ cypress/component/SidebarItem.spec.tsx
@@ -1,38 +1,27 @@
 import React from 'react'
 import { Page, Referrals16 } from '@toptal/picasso'
 
+const component = 'SidebarItem'
+
 const CollapsibleSidebar = () => (
   <Page.Sidebar>
     <Page.Sidebar.Menu>
       <Page.Sidebar.Item
-        testIds={{ header: 'referrals' }}
+        testIds={{ header: 'collapsible-menu-item' }}
         collapsible
         icon={<Referrals16 />}
         menu={
           <Page.Sidebar.Menu>
-            <Page.Sidebar.Item testIds={{ header: 'share-online' }}>
-              Share Online
+            <Page.Sidebar.Item testIds={{ header: 'submenu-item-1' }}>
+              Submenu Item 1
             </Page.Sidebar.Item>
-            <Page.Sidebar.Item>Referred Users</Page.Sidebar.Item>
-            <Page.Sidebar.Item>Commissions</Page.Sidebar.Item>
-            <Page.Sidebar.Item>Payment Options</Page.Sidebar.Item>
-            <Page.Sidebar.Item>Expected Commissions</Page.Sidebar.Item>
+            <Page.Sidebar.Item>Submenu Item 2</Page.Sidebar.Item>
           </Page.Sidebar.Menu>
         }
       >
-        Referrals
+        Collapsible Menu
       </Page.Sidebar.Item>
-      <Page.Sidebar.Item
-        collapsible
-        menu={
-          <Page.Sidebar.Menu>
-            <Page.Sidebar.Item>Community Leader</Page.Sidebar.Item>
-            <Page.Sidebar.Item>Speakers Network</Page.Sidebar.Item>
-          </Page.Sidebar.Menu>
-        }
-      >
-        Get Involved
-      </Page.Sidebar.Item>
+      <Page.Sidebar.Item>Non-collapsible Menu</Page.Sidebar.Item>
     </Page.Sidebar.Menu>
   </Page.Sidebar>
 )
@@ -41,9 +30,9 @@ describe('CollapsibleSidebar', () => {
   it('opens the closed submenu correctly', () => {
     cy.mount(<CollapsibleSidebar />)
 
-    cy.getByTestId('share-online').should('not.be.visible')
-    cy.getByTestId('referrals').realClick()
-    cy.getByTestId('share-online').should('be.visible')
-    cy.get('body').happoScreenshot()
+    cy.getByTestId('submenu-item-1').should('not.be.visible')
+    cy.getByTestId('collapsible-menu-item').realClick()
+    cy.getByTestId('submenu-item-1').should('be.visible')
+    cy.get('body').happoScreenshot({ component, variant: 'after-menu-collapsed'})
   })
 })
