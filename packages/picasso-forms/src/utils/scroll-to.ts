aaa0eeb24bf6553a8c4954ba7ccf38f6b5705745
Ozgur Kececioglu <ozgur.kececioglu@toptal.com>
2022-06-21 10:52:31 +0300

    fix: give better name for variable

--- packages/picasso-forms/src/utils/scroll-to.ts
+++ packages/picasso-forms/src/utils/scroll-to.ts
@@ -1,10 +1,10 @@
-const UNHIDDEN_INPUT_SELECTOR = 'input:not([type=hidden])'
+const NOT_HIDDEN_INPUT_SELECTOR = 'input:not([type=hidden])'
 
 export const scrollTo = (field: HTMLElement) => {
   if (typeof field.scrollIntoView === 'function') {
     field.scrollIntoView({ block: 'center', behavior: 'smooth' })
   }
   field
-    .querySelector<HTMLInputElement>(UNHIDDEN_INPUT_SELECTOR)
+    .querySelector<HTMLInputElement>(NOT_HIDDEN_INPUT_SELECTOR)
     ?.focus({ preventScroll: true })
 }
