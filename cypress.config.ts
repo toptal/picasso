affabe5939867b94fdff2b6bfbc2b58c801035fd
Daniel Ostapenko <daniel.ostapenko@toptal.com>
2022-06-23 19:12:42 +0300

    chore: use davinci webpack config for cypress component tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,3 @@
-import path from 'path'
 import happoTask from 'happo-cypress/task'
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'cypress'
@@ -17,75 +16,5 @@ export default defineConfig({
       return config
     },
     specPattern: 'cypress/component/*.spec.tsx',
-    devServer: {
-      framework: 'react',
-      bundler: 'webpack',
-      webpackConfig: {
-        module: {
-          rules: [
-            {
-              test: /\.jsx|\.tsx?$/,
-              use: [
-                {
-                  loader: require.resolve('babel-loader'),
-                  options: {
-                    presets: [
-                      require.resolve('@babel/preset-typescript'),
-                      require.resolve('@babel/preset-react'),
-                    ],
-                    plugins: [
-                      require.resolve(
-                        '@babel/plugin-proposal-nullish-coalescing-operator'
-                      ),
-                      require.resolve(
-                        '@babel/plugin-proposal-optional-chaining'
-                      ),
-                      require.resolve('babel-plugin-istanbul'),
-                    ],
-                  },
-                },
-              ],
-            },
-            {
-              test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-              loader: require.resolve('url-loader'),
-            },
-            {
-              test: /\.css$/i,
-              use: ['style-loader', 'css-loader'],
-            },
-          ],
-        },
-        resolve: {
-          mainFields: ['browser', 'main', 'module'],
-          alias: {
-            '@toptal/picasso': path.resolve(
-              process.cwd(),
-              './packages/picasso/dist-package'
-            ),
-            '@toptal/picasso-shared': path.resolve(
-              process.cwd(),
-              './packages/shared/dist-package'
-            ),
-            '@toptal/picasso-forms': path.resolve(
-              process.cwd(),
-              './packages/picasso-forms/dist-package'
-            ),
-            '@toptal/picasso-charts': path.resolve(
-              process.cwd(),
-              './packages/picasso-charts/dist-package'
-            ),
-            '@toptal/picasso-provider': path.resolve(
-              process.cwd(),
-              './packages/picasso-provider/dist-package'
-            ),
-            '@topkit/analytics-charts': path.resolve(
-              process.cwd(),
-              './packages/topkit-analytics-charts/dist-package'
-            ),
-          },
-        },
-      },
-    },
   },
 })
