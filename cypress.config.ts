4012110fee2fff9ab9ce3c46a16a894b90e818a5
Daniel Ostapenko <daniel.ostapenko@toptal.com>
2022-06-24 10:19:51 +0300

    chore: reuse aliases

--- cypress.config.ts
+++ cypress.config.ts
@@ -16,5 +16,46 @@ export default defineConfig({
       return config
     },
     specPattern: 'cypress/component/*.spec.tsx',
+    devServer: {
+      ...davinciConfig.component.devServer,
+      webpackConfig: {
+        ...davinciConfig.component.devServer.webpackConfig,
+        module: {
+          rules: [
+            {
+              test: /\.jsx|\.tsx?$/,
+              use: [
+                {
+                  loader: require.resolve('babel-loader'),
+                  options: {
+                    presets: [
+                      require.resolve('@babel/preset-typescript'),
+                      require.resolve('@babel/preset-react'),
+                    ],
+                    plugins: [
+                      require.resolve(
+                        '@babel/plugin-proposal-nullish-coalescing-operator'
+                      ),
+                      require.resolve(
+                        '@babel/plugin-proposal-optional-chaining'
+                      ),
+                      require.resolve('babel-plugin-istanbul'),
+                    ],
+                  },
+                },
+              ],
+            },
+            {
+              test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+              loader: require.resolve('url-loader'),
+            },
+            {
+              test: /\.css$/i,
+              use: ['style-loader', 'css-loader'],
+            },
+          ],
+        },
+      },
+    },
   },
 })
