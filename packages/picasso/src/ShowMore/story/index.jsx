894c866f1460de040d8e0424c8704d8e9e31a6dd
Ozgur Kececioglu <ozgur.kececioglu@toptal.com>
2022-06-27 14:40:20 +0300

    chore: improve ShowMore cypress tests

--- packages/picasso/src/ShowMore/story/index.jsx
+++ packages/picasso/src/ShowMore/story/index.jsx
@@ -13,18 +13,15 @@ page
 page
   .createChapter()
   .addExample('ShowMore/story/Default.example.tsx', 'Default')
-  .addExample('ShowMore/story/LineBreaks.example.tsx', {
-    title: 'Line Breaks',
-    takeScreenshot: false,
-  })
+  .addExample('ShowMore/story/LineBreaks.example.tsx', 'Line Breaks')
   .addExample('ShowMore/story/Expanded.example.tsx', 'Expanded')
   .addExample('ShowMore/story/CustomLimit.example.tsx', 'Custom Limit')
-  .addExample('ShowMore/story/ToggleDisabled.example.tsx', 'Disabled Toggle')
+  .addExample('ShowMore/story/ToggleDisabled.example.tsx', 'Toggle Disabled')
   .addExample('ShowMore/story/ZeroRows.example.tsx', {
-    title: 'With rows={0}',
+    title: 'With zero rows',
     takeScreenshot: false,
   })
-  .addExample('ShowMore/story/ReactNode.example.tsx', {
+  .addExample('ShowMore/story/ReactChildren.example.tsx', {
     title: 'With React children',
     takeScreenshot: false,
   })
