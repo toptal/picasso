5d74e41e4edf4a0bcb496d5a17b078c8d7eee871
Ozgur Kececioglu <ozgur.kececioglu@toptal.com>
2022-06-27 16:28:19 +0300

    chore: improve Tabs cypress tests

--- packages/picasso/src/Tab/story/index.jsx
+++ packages/picasso/src/Tab/story/index.jsx
@@ -6,18 +6,9 @@ const componentDocs = PicassoBook.createComponentDocs(Tab, 'Tabs.Tab')
 const chapter = PicassoBook.connectToPage(page => {
   page
     .createChapter('Tabs.Tab')
-    .addExample('Tab/story/Disabled.example.tsx', {
-      title: 'Disabled tab',
-      takeScreenshot: false,
-    })
-    .addExample('Tab/story/CustomValue.example.tsx', {
-      title: 'Using custom value',
-      takeScreenshot: false,
-    })
-    .addExample('Tab/story/Icon.example.tsx', {
-      title: 'With Icon',
-      takeScreenshot: false,
-    })
+    .addExample('Tab/story/Disabled.example.tsx', 'Disabled tab')
+    .addExample('Tab/story/CustomValue.example.tsx', 'Using custom value')
+    .addExample('Tab/story/Icon.example.tsx', 'With Icon')
 })
 
 export default {
