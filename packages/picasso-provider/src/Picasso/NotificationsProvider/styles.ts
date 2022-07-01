522362eb4773aa1e22da4d40958ceb177ce5813e
Ondřej Tůma <ondra.tuma@toptal.com>
2022-07-01 10:12:15 +0200

    chore: bump notistack to 1.0.10

--- packages/picasso-provider/src/Picasso/NotificationsProvider/styles.ts
+++ packages/picasso-provider/src/Picasso/NotificationsProvider/styles.ts
@@ -3,11 +3,22 @@ import { createStyles } from '@material-ui/core'
 // --- need to move to shared config
 export const headerHeight = { default: '4.5rem', smallAndMedium: '3rem' }
 
+const collapse = {
+  container: '& > div',
+  wrapper: '& > div > div',
+}
+
 export default createStyles({
+  root: {
+    [collapse.container]: {
+      pointerEvents: 'all',
+    },
+    [collapse.wrapper]: {
+      padding: `6px 0px`,
+      transition: 'padding 300ms ease 0ms',
+    },
+  },
   rootWithMargin: {
     marginTop: headerHeight.default,
-  },
-  marginWithDrawer: {
-    marginRight: '27.5rem',
   },
 })
