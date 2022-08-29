240b7d25b344ae467c0d873da740ae120e7e84de
Ozgur Kececioglu <ozgur.kececioglu@toptal.com>
2022-08-29 11:19:48 +0300

    feat(AvatarUpload): review updates

--- packages/picasso/src/AvatarUpload/DropzoneSvg/utils.ts
+++ packages/picasso/src/AvatarUpload/DropzoneSvg/utils.ts
@@ -1,17 +1,52 @@
 /**
+ * Returns the shape of the Avatar component in SVG path's format.
+ * SVG's "path" works for the function like this:
+ * M x1 y1 - move to point x, y (start point)
+ * H x2 - horizontal line to x1 (top edge)
+ * V y3 - vertical line to y2 (right edge)
+ * H x4 - horizontal line to x3 (bottom edge)
+ * L x1 y5 - close path to point x4, y4
+ * Z - draw path back to starting point (x1 y1)
+ */
+const getAvatarShape = ({
+  bottomEdgeLength,
+  startPoint,
+  leftCornerPoint,
+  rightEdgeLength,
+  topEdgeLength,
+  drawBackToStart,
+}: {
+  startPoint: number
+  topEdgeLength: number
+  rightEdgeLength: number
+  bottomEdgeLength: number
+  leftCornerPoint: number
+  drawBackToStart: boolean
+}) => {
+  return `
+    M ${startPoint} ${startPoint}
+    H ${topEdgeLength} 
+    V ${rightEdgeLength} 
+    H ${bottomEdgeLength} 
+    L ${startPoint} ${leftCornerPoint}${drawBackToStart ? ' Z' : ''}
+  `
+}
+
+/**
  * Returns the shape of the background of the avatar.
  * it should be 80x80(px)
  */
 export const getBackgroundShape = (dimensions: number, cornerSize: number) => {
   const centerShift = 3 // shift for the outline stroke
 
-  return `
-    M ${centerShift} ${centerShift}
-    H ${dimensions + centerShift} 
-    V ${dimensions + centerShift} 
-    H ${cornerSize + centerShift} 
-    L ${centerShift} ${dimensions - cornerSize + centerShift}
-  `
+  return getAvatarShape({
+    startPoint: centerShift,
+    topEdgeLength: dimensions + centerShift,
+    rightEdgeLength: dimensions + centerShift,
+    bottomEdgeLength: cornerSize + centerShift,
+    leftCornerPoint: dimensions - cornerSize + centerShift,
+    drawBackToStart: false,
+  })
 }
 
 /**
@@ -23,13 +58,14 @@ export const getOutlineShape = (dimensions: number, cornerSize: number) => {
   const centerShift = 2 // shift for the outline stroke
   const outlineStrokeWidth = 2 // width of the outline stroke
 
-  return `
-    M ${centerShift} ${centerShift}
-    H ${dimensions + centerShift + outlineStrokeWidth} 
-    V ${dimensions + centerShift + outlineStrokeWidth} 
-    H ${cornerSize + 1} 
-    L ${centerShift} ${dimensions - cornerSize + centerShift} Z
-  `
+  return getAvatarShape({
+    startPoint: centerShift,
+    topEdgeLength: dimensions + centerShift + outlineStrokeWidth,
+    rightEdgeLength: dimensions + centerShift + outlineStrokeWidth,
+    bottomEdgeLength: cornerSize + 1,
+    leftCornerPoint: dimensions - cornerSize + centerShift,
+    drawBackToStart: true,
+  })
 }
 
 /**
@@ -41,11 +77,12 @@ export const getBordersShape = (dimensions: number, cornerSize: number) => {
   const centerShift = 4 // shift for the outline stroke and border stroke
   const outlineStrokeWidth = 2 // width of the outline stroke
 
-  return `
-    M ${centerShift} ${centerShift}
-    H ${dimensions + outlineStrokeWidth} 
-    V ${dimensions + outlineStrokeWidth} 
-    H ${cornerSize + outlineStrokeWidth} 
-    L ${centerShift} ${dimensions - cornerSize + outlineStrokeWidth} Z
-  `
+  return getAvatarShape({
+    startPoint: centerShift,
+    topEdgeLength: dimensions + outlineStrokeWidth,
+    rightEdgeLength: dimensions + outlineStrokeWidth,
+    bottomEdgeLength: cornerSize + outlineStrokeWidth,
+    leftCornerPoint: dimensions - cornerSize + outlineStrokeWidth,
+    drawBackToStart: true,
+  })
 }
