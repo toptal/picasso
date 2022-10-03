ac2de6748c017c23714c31e792594b5842301efa
Nikola Đuza <nikola.duza@toptal.com>
2022-10-03 15:37:51 +0200

    feat: add mobile icon

---
+++ packages/picasso/src/Icon/Mobile16.tsx
@@ -0,0 +1,61 @@
+import React, { forwardRef, Ref } from 'react'
+import cx from 'classnames'
+import { makeStyles } from '@material-ui/core/styles'
+import { StandardProps } from '@toptal/picasso-shared'
+
+import kebabToCamelCase from '../utils/kebab-to-camel-case'
+import styles from './styles'
+const BASE_SIZE = 16
+
+type ScaleType = 1 | 2 | 3 | 4
+export interface Props extends StandardProps {
+  scale?: ScaleType
+  color?: string
+  base?: number
+}
+const useStyles = makeStyles(styles, {
+  name: 'PicassoSvgMobile16'
+})
+const SvgMobile16 = forwardRef(function SvgMobile16(
+  props: Props,
+  ref: Ref<SVGSVGElement>
+) {
+  const {
+    className,
+    style = {},
+    color,
+    scale,
+    base,
+    'data-testid': testId
+  } = props
+  const classes: Record<string, string> = useStyles(props)
+  const classNames = [classes.root, className]
+  const scaledSize = base || BASE_SIZE * Math.ceil(scale || 1)
+  const colorClassName = kebabToCamelCase(`${color}`)
+
+  if (classes[colorClassName]) {
+    classNames.push(classes[colorClassName])
+  }
+
+  const svgStyle = {
+    minWidth: `${scaledSize}px`,
+    minHeight: `${scaledSize}px`,
+    ...style
+  }
+
+  return (
+    <svg
+      viewBox='0 0 16 16'
+      fill='none'
+      className={cx(...classNames)}
+      style={svgStyle}
+      ref={ref}
+      data-testid={testId}
+    >
+      <path d='M13 0v16H3V0h10Zm-1 1H4v14h8V1ZM9 13v1H7v-1h2Z' />
+    </svg>
+  )
+})
+
+SvgMobile16.displayName = 'SvgMobile16'
+export default SvgMobile16
