aa24a16ab4977ed98b91d7a5bdc4736d896fe95f
Timofei Kachalov <timofei.kachalov@toptal.com>
2022-10-05 14:05:19 +0400

    feat(picasso): fix prop description

--- packages/picasso-provider/src/Picasso/HelmetProvider/HelmetProvider.tsx
+++ packages/picasso-provider/src/Picasso/HelmetProvider/HelmetProvider.tsx
@@ -4,6 +4,7 @@ import { HelmetProvider as BaseHelmetProvider } from 'react-helmet-async'
 interface Props {
   /** Children elements */
   children?: ReactNode
+  /** Disables usage of `<HelmetProvider>` component from `react-helmet-async` package */
   disabled?: boolean
 }
 
