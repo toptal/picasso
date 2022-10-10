79e64012f9dea37c8b7900811a014c6ea8cb83bb
TomasSlama <tomas.slama@toptal.com>
2022-10-10 22:42:46 +0200

    chore: add custom babel and webpack plugin for docgen

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,9 @@
 const webpack = require('webpack')
 const path = require('path')
 const { IgnoreNotFoundPlugin } = require('./plugins')
+const ReactDocgenTypescriptPlugin =
+  require('@storybook/react-docgen-typescript-plugin').default
 
-// example1: /packages/picasso/src/Button/Button.tsx
 const PACKAGES_COMPONENT_DECLARATION_FILE_REGEXP =
   /packages\/.*\/src\/(.*)\/\1.tsx$/
 
@@ -54,80 +55,132 @@ module.exports = {
   core: {
     builder: 'webpack5',
   },
-  webpackFinal: config => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      oneOf: [
-        {
-          test: PACKAGES_COMPONENT_DECLARATION_FILE_REGEXP,
-          use: threadLoaders,
-        },
-        { use: threadLoaders },
+  webpackFinal: async (config, options) => {
+    const typescriptOptions = await options.presets.apply('typescript')
+
+    const { reactDocgen, reactDocgenTypescriptOptions } = typescriptOptions
+
+    return {
+      ...config,
+      module: {
+        ...config.module,
+        // supress an error with dynamic path e.g. require(`${url}`)
+        // https://github.com/webpack/webpack/issues/196
+        exprContextCritical: false,
+        rules: [
+          ...config.module.rules,
+          {
+            test: /\.(ts|tsx)$/,
+            oneOf: [
+              {
+                test: PACKAGES_COMPONENT_DECLARATION_FILE_REGEXP,
+                use: threadLoaders,
+              },
+              { use: threadLoaders },
+            ],
+          },
+        ],
+      },
+      optimization: {
+        ...config.optimization,
+        minimizer: [],
+      },
+      plugins: [
+        ...config.plugins,
+        new webpack.DefinePlugin({
+          TEST_ENV: JSON.stringify(env.TEST_ENV),
+        }),
+        // https://github.com/TypeStrong/ts-loader/issues/653
+        new IgnoreNotFoundPlugin(['OverridableComponent', 'BaseProps']),
+        // until we use docs or control addon, we need custom webpack plugin for docgen
+        // https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#docs-framework-refactor-for-react
+        new ReactDocgenTypescriptPlugin({
+          ...reactDocgenTypescriptOptions,
+          // We *need* this set so that RDT returns default values in the same format as react-docgen
+          savePropValueAsString: true,
+        }),
       ],
-    })
+      node: {
+        ...config.node,
+        /*
+        The following Node.js options configure whether to use the filename of the input file relative to the context option.
+        It was added to easily construct links to Source code of components
+        */
+        __filename: true,
+      },
+      resolve: {
+        ...config.resolve,
+        mainFields: ['browser', 'main', 'module'],
+        fallback: {
+          fs: false,
+        },
+        alias: {
+          ...config.resolve.alias,
+          '~': path.resolve(__dirname, '..'),
+          '@toptal/picasso': path.resolve(__dirname, '../packages/picasso/src'),
+          '@toptal/picasso-shared': path.resolve(
+            __dirname,
+            '../packages/shared/src'
+          ),
+          '@toptal/picasso/utils': path.resolve(
+            __dirname,
+            '../packages/picasso/src/utils'
+          ),
+          '@toptal/picasso/Icon': path.resolve(
+            __dirname,
+            '../packages/picasso/src/Icon'
+          ),
+          '@toptal/picasso-forms': path.resolve(
+            __dirname,
+            '../packages/picasso-forms/src'
+          ),
+          '@toptal/picasso-charts': path.resolve(
+            __dirname,
+            '../packages/picasso-charts/src'
+          ),
+          '@topkit/analytics-charts': path.resolve(
+            __dirname,
+            '../packages/topkit-analytics-charts/src'
+          ),
+          '@toptal/picasso-provider': path.resolve(
+            __dirname,
+            '../packages/picasso-provider/src'
+          ),
+        },
+      },
+    }
+  },
+  // until we use docs or control addon, we need custom babel plugin for docgen
+  // https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#docs-framework-refactor-for-react
+  babel: async (config, options) => {
+    const typescriptOptions = await options.presets.apply('typescript', {})
 
-    // supress an error with dynamic path e.g. require(`${url}`)
-    // https://github.com/webpack/webpack/issues/196
-    config.module.exprContextCritical = false
+    const { reactDocgen } = typescriptOptions
 
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '~': path.resolve(__dirname, '..'),
-      '@toptal/picasso': path.resolve(__dirname, '../packages/picasso/src'),
-      '@toptal/picasso-shared': path.resolve(
-        __dirname,
-        '../packages/shared/src'
-      ),
-      '@toptal/picasso/utils': path.resolve(
-        __dirname,
-        '../packages/picasso/src/utils'
-      ),
-      '@toptal/picasso/Icon': path.resolve(
-        __dirname,
-        '../packages/picasso/src/Icon'
-      ),
-      '@toptal/picasso-forms': path.resolve(
-        __dirname,
-        '../packages/picasso-forms/src'
-      ),
-      '@toptal/picasso-charts': path.resolve(
-        __dirname,
-        '../packages/picasso-charts/src'
-      ),
-      '@topkit/analytics-charts': path.resolve(
-        __dirname,
-        '../packages/topkit-analytics-charts/src'
-      ),
-      '@toptal/picasso-provider': path.resolve(
-        __dirname,
-        '../packages/picasso-provider/src'
-      ),
+    if (typeof reactDocgen !== 'string') {
+      return config
     }
 
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        TEST_ENV: JSON.stringify(env.TEST_ENV),
-      }),
-      // https://github.com/TypeStrong/ts-loader/issues/653
-      new IgnoreNotFoundPlugin(['OverridableComponent', 'BaseProps'])
-    )
-
-    /*
-    The following Node.js options configure whether to use the filename of the input file relative to the context option.
-    It was added to easily construct links to Source code of components
-    */
-    config.node = {
-      __filename: true,
+    return {
+      ...config,
+      overrides: [
+        ...(config?.overrides || []),
+        {
+          test:
+            reactDocgen === 'react-docgen'
+              ? /\.(mjs|tsx?|jsx?)$/
+              : /\.(mjs|jsx?)$/,
+          plugins: [
+            [
+              require.resolve('babel-plugin-react-docgen'),
+              {
+                DOC_GEN_COLLECTION_NAME: 'STORYBOOK_REACT_CLASSES',
+              },
+            ],
+          ],
+        },
+      ],
     }
-
-    config.resolve.mainFields = ['browser', 'main', 'module']
-    config.resolve.fallback = {
-      fs: false,
-    }
-
-    config.optimization.minimizer = []
-
-    return config
   },
   reactOptions: {
     fastRefresh: true,
