35235e42c40dd399fd36826f95ba3f58f8bde4e2
Youssef Bouhjira <youssef.bouhjira@toptal.com>
2022-10-12 12:02:16 +0100

    feat: decimal point options

--- packages/picasso/src/utils/Formatters/format-amount.ts
+++ packages/picasso/src/utils/Formatters/format-amount.ts
@@ -8,17 +8,28 @@ export interface FormatAmount {
   currency?: string
   /** Locale identifiers are case-insensitive ASCII. However, it's conventional to use title case (first letter capitalized, successive letters lower case) for script code, upper case for region codes, and lower case for everything else. */
   locale?: string
+  /**More options**/
+  options?: {
+    /**Minimum number of digits to write from the decimal part**/
+    minimumFractionDigits?: 0
+
+    /**Maximum number of digits to write from the decimal part**/
+    maximumFractionDigits?: 0
+  }
 }
 
 export const formatAmount = ({
   amount,
   currency = DEFAULT_CURRENCY,
   locale = DEFAULT_LOCALE,
+  options = {},
 }: FormatAmount) => {
   const transformedAmount = typeof amount === 'string' ? Number(amount) : amount
 
   return Intl.NumberFormat(locale, {
     style: 'currency',
+    minimumFractionDigits: options?.minimumFractionDigits,
+    maximumFractionDigits: options?.maximumFractionDigits,
     currency,
   }).format(transformedAmount)
 }
