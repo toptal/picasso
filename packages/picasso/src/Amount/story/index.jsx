d9b55438b2c2a8948d05d0dd057f1b8cf4e3f746
Youssef Bouhjira <youssef.bouhjira@toptal.com>
2022-10-17 16:05:34 +0100

    feat: update packages/picasso/src/Amount/story/index.jsx
    Co-authored-by: Mikhail Korolev <mikhail.korolev@toptal.com>

--- packages/picasso/src/Amount/story/index.jsx
+++ packages/picasso/src/Amount/story/index.jsx
@@ -21,4 +21,4 @@ page
   .addExample('Amount/story/Currency.example.tsx', 'Currency Variants')
   .addExample('Amount/story/Variants.example.tsx', 'Typography Variants')
   .addExample('Amount/story/Locale.example.tsx', 'Locale Variants')
-  .addExample('Amount/story/Decimals.example.tsx', 'Decimal digits')
+  .addExample('Amount/story/Decimals.example.tsx', 'Fractional digits')
