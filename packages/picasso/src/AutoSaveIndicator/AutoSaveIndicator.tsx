4ba6b271df124ee3ffa8ed1e2e8f289f3e15d13c
Ozgur Kececioglu <ozgur.kececioglu@toptal.com>
2022-10-25 13:38:23 +0300

    feat: update indicator component

--- packages/picasso/src/AutoSaveIndicator/AutoSaveIndicator.tsx
+++ packages/picasso/src/AutoSaveIndicator/AutoSaveIndicator.tsx
@@ -1,14 +1,22 @@
 import React, { useEffect } from 'react'
 import debounce from 'debounce'
+import { makeStyles, Theme } from '@material-ui/core/styles'
+import cx from 'classnames'
 
 import Typography from '../Typography'
+import styles from './styles'
 
 export interface Props {
   saving: boolean
 }
 
-const AutosaveIndicator = ({ saving }: Props) => {
+const useStyles = makeStyles<Theme>(styles, {
+  name: 'PicassoAutosaveIndicator',
+})
+
+const AutoSaveIndicator = ({ saving }: Props) => {
   const [show, setShow] = React.useState(false)
+  const classes = useStyles()
 
   useEffect(() => {
     if (saving) {
@@ -25,10 +33,14 @@ const AutosaveIndicator = ({ saving }: Props) => {
   }, [saving])
 
   return (
-    <Typography size='xsmall' align='right'>
-      {show ? (saving ? 'Saving...' : 'Saved') : ''}
+    <Typography
+      className={cx(classes.root, { [classes.hidden]: !show })}
+      size='xsmall'
+      align='right'
+    >
+      {saving ? 'Saving...' : 'Saved'}
     </Typography>
   )
 }
 
-export default AutosaveIndicator
+export default AutoSaveIndicator
