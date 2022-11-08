63ee2fc04f0ca6741ded8e8f9c025ea3ca486535
Ozgur Kececioglu <ozgur.kececioglu@toptal.com>
2022-11-08 13:20:11 +0300

    chore: remove mui shadow values

--- packages/picasso/src/utils/Shadows/story/Default.example.tsx
+++ packages/picasso/src/utils/Shadows/story/Default.example.tsx
@@ -3,83 +3,22 @@ import { shadows } from '@toptal/picasso/utils'
 import { Container, Typography, Grid } from '@toptal/picasso'
 
 const Example = () => (
-  <Container flex direction='column' gap='medium'>
-    <Typography>
-      First 6 shadow values are overridden with BASE values. Following shadow
-      values are coming from MUI.
-    </Typography>
+  <Container flex direction='column' gap='medium' top='large'>
     <Grid spacing={80}>
-      <ShadowBox
-        shadow={shadows[0]}
-        index={0}
-        description={
-          <Typography size='xsmall' align='center'>
-            none
-          </Typography>
-        }
-      />
+      <ShadowBox shadow={shadows[0]} index={0} description={'none'} />
       <ShadowBox
         shadow={shadows[1]}
         index={1}
-        description={
-          <Typography size='xsmall' align='center'>
-            notification center, paper
-          </Typography>
-        }
+        description={'notification center, paper'}
       />
-      <ShadowBox
-        shadow={shadows[2]}
-        index={2}
-        description={
-          <Typography size='xsmall' align='center'>
-            modal
-          </Typography>
-        }
-      />
+      <ShadowBox shadow={shadows[2]} index={2} description={'modal'} />
       <ShadowBox
         shadow={shadows[3]}
         index={3}
-        description={
-          <Typography size='xsmall' align='center'>
-            notification growl
-          </Typography>
-        }
+        description={'notification growl'}
       />
-      <ShadowBox
-        shadow={shadows[4]}
-        index={4}
-        description={
-          <Typography size='xsmall' align='center'>
-            tooltip
-          </Typography>
-        }
-      />
-      <ShadowBox
-        shadow={shadows[5]}
-        index={5}
-        description={
-          <Typography size='xsmall' align='center'>
-            scroll menu
-          </Typography>
-        }
-      />
-      {shadows.slice(6).map((shadow, index) => (
-        <ShadowBox
-          key={shadow}
-          shadow={shadow}
-          index={index + 6}
-          description={
-            <>
-              {shadow.split('),').map((color, colorIndex) => (
-                <Typography key={color} size='xsmall' align='center'>
-                  {color}
-                  {colorIndex !== shadow.split('),').length - 1 ? ')' : ''}
-                </Typography>
-              ))}
-            </>
-          }
-        />
-      ))}
+      <ShadowBox shadow={shadows[4]} index={4} description={'tooltip'} />
+      <ShadowBox shadow={shadows[5]} index={5} description={'scroll menu'} />
     </Grid>
   </Container>
 )
@@ -90,12 +29,14 @@ interface ShadowBoxProps {
   description?: ReactNode
 }
 const ShadowBox = ({ index, shadow, description }: ShadowBoxProps) => (
-  <Grid.Item small={3}>
+  <Grid.Item small={4}>
     <Container padded='small' style={{ boxShadow: shadow }}>
       <Typography variant='heading' size='large' align='center'>
         {index}
       </Typography>
-      {description}
+      <Typography size='xsmall' align='center'>
+        {description}
+      </Typography>
     </Container>
   </Grid.Item>
 )
