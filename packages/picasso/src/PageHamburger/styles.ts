5c145def67c2a0b0238d475e67c459b695ea455f
Ondřej Tůma <ondra.tuma@toptal.com>
2022-11-12 12:46:35 +0100

    feat(PageHamburger): add PageHamburger component

---
+++ packages/picasso/src/PageHamburger/styles.ts
@@ -0,0 +1,39 @@
+import { Theme, createStyles } from '@material-ui/core/styles'
+
+export default ({ palette, screens, zIndex }: Theme) => {
+  const wrapperBoxShadow = `inset -1px 0px 0px 0px ${palette.grey.lighter2}`
+
+  return createStyles({
+    hamburger: {
+      pointerEvents: 'none',
+    },
+    hidden: {
+      display: 'none',
+    },
+    responsiveWrapper: {
+      position: 'fixed',
+      top: '0.75em',
+      left: '0.75em',
+      zIndex: zIndex.appBar,
+    },
+    responsiveWrapperContent: {
+      backgroundColor: palette.grey.lighter,
+      boxShadow: wrapperBoxShadow,
+      maxHeight: 'calc(100vh - 4.5rem)', // viewport minus header height
+
+      [screens('small', 'medium')]: {
+        maxHeight: 'calc(100vh - 3rem)', // viewport minus header height
+      },
+
+      // height under which maxHeight menu starts to overflow
+      // and needs to reduce height dynamically to avoid overflow
+      '@media screen and (max-height: 585px)': {
+        maxHeight: 'calc(100vh - 4.5rem)', // viewport minus header height
+
+        [screens('small', 'medium')]: {
+          maxHeight: 'calc(100vh - 3rem)', // viewport minus header height
+        },
+      },
+    },
+  })
+}
--- packages/shared/src/utils/index.ts
+++ packages/shared/src/utils/index.ts
@@ -1,1 +1,2 @@
 export { default as isBrowser } from './is-browser'
+export { default as getElementById } from './get-element-by-id'
