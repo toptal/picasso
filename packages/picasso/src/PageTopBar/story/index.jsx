c1df84158f6e1be6b6ea68783c3e14dd39022854
Ondřej Tůma <ondra.tuma@toptal.com>
2022-11-12 16:48:39 +0100

    feat(PageTopBar): separate PageTopBar into storybook components

--- packages/picasso/src/PageTopBar/story/index.jsx
+++ packages/picasso/src/PageTopBar/story/index.jsx
@@ -1,26 +1,37 @@
 import { PageTopBar } from '../PageTopBar'
+import topBarMenuStory from '../../TopBarMenu/story'
+import topBarItemStory from '../../TopBarItem/story'
+import pageTopBarMenuStory from '../../PageTopBarMenu/story'
 import PicassoBook from '~/.storybook/components/PicassoBook'
 
-const componentDocs = PicassoBook.createComponentDocs(PageTopBar, 'Page.TopBar')
-
-const chapter = PicassoBook.connectToPage(page =>
-  page
-    .createChapter('Page.TopBar', 'A PageTopBar component')
-    .addExample('PageTopBar/story/Default.example.tsx', 'Default')
-    .addExample('PageTopBar/story/Variants.example.tsx', 'Variants')
-    .addExample('PageTopBar/story/LeftContent.example.tsx', 'Left content')
-    .addExample('PageTopBar/story/RightContent.example.tsx', 'Right content')
-    .addExample('PageTopBar/story/CenterContent.example.tsx', 'Center content')
-    .addExample(
-      'PageTopBar/story/ExtraMenuContent.example.tsx',
-      'Extra header menu content'
-    )
-    .addExample('PageTopBar/story/Link.example.tsx', 'With link')
-    .addExample('PageTopBar/story/WithoutTitle.example.tsx', 'Without title')
-    .addExample('PageTopBar/story/Logo.example.tsx', 'With custom logo')
+const page = PicassoBook.section('Components').createPage(
+  'PageTopBar',
+  `A PageTopBar component
+  
+  ${PicassoBook.createSourceLink(__filename)}
+  `
 )
 
-export default {
-  componentDocs,
-  chapter,
-}
+page
+  .createTabChapter('Props')
+  .addComponentDocs({ component: PageTopBar, name: 'Page.TopBar' })
+  .addComponentDocs(topBarMenuStory.componentDocs)
+  .addComponentDocs(topBarItemStory.componentDocs)
+  .addComponentDocs(pageTopBarMenuStory.componentDocs)
+
+page
+  .createChapter()
+  .addExample('PageTopBar/story/Default.example.tsx', 'Default')
+  .addExample('PageTopBar/story/Variants.example.tsx', 'Variants')
+  .addExample('PageTopBar/story/LeftContent.example.tsx', 'Left content')
+  .addExample('PageTopBar/story/RightContent.example.tsx', 'Right content')
+  .addExample('PageTopBar/story/CenterContent.example.tsx', 'Center content')
+  .addExample(
+    'PageTopBar/story/ExtraMenuContent.example.tsx',
+    'Extra header menu content'
+  )
+  .addExample('PageTopBar/story/Link.example.tsx', 'With link')
+  .addExample('PageTopBar/story/WithoutTitle.example.tsx', 'Without title')
+  .addExample('PageTopBar/story/Logo.example.tsx', 'With custom logo')
+
+page.connect(topBarMenuStory.chapter)
