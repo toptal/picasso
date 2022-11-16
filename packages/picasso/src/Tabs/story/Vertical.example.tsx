68dc4daca5b5f89b9058626472250d14328f67db
TomasSlama <tomas.slama@toptal.com>
2022-11-16 20:44:59 +0100

    chore(Tabs): showcase example of vertical tabs pattern

--- packages/picasso/src/Tabs/story/Vertical.example.tsx
+++ packages/picasso/src/Tabs/story/Vertical.example.tsx
@@ -1,5 +1,34 @@
 import React from 'react'
 import { Container, Tabs } from '@toptal/picasso'
+import { shadows, sizes, palette } from '@toptal/picasso/utils'
+
+type TabPanelProps = {
+  children: React.ReactNode
+  value: number
+  index: number
+}
+const TabPanel = ({ children, value, index }: TabPanelProps) => (
+  <Container role='tabpanel' hidden={value !== index}>
+    {children}
+  </Container>
+)
+
+// This component is not from BASE, just an example how it can be used
+const TabsContent = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Container
+      style={{
+        flex: '1 1 auto',
+        backgroundColor: palette.grey.lightest,
+        boxShadow: shadows[1],
+        borderRadius: sizes.borderRadius.medium,
+      }}
+      padded='small'
+    >
+      {children}
+    </Container>
+  )
+}
 
 const Example = () => {
   const [value, setValue] = React.useState(0)
@@ -9,22 +38,45 @@ const Example = () => {
   }
 
   return (
-    <Container flex>
-      <Tabs value={value} orientation='vertical' onChange={handleChange}>
-        <Tabs.Tab label='Jobs' />
-        <Tabs.Tab label='Engagements' />
-        <Tabs.Tab label='Interviews' />
+    <Container
+      style={{
+        backgroundColor: palette.grey.lighter,
+      }}
+      padded='small'
+      flex
+    >
+      <Tabs onChange={handleChange} orientation='vertical' value={value}>
+        <Tabs.Tab
+          avatar='./jacqueline-with-flowers-1954-square.jpg'
+          description='UI specialist'
+          label='Jacqueline Roque'
+        />
+        <Tabs.Tab
+          avatar='./jacqueline-with-flowers-1954-square.jpg'
+          description='UI specialist'
+          label='Jacqueline Roque'
+        />
+        <Tabs.Tab
+          avatar=''
+          description='UI specialist'
+          label='Jacqueline Roque'
+        />
+        <Tabs.Tab avatar={null} description='UI specialist' label='John Doe' />
       </Tabs>
-
-      {value === 0 && (
-        <Container left='small'>Content of the first tab</Container>
-      )}
-      {value === 1 && (
-        <Container left='small'>Content of the second tab</Container>
-      )}
-      {value === 2 && (
-        <Container left='small'>Content of the third tab</Container>
-      )}
+      <TabsContent>
+        <TabPanel value={value} index={0}>
+          Content of the first tab
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Content of the second tab
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Content of the third tab
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          Content of the third tab
+        </TabPanel>
+      </TabsContent>
     </Container>
   )
 }
--- packages/picasso/src/Tabs/story/index.jsx
+++ packages/picasso/src/Tabs/story/index.jsx
@@ -1,4 +1,3 @@
-import { Tabs } from '../Tabs'
 import tabStory from '../../Tab/story'
 import PicassoBook from '~/.storybook/components/PicassoBook'
 
@@ -8,17 +7,17 @@ const page = PicassoBook.section('Layout').createPage(
   Tabs allow to switch between content sections
 
   ${PicassoBook.createBaseDocsLink(
-    'https://share.goabstract.com/7263b996-174b-4d0c-b5aa-22344bfba249?mode=build&sha=baf4205fe8e730f6bc50c064446103ccc8c988f1'
+    'https://www.figma.com/file/5SCTOPrCDcHuk5We091GBn/Product-Library?node-id=246%3A11213'
   )}
 
   ${PicassoBook.createSourceLink(__filename)}
   `
 )
 
-page
-  .createTabChapter('Props')
-  .addComponentDocs({ component: Tabs, name: 'Tabs' })
-  .addComponentDocs(tabStory.componentDocs)
+// page
+//   .createTabChapter('Props')
+//   .addComponentDocs({ component: Tabs, name: 'Tabs' })
+//   .addComponentDocs(tabStory.componentDocs)
 
 page
   .createChapter()
@@ -28,7 +27,8 @@ page
   })
   .addExample('Tabs/story/Vertical.example.tsx', {
     title: 'Vertical',
-    takeScreenshot: true,
+    takeScreenshot: false,
+    description: '⚠️ Not responsive',
   })
   .addExample('Tabs/story/ScrollButtons.example.tsx', {
     title: 'Scroll buttons',
