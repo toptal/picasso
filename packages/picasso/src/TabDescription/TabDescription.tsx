950545d03e65899b1d97780e1e0f410265d46f7d
TomasSlama <tomas.slama@toptal.com>
2022-11-16 20:35:44 +0100

    feat(Tabs): abstract description to custom component

---
+++ packages/picasso/src/TabDescription/TabDescription.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { makeStyles, Theme } from '@material-ui/core/styles'
+
+import TypographyOverflow from '../TypographyOverflow'
+import styles from './styles'
+
+interface Props {
+  children: string
+  disabled?: boolean
+}
+
+const useStyles = makeStyles<Theme>(styles, { name: 'PicassoTabDescription' })
+
+const TabDescription = ({ children, disabled }: Props) => {
+  const classes = useStyles()
+  const color = disabled ? 'inherit' : undefined
+
+  return (
+    <TypographyOverflow
+      className={classes.root}
+      size='xxsmall'
+      inline
+      color={color}
+    >
+      {children}
+    </TypographyOverflow>
+  )
+}
+
+export default TabDescription
