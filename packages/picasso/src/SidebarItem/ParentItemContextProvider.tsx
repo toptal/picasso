ff5b4e5dfa577c6eded01c0953ee91ad98c0b297
TomasSlama <tomas.slama@toptal.com>
2022-12-01 14:54:22 +0100

    feat: create parent item context

---
+++ packages/picasso/src/SidebarItem/ParentItemContextProvider.tsx
@@ -0,0 +1,46 @@
+import React, { createContext, useContext, ReactNode, useState } from 'react'
+
+export interface ContextProps {
+  /**
+   * Render indicator on the parent item
+   * when some of children has badge or tag rendered
+   */
+  isIndicatorVisible: boolean
+  /**
+   * When submenu item with badge or tag is mounted
+   */
+  handleDecoratedItemMount: () => void
+  /**
+   * When submenu item with badge or tag is unmounted
+   */
+  handleDecoratedItemUnmount: () => void
+}
+
+const Context = createContext<ContextProps>({
+  isIndicatorVisible: false,
+  handleDecoratedItemMount: () => {},
+  handleDecoratedItemUnmount: () => {},
+})
+
+export interface Props {
+  children?: ReactNode
+  isOpened: boolean
+}
+
+export const ParentItemContextProvider = ({ children, isOpened }: Props) => {
+  const [countItemsWithBadgeOrTag, updateCount] = useState(0)
+
+  const isIndicatorVisible = countItemsWithBadgeOrTag > 0 && !isOpened
+  const handleDecoratedItemMount = () => updateCount(count => count + 1)
+  const handleDecoratedItemUnmount = () => updateCount(count => count - 1)
+
+  const value = {
+    isIndicatorVisible,
+    handleDecoratedItemMount,
+    handleDecoratedItemUnmount,
+  }
+
+  return <Context.Provider value={value}>{children}</Context.Provider>
+}
+
+export const useParentItemContext = () => useContext(Context)
