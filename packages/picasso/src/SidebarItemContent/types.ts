2793b5479888bfa27fee4a53837cd0e5a166a818
TomasSlama <tomas.slama@toptal.com>
2022-12-01 12:43:00 +0100

    feat: add getBadgeProps and getTagProps util fns

---
+++ packages/picasso/src/SidebarItemContent/types.ts
@@ -0,0 +1,24 @@
+import { ReactElement, ReactNode } from 'react'
+
+import { BadgeProps } from '../Badge'
+import { VariantOnlyProps } from '../TagRectangular/types'
+
+/** Render Tag.Rectangular */
+export interface SidebarTagProps extends VariantOnlyProps {
+  content: string
+}
+
+/** Definition of the embedded badge  */
+export type SidebarBadgeProps = Omit<BadgeProps, 'size' | 'children'>
+
+export interface Props {
+  compact?: boolean
+  icon?: ReactElement
+  badge?: SidebarBadgeProps
+  tag?: SidebarTagProps
+  children?: ReactNode
+  titleCase?: boolean
+  testIds?: {
+    content?: string
+  }
+}
