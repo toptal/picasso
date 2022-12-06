934b2cb3ee6d73559b401c843b8a28614c722b8d
TomasSlama <tomas.slama@toptal.com>
2022-12-06 15:55:50 +0100

    test: fix flaky test of StaticTreeView

--- packages/picasso/src/StaticTreeView/story/index.jsx
+++ packages/picasso/src/StaticTreeView/story/index.jsx
@@ -4,10 +4,51 @@ import PicassoBook from '~/.storybook/components/PicassoBook'
 const page = PicassoBook.section('Components').createPage(
   'StaticTreeView',
   `Allows rendering a static variant of tree view
-  
+
    ${PicassoBook.createSourceLink(__filename)}
   `
 )
+
+const waitForImageToLoad = url => {
+  return new Promise((resolve, reject) => {
+    const img = new Image()
+
+    img.onerror = () =>
+      reject(new Error(`Failed to load image with url ${url}`))
+    img.onload = resolve
+    img.src = url
+  })
+}
+
+const waitForSvgImagesToRender = () => {
+  return new Promise((resolve, reject) => {
+    const svgImages = document.querySelectorAll('svg image')
+    const promises = [...svgImages]
+      .map(image => image.href)
+      .filter(Boolean)
+      .map(waitForImageToLoad)
+
+    if (promises.length === 0) {
+      // There are no images to wait for, so we can just resolve right away.
+      resolve()
+    }
+
+    Promise.all(promises)
+      .then(() => {
+        // Now that the images have loaded, we need to wait for a couple of
+        // animation frames to go by before we think they will have finished
+        // rendering.
+        return requestAnimationFrame(() => {
+          // Start render
+          requestAnimationFrame(() => {
+            // Finish rendering
+            resolve()
+          })
+        })
+      })
+      .catch(reject)
+  })
+}
 
 page.createTabChapter('Props').addComponentDocs({
   component: StaticTreeView,
@@ -18,6 +59,11 @@ page
   .createChapter()
   .addExample('StaticTreeView/story/Default.example.tsx', {
     title: 'Default',
+    takeScreenshot: {
+      beforeScreenshot: async () => {
+        await waitForSvgImagesToRender()
+      },
+    },
   })
   .addExample('StaticTreeView/story/Horizontal.example.tsx', {
     title: 'Horizontal Direction',
