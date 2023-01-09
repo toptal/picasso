6bd003b6b75d10727573107d8f215734e431979c
TomasSlama <tomas.slama@toptal.com>
2023-01-09 10:10:23 +0100

    fixup! style(Input): make end adornment always on end of the input

--- packages/picasso/src/TagSelector/story/index.jsx
+++ packages/picasso/src/TagSelector/story/index.jsx
@@ -16,30 +16,30 @@ page
 
 page
   .createChapter()
-  // .addExample('TagSelector/story/Default.example.tsx', 'Default')
-  // .addExample('TagSelector/story/OtherOption.example.tsx', {
-  //   title: 'Other option',
-  //   takeScreenshot: false,
-  // })
-  // .addExample(
-  //   'TagSelector/story/InitialSetValue.example.tsx',
-  //   'Initially set value'
-  // )
-  // .addExample('TagSelector/story/KeepOpen.example.tsx', {
-  //   title: 'With closeOnSelect disabled',
-  //   description:
-  //     'Disabling closeOnSelect can be useful when the user always have to select multiple values at the same time',
-  //   takeScreenshot: false,
-  // })
-  // .addExample('TagSelector/story/CustomOptionRenderer.example.tsx', {
-  //   title: 'Custom option rendering',
-  //   takeScreenshot: false,
-  // })
-  // .addExample('TagSelector/story/CustomLabelRenderer.example.tsx', {
-  //   title: 'Custom label rendering',
-  //   takeScreenshot: false,
-  // })
-  // .addExample('TagSelector/story/Loading.example.tsx', 'Loading')
-  // .addExample('TagSelector/story/Disabled.example.tsx', 'Disabled')
-  // .addExample('TagSelector/story/FullWidth.example.tsx', 'Full width')
+  .addExample('TagSelector/story/Default.example.tsx', 'Default')
+  .addExample('TagSelector/story/OtherOption.example.tsx', {
+    title: 'Other option',
+    takeScreenshot: false,
+  })
+  .addExample(
+    'TagSelector/story/InitialSetValue.example.tsx',
+    'Initially set value'
+  )
+  .addExample('TagSelector/story/KeepOpen.example.tsx', {
+    title: 'With closeOnSelect disabled',
+    description:
+      'Disabling closeOnSelect can be useful when the user always have to select multiple values at the same time',
+    takeScreenshot: false,
+  })
+  .addExample('TagSelector/story/CustomOptionRenderer.example.tsx', {
+    title: 'Custom option rendering',
+    takeScreenshot: false,
+  })
+  .addExample('TagSelector/story/CustomLabelRenderer.example.tsx', {
+    title: 'Custom label rendering',
+    takeScreenshot: false,
+  })
+  .addExample('TagSelector/story/Loading.example.tsx', 'Loading')
+  .addExample('TagSelector/story/Disabled.example.tsx', 'Disabled')
+  .addExample('TagSelector/story/FullWidth.example.tsx', 'Full width')
   .addExample('TagSelector/story/Status.example.tsx', 'Status')
