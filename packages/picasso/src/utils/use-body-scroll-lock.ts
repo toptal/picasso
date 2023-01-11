867ad51bd2e421660ba2da451d633d67e515da07
Alexander Shahorsky <alexander.shahorsky@toptal.com>
2023-01-11 19:01:39 +0100

    fix(Modal): decouple body scroll lock management from ModalManager

---
+++ packages/picasso/src/utils/use-body-scroll-lock.ts
@@ -0,0 +1,63 @@
+import { isBrowser } from '@toptal/picasso-shared'
+import { useEffect, useMemo } from 'react'
+
+const layers = new Set<number>();
+let scrollLock: { prevBodyOverflow: string } | undefined = undefined;
+
+export const useBodyScrollLock = (isLocked: boolean) => {
+  const layerId = useMemo(generateLayerId, [])
+
+  useEffect(() => {
+    if (isLocked) {
+      layers.add(layerId)
+      syncBodyScrollLock()
+    }
+
+    return () => {
+      layers.delete(layerId)
+      syncBodyScrollLock()
+    }
+  }, [layerId, isLocked])
+}
+
+const generateLayerId = (() => {
+  let count = 0
+
+  return () => {
+    count = count + 1
+    
+    return count;
+  }
+})()
+
+const syncBodyScrollLock = () => {
+  if (layers.size > 0) {
+    addBodyScrollLock()
+  } else {
+    removeBodyScrollLock()
+  }  
+}
+
+const addBodyScrollLock = () => {
+  if (!isBrowser()) {
+    return
+  }
+
+  if (!scrollLock) {
+    scrollLock = {
+      prevBodyOverflow: document.body.style.overflow,
+    }
+    document.body.style.overflow = 'hidden'
+  }
+}
+
+const removeBodyScrollLock = () => {
+  if (!isBrowser()) {
+    return
+  }
+
+  if (scrollLock) {
+    document.body.style.overflow = scrollLock.prevBodyOverflow
+    scrollLock = undefined
+  }
+}
