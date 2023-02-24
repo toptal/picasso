10ea0bdf8142a6c8b76f0721a7f93143ad9fd8b3
TomasSlama <tomas.slama@toptal.com>
2023-02-24 12:28:45 +0100

    fix: storybook

--- packages/picasso/src/Carousel/story/index.jsx
+++ packages/picasso/src/Carousel/story/index.jsx
@@ -7,32 +7,32 @@ page
   .createTabChapter('Props')
   .addComponentDocs({ component: Carousel, name: 'Carousel' })
 
-page.createChapter().addExample('Carousel/story/Default.example.jsx', {
+page.createChapter().addExample('Carousel/story/Default.example.tsx', {
   title: 'Default',
   takeScreenshot: false,
 })
-page.createChapter().addExample('Carousel/story/SlidesToShow.example.jsx', {
+page.createChapter().addExample('Carousel/story/SlidesToShow.example.tsx', {
   title: 'Slides to show',
   description:
     'When not a whole number is used (2.5), it shows gradient over the last item by default',
   takeScreenshot: false,
 })
-page.createChapter().addExample('Carousel/story/Navigation.example.jsx', {
+page.createChapter().addExample('Carousel/story/Navigation.example.tsx', {
   takeScreenshot: false,
   title: 'Navigation',
 })
 
-page.createChapter().addExample('Carousel/story/Events.example.jsx', {
+page.createChapter().addExample('Carousel/story/Events.example.tsx', {
   title: 'Events',
   takeScreenshot: false,
 })
-page.createChapter().addExample('Carousel/story/Autoplay.example.jsx', {
+page.createChapter().addExample('Carousel/story/Autoplay.example.tsx', {
   title: 'Autoplay',
   description:
     'Autoplay will start only after the carousel is visible on the screen',
   takeScreenshot: false,
 })
-page.createChapter().addExample('Carousel/story/Rewind.example.jsx', {
+page.createChapter().addExample('Carousel/story/Rewind.example.tsx', {
   title: 'Rewind',
   takeScreenshot: false,
 })
