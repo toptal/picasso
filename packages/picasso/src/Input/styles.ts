153d35fbd20e922c1727fb7880f9b3b79506c46c
TomasSlama <tomas.slama@toptal.com>
2023-03-21 09:18:53 +0100

    refactor(Input): use reusable style function

--- packages/picasso/src/Input/styles.ts
+++ packages/picasso/src/Input/styles.ts
@@ -1,26 +1,23 @@
 import { Theme, createStyles } from '@material-ui/core/styles'
-import { alpha } from '@toptal/picasso-shared'
 
+import highlightAutofillStyles from '../InputBase/highlightAutofillStyles'
 import '../InputBase/styles'
 import '../InputLabel/styles'
 import '../OutlinedInput/styles'
 import '../InputAdornment/styles'
 
-export default ({ palette }: Theme) =>
+export default (theme: Theme) =>
   createStyles({
     root: {
       fontSize: '1rem',
-      backgroundColor: palette.common.white,
+      backgroundColor: theme.palette.common.white,
       cursor: 'text',
     },
     rootMultiline: {
       height: 'auto',
     },
-    highlightAutofill: {
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      backgroundColor: alpha(palette.yellow.lighter!, 0.6),
-    },
     inputMultilineResizable: {
       resize: 'vertical',
     },
+    ...highlightAutofillStyles(theme),
   })
