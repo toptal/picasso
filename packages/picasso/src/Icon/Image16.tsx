f07691f7551b6203a162a29e107140590a759880
Denys Medynskyi <denys.medynskyi@toptal.com>
2023-03-22 09:03:48 +0200

    feat: run generate icons (generates broken svg)

--- packages/picasso/src/Icon/Image16.tsx
+++ packages/picasso/src/Icon/Image16.tsx
@@ -53,22 +53,9 @@ const SvgImage16 = forwardRef(function SvgImage16(
       data-testid={testId}
     >
       <path
-        fill-rule='evenodd'
-        clip-rule='evenodd'
-        d='M0 1H16V14H0V1ZM1 2V13H15V2H1Z'
-        fill='#455065'
-      />
-      <path
-        fill-rule='evenodd'
-        clip-rule='evenodd'
-        d='M12.5 3.79297L15.8535 7.14652L15.1464 7.85363L12.5 5.20718L6.59868 11.1085L4.55274 10.0855L0.799951 12.9001L0.199951 12.1001L4.44717 8.91467L6.40123 9.8917L12.5 3.79297Z'
-        fill='#455065'
-      />
-      <path
-        fill-rule='evenodd'
-        clip-rule='evenodd'
-        d='M5 5C4.44772 5 4 5.44772 4 6C4 6.55228 4.44772 7 5 7C5.55228 7 6 6.55228 6 6C6 5.44772 5.55228 5 5 5ZM3 6C3 4.89543 3.89543 4 5 4C6.10457 4 7 4.89543 7 6C7 7.10457 6.10457 8 5 8C3.89543 8 3 7.10457 3 6Z'
-        fill='#455065'
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M0 1h16v13H0V1Zm1 1v11h14V2H1Z'
       />
     </svg>
   )
--- packages/picasso/src/Icon/Image24.tsx
+++ packages/picasso/src/Icon/Image24.tsx
@@ -52,24 +52,7 @@ const SvgImage24 = forwardRef(function SvgImage24(
       ref={ref}
       data-testid={testId}
     >
-      <path
-        fill-rule='evenodd'
-        clip-rule='evenodd'
-        d='M0 2H24V21H0V2ZM1 3V20H23V3H1Z'
-        fill='#455065'
-      />
-      <path
-        fill-rule='evenodd'
-        clip-rule='evenodd'
-        d='M18.4417 6.81299L23.8124 11.1095L23.1877 11.8904L18.5583 8.18694L10.6191 17.1185L6.53593 15.0769L0.777356 18.916L0.222656 18.0839L6.46409 13.923L10.3809 15.8814L18.4417 6.81299Z'
-        fill='#455065'
-      />
-      <path
-        fill-rule='evenodd'
-        clip-rule='evenodd'
-        d='M8 7C6.89543 7 6 7.89543 6 9C6 10.1046 6.89543 11 8 11C9.10457 11 10 10.1046 10 9C10 7.89543 9.10457 7 8 7ZM5 9C5 7.34315 6.34315 6 8 6C9.65685 6 11 7.34315 11 9C11 10.6569 9.65685 12 8 12C6.34315 12 5 10.6569 5 9Z'
-        fill='#455065'
-      />
+      <path d='M0 2h24v19H0V2Zm1 1v17h22V3H1Z' />
     </svg>
   )
 })
