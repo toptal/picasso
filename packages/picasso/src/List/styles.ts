712b1136e9ac13c5f9006db42a59f7ae08a09f0f
Augusto Moura <augusto.moura@toptal.com>
2023-04-10 00:37:00 -0300

    feat(List): move to em units

--- packages/picasso/src/List/styles.ts
+++ packages/picasso/src/List/styles.ts
@@ -1,22 +1,26 @@
 import type { Theme } from '@material-ui/core/styles'
 import { createStyles } from '@material-ui/core/styles'
+import { em, fromPx } from '@toptal/picasso-shared'
 
 import { listStyleTypes } from '../ListItem/styles'
 import { palette } from '../utils'
 
-export default ({ typography }: Theme) =>
-  createStyles({
+export default ({ typography }: Theme) => {
+  const toEmUnit = (px: number) => em(px, fromPx(typography.fontSizes.medium))
+
+  return createStyles({
     root: {
       fontSize: typography.fontSizes.medium,
       color: palette.common.black,
-      marginTop: '4px',
-      paddingLeft: '16px',
+      marginTop: toEmUnit(4),
+      paddingLeft: toEmUnit(16),
     },
     unordered: {
-      paddingLeft: '24px',
+      paddingLeft: toEmUnit(24),
     },
     firstLevel: {
-      paddingLeft: '16px',
+      paddingLeft: toEmUnit(16),
     },
     ...listStyleTypes,
   })
+}
