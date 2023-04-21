d4e2c6a40411135cf403dd0f180c8e69000fc3b2
Mikhail Korolev <mikhail.korolev@toptal.com>
2023-04-21 14:37:58 +0200

    chore: make prop table responsive in Picasso Storybook (#3524)

--- .storybook/components/PicassoBook/components/PropsTable/styles.ts
+++ .storybook/components/PicassoBook/components/PropsTable/styles.ts
@@ -3,58 +3,59 @@ import { Theme, createStyles } from '@material-ui/core/styles'
 export default ({ palette }: Theme) =>
   createStyles({
     root: {
-      width: '100%'
+      width: '100%',
+      overflowX: 'auto',
     },
     table: {
-      width: '100%'
+      width: '100%',
     },
     name: {
-      width: '100px'
+      width: '100px',
     },
     type: {
-      width: '1%'
+      width: '1%',
     },
     description: {
-      width: '100%'
+      width: '100%',
     },
     defaultValue: {
-      width: '1%'
+      width: '1%',
     },
     tooltipTarget: {
       borderBottom: '1px dotted',
       fontWeight: 600,
-      cursor: 'help'
+      cursor: 'help',
     },
     highlight: {
       backgroundColor: 'rgb(236, 236, 236, 0.5)',
       borderRadius: '0.4em',
       padding: '0.3em 0.7em',
-      fontWeight: 600
+      fontWeight: 600,
     },
     typeCell: {
-      whiteSpace: 'nowrap'
+      whiteSpace: 'nowrap',
     },
     defaultValueCell: {
-      whiteSpace: 'nowrap'
+      whiteSpace: 'nowrap',
     },
     descriptionCell: {
       paddingTop: '1em',
-      paddingBottom: '1em'
+      paddingBottom: '1em',
     },
     enums: {
       marginTop: '1em',
       display: 'flex',
       flexWrap: 'wrap',
-      alignItems: 'center'
+      alignItems: 'center',
     },
     enumLabel: {
       marginRight: '0.2em',
-      marginBottom: '0.2em'
+      marginBottom: '0.2em',
     },
     enum: {
       fontSize: '0.8rem',
       marginRight: '0.2em',
-      marginBottom: '0.2em'
+      marginBottom: '0.2em',
     },
     markdown: {
       '& code': {
@@ -64,21 +65,21 @@ export default ({ palette }: Theme) =>
         fontWeight: 400,
         fontSize: '0.8em',
         fontFamily:
-          "'Monaco', 'Menlo', 'Ubuntu Mono', 'Consolas', 'source-code-pro', monospace"
+          "'Monaco', 'Menlo', 'Ubuntu Mono', 'Consolas', 'source-code-pro', monospace",
       },
       '& p:first-child': {
-        marginTop: 0
+        marginTop: 0,
       },
       '& p:last-child': {
-        marginBottom: 0
+        marginBottom: 0,
       },
       '& pre': {
         backgroundColor: 'rgb(236, 236, 236, 0.5)',
 
         '& code': {
           padding: '0em',
-          backgroundColor: 'initial'
-        }
-      }
-    }
+          backgroundColor: 'initial',
+        },
+      },
+    },
   })
