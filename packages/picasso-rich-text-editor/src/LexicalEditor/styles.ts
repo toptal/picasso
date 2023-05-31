9759c599f87f9ff4a4b51afd5f0850b60a1b115b
Dmitry Maklygin <dmitry.maklygin@toptal.com>
2023-05-31 16:49:49 +0200

    feat: replace base Quill editor with Lexical (#3586)

---
+++ packages/picasso-rich-text-editor/src/LexicalEditor/styles.ts
@@ -0,0 +1,40 @@
+// import type { Theme } from '@material-ui/core/styles'
+import { createStyles } from '@material-ui/core/styles'
+
+export default () => {
+  return createStyles({
+    editorContainer: {
+      height: '12.5em',
+      overflowY: 'hidden',
+      resize: 'vertical',
+      position: 'relative',
+      // ...listStyles,
+      // ...margins,
+      // ...quillSpecificStyles(theme),
+    },
+
+    contentEditable: {
+      outline: 'none',
+      height: '100%',
+      padding: '1em 0.5em',
+      tabSize: 4,
+      overflowY: 'auto',
+    },
+
+    placeholder: {
+      overflow: 'hidden',
+      position: 'absolute',
+      textOverflow: 'ellipsis',
+      top: '0px',
+      left: '0px',
+      userSelect: 'none',
+      whiteSpace: 'nowrap',
+      display: 'inline-block',
+      pointerEvents: 'none',
+    },
+
+    paragraph: {
+      margin: '0 0 0.5rem',
+    },
+  })
+}
--- packages/picasso-rich-text-editor/src/QuillEditor/formats/use-typography-classes.ts
+++
@@ -1,9 +0,0 @@
-import type { Theme } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core'
-import typographyStyles from '@toptal/picasso/Typography/styles'
-
-const useTypographyClasses = makeStyles<Theme>(typographyStyles, {
-  name: 'TextEditorTypography',
-})
-
-export default useTypographyClasses
