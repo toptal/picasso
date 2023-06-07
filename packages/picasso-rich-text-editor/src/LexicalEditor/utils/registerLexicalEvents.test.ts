a5f5111c8145e9e2de0b7d24990183d8f84ecc79
Mikhail Korolev <mikhail.korolev@toptal.com>
2023-06-07 12:37:18 +0200

    feat: add Toolbar for LexicalEditor (#3601)
    * feat: add Toolbar for LexicalEditor
    
    * chore: review amends

---
+++ packages/picasso-rich-text-editor/src/LexicalEditor/utils/registerLexicalEvents.test.ts
@@ -0,0 +1,71 @@
+import { mergeRegister } from '@lexical/utils'
+import { COMMAND_PRIORITY_CRITICAL, SELECTION_CHANGE_COMMAND } from 'lexical'
+
+import { registerLexicalEvents } from './registerLexicalEvents'
+import type { LexicalRegisterParams } from './registerLexicalEvents'
+
+jest.mock('@lexical/utils', () => ({
+  mergeRegister: jest.fn(),
+}))
+
+const mockDispatch = jest.fn()
+const mockUpdateToolbar = jest.fn()
+const mockMergeRegister = mergeRegister as jest.MockedFunction<
+  typeof mergeRegister
+>
+const mockedEditorListenerCleanup = jest.fn()
+const mockedEditorCommandsCleanup = jest.fn()
+
+describe('registerLexicalEvents', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should register listeners and commands correctly and return a cleanup function', () => {
+    const mockEditor = {
+      registerEditableListener: jest.fn().mockReturnValueOnce(() => {}),
+      registerUpdateListener: jest.fn().mockReturnValueOnce(() => {}),
+      registerCommand: jest
+        .fn()
+        .mockReturnValueOnce(() => mockedEditorCommandsCleanup),
+    }
+    const mockActiveEditor = {
+      registerUpdateListener: jest.fn().mockReturnValueOnce(() => {}),
+    }
+
+    mockMergeRegister
+      .mockImplementation(() => () => {
+        mockEditor.registerEditableListener()
+        mockActiveEditor.registerUpdateListener()
+      })
+      .mockReturnValueOnce(() => mockedEditorListenerCleanup)
+
+    const params = {
+      editor: mockEditor,
+      activeEditor: mockActiveEditor,
+      updateToolbar: mockUpdateToolbar,
+      dispatch: mockDispatch,
+    }
+
+    const cleanup = registerLexicalEvents(
+      params as unknown as LexicalRegisterParams
+    )
+
+    expect(mockEditor.registerEditableListener).toHaveBeenCalledTimes(1)
+    expect(mockActiveEditor.registerUpdateListener).toHaveBeenCalledTimes(1)
+    expect(mockEditor.registerCommand).toHaveBeenCalledTimes(1)
+    expect(mockEditor.registerCommand).toHaveBeenCalledWith(
+      SELECTION_CHANGE_COMMAND,
+      expect.any(Function),
+      COMMAND_PRIORITY_CRITICAL
+    )
+    expect(mergeRegister).toHaveBeenCalledTimes(1)
+
+    // Simulate invoking the cleanup function
+    cleanup()
+
+    // Assert cleanup
+    expect(mockedEditorCommandsCleanup).toHaveBeenCalledTimes(1)
+    expect(mockedEditorListenerCleanup).toHaveBeenCalledTimes(1)
+  })
+})
