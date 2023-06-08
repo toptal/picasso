da271e94fca6a801a0b5ae849ab50651aa762ba2
Dmitry Maklygin <dmitry.maklygin@toptal.com>
2023-06-08 19:47:02 +0200

    fix: reexport change handler type

--- packages/picasso/src/RichTextEditor/index.ts
+++ packages/picasso/src/RichTextEditor/index.ts
@@ -1,8 +1,10 @@
 import type { OmitInternalProps } from '@toptal/picasso-shared'
 
 import type { Props } from './RichTextEditor'
-export type { CustomEmojiGroup } from './types'
+export type {
+  CustomEmojiGroup,
+  ChangeHandler as RichTextEditorChangeHandler,
+} from './types'
 
 export { default } from './RichTextEditor'
-export type { RichTextEditorChangeHandler } from './types'
 export type RichTextEditorProps = OmitInternalProps<Props>
