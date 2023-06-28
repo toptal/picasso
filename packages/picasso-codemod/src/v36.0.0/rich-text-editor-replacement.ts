4975888b28c9f5247f41b897575b79694a6dd352
Dmitry Maklygin <dmitry.maklygin@toptal.com>
2023-06-28 13:55:14 +0300

    chore: update version

--- packages/picasso-codemod/src/v36.0.0/rich-text-editor-replacement.ts
+++ packages/picasso-codemod/src/v36.0.0/rich-text-editor-replacement.ts
@@ -13,7 +13,7 @@ const specificModules = [
 ]
 const picassoVersion = '36.0.0'
 const picassoFormsVersion = '58.0.0'
-const picassoRichTextEditorVersion = '1.0.2'
+const picassoRichTextEditorVersion = '1.0.1'
 
 // Get current execution directory
 const execDir = process.cwd()
