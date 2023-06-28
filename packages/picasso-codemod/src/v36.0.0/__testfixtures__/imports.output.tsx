8b24606b37811de2ca363f7e9ca8f9181c11cf0d
Dmitry Maklygin <dmitry.maklygin@toptal.com>
2023-06-28 16:15:32 +0300

    feat: [FX-4053] create codemods for RTE (#3657)
    * chore: create codemods for rte
    
    * chore: adjust codemod
    
    * chore: update eslint rules for codemod
    
    * chore: update eslintignore
    
    * chore: update prettier ignore
    
    * chore: update version

---
+++ packages/picasso-codemod/src/v36.0.0/__testfixtures__/imports.output.tsx
@@ -0,0 +1,10 @@
+import { RichTextEditor } from '@toptal/picasso-forms'
+import type { CustomEmojiGroup } from '@toptal/picasso-rich-text-editor/RichTextEditor';
+import type { CustomEmoji } from '@toptal/picasso-rich-text-editor/RichTextEditor/types';
+import type { RichTextEditorProps as LocalRichTextEditorProps } from '@toptal/picasso-rich-text-editor';
+import { Container, Typography } from '@toptal/picasso';
+import { RichText as LocalRichText } from '@toptal/picasso-rich-text-editor';
+import { RichText } from '@toptal/picasso-rich-text-editor';
+import { Icon } from '@toptal/picasso'
+import { Something, transformString } from 'some-module'
+import { htmlToHast } from '@toptal/picasso-rich-text-editor/utils';
