d6cc8d05a51e33b478509019ede53a673d629714
Augusto Moura <augusto.moura@toptal.com>
2023-06-28 19:01:19 -0300

    fix: tidy merge conflicts from rebasing the feature branch

--- packages/picasso-rich-text-editor/src/index.ts
+++ packages/picasso-rich-text-editor/src/index.ts
@@ -1,4 +1,7 @@
 export { default as RichTextEditor } from './RichTextEditor'
-export type { RichTextEditorProps } from './RichTextEditor'
+export type {
+  RichTextEditorProps,
+  RichTextEditorChangeHandler,
+} from './RichTextEditor'
 export { default as RichText } from './RichText'
 export type { RichTextProps, ASTType } from './RichText'
