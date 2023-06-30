874b88bdef01ae3ef4d9d23219956bea2e99cf40
Aleksandr Shumilov <alexander.shumilov@toptal.com>
2023-06-30 19:39:48 +0300

    feat: clean up output html (#3674)

---
+++ packages/picasso-rich-text-editor/src/LexicalEditor/utils/cleanupHtmlOutput.test.ts
@@ -0,0 +1,47 @@
+import { cleanupHtmlOutput } from './cleanupHtmlOutput'
+
+// Test cases structure:
+// removeWhat (string), removeFrom (string), input (string), expected (string)
+const testCases = [
+  [
+    '<i>',
+    '<i> > <em>',
+    '<p><i><em>italic text</em></i></p>',
+    '<p><em>italic text</em></p>',
+  ],
+  [
+    '<b>',
+    '<b> > <strong>',
+    '<p><b><strong>Heading 1 with 🧙🏼‍♂️</strong></b></p>',
+    '<p><strong>Heading 1 with 🧙🏼‍♂️</strong></p>',
+  ],
+  ['<span>', '<h3> > <span>', '<h3><span>test</span></h3>', '<h3>test</h3>'],
+  ['<span>', '<p> > <span>', '<p><span>test</span></p>', '<p>test</p>'],
+  [
+    'multiple <span> tags',
+    '<p> > <span>',
+    '<p><span>italic </span><i><em>text</em></i><span> test</span></p>',
+    '<p>italic <em>text</em> test</p>',
+  ],
+]
+
+describe('cleanupHtmlOutput', () => {
+  it.each(testCases)(
+    'removes %s from %s combinations',
+    (what, from, input, expected) => {
+      const result = cleanupHtmlOutput(input)
+
+      expect(result).toBe(expected)
+    }
+  )
+
+  it('replaces <i> with <em> tag', () => {
+    const result = cleanupHtmlOutput(
+      '<p><i><em>test </em></i><i><b><strong>test</strong></b></i><span> test</span></p>'
+    )
+
+    expect(result).toBe(
+      '<p><em>test </em><em><strong>test</strong></em> test</p>'
+    )
+  })
+})
