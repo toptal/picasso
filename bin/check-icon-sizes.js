82120e864cccc6140bd9332d25314a70b27031df
Aleksandr Shumilov <alexander.shumilov@toptal.com>
2023-07-10 14:54:25 +0200

    fix: address feedback

--- bin/check-icon-sizes.js
+++ bin/check-icon-sizes.js
@@ -1,21 +1,39 @@
 const fs = require('fs')
 const path = require('path')
+const { execSync } = require('child_process')
 
 /**
  * Script checks if all icons have 16 and 24 pixels variants.
  */
 
-const ICON_COMPONENTS_DIRECTORY = './packages/picasso/src/Icon'
+const ICON_COMPONENTS_DIRECTORY = 'packages/picasso/src/Icon'
 // TODO: add missing icons and remove ability to ignore icons
 const IGNORED_ICONS = ['Ach', 'CreditCard']
 
 const getIconName = iconFileName => iconFileName.replace(/(16|24).tsx/, '')
+
+const getChangedFiles = () => {
+  const command = `git diff --name-only --cached`
+  const diffOutput = execSync(command).toString()
+
+  return diffOutput.toString().split('\n').filter(Boolean)
+}
 
 const checkIfIconIsComplete = (iconName, icons) =>
   icons.indexOf(`${iconName}16.tsx`) > -1 &&
   icons.indexOf(`${iconName}24.tsx`) > -1
 
 const findIncompleteIcons = () => {
+  const changedFiles = getChangedFiles()
+  const iconsWereChanged =
+    changedFiles.filter(fileName =>
+      fileName.startsWith(ICON_COMPONENTS_DIRECTORY)
+    ).length > 0
+
+  if (!iconsWereChanged) {
+    process.exit(0)
+  }
+
   const iconFileNames = fs
     .readdirSync(ICON_COMPONENTS_DIRECTORY)
     .filter(
@@ -44,8 +62,8 @@ const incompleteIcons = findIncompleteIcons()
 
 if (incompleteIcons.length > 0) {
   console.error(
-    `The following icons need to have both 16 and 24 pixels variants generated: ${incompleteIcons.join(
-      ', '
+    `The following icons need to have both 16 and 24 pixels variants generated:\n${incompleteIcons.join(
+      '\n'
     )}`
   )
   process.exit(1)
