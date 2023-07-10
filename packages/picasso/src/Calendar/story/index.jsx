9944a6ef6672038488a4691ca038092ef233567b
Oleksandr Nechai <oleksandr.nechai@toptal.com>
2023-07-10 18:04:28 +0200

    chore(Calendar): add stories (#3711)
    * chore(Calendar): add stories
    
    * fix: texts and format
    
    * fix: flaky tests
    
    * fix: slight change to default dates

--- packages/picasso/src/Calendar/story/index.jsx
+++ packages/picasso/src/Calendar/story/index.jsx
@@ -15,4 +15,30 @@ page.createTabChapter('Props').addComponentDocs({
   name: 'Calendar',
 })
 
-page.createChapter().addExample('Calendar/story/Default.example.tsx', 'Default')
+page
+  .createChapter()
+  .addExample('Calendar/story/Default.example.tsx', 'Default')
+  .addExample(
+    'Calendar/story/WithSelectableDaysLimit.example.tsx',
+    'With selectable days limit'
+  )
+  .addExample('Calendar/story/Range.example.tsx', 'Range')
+  .addExample('Calendar/story/WithCustomDayRendering.example.tsx', {
+    title: 'With custom day rendering',
+    description: 'Hover over the days to see a tooltip',
+    takeScreenshot: false,
+  })
+  .addExample(
+    'Calendar/story/WithDisabledIntervals.example.tsx',
+    'With disabled intervals'
+  )
+  .addExample(
+    'Calendar/story/WithIndicatedIntervals.example.tsx',
+    'With indicated intervals'
+  )
+  .addExample('Calendar/story/WithTwoMonths.example.tsx', 'With two months')
+  .addExample('Calendar/story/WithWeekStartsOnSunday.example.tsx', {
+    title: 'With week starts on Sunday',
+    description:
+      'Calendars that start on Sunday, which you’ll more often see in the US, use an old-fashioned notation based on Christianity. Which makes the day that Christians go to church the first day of the week.',
+  })
