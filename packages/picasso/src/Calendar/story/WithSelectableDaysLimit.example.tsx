8639f61abfdc171e347634bf5ccc11cb5ecc994c
OleksandrNechai <oleksandr.nechai@toptal.com>
2023-07-10 15:13:36 +0200

    chore(Calendar): add stories

---
+++ packages/picasso/src/Calendar/story/WithSelectableDaysLimit.example.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import type { DateOrDateRangeType } from '@toptal/picasso'
+import { Calendar } from '@toptal/picasso'
+
+const WithSelectableDaysLimitExample = () => {
+  const [value, setValue] = useState<DateOrDateRangeType>(new Date())
+
+  return (
+    <Calendar
+      value={value}
+      onChange={setValue}
+      minDate={new Date('2021-07-08')}
+      maxDate={new Date('2021-07-15')}
+    />
+  )
+}
+
+export default WithSelectableDaysLimitExample
