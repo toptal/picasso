6825bb71e5580a2543466685be21d97f48326913
Aleksandr Shumilov <alexander.shumilov@toptal.com>
2023-08-17 09:10:00 +0200

    fix: page background color even without sidebar

--- packages/picasso/src/Page/styles.ts
+++ packages/picasso/src/Page/styles.ts
@@ -3,12 +3,13 @@ import { createStyles } from '@material-ui/core/styles'
 
 import { headerHeight } from '../PageTopBar/constants'
 
-export default ({ layout }: Theme) =>
+export default ({ layout, palette }: Theme) =>
   createStyles({
     root: {
       display: 'flex',
       flexDirection: 'column',
       height: '100%',
+      background: palette.grey.lightest,
       ...(layout.contentMinWidth && { minWidth: layout.contentMinWidth }),
 
       '& > footer, & > header': {
