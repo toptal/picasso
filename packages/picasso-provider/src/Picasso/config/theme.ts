5a5c591f8a8f2d3196c57020da4f540dbe99e657
Dmitry Maklygin <dmitry.maklygin@toptal.com>
2023-09-01 10:56:32 +0300

    feat: [FX-4259] add new spacing values to picasso (#3835)
    * feat: add new spacing values to picasso
    
    * chore: enhance spacings
    
    * chore: update spacings
    
    * chore: add unit tests for spacings
    
    * chore: update changeset
    
    * chore: fix story

--- packages/picasso-provider/src/Picasso/config/theme.ts
+++ packages/picasso-provider/src/Picasso/config/theme.ts
@@ -1,12 +1,22 @@
 import type { BreakpointKeys } from './breakpoints'
 import type { Layout } from './layout'
 import type { Sizes } from './sizes'
+import type spacings from './spacings'
+import type { PicassoSpacing } from './spacings'
 
 declare module '@material-ui/core/styles' {
   interface Theme {
     layout: Layout
     sizes: Sizes
     screens: (...sizes: BreakpointKeys[]) => string
+    spacings: Record<keyof typeof spacings, PicassoSpacing>
+  }
+
+  interface ThemeOptions {
+    layout?: Layout
+    sizes?: Sizes
+    screens?: (...sizes: BreakpointKeys[]) => string
+    spacings?: Record<keyof typeof spacings, PicassoSpacing>
   }
 
   interface ThemeOptions {
