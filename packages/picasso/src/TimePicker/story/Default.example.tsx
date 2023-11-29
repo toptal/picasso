1eb33512c8d9bc1211f6612b636eb5e393b9920f
Aleksandr Shumilov <alexander.shumilov@toptal.com>
2023-11-29 10:28:51 +0100

    Update story for easier testing

--- packages/picasso/src/TimePicker/story/Default.example.tsx
+++ packages/picasso/src/TimePicker/story/Default.example.tsx
@@ -1,18 +1,28 @@
-import React, { useState } from 'react'
-import { TimePicker } from '@toptal/picasso'
+// TODO: revert before merge
+import React from 'react'
+import { Container } from '@toptal/picasso'
+import { SPACING_4 } from '@toptal/picasso/utils'
+import {
+  FormNonCompound as Form,
+  SubmitButton,
+  DatePicker,
+  TimePicker,
+} from '@toptal/picasso-forms'
 
-const DefaultExample = () => {
-  const [timepickerValue, setTimepickerValue] = useState<string>('18:00')
-
-  const handleChange = (
-    e: React.ChangeEvent<
-      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+const Example = () => {
+  return (
+    <Form
+      autoComplete='off'
+      onSubmit={values => window.alert(JSON.stringify(values, undefined, 2))}
+      initialValues={{}}
     >
-  ) => {
-    setTimepickerValue(e.target.value)
-  }
-
-  return <TimePicker onChange={handleChange} value={timepickerValue} />
+      <DatePicker required name='default-dateOfBirth' label='Date of birth' />
+      <TimePicker required name='default-timeOfBirth' label='Time of birth' />
+      <Container top={SPACING_4}>
+        <SubmitButton>Submit</SubmitButton>
+      </Container>
+    </Form>
+  )
 }
 
-export default DefaultExample
+export default Example
