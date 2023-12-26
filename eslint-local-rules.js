421312a9303ca029184e0acbee9af2b2b7c07e2e
Mikhail Korolev <mikhail.korolev@toptal.com>
2023-12-26 13:50:35 +0100

    [FX-4486] Lint all deprecation warnings to have a ticket number (#4063)
    * [FX-4486] Lint all deprecation warnings to have a ticket number
    
    * Fix rule condition
    
    * Fix description

---
+++ eslint-local-rules.js
@@ -0,0 +1,40 @@
+const FUNCTION_NAMES = ['usePropDeprecationWarning', 'useDeprecationWarning']
+
+module.exports = {
+  'future-proof-deprecation-warning': {
+    meta: {
+      type: 'suggestion',
+      fixable: null,
+      docs: {
+        description:
+          'Ensure a deprecation warning is preceded by a TODO comment with a ticket reference',
+        category: 'Picasso Best Practices',
+        recommended: false,
+      },
+      schema: [],
+    },
+    create: function (context) {
+      return {
+        CallExpression(node) {
+          if (FUNCTION_NAMES.includes(node.callee.name)) {
+            const sourceCode = context.getSourceCode()
+            const commentBefore = sourceCode.getCommentsBefore(node)
+
+            const hasTodoComment = commentBefore.some(
+              comment =>
+                comment.value.includes('TODO') ||
+                comment.value.includes('FIXME')
+            )
+
+            if (!hasTodoComment) {
+              context.report({
+                node,
+                message: `${node.callee.name} must be preceded by a TODO comment with a ticket reference `,
+              })
+            }
+          }
+        },
+      }
+    },
+  },
+}
