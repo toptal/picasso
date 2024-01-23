b89671fe8df8c68b6d8fb63bd87ab52c812ec410
Augusto Moura <augusto.moura@toptal.com>
2024-01-23 01:56:06 -0300

    Add build:all dependency to test:unit

--- packages/base/Tooltip/src/Tooltip/index.ts
+++ packages/base/Tooltip/src/Tooltip/index.ts
@@ -1,7 +1,7 @@
 import type { OmitInternalProps } from '@toptal/picasso-shared'
 
 import type { Props as InternalTooltipProps } from './Tooltip'
-export { default as Tooltip, DelayType } from './Tooltip'
+export { default as Tooltip } from './Tooltip'
 export type TooltipProps = OmitInternalProps<InternalTooltipProps>
 export type { DelayType, MaxWidthType, PlacementType } from './Tooltip'
 /** @deprecated [FX-4714] Use TooltipProps instead */
