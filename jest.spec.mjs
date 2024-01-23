94a194a3e655442dcd7d7f1ae08af9f32b31b7d4
Augusto Moura <augusto.moura@toptal.com>
2024-01-23 00:36:40 -0300

    Add extra modules to Jest transform

--- jest.spec.mjs
+++ jest.spec.mjs
@@ -1,5 +1,20 @@
 import davinciJestConfig from '@toptal/davinci-qa/src/configs/jest.swc.config.js'
 
+// These are mostly ES6 modules that need to be on CJS for Jest
+const NODE_MODULES_TO_IGNORE_TRANSFORM = [
+  '@toptal',
+  '@topkit',
+  'd3',
+  'internmap',
+  'robust-predicates',
+  'delaunator',
+  'hast',
+  'property-information',
+  'comma-separated-tokens',
+  'space-separated-tokens',
+  'web-namespaces',
+].join('|')
+
 const config = {
   ...davinciJestConfig,
   roots: ['<rootDir>/packages'],
@@ -8,7 +23,9 @@ const config = {
     ...davinciJestConfig.moduleNameMapper,
   },
   setupFiles: ['jest-canvas-mock', './jest.setup.js'],
-  transformIgnorePatterns: [],
+  transformIgnorePatterns: [
+    `node_modules/(?!${NODE_MODULES_TO_IGNORE_TRANSFORM})`,
+  ],
   coverageReporters: ['json', 'text-summary'],
   // TODO: This option does not work due to bug in davinci-qa https://toptal-core.atlassian.net/browse/FX-4311
   // collectCoverageFrom: ['packages/**/*.{ts,tsx}'],
