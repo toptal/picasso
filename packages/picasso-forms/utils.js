5ba02f38f90cd037bda9d1051e0ebbcce25843a8
Mikhail Korolev <mikhail.korolev@toptal.com>
2024-02-15 13:40:24 +0200

    Export picasso-forms utils from source

--- packages/picasso-forms/utils.js
+++ packages/picasso-forms/utils.js
@@ -1,4 +1,4 @@
 // This file is here for the sake of compatability to allow TS versions below 5
 // to import from "@toptal/picasso-forms/utils"
 // When possible, import directly from "@toptal/picasso-forms"
-export * from './src/utils'
+export * from './dist-package/src/utils'
