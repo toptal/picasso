64893254eae3629ccf211c2a749f4bb99847a75d
TomasSlama <tomas.slama@toptal.com>
2024-02-19 10:46:55 +0100

    Add BASE spacings to the theme config

--- packages/picasso-tailwind/src/index.js
+++ packages/picasso-tailwind/src/index.js
@@ -8,6 +8,17 @@ module.exports = {
       lg: '1024px',
       xl: '1440px',
     },
+    spacing: {
+      0: '0',
+      1: '0.25rem',
+      2: '0.5rem',
+      3: '0.75rem',
+      4: '1rem',
+      6: '1.5rem',
+      8: '2rem',
+      10: '2.5rem',
+      12: '3rem',
+    },
     borderRadius: {
       sm: '4px',
       md: '8px',
