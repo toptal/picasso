83f24c7fb0f60941fff7388253d5812694ac4eae
Tomáš Sláma <tomas.slama@toptal.com>
2024-02-20 13:33:57 +0100

    Add BASE spacings to the theme config (#4146)

---
+++ .changeset/tender-crabs-accept.md
@@ -0,0 +1,5 @@
+---
+'@toptal/picasso-tailwind': major
+---
+
+- override default Tailwind spacings with BASE spacings
--- packages/picasso-tailwind/src/index.js
+++ packages/picasso-tailwind/src/index.js
@@ -8,6 +8,18 @@ module.exports = {
       lg: '1024px',
       xl: '1440px',
     },
+    // https://toptal-core.atlassian.net/wiki/spaces/Base/pages/3217031216/Spacing
+    spacing: {
+      0: '0',
+      1: '0.25rem',
+      2: '0.5rem',
+      3: '0.75rem',
+      4: '1rem',
+      6: '1.5rem',
+      8: '2rem',
+      10: '2.5rem',
+      12: '3rem',
+    },
     borderRadius: {
       sm: '4px',
       md: '8px',
