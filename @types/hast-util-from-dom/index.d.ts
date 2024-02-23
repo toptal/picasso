87d8a7e3d7142d6f9985bc7da90d5be14d08e0d3
TomasSlama <tomas.slama@toptal.com>
2024-02-23 14:43:23 +0100

    Simplify types

--- @types/hast-util-from-dom/index.d.ts
+++ @types/hast-util-from-dom/index.d.ts
@@ -1,14 +1,3 @@
-import type { Parent, Root } from 'hast'
-
 declare module 'hast-util-from-dom' {
-  type HastChild = Parent['children'][number]
-  type HastNode = HastChild | Root
-
-  type Options = {
-    afterTransform: Function
-  }
-  export default function fromDOM(
-    node: Document | Element,
-    options?: Options
-  ): HastNode
+  export default function fromDOM(node: Document | Element): any
 }
