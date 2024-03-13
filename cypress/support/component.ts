6194ed4678fd851ecc2cf23acbf0b257fa090e0c
Aleksandr Shumilov <alexander.shumilov@toptal.com>
2024-03-13 14:51:51 +0100

    Address feedback

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -6,4 +6,4 @@ import './commands.jsx'
 /* eslint-disable-next-line import/no-extraneous-dependencies */
 import '@toptal/davinci-qa/src/configs/cypress/support'
 // Import style file to support Tailwind CSS classes in Cypress tests.
-import './../assets/tailwind.css'
+import '../assets/tailwind.css'
