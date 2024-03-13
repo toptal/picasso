4e374318c5eaab30804fd327e51cecc2774018af
Aleksandr Shumilov <alexander.shumilov@toptal.com>
2024-03-13 14:59:10 +0100

    Address feedback

--- packages/base/Paper/src/Paper/Paper.tsx
+++ packages/base/Paper/src/Paper/Paper.tsx
@@ -41,8 +41,6 @@ export const Paper = forwardRef<HTMLDivElement, Props>(function Paper(
       data-testid={dataTestId}
       square
     >
-      {/* TODO: remove before merge */}
-      <div className='bg-green-500 text-xxl'>TEST</div>
       {children}
     </MUIPaper>
   )
