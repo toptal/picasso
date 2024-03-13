876c631e68070fede3f6f61e5268558127ee41e9
Aleksandr Shumilov <alexander.shumilov@toptal.com>
2024-03-13 15:49:08 +0100

    [FX-5012] Fix Tailwind styles in component tests (#4177)

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -5,3 +5,5 @@ import 'cypress-real-events/support'
 import './commands.jsx'
 /* eslint-disable-next-line import/no-extraneous-dependencies */
 import '@toptal/davinci-qa/src/configs/cypress/support'
+// Import style file to support Tailwind CSS classes in Cypress tests.
+import '../assets/tailwind.css'
