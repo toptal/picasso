d71f18a0835ec9758468618a65b8b84755748ef3
Aleksandr Shumilov <alexander.shumilov@toptal.com>
2024-03-14 18:42:48 +0100

    Fix

--- packages/base/Paper/src/Paper/Paper.tsx
+++ packages/base/Paper/src/Paper/Paper.tsx
@@ -28,6 +28,7 @@ export const Paper = forwardRef<HTMLDivElement, Props>(function Paper(
       className={cx('bg-white', `shadow-${elevation}`, className)}
       style={{
         color: 'unset',
+        transition: 'box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
         ...style,
       }}
       {...rest}
