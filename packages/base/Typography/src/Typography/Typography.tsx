3f75fa288f68619923a2b9d655960963ed5ef2ff
Mikhail Korolev <mikhail.korolev@toptal.com>
2024-03-21 13:15:45 +0100

    Migrate Typography to Tailwind

--- packages/base/Typography/src/Typography/Typography.tsx
+++ packages/base/Typography/src/Typography/Typography.tsx
@@ -1,9 +1,6 @@
-import type { ReactNode, HTMLAttributes } from 'react'
+import type { ReactNode, HTMLAttributes, ElementType } from 'react'
 import React, { forwardRef } from 'react'
-import type { Theme } from '@material-ui/core/styles'
-import { makeStyles } from '@material-ui/core/styles'
-import type { PropTypes } from '@material-ui/core'
-import { Typography as MUITypography } from '@material-ui/core'
+import cx from 'classnames'
 import type {
   StandardProps,
   ColorType,
@@ -12,9 +9,92 @@ import type {
 } from '@toptal/picasso-shared'
 import { toTitleCase } from '@toptal/picasso-utils'
 
-import styles from './styles'
-import toMuiVariant from './utils/to-mui-variant'
-import getTypographyClassName from './utils/get-typography-class-name'
+import variantToElement from './utils/variant-to-element'
+
+const COLORS: Record<NonNullable<Props['color']>, string> = {
+  green: 'text-green-600',
+  red: 'text-red-500',
+  yellow: 'text-yellow-500',
+  'light-grey': 'text-gray-400',
+  grey: 'text-gray-500',
+  'grey-main-2': 'text-gray-600',
+  'dark-grey': 'text-graphite-700',
+  black: 'text-black',
+  'light-blue': 'text-blue-400',
+  inherit: 'text-inherit',
+}
+const WEIGHTS: Record<NonNullable<Props['weight']>, string> = {
+  regular: 'font-regular',
+  semibold: 'font-semibold',
+  inherit: 'font-inherit-weight',
+}
+
+const ALIGN: Record<NonNullable<Props['align']>, string> = {
+  inherit: 'text-align-inherit',
+  left: 'text-left',
+  center: 'text-center',
+  right: 'text-right',
+  justify: 'text-justify',
+}
+
+const UNDERLINE: Record<NonNullable<Props['underline']>, string> = {
+  solid: 'underline decoration-solid',
+  dashed: 'underline decoration-dashed',
+}
+
+const VARIANT_SIZE: Record<
+  NonNullable<Props['variant']>,
+  Record<NonNullable<Props['size']>, string>
+> = {
+  heading: {
+    xxsmall: 'font-semibold text-2xs',
+    xsmall: 'text-black font-semibold text-xxs',
+    small: 'text-black font-semibold text-md',
+    medium: 'text-black font-semibold text-lg',
+    large: 'text-black font-semibold text-xl',
+    xlarge: 'text-black font-semibold text-2xl',
+    inherit: '',
+  },
+  body: {
+    xxsmall: 'font-regular text-2xs',
+    xsmall: 'font-regular text-xxs',
+    small: 'font-regular text-sm',
+    medium: 'font-regular text-md',
+    large: 'font-regular text-lg',
+    xlarge: 'font-regular text-xl',
+    inherit: 'font-regular font-inherit-size leading-[1.5em]',
+  },
+}
+const VARIANT_COLOR: Record<
+  NonNullable<Props['variant']>,
+  Record<NonNullable<Props['size']>, string>
+> = {
+  heading: {
+    xxsmall: 'text-black',
+    xsmall: 'text-black',
+    small: 'text-black',
+    medium: 'text-black',
+    large: 'text-black',
+    xlarge: 'text-black',
+    inherit: '',
+  },
+  body: {
+    xxsmall: 'text-graphite-700',
+    xsmall: 'text-graphite-700',
+    small: 'text-graphite-700',
+    medium: 'text-graphite-700',
+    large: 'text-black',
+    xlarge: 'text-black',
+    inherit: 'text-graphite-700',
+  },
+}
+
+export type TypographyAlign =
+  | 'inherit'
+  | 'left'
+  | 'center'
+  | 'right'
+  | 'justify'
 
 export interface Props
   extends StandardProps,
@@ -27,7 +107,7 @@ export interface Props
   /** Controls whether the Typography is inline or not */
   inline?: boolean
   /** Text align of the inner text */
-  align?: PropTypes.Alignment
+  align?: TypographyAlign
   /** Size of the inner text */
   size?:
     | SizeType<'xxsmall' | 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge'>
@@ -47,10 +127,6 @@ export interface Props
   /** Controls when the Typography should have line through */
   lineThrough?: boolean
 }
-
-const useStyles = makeStyles<Theme, Props>(styles, {
-  name: 'PicassoTypography',
-})
 
 export const Typography = forwardRef<HTMLElement, Props>(function Typography(
   props,
@@ -74,35 +150,40 @@ export const Typography = forwardRef<HTMLElement, Props>(function Typography(
     weight,
     ...rest
   } = props
-  const classes = useStyles(props)
+
+  const Component: ElementType = as || variantToElement(variant, size)
+
+  const getColor = () => {
+    if (invert) {
+      return 'text-white'
+    }
+    if (color) {
+      return COLORS[color]
+    }
+
+    return VARIANT_COLOR[variant][size]
+  }
 
   return (
-    <MUITypography
-      {...rest}
+    <Component
       ref={ref}
-      align={align}
-      className={className}
-      classes={{
-        root: getTypographyClassName(classes, {
-          variant,
-          size,
-          color,
-          weight,
-          underline,
-          invert,
-          lineThrough,
-          as,
-        }),
-      }}
+      className={cx(
+        'm-0',
+        VARIANT_SIZE[variant][size],
+        getColor(),
+        weight ? WEIGHTS[weight] : undefined,
+        align ? ALIGN[align] : undefined,
+        underline ? UNDERLINE[underline] : undefined,
+        noWrap ? 'whitespace-nowrap' : undefined,
+        lineThrough ? 'line-through' : undefined,
+        inline ? 'inline' : undefined,
+        className
+      )}
       style={style}
-      variant={toMuiVariant(variant, size)}
-      display={inline ? 'inline' : 'initial'}
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      component={as!}
-      noWrap={noWrap}
+      {...rest}
     >
       {titleCase ? toTitleCase(children) : children}
-    </MUITypography>
+    </Component>
   )
 })
 
