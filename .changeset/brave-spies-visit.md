49e6df2be1c56332edd07b65db39bc431e84470a
Mikhail Korolev <mikhail.korolev@toptal.com>
2024-03-25 13:33:16 +0100

    Add TableSortableCell

---
+++ .changeset/brave-spies-visit.md
@@ -0,0 +1,5 @@
+---
+'@toptal/picasso-table': minor
+---
+
+- added TableSortableCell, enabling the ability to sort table columns
---
+++ packages/base/Table/src/TableSortableCell/test.tsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import { render } from '@toptal/picasso-test-utils'
+
+import type { TableConfig } from '../Table'
+import { TableContext } from '../Table'
+import { TableCompound as Table } from '../TableCompound'
+import type { Props } from './TableSortableCell'
+
+const renderTableCell = (
+  { children = 'Cell', ...rest }: Partial<Props> = {},
+  { spacing = 'regular', variant = 'bordered' }: Partial<TableConfig> = {}
+) => {
+  return render(
+    <TableContext.Provider value={{ spacing, variant }}>
+      <Table>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell {...rest}>{children}</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    </TableContext.Provider>
+  )
+}
+
+describe('TableCell', () => {
+  it('renders', () => {
+    const { container } = renderTableCell()
+
+    expect(container).toMatchSnapshot()
+  })
+
+  it('renders compact', () => {
+    const { container } = renderTableCell({}, { spacing: 'compact' })
+
+    expect(container).toMatchSnapshot()
+  })
+
+  it('renders narrow', () => {
+    const { container } = renderTableCell({}, { spacing: 'narrow' })
+
+    expect(container).toMatchSnapshot()
+  })
+
+  it('sets attribuites correctly', () => {
+    const { getByTestId } = renderTableCell({
+      'data-testid': 'cell',
+      rowSpan: 10,
+      colSpan: 2,
+      style: { background: 'red' },
+    })
+
+    const cell = getByTestId('cell')
+
+    expect(cell).toHaveAttribute('rowspan', '10')
+    expect(cell).toHaveAttribute('colspan', '2')
+    expect(cell).toHaveAttribute('style', 'background: red;')
+  })
+
+  it('capitalizes in head if titleCase is truthy', () => {
+    const { getByTestId } = render(
+      <Table>
+        <Table.Head>
+          <Table.Row>
+            <Table.Cell data-testid='head-cell'>head</Table.Cell>
+          </Table.Row>
+        </Table.Head>
+      </Table>,
+      undefined,
+      { titleCase: true }
+    )
+
+    expect(getByTestId('head-cell')).toHaveTextContent('Head')
+  })
+
+  it('does not capitalize in head if titleCase is falsy', () => {
+    const { getByTestId } = render(
+      <Table>
+        <Table.Head>
+          <Table.Row>
+            <Table.Cell data-testid='head-cell'>head</Table.Cell>
+          </Table.Row>
+        </Table.Head>
+      </Table>
+    )
+
+    expect(getByTestId('head-cell')).toHaveTextContent('head')
+  })
+})
