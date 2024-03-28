1e876490fcb302f2c6bddd328364c3941f8d57c7
Wadii Basmi <wadii.basmi@toptal.com>
2024-03-28 14:10:18 +0000

    Remove excess of fields

--- packages/picasso-query-builder/src/QueryBuilder/story/FieldDescription.example.tsx
+++ packages/picasso-query-builder/src/QueryBuilder/story/FieldDescription.example.tsx
@@ -32,40 +32,6 @@ const fields = [
     name: 'field2',
     label: 'Last name',
   },
-  {
-    name: 'field3',
-    label: 'City',
-  },
-  {
-    name: 'field4',
-    label: 'Country',
-    tooltip: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    name: 'field5',
-    label: 'Zip code',
-    tooltip: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    name: 'field6',
-    label: 'Region',
-  },
-  {
-    name: 'field7',
-    label: 'Social security number',
-  },
-  {
-    name: 'field8',
-    label: 'Registration date',
-  },
-  {
-    name: 'field9',
-    label: 'Last login date',
-  },
-  {
-    name: 'field10',
-    label: 'Role',
-  },
 ]
 
 const Example = () => {
