a84176efe0e1c3570ad6ea6758959dc733f44c0b
Adrian Contiu <adrian.contiu@toptal.com>
2024-04-11 10:30:54 +0300

    Small refactor on className

--- packages/base/Table/src/TableHead/TableHead.tsx
+++ packages/base/Table/src/TableHead/TableHead.tsx
@@ -22,7 +22,7 @@ export const TableHead = forwardRef<HTMLTableSectionElement, Props>(
           {...rest}
           ref={ref}
           className={twMerge(
-            'border-b border-[0] border-solid border-gray-200',
+            'border-[0] border-solid border-b-[1px] border-gray-200',
             className
           )}
           style={style}
