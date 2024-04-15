3715306dc45ad7e97388160039a0f0d2c7c37cac
Adrian Contiu <adrian.contiu@toptal.com>
2024-04-15 10:46:13 +0300

    [FX-4622] Migrate TableBody to TailwindCSS (#4240)

--- packages/base/Table/src/TableBody/TableBody.tsx
+++ packages/base/Table/src/TableBody/TableBody.tsx
@@ -1,11 +1,7 @@
 import type { ReactNode, HTMLAttributes } from 'react'
 import React, { forwardRef, useContext } from 'react'
-import type { Theme } from '@material-ui/core/styles'
-import { makeStyles } from '@material-ui/core/styles'
-import { TableBody as MUITableBody } from '@material-ui/core'
 import type { BaseProps } from '@toptal/picasso-shared'
 
-import styles from './styles'
 import { TableSectionContext, TableSection, TableContext } from '../Table'
 
 export interface Props
@@ -32,25 +28,16 @@ const stripeRows = (children: React.ReactNode) => {
   })
 }
 
-const useStyles = makeStyles<Theme>(styles, { name: 'PicassoTableBody' })
-
 export const TableBody = forwardRef<HTMLTableSectionElement, Props>(
   function TableBody(props, ref) {
     const { className, style, children, ...rest } = props
-    const classes = useStyles()
     const { variant } = useContext(TableContext)
 
     return (
       <TableSectionContext.Provider value={TableSection.BODY}>
-        <MUITableBody
-          {...rest}
-          ref={ref}
-          classes={classes}
-          className={className}
-          style={style}
-        >
+        <tbody {...rest} ref={ref} className={className} style={style}>
           {variant === 'striped' ? stripeRows(children) : children}
-        </MUITableBody>
+        </tbody>
       </TableSectionContext.Provider>
     )
   }
