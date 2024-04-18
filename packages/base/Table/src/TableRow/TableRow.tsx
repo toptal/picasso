24de7b8aa2fee0e479fe546d724daa068b2b313b
Adrian Contiu <adrian.contiu@toptal.com>
2024-04-18 16:38:09 +0300

    Address Dmitry review comments

--- packages/base/Table/src/TableRow/TableRow.tsx
+++ packages/base/Table/src/TableRow/TableRow.tsx
@@ -41,7 +41,7 @@ export const TableRow = forwardRef<HTMLTableRowElement, Props>(
         className={twMerge(
           isBordered && 'border-0 border-solid border-b border-gray-200',
           stripeEven && 'bg-gray-100',
-          hover && 'hover:bg-blue-100',
+          hover && 'hover:bg-blue-100 transition-colors',
           selected && 'bg-blue-100',
           className
         )}
