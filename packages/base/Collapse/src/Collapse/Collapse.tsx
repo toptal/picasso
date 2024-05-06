16d335694aacbe4530997b42a18dc8ccbf7e1706
TomasSlama <tomas.slama@toptal.com>
2024-05-06 17:15:10 +0200

    Make the collapse work

--- packages/base/Collapse/src/Collapse/Collapse.tsx
+++ packages/base/Collapse/src/Collapse/Collapse.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, forwardRef } from 'react'
+import React, { useRef, forwardRef } from 'react'
 import { Transition } from 'react-transition-group'
 import type { BaseProps, TransitionProps } from '@toptal/picasso-shared'
 import { useMultipleForwardRefs } from '@toptal/picasso-utils'
@@ -17,68 +17,44 @@ export interface Props extends TransitionProps, BaseProps {
 
 export const Collapse = forwardRef<HTMLDivElement, Props>(
   (
-    { children, className, in: inProps, timeout = 350, unmountOnExit, ...rest },
+    {
+      children,
+      className,
+      in: inProps,
+      timeout = 350,
+      unmountOnExit,
+      onEnter,
+      onExited,
+      ...rest
+    },
     ref
   ) => {
     const collapseRef = useRef<HTMLDivElement>(null)
     const combinedRef = useMultipleForwardRefs([ref, collapseRef])
 
-    const [height, setHeight] = useState<number>()
-
-    const onEntering = () => {
-      // onShow && onShow()
-      if (collapseRef.current) {
-        setHeight(collapseRef.current.scrollHeight)
-      }
-    }
-
-    const onEntered = () => {
-      setHeight(0)
-    }
-
-    const onExit = () => {
-      if (collapseRef.current) {
-        setHeight(collapseRef.current.scrollHeight)
-      }
-    }
-
-    const onExiting = () => {
-      // onHide && onHide()
-
-      setHeight(0)
-    }
-
-    const onExited = () => {
-      setHeight(0)
+    const transitionStyles = {
+      entering: { height: collapseRef?.current?.scrollHeight },
+      entered: { height: collapseRef?.current?.scrollHeight },
+      exiting: { height: 0 },
+      exited: { height: 0 },
     }
 
     return (
       <Transition
         in={inProps}
         nodeRef={collapseRef}
-        onEntering={onEntering}
         unmountOnExit={unmountOnExit}
-        onEntered={onEntered}
-        onExit={onExit}
-        onExiting={onExiting}
-        onExited={onExited}
         timeout={timeout}
+        onEnter={onEnter}
+        onExited={onExited}
       >
-        {state => {
-          const currentHeight = height === 0 ? null : { height }
-          const isAnimating = state === 'entering' || state === 'exiting'
-
+        {(state: 'entering' | 'entered' | 'exiting' | 'exited') => {
           return (
             <div
-              className={classNames(className, {
-                'overflow-hidden h-0': isAnimating,
-                hidden: state === 'exited',
-              })}
+              className={classNames(className, 'overflow-hidden')}
               style={{
-                ...currentHeight,
-                transition: isAnimating
-                  ? `height ${timeout}ms ease`
-                  : undefined,
+                ...transitionStyles[state],
+                transition: `height ${timeout}ms ease`,
               }}
               {...rest}
               ref={combinedRef}
