fac5181f3f4aea8d18a697d72bebebb7200376ea
Mikhail Korolev <mikhail.korolev@toptal.com>
2024-05-08 16:03:16 +0200

    [FX-5183] Add Collapse transition (#4286)
    * Add Collapse transition
    
    * Add changeset
    
    * Fix test
    
    * Review suggestions
    
    * Add onExited handler

--- packages/base/Collapse/src/Collapse/story/Default.example.tsx
+++ packages/base/Collapse/src/Collapse/story/Default.example.tsx
@@ -1,5 +1,5 @@
 import { Container, Button } from '@toptal/picasso'
-import { Fade } from '@toptal/picasso-fade'
+import { Collapse } from '@toptal/picasso-collapse'
 import React, { useState } from 'react'
 
 const Example = () => {
@@ -10,11 +10,11 @@ const Example = () => {
   return (
     <Container>
       <Button onClick={handleOnClick} className='mb-4'>
-        Toggle Fade
+        Toggle Collapse
       </Button>
-      <Fade in={faded} timeout={350}>
-        <div className='bg-gray-100 p-4 rounded-md'>Fade in content</div>
-      </Fade>
+      <Collapse in={faded} timeout={400}>
+        <div className='bg-gray-100 p-4 rounded-md'>Collapse content</div>
+      </Collapse>
     </Container>
   )
 }
