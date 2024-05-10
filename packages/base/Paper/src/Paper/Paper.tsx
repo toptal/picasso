687688185521ae426f16f6710bd24234093e247c
Aleksandr Shumilov <alexander.shumilov@toptal.com>
2024-05-10 13:04:39 +0200

    Refactor shadows mapping

--- packages/base/Paper/src/Paper/Paper.tsx
+++ packages/base/Paper/src/Paper/Paper.tsx
@@ -9,11 +9,39 @@ export interface Props extends BaseProps, HTMLAttributes<HTMLDivElement> {
   children: ReactNode
 }
 
+const shadowsMapping: Record<number, string> = {
+  0: 'shadow-0',
+  1: 'shadow-1',
+  2: 'shadow-2',
+  3: 'shadow-3',
+  4: 'shadow-4',
+  5: 'shadow-5',
+  6: 'shadow-6',
+  7: 'shadow-7',
+  8: 'shadow-8',
+  9: 'shadow-9',
+  10: 'shadow-10',
+  11: 'shadow-11',
+  12: 'shadow-12',
+  13: 'shadow-13',
+  14: 'shadow-14',
+  15: 'shadow-15',
+  16: 'shadow-16',
+  17: 'shadow-17',
+  18: 'shadow-18',
+  19: 'shadow-19',
+  20: 'shadow-20',
+  21: 'shadow-21',
+  22: 'shadow-22',
+  23: 'shadow-23',
+  24: 'shadow-24',
+}
+
 export const Paper = forwardRef<HTMLDivElement, Props>(function Paper(
   props,
   ref
 ) {
-  const { className, elevation, style, children, ...rest } = props
+  const { className, elevation = 1, style, children, ...rest } = props
 
   /*
   TODO: [FX-5003] Deprecate legacy shadow classes
@@ -28,7 +56,7 @@ export const Paper = forwardRef<HTMLDivElement, Props>(function Paper(
       ref={ref}
       className={twMerge(
         'bg-white',
-        `shadow-${elevation}`,
+        shadowsMapping[elevation],
         'transition-shadow duration-300 delay-0',
         className
       )}
