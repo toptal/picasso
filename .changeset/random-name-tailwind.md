c9028d1d94b3ff5df289591c7ed1f35af2fde30f
Aleksandr Shumilov <alexander.shumilov@toptal.com>
2024-05-10 12:06:34 +0200

    Address feedback

--- .changeset/random-name-tailwind.md
+++ .changeset/random-name-tailwind.md
@@ -2,6 +2,5 @@
 '@toptal/picasso-tailwind': minor
 ---
 
-- add utility classes for width that requires `calc()`
 - extend maximum width classes with fractions of 12
 - extend width, height, and padding classes for inputs
--- packages/base/Grid/src/Grid/utils/get-grid-spacing-class-name.ts
+++ packages/base/Grid/src/Grid/utils/get-grid-spacing-class-name.ts
@@ -1,14 +1,14 @@
 import type { GridSpacing } from '../../types'
 
 const gridWidthAndMarginMapping: { [K in GridSpacing]: string } = {
-  0: 'w-[calc(100%-0px)] -m-[0px]',
-  8: 'w-[calc(100%-8px)] -m-[4px]',
-  16: 'w-[calc(100%-16px)] -m-[8px]',
-  24: 'w-[calc(100%-24px)] -m-[12px]',
-  32: 'w-[calc(100%-32px)] -m-[16px]',
-  64: 'w-[calc(100%-64px)] -m-[32px]',
-  72: 'w-[calc(100%-72px)] -m-[36px]',
-  80: 'w-[calc(100%-80px)] -m-[40px]',
+  0: 'w-[calc(100%+0px)] -m-[0px]',
+  8: 'w-[calc(100%+8px)] -m-[4px]',
+  16: 'w-[calc(100%+16px)] -m-[8px]',
+  24: 'w-[calc(100%+24px)] -m-[12px]',
+  32: 'w-[calc(100%+32px)] -m-[16px]',
+  64: 'w-[calc(100%+64px)] -m-[32px]',
+  72: 'w-[calc(100%+72px)] -m-[36px]',
+  80: 'w-[calc(100%+80px)] -m-[40px]',
 }
 
 export const getGridSpacingClassName = (spacing: GridSpacing) => {
