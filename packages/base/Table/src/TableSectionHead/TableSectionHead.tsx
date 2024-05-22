9a53b2ba0dd4726b739cda6660eb2f8776415874
Adrian Contiu <adrian.contiu@toptal.com>
2024-05-22 19:57:32 +0300

    Migrate TableSectionHead to Tailwind

--- packages/base/Table/src/TableSectionHead/TableSectionHead.tsx
+++ packages/base/Table/src/TableSectionHead/TableSectionHead.tsx
@@ -1,14 +1,11 @@
 import type { ReactElement, HTMLAttributes, FunctionComponent } from 'react'
 import React, { forwardRef } from 'react'
 import type { BaseProps } from '@toptal/picasso-shared'
-import type { Theme } from '@material-ui/core/styles'
-import { makeStyles } from '@material-ui/core/styles'
 
 import { TableSection, TableSectionContext } from '../Table'
 import { TableCell } from '../TableCell'
 import { TableRow } from '../TableRow'
 import { TableBody } from '../TableBody'
-import styles from './styles'
 
 const MAX_COL_SPAN = 100
 
@@ -19,24 +16,25 @@ export interface Props
   icon?: ReactElement
 }
 
-const useStyles = makeStyles<Theme>(styles, {
-  name: 'PicassoTableSectionHead',
-})
-
 export const TableSectionHead: FunctionComponent<Props> = forwardRef<
   HTMLTableSectionElement,
   Props
 >(function TableSectionHead(props, ref) {
   const { colSpan, icon, children, style } = props
 
-  const classes = useStyles()
-
   return (
     <TableBody ref={ref}>
-      <TableRow className={classes.sectionHeaderRow} style={style}>
+      <TableRow
+        className='min-h-10 border-y border-solid border-gray-200'
+        style={style}
+      >
         <TableSectionContext.Provider value={TableSection.HEAD}>
           <TableCell colSpan={colSpan}>
-            {icon && <span className={classes.iconWrapper}>{icon}</span>}
+            {icon && (
+              <span className='inline-block align-middle mr-3 -mt-[1px]'>
+                {icon}
+              </span>
+            )}
             {children}
           </TableCell>
         </TableSectionContext.Provider>
