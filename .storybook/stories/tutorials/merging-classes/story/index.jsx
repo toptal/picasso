9e411ae527f2528238c7cb6129a42a2d2e6823c6
Ruslan Sedziukh <ruslan.sedziukh@toptal.com>
2024-05-23 15:59:59 +0300

    Add tutorial

---
+++ .storybook/stories/tutorials/merging-classes/story/index.jsx
@@ -0,0 +1,34 @@
+import PicassoBook from '~/.storybook/components/PicassoBook'
+
+const mergingClassesPage = PicassoBook.section('Tutorials').createPage(
+  'Merging classes',
+  'How to merge classes with respect of Picasso TailwindCSS theme'
+)
+
+mergingClassesPage.createChapter().addTextSection(
+  // prettier-ignore
+  '## picasso-tailwind-merge\n' +
+
+    '`picasso-tailwind-merge` is a package with tools that should be used for merging and joining classes.\n' +
+    
+    '\n' +
+
+    'It contains:\n' +  
+      '* `twMerge`\n' +
+      '* `twJoin`\n' + 
+
+  '## twMerge\n' +
+
+    '`twMerge` can correctly merge classes from TailwindCSS default theme. ' +
+    'However Picasso is using extended theme. That is why `twMerge` from `picasso-tailwind-merge`' +
+    'should be used instead of `twMerge` from `tailwind-merge` package.\n' + 
+
+    '\n'+
+  
+    'Just import `twMerge` from `picasso-tailwind-merge` and use it as regular `twMerge`.' + 
+    'No additional configurations required.\n' + 
+
+  '## twJoin\n' +
+
+    '`twJoin` is also included in `picasso-tailwind-merge`.'
+)
