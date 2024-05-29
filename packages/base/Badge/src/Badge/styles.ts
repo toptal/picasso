3b458742913aabee1af379f6475594ff5ebdaa86
Ruslan Sedziukh <ruslan.sedziukh@toptal.com>
2024-05-29 13:15:42 +0300

    [FX-4608] Migrate Badge to TailwindCSS

--- packages/base/Badge/src/Badge/styles.ts
+++ packages/base/Badge/src/Badge/styles.ts
@@ -2,6 +2,8 @@ import { rem } from '@toptal/picasso-shared'
 import type { Theme, StyleRules } from '@material-ui/core/styles'
 import { createStyles } from '@material-ui/core/styles'
 
+import type { SizeType, VariantType } from './Badge'
+
 const getSizeProps = (unitInRem: string): StyleRules[keyof {}] => ({
   borderRadius: unitInRem,
   height: unitInRem,
@@ -44,3 +46,14 @@ export default ({ palette, typography }: Theme) =>
       ...getSizeProps(rem('20px')),
     },
   })
+
+export const classByVariant: Record<VariantType, string> = {
+  white: 'bg-white text-graphite-700 border-gray-400',
+  red: 'text-white bg-red-500 border-red-500',
+}
+
+export const classBySize: Record<SizeType, string> = {
+  small: 'leading-[10px] rounded-[0.75rem] h-3 min-w-3',
+  medium: 'rounded-[1rem] h-4 min-w-4',
+  large: 'px-[3px] rounded-[1.25rem] h-[1.25rem] min-w-[1.25rem]',
+}
