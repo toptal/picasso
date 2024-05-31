bef0e4bae9878daee1483e1ca031442560112598
Adrian Contiu <adrian.contiu@toptal.com>
2024-05-31 13:21:35 +0300

    Remove material-ui styles

--- packages/base/Checkbox/src/CheckboxGroup/CheckboxGroup.tsx
+++ packages/base/Checkbox/src/CheckboxGroup/CheckboxGroup.tsx
@@ -1,44 +1,37 @@
 /* eslint-disable react/no-array-index-key */
+import type { TableHTMLAttributes } from 'react'
 import React from 'react'
-import type { FormGroupProps } from '@material-ui/core'
-import { FormGroup } from '@material-ui/core'
-import type { Theme } from '@material-ui/core/styles'
-import { makeStyles, useTheme } from '@material-ui/core/styles'
-import cx from 'classnames'
 import type { GridSizeProps, GridProps } from '@toptal/picasso-grid'
 import { GridCompound as Grid } from '@toptal/picasso-grid'
-
-import styles from './styles'
+import { twMerge } from '@toptal/picasso-tailwind-merge'
 
 type GridSpacing = GridProps['spacing']
 
-export interface Props extends FormGroupProps, GridSizeProps {
+export interface Props
+  extends TableHTMLAttributes<HTMLTableElement>,
+    GridSizeProps {
   /** Align checkboxes horizontally  */
   horizontal?: boolean
   /** Defines amount of space between checkbox components (in px) */
   spacing?: GridSpacing
 }
 
-const useStyles = makeStyles<Theme>(styles, {
-  name: 'PicassoCheckboxGroup',
-})
-
 const CheckboxGroup = (props: Props) => {
   const { horizontal, spacing, xs, sm, md, lg, xl, className, ...rest } = props
 
-  const classes = useStyles()
-  const { spacing: themeSpacing } = useTheme()
-
   const direction = horizontal ? 'row' : 'column'
-  const gridSpacing = spacing ?? horizontal ? themeSpacing(2) : 0
+  const gridSpacing = spacing ?? horizontal ? 16 : 0
 
   const children = React.Children.toArray(rest.children)
 
   return (
-    <FormGroup
+    <div
       {...rest}
-      classes={classes}
-      className={cx(classes.root, className)}
+      className={twMerge(
+        'flex flex-col flex-wrap -my-2',
+        horizontal ? 'flex-row' : undefined,
+        className
+      )}
     >
       <Grid
         direction={direction}
@@ -48,7 +41,7 @@ const CheckboxGroup = (props: Props) => {
         {children.map((child, index) => (
           <Grid.Item
             key={index}
-            className='leading-none [&&]:pt-0 [&&]:pb-0'
+            className='leading-none [&&]:pt-0 [&&]:pb-0 [&&>label]:mb-[0.5em]'
             xs={xs}
             sm={sm}
             md={md}
@@ -59,7 +52,7 @@ const CheckboxGroup = (props: Props) => {
           </Grid.Item>
         ))}
       </Grid>
-    </FormGroup>
+    </div>
   )
 }
 
