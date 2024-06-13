a33991dc8eea3b21ef750014d3e3e7727927cd1c
Augusto Moura <augusto.moura@toptal.com>
2024-06-13 13:21:30 -0300

    [FX-4585] Migrate NativeSelect to tailwind (#4360)
    * Replace JSS classes with TailwindCSS
    
    * Migrate SelectCaret to TailwindCSS
    
    * Remove styles file
    
    * Remove dependency from material-ui on NativeSelect
    
    * Add missing multiple prop
    
    * Remove material-ui from Select package.json
    
    * Remove classnames dependency
    
    * Update yarn lock
    
    * Update alpha colors to opacity syntax
    
    * Add changeset
    
    * Update test snapshots
    
    * Fix errors from migration
    
    * Update test snapshots

---
+++ packages/base/Select/src/NativeSelect/NativeSelectInput.tsx
@@ -0,0 +1,47 @@
+import React, { forwardRef } from 'react'
+import { twMerge, twJoin } from '@toptal/picasso-tailwind-merge'
+
+type NativeSelectInputProps = {
+  className?: string
+  disabled?: boolean
+  IconComponent?: React.ElementType
+  multiple?: boolean
+} & React.HTMLAttributes<HTMLSelectElement>
+
+export const NativeSelectInput = forwardRef<
+  HTMLSelectElement,
+  NativeSelectInputProps
+>((props, ref) => {
+  const { className, disabled, multiple, IconComponent, ...other } = props
+
+  return (
+    <>
+      <select
+        className={twMerge(
+          `appearance-none select-none rounded-none min-w-4 cursor-pointer
+           focus:bg-black/[.05] focus:rounded-none`,
+          disabled && 'cursor-default',
+          multiple ? 'h-auto' : '[&_option]:bg-white [&_optgroup]:bg-white',
+          className,
+          'pr-6' // From MUI, this rule is more specific and should override classname
+        )}
+        disabled={disabled}
+        multiple={multiple}
+        ref={ref}
+        {...other}
+      />
+      {multiple
+        ? null
+        : IconComponent && (
+            <IconComponent
+              className={twJoin(
+                'absolute right-0 top-[calc(50%-12px)] pointer-events-none',
+                disabled ? 'text-black/[.26]' : 'text-black/[.54]'
+              )}
+            />
+          )}
+    </>
+  )
+})
+
+NativeSelectInput.displayName = 'NativeSelectInput'
