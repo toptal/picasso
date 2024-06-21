a650493d70a057d6620c4fe208e5311750a50d6c
Augusto Moura <augusto.moura@toptal.com>
2024-06-20 23:13:28 -0300

    Add test for twMerge sync values

--- packages/picasso-tailwind-merge/src/test.ts
+++ packages/picasso-tailwind-merge/src/test.ts
@@ -1,9 +1,21 @@
-import { twMerge } from './twMerge'
+import * as picassoTailwindConfig from '@toptal/picasso-tailwind'
+
+import { PICASSO_TW_FONT_SIZES, twMerge } from './twMerge'
 
 describe('twMerge', () => {
   it('merges font size classes correctly', () => {
     expect(twMerge('font-inherit-size text-button-large text-2xs')).toBe(
       'text-2xs'
     )
+  })
+
+  describe('PICASSO_TW_FONT_SIZES', () => {
+    it('contains all font sizes from picasso-tailwind', () => {
+      expect(PICASSO_TW_FONT_SIZES).toStrictEqual(
+        Object.keys(picassoTailwindConfig.theme.fontSize).map(
+          key => `text-${key}`
+        )
+      )
+    })
   })
 })
