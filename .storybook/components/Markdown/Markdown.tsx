b9efc1ec627eec11c4ac76466e22351bca211c75
Mikhail Korolev <mikhail.korolev@toptal.com>
2024-07-29 16:48:49 +0200

    Remove storybook-readme

--- .storybook/components/Markdown/Markdown.tsx
+++ .storybook/components/Markdown/Markdown.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import cx from 'classnames'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import { BaseProps } from '@toptal/picasso-shared'
@@ -9,21 +9,28 @@ import styles from './styles'
 
 interface Props extends BaseProps {
   children: string
+  className?: string
 }
 
 const useStyles = makeStyles<Theme>(styles, { name: 'PicassoMarkdown' })
 
 const Markdown = (props: Props): JSX.Element => {
-  const { children } = props
+  const { children, className } = props
 
   const classes = useStyles()
 
   return (
-    <ReactMarkdown className={classes.root} remarkPlugins={[remarkGfm]}>
+    <ReactMarkdown
+      className={cx(classes.root, className)}
+      remarkPlugins={[remarkGfm]}
+    >
       {children}
     </ReactMarkdown>
   )
 }
+
+export const createMarkdownPage = (markdown: string) => () =>
+  <Markdown className='markdown-body'>{markdown}</Markdown>
 
 Markdown.displayName = 'Markdown'
 
--- packages/picasso-charts/src/story/index.jsx
+++ packages/picasso-charts/src/story/index.jsx
@@ -1,10 +1,13 @@
-import { doc } from 'storybook-readme'
-
+import { createMarkdownPage } from '~/.storybook/components/Markdown'
 import PicassoBook from '~/.storybook/components/PicassoBook'
 import README from '../../README.md'
 import CHANGELOG from '../../CHANGELOG.md'
 
 const section = PicassoBook.section('Picasso Charts')
 
-section.createDocPage('README', doc(README), { alwaysOnTop: true })
-section.createDocPage('CHANGELOG', doc(CHANGELOG), { alwaysOnTop: true })
+section.createDocPage('README', createMarkdownPage(README), {
+  alwaysOnTop: true,
+})
+section.createDocPage('CHANGELOG', createMarkdownPage(CHANGELOG), {
+  alwaysOnTop: true,
+})
