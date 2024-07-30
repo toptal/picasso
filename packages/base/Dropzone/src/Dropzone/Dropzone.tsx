074e7275cee1c235afbe449863548e0f5a53d077
Aleksandr Shumilov <alexander.shumilov@toptal.com>
2024-07-30 18:15:55 +0100

    Update tests

--- packages/base/Dropzone/src/Dropzone/Dropzone.tsx
+++ packages/base/Dropzone/src/Dropzone/Dropzone.tsx
@@ -93,6 +93,7 @@ export const Dropzone = forwardRef<HTMLInputElement, Props>(function Dropzone(
         direction='column'
         alignItems='center'
         rounded
+        data-disabled={isDisabled}
         data-testid={dataTestId}
         {...getRootProps({})}
         className={twJoin(
--- packages/base/Dropzone/src/Dropzone/test.tsx
+++ packages/base/Dropzone/src/Dropzone/test.tsx
@@ -53,7 +53,7 @@ describe('Dropzone', () => {
         ],
       })
 
-      expect(getByTestId('dropzone').className).toContain('disabled')
+      expect(getByTestId('dropzone').getAttribute('data-disabled')).toBe('true')
     })
   })
 
