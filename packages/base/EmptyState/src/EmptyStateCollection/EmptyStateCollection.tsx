ead8d50cfc224b638d2c7ac1efca97aa7c4a904a
Adrian Contiu <adrian.contiu@toptal.com>
2024-08-02 12:10:48 +0300

    [FX-5529] Migrate EmptyState to TailwindCSS (#4437)

--- packages/base/EmptyState/src/EmptyStateCollection/EmptyStateCollection.tsx
+++ packages/base/EmptyState/src/EmptyStateCollection/EmptyStateCollection.tsx
@@ -1,30 +1,23 @@
 import type { HTMLAttributes, ReactElement } from 'react'
 import React, { forwardRef, cloneElement } from 'react'
-import type { Theme } from '@material-ui/core/styles'
-import { makeStyles } from '@material-ui/core/styles'
 import { Search16 as Search } from '@toptal/picasso-icons'
 import { Container } from '@toptal/picasso-container'
 import { Typography } from '@toptal/picasso-typography'
-
-import styles from './styles'
 
 export interface Props extends HTMLAttributes<HTMLDivElement> {
   /** Adds <Icon /> before EmptyStateCollection content */
   icon?: ReactElement
 }
 
-const useStyles = makeStyles<Theme>(styles, {
-  name: 'PicassoEmptyStateCollection',
-})
-
 export const EmptyStateCollection = forwardRef<HTMLDivElement, Props>(
   function EmptyStateCollection(props, ref) {
-    const classes = useStyles()
     const { icon, children, style, ...rest } = props
+
     const iconProps = {
-      className: classes.icon,
+      className: 'basis-4 min-w-4 h-4',
       color: 'dark-grey' as const,
     }
+
     const iconElement = icon ? (
       cloneElement(icon, iconProps)
     ) : (
@@ -33,12 +26,7 @@ export const EmptyStateCollection = forwardRef<HTMLDivElement, Props>(
 
     return (
       <Container {...rest} alignItems='center' flex ref={ref} style={style}>
-        <Container
-          alignItems='center'
-          className={classes.iconWrapper}
-          flex
-          right='xsmall'
-        >
+        <Container alignItems='center' flex right='xsmall'>
           {iconElement}
         </Container>
         <Container>
--- packages/base/EmptyState/src/EmptyStatePage/styles.ts
+++
@@ -1,9 +0,0 @@
-import { createStyles } from '@material-ui/core/styles'
-
-export default () =>
-  createStyles({
-    image: {
-      width: '8rem',
-      height: '8rem',
-    },
-  })
