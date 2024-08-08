94a90678e3bcd307db03684091cf6ea7b55341fa
Aleksandr Shumilov <alexander.shumilov@toptal.com>
2024-08-08 14:06:19 +0100

    Fix

--- packages/base/Dropdown/src/DropdownArrow/styles.ts
+++ packages/base/Dropdown/src/DropdownArrow/styles.ts
@@ -3,10 +3,6 @@ import { createStyles } from '@material-ui/core/styles'
 export default () =>
   createStyles({
     root: {
-      width: 0,
-      height: 0,
-      borderStyle: 'solid',
-      borderColor: 'currentColor transparent transparent transparent',
       marginLeft: '0.7em',
     },
   })
