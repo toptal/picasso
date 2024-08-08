e48de3a21437e33497ca8d6d5613caf6f4bb49d8
Ruslan Sedziukh <ruslan.sedziukh@toptal.com>
2024-08-08 10:27:38 +0300

    [FX-5534] Migrate FormField and FormActionsContainer to TailwindCSS

--- packages/base/Form/src/FormActionsContainer/FormActionsContainer.tsx
+++ packages/base/Form/src/FormActionsContainer/FormActionsContainer.tsx
@@ -1,27 +1,21 @@
 import React from 'react'
 import type { ReactNode } from 'react'
-import cx from 'classnames'
-import type { Theme } from '@material-ui/core/styles'
-import { makeStyles } from '@material-ui/core/styles'
 import type { ContainerProps } from '@toptal/picasso-container'
 import { Container } from '@toptal/picasso-container'
 
-import styles from './styles'
 import { useFieldsLayoutContext } from '../FieldsLayout'
-import { createLabelWidthStyles } from '../FormField/styles'
+import {
+  createLabelWidthStyles,
+  horizontalLayoutClasses,
+} from '../FormField/styles'
 
 export interface Props extends Omit<ContainerProps, 'children'> {
   /** Content */
   children: ReactNode
 }
 
-const useStyles = makeStyles<Theme>(styles, {
-  name: 'PicassoFormActionsContainer',
-})
-
 const FormActionsContainer = ({ children, style, ...rest }: Props) => {
   const { layout, labelWidth } = useFieldsLayoutContext()
-  const classes = useStyles()
 
   if (layout === 'vertical') {
     return <Container {...rest}>{children}</Container>
@@ -31,7 +25,7 @@ const FormActionsContainer = ({ children, style, ...rest }: Props) => {
 
   return (
     <Container
-      className={cx(classes.horizontalLayout)}
+      className={horizontalLayoutClasses}
       {...rest}
       style={{ ...style, ...labelWidthStyles }}
     >
--- packages/base/Form/src/FormField/styles.ts
+++ packages/base/Form/src/FormField/styles.ts
@@ -1,6 +1,4 @@
-import { createStyles } from '@material-ui/core/styles'
 import type { BreakpointKeys } from '@toptal/picasso-provider'
-import type { Theme } from '@material-ui/core'
 
 import { DEFAULT_LABEL_WIDTH_SIZE } from '../FieldsLayout'
 import type {
@@ -12,22 +10,7 @@ export const getLabelWithName = (breakpoint: BreakpointKeys) => {
   return `--form-label-width--${breakpoint}`
 }
 
-export const HORISONTAL_LABEL_COLUMN_WIDTH = '17rem'
 export const FORM_LABEL_WIDTH_CSS_VARIABLE = '--form-label-width'
-
-/**
- * Generates CSS variables for each breakpoint to set --form-label-width
- */
-export const createBreakpointsForLabelWidth = (theme: Theme) =>
-  [...theme.breakpoints.keys].reduce(
-    (acc, breakpoint) => ({
-      ...acc,
-      [theme.breakpoints.up(breakpoint)]: {
-        [FORM_LABEL_WIDTH_CSS_VARIABLE]: `var(${getLabelWithName(breakpoint)})`,
-      },
-    }),
-    {}
-  )
 
 const isLabelColumnSize = (
   labelWidth: LabelColumnSize | ResponsiveLabelColumnSize
@@ -64,60 +47,14 @@ export const createLabelWidthStyles = (
   )
 }
 
-export const createStylesForHorizontalLayout = (theme: Theme) => {
-  return {
-    display: 'grid',
-    // --form-label-width is passed down from cascading style, in this case from Form
-    '--label-width': `calc(${HORISONTAL_LABEL_COLUMN_WIDTH} / 4 * var(${FORM_LABEL_WIDTH_CSS_VARIABLE}, ${DEFAULT_LABEL_WIDTH_SIZE}))`,
-    gridTemplateColumns: `var(--label-width) 1fr`,
-    gap: '0 32px', // 0 and lg, respectively
-    gridTemplateRows: 'auto auto',
-    gridTemplateAreas: `
-        "label input"
-        "hint error"
-      `,
-    width: '100%',
+export const horizontalLayoutClasses = `
+  grid w-full gap-x-[32px] gap-y-0 grid-rows-[auto_auto] 
+  grid-cols-[calc((17rem/4)*var(--form-label-width,3))_1fr]
+  [grid-template-areas:"label_input"_"hint_error"]
 
-    // create media queries for each breakpoint to set --form-label-width
-    ...createBreakpointsForLabelWidth(theme),
-  }
-}
-
-export default (theme: Theme) =>
-  createStyles({
-    root: {
-      fontSize: '1rem',
-
-      '& + &': {
-        marginTop: '1em',
-      },
-      '& $error + $hint': {
-        marginTop: 0,
-      },
-    },
-
-    adornment: {
-      position: 'relative',
-      paddingRight: '2rem',
-    },
-    autoSaveIndicator: {
-      position: 'absolute',
-      top: 0,
-      right: 0,
-
-      '&$hasMultilineCounter': {
-        top: '-0.875rem',
-      },
-    },
-
-    horizontalLayout: createStylesForHorizontalLayout(theme),
-
-    horizontalLayoutAdornment: {
-      gridArea: 'error',
-    },
-
-    // These classes might still be used in selectors
-    hasMultilineCounter: {},
-    hint: {},
-    error: {},
-  })
+  xs:[--form-label-width:var(--form-label-width--xs)]
+  sm:[--form-label-width:var(--form-label-width--sm)]
+  md:[--form-label-width:var(--form-label-width--md)]
+  lg:[--form-label-width:var(--form-label-width--lg)]
+  xl:[--form-label-width:var(--form-label-width--xl)]
+  `
