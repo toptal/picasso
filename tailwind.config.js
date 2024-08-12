e53d077936e7dd685933eec5fb9e623ad1803ad6
Aleksandr Shumilov <alexander.shumilov@toptal.com>
2024-08-12 18:35:11 +0100

    Fix eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
+// Ignore missing imports as packages are definitely present in the project
+/* eslint-disable import/no-extraneous-dependencies */
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['packages/**/*.{ts,tsx}'],
-  // Ignore missing imports as packages are definitely present in the project
-  // eslint-disable-next-line import/no-extraneous-dependencies
   presets: [
     require('@toptal/base-tailwind'),
     require('@toptal/picasso-tailwind'),
