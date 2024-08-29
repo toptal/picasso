9e089cc183a95391f73dc0bf28c59b3c1bfa4758
Tomáš Sláma <tomas.slama@toptal.com>
2024-08-29 14:26:45 +0200

    [FX-5854] Export `Avatar.Wrapper` to support custom icons (#4511)

--- packages/base/Avatar/src/Avatar/story/Sizes.example.tsx
+++ packages/base/Avatar/src/Avatar/story/Sizes.example.tsx
@@ -4,83 +4,53 @@ import { SPACING_4 } from '@toptal/picasso-utils'
 
 const Example = () => (
   <div>
-    <Container>
-      <Container inline>
-        <Avatar
-          size='xxsmall'
-          alt='Jacqueline Roque. Pablo Picasso, 1954. Small'
-          name='Jacqueline Roque'
-          src='./jacqueline-with-flowers-1954-square.jpg'
-        />
-      </Container>
-      <Container inline left={SPACING_4}>
-        <Avatar
-          size='xsmall'
-          alt='Jacqueline Roque. Pablo Picasso, 1954. Small'
-          name='Jacqueline Roque'
-          src='./jacqueline-with-flowers-1954-square.jpg'
-        />
-      </Container>
-      <Container inline left={SPACING_4}>
-        <Avatar
-          size='small'
-          alt='Jacqueline Roque. Pablo Picasso, 1954. Small'
-          name='Jacqueline Roque'
-          src='./jacqueline-with-flowers-1954-square.jpg'
-        />
-      </Container>
-      <Container inline left={SPACING_4}>
-        <Avatar
-          size='medium'
-          alt='Jacqueline Roque. Pablo Picasso, 1954. Medium'
-          name='Jacqueline Roque'
-          src='./jacqueline-with-flowers-1954-square.jpg'
-        />
-      </Container>
-      <Container inline left={SPACING_4}>
-        <Avatar
-          size='large'
-          alt='Jacqueline Roque. Pablo Picasso, 1954. Large'
-          name='Jacqueline Roque'
-          src='./jacqueline-with-flowers-1954-square.jpg'
-        />
-      </Container>
+    <Container top={SPACING_4} flex gap={SPACING_4}>
+      <Avatar
+        size='xxsmall'
+        alt='Jacqueline Roque. Pablo Picasso, 1954. Small'
+        name='Jacqueline Roque'
+        src='./jacqueline-with-flowers-1954-square.jpg'
+      />
+      <Avatar
+        size='xsmall'
+        alt='Jacqueline Roque. Pablo Picasso, 1954. Small'
+        name='Jacqueline Roque'
+        src='./jacqueline-with-flowers-1954-square.jpg'
+      />
+      <Avatar
+        size='small'
+        alt='Jacqueline Roque. Pablo Picasso, 1954. Small'
+        name='Jacqueline Roque'
+        src='./jacqueline-with-flowers-1954-square.jpg'
+      />
+      <Avatar
+        size='medium'
+        alt='Jacqueline Roque. Pablo Picasso, 1954. Medium'
+        name='Jacqueline Roque'
+        src='./jacqueline-with-flowers-1954-square.jpg'
+      />
+      <Avatar
+        size='large'
+        alt='Jacqueline Roque. Pablo Picasso, 1954. Large'
+        name='Jacqueline Roque'
+        src='./jacqueline-with-flowers-1954-square.jpg'
+      />
     </Container>
 
-    <Container top={SPACING_4}>
-      <Container inline>
-        <Avatar size='xxsmall' name='Jacqueline Roque' />
-      </Container>
-      <Container inline left={SPACING_4}>
-        <Avatar size='xsmall' name='Jacqueline Roque' />
-      </Container>
-      <Container inline left={SPACING_4}>
-        <Avatar size='small' name='Jacqueline Roque' />
-      </Container>
-      <Container inline left={SPACING_4}>
-        <Avatar size='medium' name='Jacqueline Roque' />
-      </Container>
-      <Container inline left={SPACING_4}>
-        <Avatar size='large' name='Jacqueline Roque' />
-      </Container>
+    <Container top={SPACING_4} flex gap={SPACING_4}>
+      <Avatar size='xxsmall' name='Jacqueline Roque' />
+      <Avatar size='xsmall' name='Jacqueline Roque' />
+      <Avatar size='small' name='Jacqueline Roque' />
+      <Avatar size='medium' name='Jacqueline Roque' />
+      <Avatar size='large' name='Jacqueline Roque' />
     </Container>
 
-    <Container top={SPACING_4}>
-      <Container inline>
-        <Avatar size='xxsmall' />
-      </Container>
-      <Container inline left={SPACING_4}>
-        <Avatar size='xsmall' />
-      </Container>
-      <Container inline left={SPACING_4}>
-        <Avatar size='small' />
-      </Container>
-      <Container inline left={SPACING_4}>
-        <Avatar size='medium' />
-      </Container>
-      <Container inline left={SPACING_4}>
-        <Avatar size='large' />
-      </Container>
+    <Container top={SPACING_4} flex gap={SPACING_4}>
+      <Avatar size='xxsmall' />
+      <Avatar size='xsmall' />
+      <Avatar size='small' />
+      <Avatar size='medium' />
+      <Avatar size='large' />
     </Container>
   </div>
 )
--- packages/base/Avatar/src/AvatarCompound/index.ts
+++ packages/base/Avatar/src/AvatarCompound/index.ts
@@ -1,6 +1,7 @@
-import { Avatar } from '../Avatar'
+import { Avatar, AvatarWrapper } from '../Avatar'
 import { AvatarGroup } from '../AvatarGroup'
 
 export const AvatarCompound = Object.assign(Avatar, {
   Group: AvatarGroup,
+  Wrapper: AvatarWrapper,
 })
