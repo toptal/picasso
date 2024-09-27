8d8e229e3eb5985712feb725d95d518b29825ab4
TomasSlama <tomas.slama@toptal.com>
2024-09-27 12:11:03 +0200

    Update tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,27 @@
+
+
+// Mock for IntersectionObserver
+class IntersectionObserverMock {
+  constructor(callback) {
+    this.callback = callback;
+    this.observables = [];
+  }
+
+  observe(element) {
+    this.observables.push(element);
+    this.callback([{ isIntersecting: true, target: element }]); // Customize as needed
+  }
+
+  unobserve(element) {
+    this.observables = this.observables.filter(obs => obs !== element);
+  }
+
+  disconnect() {
+    this.observables = [];
+  }
+}
+
+// Mock global IntersectionObserver
+global.IntersectionObserver = IntersectionObserverMock;
 global.TextEncoder = require('util').TextEncoder;
 global.TextDecoder = require('util').TextDecoder;
