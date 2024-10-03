09e3ad7117b14b839180ca32797d8b99137746d7
Ruslan Sedziukh <ruslan.sedziukh@toptal.com>
2024-10-03 11:51:46 +0300

    [FX-6047] Fix UserBadge padding in PageTopBarMenu

--- packages/base/UserBadge/src/UserBadge/UserBadge.tsx
+++ packages/base/UserBadge/src/UserBadge/UserBadge.tsx
@@ -101,7 +101,7 @@ export const UserBadge = forwardRef<HTMLDivElement, Props>(function UserBadge(
       ref={ref}
       flex
       alignItems={alignItems}
-      className={twMerge('text-[1rem]', className)}
+      className={twMerge('text-[1rem]', classes?.root, className)}
       style={style}
     >
       {UserBadgeAvatar}
