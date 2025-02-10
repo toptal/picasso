a182a86b8d0954b7bbf74aad5cedfb6f4dcbde1d
Vedran Ivanac <vedran.ivanac@toptal.com>
2025-02-10 13:01:40 +0100

    Revert back to 1280

--- packages/base/Page/src/PageSidebar/PageSidebar.tsx
+++ packages/base/Page/src/PageSidebar/PageSidebar.tsx
@@ -1,19 +1,17 @@
 import { useSidebar, usePageTopBar } from '@toptal/picasso-provider'
 import type { BaseProps, SizeType } from '@toptal/picasso-shared'
-import { twMerge, twJoin } from '@toptal/picasso-tailwind-merge'
+import { twMerge } from '@toptal/picasso-tailwind-merge'
 import type { ReactNode } from 'react'
 import React, { forwardRef, useCallback, useEffect, useState } from 'react'
-import { ButtonCircular } from '@toptal/picasso-button'
 import { Container } from '@toptal/picasso-container'
-import { BackMinor16, ChevronRight16 } from '@toptal/picasso-icons'
 import { noop } from '@toptal/picasso-utils'
 
 import { PageHamburgerPortal, useHamburgerContext } from '../PageHamburger'
 import { SidebarItem } from '../SidebarItem'
 import { SidebarLogo } from '../SidebarLogo'
 import { SidebarMenu } from '../SidebarMenu'
-import { SidebarContextProvider } from './SidebarContextProvider'
 import type { VariantType } from './types'
+import PageSidebarDesktop from './PageSidebarDesktop'
 
 export interface Props extends BaseProps {
   /** Style variant of Sidebar and subcomponents */
@@ -70,18 +68,10 @@ export const PageSidebar = forwardRef<HTMLDivElement, Props>(function Sidebar(
 
   const [isCollapsed, setIsCollapsed] = useState(!!defaultCollapsed)
   const [isHovered, setIsHovered] = useState(false)
-  const [expandedItemKey, setExpandedItemKey] = useState<number | null>(null)
 
   const { setHasSidebar } = useSidebar()
   const { hasPageHamburger } = useHamburgerContext()
   const { hasTopBar } = usePageTopBar()
-
-  useEffect(() => {
-    // Clear expanded submenu on sidebar collapse
-    if (isCollapsed) {
-      setExpandedItemKey(null)
-    }
-  }, [isCollapsed])
 
   useEffect(() => {
     setHasSidebar(true)
@@ -91,7 +81,7 @@ export const PageSidebar = forwardRef<HTMLDivElement, Props>(function Sidebar(
     }
   }, [setHasSidebar])
 
-  const handleCollapseButtonClick = useCallback(() => {
+  const handleCollapse = useCallback(() => {
     setIsCollapsed(previousState => !previousState)
     onCollapse()
   }, [setIsCollapsed, onCollapse])
@@ -106,7 +96,7 @@ export const PageSidebar = forwardRef<HTMLDivElement, Props>(function Sidebar(
         'text-lg/[inherit]',
         'relative',
         'transition-[width] ease-in-out delay-[225ms]',
-        'hidden lg:block',
+        'hidden min-[1280px]:block',
         '[&]:before:absolute',
         '[&]:before:content-[""]',
         '[&]:before:left-0',
@@ -130,52 +120,18 @@ export const PageSidebar = forwardRef<HTMLDivElement, Props>(function Sidebar(
       onMouseEnter={collapsible ? () => setIsHovered(true) : noop}
       onMouseLeave={collapsible ? () => setIsHovered(false) : noop}
     >
-      {hasPageHamburger && (
+      {hasPageHamburger ? (
         <PageHamburgerPortal>{children}</PageHamburgerPortal>
-      )}
-      {!hasPageHamburger && (
-        <div
-          style={{
-            maxHeight: wrapperMaxHeight,
-          }}
-          className={twJoin(
-            'h-full',
-            !disableSticky &&
-              'max-h-[calc(100vh-var(--header-height,3.5rem))] sticky top-[var(--header-height,3.5rem)]'
-          )}
+      ) : (
+        <PageSidebarDesktop
+          wrapperMaxHeight={wrapperMaxHeight}
+          disableSticky={disableSticky}
+          isCollapsed={isCollapsed}
+          isHovered={isHovered}
+          onCollapse={handleCollapse}
         >
-          <Container
-            flex
-            direction='column'
-            className='scrollable-content h-full overflow-y-auto pt-4 pb-2 px-0'
-            data-testid={testIds?.scrollableContainer}
-          >
-            {collapsible && (
-              <ButtonCircular
-                className={twMerge(
-                  // TODO: [FX-XXXX] technical debt: button color/background/radius/shadow shouldn't be overwritten
-                  'absolute -right-3 top-3 invisible text-graphite-700 bg-white rounded-full shadow-[0_0_0_1px_rgba(0,0,0,0.04),0_0_8px_0_rgba(0,0,0,0.16)] z-[100] hover:text-white hover:bg-blue-500',
-                  isHovered && 'visible'
-                )}
-                onClick={handleCollapseButtonClick}
-                icon={isCollapsed ? <ChevronRight16 /> : <BackMinor16 />}
-                aria-label='collapse sidebar'
-                variant='primary'
-                data-testid={testIds?.collapseButton}
-              />
-            )}
-            <div className='order-[50] flex-1 h-full' />
-            <SidebarContextProvider
-              isCollapsed={isCollapsed}
-              isHovered={isHovered}
-              variant={variant}
-              expandedItemKey={expandedItemKey}
-              setExpandedItemKey={setExpandedItemKey}
-            >
-              {children}
-            </SidebarContextProvider>
-          </Container>
-        </div>
+          {children}
+        </PageSidebarDesktop>
       )}
     </Container>
   )
