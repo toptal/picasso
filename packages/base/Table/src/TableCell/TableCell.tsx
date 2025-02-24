efebf2166a9bce7903bcef2c6fce9c7676bcee79
Daniel Ostapenko <daniel.ostapenko@toptal.com>
2025-02-24 12:30:14 +0100

    Fix right padding to match left in Table

--- packages/base/Table/src/TableCell/TableCell.tsx
+++ packages/base/Table/src/TableCell/TableCell.tsx
@@ -100,7 +100,7 @@ export const TableCell = forwardRef<HTMLTableCellElement, Props>(
         {...rest}
         ref={ref}
         className={twMerge(
-          'h-10 px-2 py-2 border-none last:pr-6',
+          'h-10 px-2 py-2 border-none last:pr-4',
           spacingClasses[spacing],
           isHead && 'text-left',
           className
