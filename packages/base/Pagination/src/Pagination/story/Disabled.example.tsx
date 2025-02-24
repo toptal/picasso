862452d8374596b6000f3df2eb12aa16b59a5219
Diego Guerra <diego.guerra@toptal.com>
2025-02-24 08:33:50 +0800

    Example with nextDisabled

--- packages/base/Pagination/src/Pagination/story/Disabled.example.tsx
+++ packages/base/Pagination/src/Pagination/story/Disabled.example.tsx
@@ -1,15 +1,37 @@
 import React from 'react'
-import { Pagination } from '@toptal/picasso'
+import { Pagination, Container, Typography } from '@toptal/picasso'
+import { SPACING_2, SPACING_4 } from '@toptal/picasso-utils'
 
 const Example = () => (
-  <div>
-    <Pagination
-      activePage={4}
-      disabled
-      onPageChange={handlePageChange}
-      totalPages={10}
-    />
-  </div>
+  <Container flex direction='column' justifyContent='space-between'>
+    <Container bottom={SPACING_2}>
+      <Typography variant='heading' size='small'>
+        Disabled
+      </Typography>
+    </Container>
+    <Container bottom={SPACING_4}>
+      <Pagination
+        activePage={4}
+        disabled
+        onPageChange={handlePageChange}
+        totalPages={10}
+      />
+    </Container>
+
+    <Container bottom={SPACING_2}>
+      <Typography variant='heading' size='small'>
+        Next disabled
+      </Typography>
+    </Container>
+    <Container>
+      <Pagination
+        activePage={4}
+        nextDisabled
+        onPageChange={handlePageChange}
+        totalPages={10}
+      />
+    </Container>
+  </Container>
 )
 
 const handlePageChange = () => {}
