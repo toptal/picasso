e8f41adecede4307cbb8c052742d420538d689bd
Vedran Ivanac <vedran.ivanac@toptal.com>
2025-03-11 14:50:45 +0100

    Fix

--- packages/base/Page/src/SidebarItemContent/styles.ts
+++ packages/base/Page/src/SidebarItemContent/styles.ts
@@ -1,7 +1,7 @@
 export const styles = {
-  noWrap: 'flex-1 min-w-0 truncate',
-  withIcon: 'ml-1.5',
+  noWrap: 'flex-1 min-w-0',
+  withIcon: 'ml-[.375rem]',
   iconWrapper: 'w-[1em] h-[1em] relative',
   compactIndicator: 'absolute top-0 right-0 translate-x-1/2 -translate-y-1/2',
-  expandedIndicator: 'mr-1.5',
+  expandedIndicator: 'mr-[.375rem]',
 } as const
