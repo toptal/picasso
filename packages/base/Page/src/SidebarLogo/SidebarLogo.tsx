991d0dcf35a9846d181c0796c9569e6b97996722
tmatijev <tomislav.matijevic@toptal.com>
2025-04-01 11:01:55 +0200

    Fix Lint

--- packages/base/Page/src/SidebarLogo/SidebarLogo.tsx
+++ packages/base/Page/src/SidebarLogo/SidebarLogo.tsx
@@ -1,12 +1,9 @@
 import type { ReactNode, HTMLAttributes } from 'react'
 import React, { forwardRef } from 'react'
 import type { BaseProps } from '@toptal/picasso-shared'
-import type { Theme } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core'
-import cx from 'classnames'
+import { twMerge } from '@toptal/picasso-tailwind-merge'
 import { Container } from '@toptal/picasso-container'
 
-import styles from './styles'
 import { useSidebarContext } from '../PageSidebar/SidebarContextProvider'
 
 export interface Props
@@ -19,17 +16,11 @@ export interface Props
   fullLogo?: ReactNode
 }
 
-const useStyles = makeStyles<Theme>(styles, {
-  name: 'PicassoSidebarLogo',
-})
-
 export const SidebarLogo = forwardRef<HTMLDivElement, Props>(
   function SidebarLogo(
     { children, collapsedLogo, fullLogo, className, ...rest },
     ref
   ) {
-    const classes = useStyles()
-
     const { isCollapsed: isSidebarCollapsed } = useSidebarContext()
 
     const logo = isSidebarCollapsed ? collapsedLogo : fullLogo
@@ -37,7 +28,7 @@ export const SidebarLogo = forwardRef<HTMLDivElement, Props>(
     return (
       <Container
         {...rest}
-        className={cx(classes.root, className)}
+        className={twMerge('flex-shrink-0 block overflow-hidden', className)}
         ref={ref}
         bottom='xsmall'
         left='large'
