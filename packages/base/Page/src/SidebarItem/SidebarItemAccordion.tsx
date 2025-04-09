ea49927fb27852a89a50432cbec510bfc3706590
George Aidonidis <george.aidonidis@toptal.com>
2025-04-09 14:48:14 +0300

    [BILL-💵, FF-12] Migrate sidebar item to tailwind (#4695)
    * Migrate sidebar item to tailwind
    * Add changeset
    * Update snapshots
    * Review amends
    * Update snapshots

--- packages/base/Page/src/SidebarItem/SidebarItemAccordion.tsx
+++ packages/base/Page/src/SidebarItem/SidebarItemAccordion.tsx
@@ -1,20 +1,13 @@
-import type { Theme } from '@material-ui/core/styles'
-import { makeStyles } from '@material-ui/core/styles'
-import cx from 'classnames'
 import type { ChangeEvent } from 'react'
 import React, { forwardRef, useCallback } from 'react'
 import { Accordion } from '@toptal/picasso-accordion'
 import { ArrowDownMinor16 } from '@toptal/picasso-icons'
 
-import styles from './styles'
 import { SubMenuContextProvider } from './SubMenuContextProvider'
 import { ParentItemContextProvider } from './ParentItemContextProvider'
 import type { Props } from './types'
 import { SidebarItemHeader } from './SidebarItemHeader'
-
-const useStyles = makeStyles<Theme>(styles, {
-  name: 'PicassoSidebarItemAccordion',
-})
+import { styles } from './styles'
 
 export const SidebarItemAccordion = forwardRef<HTMLElement, Props>(
   function SidebarItemAccordion(props: Props, ref) {
@@ -28,7 +21,6 @@ export const SidebarItemAccordion = forwardRef<HTMLElement, Props>(
       icon,
       compact,
     } = props
-    const classes = useStyles()
 
     const handleAccordionChange = useCallback(
       (event: ChangeEvent<{}>, isAccordionExpanded: boolean) => {
@@ -53,23 +45,15 @@ export const SidebarItemAccordion = forwardRef<HTMLElement, Props>(
         <Accordion
           onChange={handleAccordionChange}
           classes={{
-            summary: classes.collapsibleWrapper,
-            content: classes.content,
+            summary: 'p-0 pl-4 mx-4',
+            content: 'max-w-full',
           }}
           content={content}
           borders='none'
           disabled={disabled}
           expanded={isExpanded}
           expandIcon={
-            <ArrowDownMinor16
-              className={cx(
-                classes.expandIcon,
-                classes[`${variant}ExpandIcon`],
-                {
-                  [classes.expandIconDisabled]: disabled,
-                }
-              )}
-            />
+            <ArrowDownMinor16 className={styles.icon(variant, disabled)} />
           }
         >
           <SidebarItemHeader {...props} ref={ref} />
--- packages/base/Page/src/SidebarItem/styles.ts
+++ packages/base/Page/src/SidebarItem/styles.ts
@@ -1,32 +1,12 @@
-import type { Theme } from '@material-ui/core/styles'
-import { createStyles } from '@material-ui/core/styles'
+import { twMerge } from '@toptal/picasso-tailwind-merge'
 
-export default ({ palette }: Theme) =>
-  createStyles({
-    root: {
-      fontSize: '1rem',
-    },
-    collapsibleWrapper: {
-      padding: '0 0 0 1rem',
-      margin: '0 1rem',
-    },
-    content: {
-      // to cover text overflow in the sub-menu header item
-      maxWidth: '100%',
-    },
-    expandIcon: {
-      fontSize: '1em',
-    },
-    compactDropdown: {
-      marginLeft: '0.5em',
-    },
-    lightExpandIcon: {
-      color: palette.grey.dark,
-    },
-    darkExpandIcon: {
-      color: palette.grey.main,
-    },
-    expandIconDisabled: {
-      color: palette.grey.main,
-    },
-  })
+import type { VariantType } from '../PageSidebar/types'
+
+export const styles = {
+  icon: (variant?: VariantType, disabled?: boolean) =>
+    twMerge([
+      'text-base',
+      variant === 'light' ? 'text-graphite-700' : 'text-gray-500',
+      disabled && 'text-gray-500',
+    ]),
+}
