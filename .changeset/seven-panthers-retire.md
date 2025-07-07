54593efdbe6cb655584e06023ea5e5378e0e2410
Rafał Kwaśniewski <rafal.kwasniewski@toptal.com>
2025-07-07 14:58:38 +0700

    [FF-19] Remove usage of classnames and Material-UI's createStyles (#4781)

---
+++ .changeset/seven-panthers-retire.md
@@ -0,0 +1,6 @@
+---
+'@toptal/picasso-drawer': patch
+---
+
+- remove usage of Material-UI's "createStyles"
+- use "@toptal/picasso-tailwind-merge" instead of "classnames"
--- packages/base/Drawer/src/DrawerTitle/DrawerTitle.tsx
+++ packages/base/Drawer/src/DrawerTitle/DrawerTitle.tsx
@@ -2,7 +2,6 @@ import type { ReactNode } from 'react'
 import React from 'react'
 import type { BaseProps } from '@toptal/picasso-shared'
 import { Typography } from '@toptal/picasso-typography'
-import { Container } from '@toptal/picasso-container'
 
 export interface Props extends BaseProps {
   title?: ReactNode
@@ -18,15 +17,11 @@ export const DrawerTitle = ({ title }: Props) => {
   }
 
   return (
-    <Container
-      flex
-      alignItems='center'
-      className='border-gray-100 border-0 border-solid border-b py-4 pr-[4rem] pl-6'
-    >
+    <div className='flex items-center border-gray-100 border-0 border-solid border-b py-4 pr-[4rem] pl-6'>
       <Typography variant='heading' size='medium' className='grow'>
         {title}
       </Typography>
-    </Container>
+    </div>
   )
 }
 
