a17d0d42fdb55fd2363539413b77b37ca6475e12
Paul Timmermann <pltimme@gmail.com>
2026-03-20 15:54:40 +0000

    test(Paper): add borderRadius prop tests

--- packages/base/Paper/src/Paper/test.tsx
+++ packages/base/Paper/src/Paper/test.tsx
@@ -20,4 +20,26 @@ describe('Paper', () => {
 
     expect(container).toMatchSnapshot()
   })
+
+  describe('borderRadius', () => {
+    it.each(['none', 'sm', 'md', 'full'] as const)(
+      'applies %s border radius class',
+      borderRadius => {
+        const { container } = render(
+          <Paper borderRadius={borderRadius}>Content</Paper>
+        )
+
+        expect(container.firstChild).toMatchSnapshot()
+      }
+    )
+
+    it('applies no border radius class when borderRadius is not set', () => {
+      const { container } = render(<Paper>Content</Paper>)
+
+      expect(container.firstChild).not.toHaveClass('rounded-none')
+      expect(container.firstChild).not.toHaveClass('rounded-sm')
+      expect(container.firstChild).not.toHaveClass('rounded-md')
+      expect(container.firstChild).not.toHaveClass('rounded-full')
+    })
+  })
 })
