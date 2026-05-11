d279c7ff65042c03fbc02afd5d89b7962b3ac7fe
Vedran Ivanac <vedran.ivanac@toptal.com>
2026-05-11 19:28:02 +0200

    [PF-1994] Migrate Backdrop from @mui/base to @base-ui/react
    Tier 0 component. See PR description for prop-surface diff,
    import diff, and Happo summary.
    
    Refs: PF-1994

--- packages/base/Backdrop/src/Backdrop/Backdrop.tsx
+++ packages/base/Backdrop/src/Backdrop/Backdrop.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import cx from 'classnames'
 import { Fade } from '@toptal/picasso-fade'
-import type { ModalBackdropSlotProps } from '@mui/base/Modal'
 
-export interface Props extends ModalBackdropSlotProps {
+export interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  /** If `true`, the backdrop is visible */
+  open: boolean
   /** The duration for the transition, in milliseconds */
   transitionDuration?: number
   /** If true, the backdrop is invisible */
   invisible?: boolean
-  className?: string
+  /** Passed through by @mui/base/Modal's slot machinery; ignored at render. */
+  ownerState?: unknown
 }
 
 export const Backdrop = React.forwardRef<HTMLDivElement, Props>(
@@ -17,12 +19,11 @@ export const Backdrop = React.forwardRef<HTMLDivElement, Props>(
       transitionDuration = 300,
       open,
       invisible = false,
-      /* eslint-disable @typescript-eslint/no-unused-vars */
-      // we want to omit ownerState from spreading to the DOM
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       ownerState,
-      // @mui/base/Modal adds default class wich we don't need .base-Modal-backdrop
-      className,
-      /* eslint-enable */
+      // @mui/base/Modal injects its own .base-Modal-backdrop className; drop it.
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      className: _className,
       ...rest
     },
     ref
