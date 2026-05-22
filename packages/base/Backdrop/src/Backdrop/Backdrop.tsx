4df4c9f9597ebc6f4a4e736348443c67b930632d
Vedran Ivanac <vedran.ivanac@toptal.com>
2026-05-22 14:05:52 +0200

    [PF-1994] Migrate Backdrop to @base-ui/react + Tailwind (#4954)

--- packages/base/Backdrop/src/Backdrop/Backdrop.tsx
+++ packages/base/Backdrop/src/Backdrop/Backdrop.tsx
@@ -1,40 +1,41 @@
 import React from 'react'
 import cx from 'classnames'
 import { Fade } from '@toptal/picasso-fade'
-import type { ModalBackdropSlotProps } from '@mui/base/Modal'
+import type { BaseProps } from '@toptal/picasso-shared'
 
-export interface Props extends ModalBackdropSlotProps {
+export interface Props
+  extends BaseProps,
+    React.HTMLAttributes<HTMLDivElement> {
   /** The duration for the transition, in milliseconds */
   transitionDuration?: number
+  /** If `true`, the backdrop is shown */
+  open: boolean
   /** If true, the backdrop is invisible */
   invisible?: boolean
-  className?: string
+  ownerState?: unknown
 }
 
 export const Backdrop = React.forwardRef<HTMLDivElement, Props>(
-  (
-    {
+  (props, ref) => {
+    const {
       transitionDuration = 300,
       open,
       invisible = false,
-      /* eslint-disable @typescript-eslint/no-unused-vars */
-      // we want to omit ownerState from spreading to the DOM
-      ownerState,
-      // @mui/base/Modal adds default class wich we don't need .base-Modal-backdrop
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      ownerState: _ownerState,
       className,
-      /* eslint-enable */
       ...rest
-    },
-    ref
-  ) => {
+    } = props
+
     return (
       <Fade in={open} timeout={transitionDuration}>
         <div
           className={cx(
-            'fixed -z-[1] inset-0 bg-black ',
+            'fixed -z-[1] inset-0 bg-black',
             '-webkit-tap-highlight-color-transparent',
             { 'bg-black/0': invisible },
-            { 'bg-black/50': !invisible }
+            { 'bg-black/50': !invisible },
+            className
           )}
           ref={ref}
           {...rest}
