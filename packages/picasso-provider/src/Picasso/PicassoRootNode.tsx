0bded7d3dd7b99613550281138a37ca02e5f87f3
Vedran Ivanac <vedran.ivanac@toptal.com>
2026-07-06 16:25:05 +0200

    Restyle PicassoRootNode + scrollbar fix off MUI v4 JSS

--- packages/picasso-provider/src/Picasso/PicassoRootNode.tsx
+++ packages/picasso-provider/src/Picasso/PicassoRootNode.tsx
@@ -1,29 +1,18 @@
 import type { ReactNode } from 'react'
 import React, { forwardRef } from 'react'
-import type { Theme } from '@material-ui/core/styles'
-import { makeStyles } from '@material-ui/core/styles'
 
-import globalStyles from './styles'
+import { root } from './styles'
 
 export interface PicassoRootNodeProps {
   children?: ReactNode
 }
 
-const useGlobalStyles = makeStyles<Theme>(globalStyles, {
-  name: 'Picasso',
-})
-
 const PicassoRootNode = forwardRef<HTMLDivElement, PicassoRootNodeProps>(
-  (props, ref) => {
-    const { children } = props
-    const classes = useGlobalStyles()
-
-    return (
-      <div ref={ref} className={classes.root}>
-        {children}
-      </div>
-    )
-  }
+  ({ children }, ref) => (
+    <div ref={ref} className={root}>
+      {children}
+    </div>
+  )
 )
 
 PicassoRootNode.displayName = 'PicassoRootNode'
--- packages/picasso-provider/src/PreventPageWidthChangeOnScrollbar/PreventPageWidthChangeOnScrollbar.tsx
+++ packages/picasso-provider/src/PreventPageWidthChangeOnScrollbar/PreventPageWidthChangeOnScrollbar.tsx
@@ -1,20 +1,19 @@
-import type { Theme } from '@material-ui/core/styles'
-import { makeStyles } from '@material-ui/core/styles'
+import React from 'react'
 
-import styles from './styles'
-
-const useStyles = makeStyles<Theme>(styles, {
-  name: 'PicassoPreventPageWidthChangeOnScrollbar',
-})
+import { preventPageWidthChangeCss } from './styles'
 
 /**
  * This component applies global styles that fix the problem with jumping page width.
  * The problem comes from disappearing vertical page scrollbar due to varying page height.
  */
 const PreventPageWidthChangeOnScrollbar = () => {
-  useStyles()
+  const css = preventPageWidthChangeCss()
 
-  return null
+  if (!css) {
+    return null
+  }
+
+  return <style dangerouslySetInnerHTML={{ __html: css }} />
 }
 
 export default PreventPageWidthChangeOnScrollbar
