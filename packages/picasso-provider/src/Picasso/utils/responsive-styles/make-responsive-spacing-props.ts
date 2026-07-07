6ab9ac1879179a2222f99c17b4b59623f446da39
Vedran Ivanac <vedran.ivanac@toptal.com>
2026-07-07 16:29:53 +0200

    Port responsive-spacing engine off JSS; retire SSR helper

--- packages/picasso-provider/src/Picasso/utils/responsive-styles/make-responsive-spacing-props.ts
+++ packages/picasso-provider/src/Picasso/utils/responsive-styles/make-responsive-spacing-props.ts
@@ -1,29 +1,23 @@
 import cx from 'classnames'
-import type { Theme } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core'
 
-import { mediaQueriesClasses } from './media-queries-classes'
-import type { ResponsiveCssProp, ResponsiveCssSpacings } from './types'
-import { getBreakpointClassNamesToUse } from './get-breakpoint-class-names-to-use'
 import { generateVariableInlineStyles } from './generate-variable-inline-styles'
+import { getBreakpointClassNamesToUse } from './get-breakpoint-class-names-to-use'
+import { injectResponsiveStyles } from './inject-responsive-styles'
+import type { ResponsiveCssProp, ResponsiveCssSpacings } from './types'
 
 const makeResponsiveSpacingProps = <K extends ResponsiveCssProp>(
   props: readonly K[],
   name: string
 ) => {
-  const useStyles = makeStyles<Theme>(
-    (theme: Theme) => mediaQueriesClasses(props, theme),
-    {
-      name: name,
-    }
-  )
-
   const useResponsiveClassNames = (propValues: ResponsiveCssSpacings<K>) => {
-    const classes = useStyles()
-    const mediaQueries = getBreakpointClassNamesToUse(props, propValues)
+    // Inject the media-query stylesheet on first use (client-only, idempotent),
+    // during render so the classes' CSS is present synchronously.
+    injectResponsiveStyles(props, name)
+
+    const classNames = getBreakpointClassNamesToUse(props, propValues)
 
     return {
-      className: cx(...mediaQueries.map(brkClassName => classes[brkClassName])),
+      className: cx(...classNames.map(className => `${name}-${className}`)),
       style: generateVariableInlineStyles(props, propValues),
     }
   }
