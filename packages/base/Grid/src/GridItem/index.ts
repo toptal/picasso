18238c2b036cbb0a4b4f1cc7c430b32ecb8334ca
Ante Zebic <a.zebic@toptal.com>
2026-07-07 16:43:31 +0200

    [PF-2208] Fix Grid.Item gridsize type

--- packages/base/Grid/src/GridItem/index.ts
+++ packages/base/Grid/src/GridItem/index.ts
@@ -1,7 +1,9 @@
 import type { OmitInternalProps } from '@toptal/picasso-shared'
 
-import type { Props, GridSizes } from './GridItem'
+import type { Props, GridItemSizeProps } from './GridItem'
 
 export { default as GridItem } from './GridItem'
 export type GridItemProps = OmitInternalProps<Props>
-export type GridSizeProps = GridSizes
+export type { GridItemSizeProps } from './GridItem'
+/** @deprecated Use `GridItemSizeProps` instead. [PF-2208] */
+export type GridSizeProps = GridItemSizeProps
